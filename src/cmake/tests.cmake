90147cc46fa262c5870c0e457e431dd3135bd23f
Charles Hathaway <chathaway@logrit.com>
2018-08-15 16:58:07 -0400

    Added more CREATE TABLE tests, reverted a hack
    The hack was to hard-code the output header file for the lexer to
    "../build/lexer.h", which wouldn't work if the build directory was
    somewhere else. Moved this to the CMake file and pass in an absolute
    path.

--- src/cmake/tests.cmake
+++ src/cmake/tests.cmake
@@ -1,3 +1,3 @@
 enable_testing()
 
-add_test(testCreateTables octo --input-file ${PROJECT_SOURCE_DIR}/samples/create_table.sql)
+add_test(testCreateTables octo --dry-run --input-file ${PROJECT_SOURCE_DIR}/samples/create_table.sql)

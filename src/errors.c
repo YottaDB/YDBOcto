240851f5c80c6777f9b499efa15b3659166676e4
Charles Hathaway <charles@yottadb.com>
2019-03-05 10:21:20 -0500

    [#71] Include file and line number in log messages

--- src/errors.c
+++ src/errors.c
@@ -9,14 +9,14 @@
 #include "octo.h"
 #include "errors.h"
 
-const char *log_prefix = "[%5s] %04d-%02d-%02d %02d:%02d:%02d : ";
+const char *log_prefix = "[%5s] %s:%d %04d-%02d-%02d %02d:%02d:%02d : ";
 
 /**
  * Logs error at level, formatting output and sending to the correct location.
  *
  * If level is FATAL, terminates the process and calls ydb_fork_n_core
  */
-void octo_log(enum ERROR_LEVEL level, enum ERROR error, ...) {
+void octo_log(int line, char *file, enum ERROR_LEVEL level, enum ERROR error, ...) {
 	va_list args;
 	va_start(args, error);
 	const char *type;
@@ -50,6 +50,8 @@ void octo_log(enum ERROR_LEVEL level, enum ERROR error, ...) {
 		break;
 	}
 	fprintf(stderr, log_prefix, type,
+		file,
+		line,
 	        local_time.tm_year + 1900,
 	        local_time.tm_mon + 1,
 	        local_time.tm_mday,
--- src/errors.h
+++ src/errors.h
@@ -46,33 +46,33 @@ static const char *err_format_str[] = {
 #undef ERROR_DEF
 #undef ERROR_END
 
-void octo_log(enum ERROR_LEVEL level, enum ERROR error, ...);
+void octo_log(int line, char *file, enum ERROR_LEVEL level, enum ERROR error, ...);
 
 #define YDB_ERROR_CHECK(status, z_status, msg) do {   \
-		if(YDB_OK != status) {                              \
-			YDB_LITERAL_TO_BUFFER("$ZSTATUS", (z_status));    \
-			INIT_YDB_BUFFER((msg), MAX_STR_CONST);            \
-			ydb_get_s(z_status, 0, NULL, (msg));              \
-			(msg)->buf_addr[(msg)->len_used] = '\0';          \
-			/** TODO: not all ydb errs FATAL */               \
-			octo_log(FATAL, ERR_YOTTADB, (msg)->buf_addr);    \
-			free((msg)->buf_addr);                            \
-			(msg)->len_used = 0;                              \
-			(msg)->len_alloc = 0;                             \
-		}                                                   \
+	if(YDB_OK != status) {                              \
+		YDB_LITERAL_TO_BUFFER("$ZSTATUS", (z_status));    \
+		INIT_YDB_BUFFER((msg), MAX_STR_CONST);            \
+		ydb_get_s(z_status, 0, NULL, (msg));              \
+		(msg)->buf_addr[(msg)->len_used] = '\0';          \
+		/** TODO: not all ydb errs FATAL */               \
+		octo_log(__LINE__, __FILE__, FATAL, ERR_YOTTADB, (msg)->buf_addr);    \
+		free((msg)->buf_addr);                            \
+		(msg)->len_used = 0;                              \
+		(msg)->len_alloc = 0;                             \
+	}                                                   \
 } while(0);
 
 #define TRACE(err, ...) TRACE >= config->record_error_level  \
-	? octo_log(TRACE, err, ## __VA_ARGS__) : FALSE;
+	? octo_log(__LINE__, __FILE__, TRACE, err, ## __VA_ARGS__) : FALSE;
 #define INFO(err, ...) INFO >= config->record_error_level  \
-	? octo_log(INFO, err, ## __VA_ARGS__) : FALSE;
+	? octo_log(__LINE__, __FILE__, INFO, err, ## __VA_ARGS__) : FALSE;
 #define DEBUG(err, ...) DEBUG >= config->record_error_level  \
-	? octo_log(DEBUG, err, ## __VA_ARGS__) : FALSE;
+	? octo_log(__LINE__, __FILE__, DEBUG, err, ## __VA_ARGS__) : FALSE;
 #define WARNING(err, ...) WARNING >= config->record_error_level  \
-	? octo_log(WARNING, err, ## __VA_ARGS__) : FALSE;
+	? octo_log(__LINE__, __FILE__, WARNING, err, ## __VA_ARGS__) : FALSE;
 #define ERROR(err, ...) ERROR >= config->record_error_level  \
-	? octo_log(ERROR, err, ## __VA_ARGS__) : FALSE;
+	? octo_log(__LINE__, __FILE__, ERROR, err, ## __VA_ARGS__) : FALSE;
 #define FATAL(err, ...) FATAL >= config->record_error_level  \
-	? octo_log(FATAL, err, ## __VA_ARGS__) : FALSE;
+	? octo_log(__LINE__, __FILE__, FATAL, err, ## __VA_ARGS__) : FALSE;
 
 #endif

e2b6a954efb8c0b0143b5ecaa977f360f3f4ffe8
Jon Badiali <jon@yottadb.com>
2019-04-15 17:01:16 -0400

    Added tests and support functions for make_data_row

--- src/rocto/test_make_data_row.c
+++ src/rocto/test_make_data_row.c
@@ -29,38 +29,104 @@
 #include "message_formats.h"
 
 static void test_null_input(void **state) {
-	DataRow *response = make_data_row(NULL, 0);
-	int expected_length = 6;
+	DataRow *response = NULL;
+	DataRow *received_response = NULL;
+	ErrorResponse *err = NULL;
+
+	int expected_length = sizeof(unsigned int) + sizeof(short int);
+	response = make_data_row(NULL, 0);
+	received_response = read_data_row((BaseMessage*)&response->type, &err);
+
 	// Standard checks
-	assert_non_null(response);
-	assert_int_equal(response->length, htonl(expected_length));
+	assert_non_null(received_response);
+	assert_null(received_response->parms);
+	assert_int_equal(received_response->length, expected_length);
+	assert_int_equal(received_response->num_columns, 0);
 
 	free(response);
+	free_data_row(received_response);
 }
 
 static void test_one_parms(void **state) {
-	int num_parms = 1;
+	DataRow *response = NULL;
+	DataRow *received_response = NULL;
+	ErrorResponse *err = NULL;
+	int num_parms = 1, i = 0;
 	DataRowParm parms[num_parms];
 
 	memset(parms, 0, sizeof(DataRowParm) * num_parms);
-	parms[0].value = "helloWorld";
-	parms[0].length = strlen(parms[0].value);
-	
-	DataRow *response = make_data_row(parms, 1);
-	// DataRow + DataRowParms + string in each parm + null char
-	int expected_length = 6 + (sizeof(unsigned int)) * num_parms
-		+ parms[0].length;
+	char *parm_value = "helloWorld";
+	int parm_length = strlen(parm_value);
+	parms[0].length = parm_length;
+	parms[0].value = (char*)malloc(parm_length * sizeof(char));
+	strncpy(parms[0].value, parm_value, parm_length);
+	// DataRow.length + DataRow.num_columns + DataRowParms (each parm: length + string value)
+	int expected_length = sizeof(unsigned int) + sizeof(short int) + (sizeof(unsigned int)) * num_parms + parm_length;
+
+	response = make_data_row(parms, num_parms);
+	received_response = read_data_row((BaseMessage*)&response->type, &err);
+
 	// Standard checks
-	assert_non_null(response);
-	assert_int_equal(ntohl(response->length), expected_length);
+	assert_non_null(received_response);
+	assert_non_null(received_response->parms);
+	assert_int_equal(received_response->length, expected_length);
+	assert_int_equal(received_response->num_columns, num_parms);
+	assert_int_equal(received_response->parms[0].length, parm_length);
+	assert_memory_equal(received_response->parms[0].value, parm_value, parm_length);
 
 	free(response);
+	free_data_row(received_response);
+	free(parms[0].value);
+}
+
+static void test_multi_parms(void **state) {
+	DataRow *response = NULL;
+	DataRow *received_response = NULL;
+	ErrorResponse *err = NULL;
+	int num_parms = 2, i = 0;
+	DataRowParm parms[num_parms];
+	memset(parms, 0, sizeof(DataRowParm) * num_parms);
+
+	char *parm1_value = "helloWorld";
+	int parm1_length = strlen(parm1_value);
+	parms[0].length = parm1_length;
+	parms[0].value = (char*)malloc(parm1_length * sizeof(char));
+	strncpy(parms[0].value, parm1_value, parm1_length);
+
+	char *parm2_value = "helloUniverse";
+	int parm2_length = strlen(parm2_value);
+	parms[1].length = parm2_length;
+	parms[1].value = (char*)malloc(parm2_length * sizeof(char));
+	strncpy(parms[1].value, parm2_value, parm2_length);
+
+	// DataRow.length + DataRow.num_columns + DataRowParms (each parm: length + string value)
+	int expected_length = sizeof(unsigned int) + sizeof(short int)
+		+ (sizeof(unsigned int)) * num_parms + parm1_length + parm2_length;
+
+	response = make_data_row(parms, num_parms);
+	received_response = read_data_row((BaseMessage*)&response->type, &err);
+
+	// Standard checks
+	assert_non_null(received_response);
+	assert_non_null(received_response->parms);
+	assert_int_equal(received_response->length, expected_length);
+	assert_int_equal(received_response->num_columns, num_parms);
+	assert_int_equal(received_response->parms[0].length, parm1_length);
+	assert_memory_equal(received_response->parms[0].value, parm1_value, parm1_length);
+	assert_int_equal(received_response->parms[1].length, parm2_length);
+	assert_memory_equal(received_response->parms[1].value, parm2_value, parm2_length);
+
+	free(response);
+	free_data_row(received_response);
+	free(parms[0].value);
+	free(parms[1].value);
 }
 
 int main(void) {
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(test_null_input),
-		cmocka_unit_test(test_one_parms)
+		cmocka_unit_test(test_one_parms),
+		cmocka_unit_test(test_multi_parms),
 	};
 	return cmocka_run_group_tests(tests, NULL, NULL);
 }

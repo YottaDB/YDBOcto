43537f3e82197ea3506462d77e34f356f6454995
Jon Badiali <jon@yottadb.com>
2019-05-16 17:48:59 -0400

    Added outline for binary deserialization
    Signed-off-by: Jon Badiali <jon@yottadb.com>

---
+++ src/rocto/deserialize.c
@@ -0,0 +1,77 @@
+/* Copyright (C) 2018-2019 YottaDB, LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include <libyottadb.h>
+
+#include "octo.h"
+#include "octo_types.h"
+#include "message_formats.h"
+#include "rocto.h"
+
+unsigned char bin_to_bool(unsigned char *bin) {
+}
+
+char bin_to_char(unsigned char *bin) {
+}
+
+short int bin_to_int2(unsigned char *bin) {
+}
+
+int bin_to_int4(unsigned char *bin) {
+}
+
+long int bin_to_int8(unsigned char *bin) {
+}
+
+float bin_to_float4(unsigned char *bin) {
+}
+
+double bin_to_float8(unsigned char *bin) {
+}
+
+int bin_to_oid(unsigned char *bin) {
+}
+
+// HSTORE?
+
+// BYTEA?
+
+// VARBIT/BIT?
+
+// TIME/TIMETZ?
+
+// TIMESTAMP/TIMESTAMPTZ?
+
+// DATE?
+
+// MACADDR?
+
+// UUID?
+
+// ARRAY?
+
+// RANGE?
+
+// POINT?
+
+// BOX?
+
+// PATH?
+
+// INET?

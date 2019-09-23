807298dd8ed0e4c01fc52545dc22e709be826688
Narayanan Iyer <nars@yottadb.com>
2019-09-23 14:04:36 -0400

    [#321] LIMIT returns more rows than requested if there is a WHERE clause with OR usages
    The main fix for this is to tmpl_tablejoin.ctemplate to also generate M code for checking
    the LIMIT clause in case a key is fixed (key->value is non-NULL). Previously we were
    doing this only in case key->value is NULL.
    
    While at this, it was noticed that we were unnecessarily generating M code to use more
    subscripts than necessary in tmpl_duplication_check.ctemplate and that has been fixed
    so we reduce the chances of a MAXNRSUBSCRIPTS error. The test_composite_key bats test
    has been enhanced to test this. While coming up with this test case, fixed
    tmpl_physical_plan.ctemplate to not generate multiple identical M lines (to generate
    the xref) in case of a composite key with multiple key columns (all of which have the
    same cross_reference_output_key).
    
    Existing reference files were fixed to account for the revised generated M code.
    
    The specific test identified in the description of #321 in gitlab has been added to the
    test_limit bats test. Took this opportunity to also add the specific test in the description
    of #299 to the test_limit bats test.
    
    genrandomqueries.m has been enhanced to test for #321 and #299 in a variety of ways. The WHERE
    clause is now randomly generated with a variety of boolean expressions.
    
    genrandomqueries.m and test_helpers.bash.in have been together enhanced to also do testing
    in case LIMIT has been randomly generated by just counting the # of rows from psql vs octo
    instead of comparing the actual output.
    
    Note that an occasional randomly generated query could fail due to #324 (fixed in a separate
    upcoming commit).

---
+++ tests/fixtures/compositekey.sql
@@ -0,0 +1,25 @@
+#################################################################
+#								#
+# Copyright (c) 2019 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+CREATE TABLE composite (
+	id0 INTEGER PRIMARY KEY,
+	id1 INTEGER KEY NUM 1,
+	id2 INTEGER KEY NUM 2,
+	id3 INTEGER KEY NUM 3,
+	id4 INTEGER KEY NUM 4,
+	id5 INTEGER KEY NUM 5,
+	id6 INTEGER KEY NUM 6,
+	id7 INTEGER KEY NUM 7,
+	name VARCHAR(20)
+	)
+GLOBAL "^composite(keys(""id0""),keys(""id1""),keys(""id2""),keys(""id3""),keys(""id4""),keys(""id5""),keys(""id6""),keys(""id7""))";
+
--- tests/fixtures/genrandomqueries.m
+++ tests/fixtures/genrandomqueries.m
@@ -20,11 +20,54 @@
 genrandomqueries	;
 	set primarykey("customers")="customer_id"
 	set primarykey("orders")="order_id"
+	; Define possible values of columns in customers and orders table (later used by WHERE clause)
+	; Might be used by ON clause in the future too when #311 is fixed ###TMPDISABLE
+	set numcolumns("customers")=4
+	set numcolumns("customers",0)="customer_id"
+	set numcolumns("customers",1)="first_name"
+	set numcolumns("customers",2)="last_name"
+	set numcolumns("customers",3)="zipcode"
+	set numcolumns("orders")=2
+	set numcolumns("orders",0)="order_date"
+	set numcolumns("orders",1)="order_amount"
+	;
+	set numtype("customers","customer_id")=""	; indicates numeric; non-existence implies VARCHAR/CHAR
+	set columns("customers","customer_id",1)=""
+	set columns("customers","customer_id",2)=""
+	set columns("customers","customer_id",3)=""
+	set columns("customers","customer_id",4)=""
+	set columns("customers","customer_id",5)=""
+	set columns("customers","first_name","George")=""
+	set columns("customers","first_name","John")=""
+	set columns("customers","first_name","Thomas")=""
+	set columns("customers","first_name","James")=""
+	set columns("customers","last_name","Washington")=""
+	set columns("customers","last_name","Adams")=""
+	set columns("customers","last_name","Jefferson")=""
+	set columns("customers","last_name","Madison")=""
+	set columns("customers","last_name","Monroe")=""
+	set columns("customers","zipcode","22121")=""
+	set columns("customers","zipcode","02169")=""
+	set columns("customers","zipcode","22902")=""
+	set columns("customers","zipcode","22960")=""
+	set columns("orders","order_date","07/04/1776")=""
+	set columns("orders","order_date","03/14/1760")=""
+	set columns("orders","order_date","05/23/1784")=""
+	set columns("orders","order_date","09/03/1790")=""
+	set columns("orders","order_date","07/21/1795")=""
+	set columns("orders","order_date","11/27/1787")=""
+	set columns("orders","order_amount","$234.56")=""
+	set columns("orders","order_amount","$78.50")=""
+	set columns("orders","order_amount","$124.00")=""
+	set columns("orders","order_amount","$65.50")=""
+	set columns("orders","order_amount","$25.50")=""
+	set columns("orders","order_amount","$14.40")=""
+	;
 	set joinstr(0)="inner join"
 	set joinstr(1)="left join"
 	set joinstr(2)="right join"
 	set joinstr(3)="full join"
-	set numqueries=20	; generate 20 queries so as not to take a long time for this test to run in pipeline
+	set numqueries=30	; generate 30 queries so as not to take a long time for this test to run in pipeline
 	set q=0
 	for  do  quit:q=numqueries
 	. set numjoins=1+(q#8)	; can be 1-way, 2-way, 3-way, ... up to 7-way join
@@ -53,29 +96,57 @@ genrandomqueries	;
 	. . set:modulo=1 notequalchosen=1
 	. . set sqlquery=sqlquery_"= "_tablealias(i)_(i)_".customer_id"
 	. ; Add optional WHERE
-	. if $random(2) do
-	. ; ###TMPDISABLE Add WHERE clause here (needs some thought)
+	. if (1=numjoins)&$random(2) do	 ; ###TMPDISABLE Remove (1=numjoins) once #311 is fixed
+	. . set sqlquery=sqlquery_" where "_$$boolexpr(4)
 	. ; Add optional ORDER BY
-	. if $random(2) do
+	. set orderby=$random(2)
+	. if orderby do
 	. . ; Currently choose only ONE column for ORDER BY. When #228 is fixed, enable multiple columns here ###TMPDISABLE
 	. . set sqlquery=sqlquery_" order by "_column($random(numcols))
-	. . ; Add optional LIMIT (do this only if ORDER BY is also chosen as otherwise output order could be different
-	. . ; between Postgres and Octo and hence we cannot reliably get the test to pass.
-	. . ; Also can only do this if the # of columns selected for ORDER BY is the same as the # of SELECT columns
-	. . ; Or else the order of row output could differ in the columns that are not selected.
-	. . if (1=numcols)&$random(2) do 	; ###TMPDISABLE : change 1=numcols to numorderbycols=numcols when #228 is fixed
-	. . . set sqlquery=sqlquery_" limit "_$random(10)
+	. ; Add optional LIMIT
+	. ; Note that in this case, the output order between Postgres and Octo could be different
+	. ; and hence we cannot reliably get the test to pass.
+	. ; Also can only do this if the # of columns selected for ORDER BY is the same as the # of SELECT columns
+	. ; Or else the order of row output could differ in the columns that are not selected.
+	. set limit=$random(2)
+	. if limit do
+	. . set sqlquery=sqlquery_" limit "_$random(10)
 	. set sqlquery=sqlquery_";"
+	. set exactcheck=('limit)!(orderby&(1=numcols)) ; ###TMPDISABLE : change 1=numcols to numorderbycols=numcols when #228 is fixed
 	. ; The below if check is because postgres issues the following error in this case
 	. ;	--> ERROR:  FULL JOIN is only supported with merge-joinable or hash-joinable join conditions
 	. quit:fulljoinchosen&notequalchosen
 	. quit:notequalchosen			; ###TMPDISABLE until #311 is fixed
 	. set file="jointest"_$translate($justify($incr(q),2)," ","0")_".sql"
 	. open file:(newversion)  use file
+	. write:'exactcheck "-- rowcount-only-check",!
 	. write sqlquery,!
 	. close file
 	quit
 	;
+boolexpr(maxdepth)
+	;
+	new d,t,col,o,oper,value,ncol,colname,nchoice,k,subs,boolstr,delim,depth
+	if maxdepth=0  do  quit boolstr
+	. set t=1+$random(numjoins)
+	. set col=tablealias(t)_t
+	. set ncol=$random(numcolumns(table(t))),colname=numcolumns(table(t),ncol)
+	. set col=col_"."_colname
+	. set o=$random(6)
+	. set oper=$select(o=0:"=",o=1:"<",o=2:">",o=3:"!=",o=4:"IS NULL",o=5:"IS NOT NULL")
+	. set value=""
+	. if o<4 do
+	. . set value=" "
+	. . set nchoice=$random(5)
+	. . set subs="" for k=1:1 set subs=$order(columns(table(t),colname,subs)) quit:(subs="")!(k>nchoice)
+	. . set delim=$select($data(numtype(table(t),colname)):"",1:"'")
+	. . set value=value_delim_subs_delim
+	. set boolstr="("_col_" "_oper_value_")"
+	set depth=1+$random(maxdepth)
+	set boolstr=$$boolexpr(maxdepth-1)
+	set oper=$select($random(2):"OR",1:"AND")
+	for d=2:1:depth set boolstr=boolstr_" "_oper_" "_$$boolexpr(maxdepth-1)
+	quit boolstr
 	;
 genouterjoinonpastas;
 	set query($incr(query))="select pastas.id, firstName, lastName, pastaName from names4 inner join pastas on pastas.pastaName = names4.favoritePasta order by pastas.id;"

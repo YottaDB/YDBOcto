ba2eb065549becd23b62692144153918ad75b5bd
Narayanan Iyer <nars@yottadb.com>
2019-10-15 17:50:14 -0400

    [#338] SELECT from sub-query now returns correct results if sub-query had constants in its select column list
    The main fix is to `src/get_column_piece_number.c` where in the `column_STATEMENT` case, we were
    going through some fancy logic to figure out the number of this column in the table. As part of a
    recent commit, the field `column->column_number` was introduced and so this is readily usable
    and avoids a lot of the complexity in this file. Now that the correct column # is being returned
    (previously it used to always return 1 in case of a DERIVED_COLUMN from a sub-query) the right
    pieces are extracted in the parent query resulting in the correct output.
    
    A new TSC14 subtest in the test_select_columns bats test verifies this issue is fixed.
    On a related note, various existing reference files in the test_select_columns test were
    renamed to follow the new convention (TOO*.ref to TSC*.ref).

--- src/get_column_piece_number.c
+++ src/get_column_piece_number.c
@@ -19,36 +19,31 @@
 #include "octo_types.h"
 
 int get_column_piece_number(SqlColumnAlias *alias, SqlTableAlias *table_alias) {
-	SqlColumn *column;
-	SqlValue *value;
-	SqlColumnListAlias *cur_cl_alias, *start_cl_alias, *t_cl_alias;
-	char *column_name1, *column_name2;
-	int part = 1;
+	SqlColumn		*column;
+	SqlValue		*value;
+	SqlColumnListAlias	*cur_cl_alias, *start_cl_alias, *t_cl_alias;
+	char			*column_name1, *column_name2;
+	int			part;
 
-	if(alias->column->type == column_STATEMENT) {
+	if (column_STATEMENT == alias->column->type) {
 		UNPACK_SQL_STATEMENT(column, alias->column, column);
-		if(column->columnName == 0) {
-			WARNING(CUSTOM_ERROR, "No alias provided for column in nested SQL expression, selecting first column from nested result");
-			return part;
-		}
-		UNPACK_SQL_STATEMENT(value, column->columnName, value);
-		column_name1 = value->v.string_literal;
-	} else {
-		UNPACK_SQL_STATEMENT(t_cl_alias, alias->column, column_list_alias);
-		UNPACK_SQL_STATEMENT(value, t_cl_alias->alias, value);
-		column_name1 = value->v.string_literal;
+		assert(column->column_number);
+		return column->column_number;
 	}
+	UNPACK_SQL_STATEMENT(t_cl_alias, alias->column, column_list_alias);
+	UNPACK_SQL_STATEMENT(value, t_cl_alias->alias, value);
+	column_name1 = value->v.string_literal;
 	UNPACK_SQL_STATEMENT(start_cl_alias, table_alias->column_list, column_list_alias);
 	cur_cl_alias = start_cl_alias;
+	part = 1;
 	do {
 		UNPACK_SQL_STATEMENT(value, cur_cl_alias->alias, value);
 		column_name2 = value->v.string_literal;
-		if(strcmp(column_name1, column_name2) == 0) {
+		if (!strcmp(column_name1, column_name2))
 			break;
-		}
 		part++;
 		cur_cl_alias = cur_cl_alias->next;
 	} while(cur_cl_alias != start_cl_alias);
-	assert(part == 1 || cur_cl_alias != start_cl_alias);
+	assert((1 == part) || (cur_cl_alias != start_cl_alias));
 	return part;
 }

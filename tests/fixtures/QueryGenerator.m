86eae60614ee2b3f15e42c1617667eecb722378a
Chris Lepore <clepore@yottadb.com>
2019-11-27 16:20:22 -0500

    Finished Simple Where Clause Started adding whitelist functionality Updated test_helpers to better deal with whitespace/other issues in Octo output Started adding where clause Started adding SQL Grammar rules, both missing ones and new ones Added necessary supporting functions Slightly modified existing code to work better going forward Increased query count Applied fix to resolve pipeline issue Drastically changed most functions to take advantage of the checks to ensure proper type matches Addressed comments on merge request

--- tests/fixtures/QueryGenerator.m
+++ tests/fixtures/QueryGenerator.m
@@ -10,14 +10,10 @@
 ;								;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-	; Glossary:
-	; WHITELIST - A list of acceptable tables to select columns/row information/other data
-	;             from. Acceptable tables being those that were selected for use in the
-	;             query, from which columns and data can be pulled from, ensuring valid queries.
-
 	set initDone("setQuantifier")=0
 	set initDone("chooseTable")=0
 	set initDone("chooseColumn")=0
+	set initDone("whereClause")=0
 
 	set arguments=$zcmdline
 	set sqlFile=$piece(arguments," ",1)
@@ -32,12 +28,14 @@
 	do readSQL(sqlFile)
 	do readZWR(zwrFile)
 
+	new i
 	for i=1:1:runCount do
 	. set query="SELECT "
 	. set query=query_$$setQuantifier
-	. set fc=$$fromClause
-	. set query=query_$$selectList
+	. set fc=$$fromClause ; fromClause needs to run before selectList
+	. set query=query_$$selectList($random(3)+1)
 	. set query=query_" "_fc
+	. set query=query_$$tableExpression
 	. write query,!
 	.
 	. set file=prefix_i_".sql"
@@ -45,6 +43,9 @@
 	. use file
 	. write query,!
 	. close file
+	. ; The tableColumn LVN exists for each individual query, thus it needs to be killed
+	. ; after each query is created
+	. kill tableColumn
 
 	quit
 
@@ -60,7 +61,8 @@ readSQL(file)
 	kill line(nlines)  if $increment(nlines,-1) ;kills line that contains EOF
 
 	for i=1:1:nlines do
-	. if ($extract(line(i),1)'="#")&($extract(line(i),1)'="") do
+	. ; Filters out comment lines (start with '--' or '#'), and/or blank lines
+	. if (($extract(line(i),1)'="#")&($extract(line(i),1)'="")&($extract(line(i),2)'="--")) do
 	. . ;TABLE NAMES
 	. . set tableNameStart=$find(line(i),"CREATE TABLE ")
 	. . set tableNameEnd=$find(line(i)," ",tableNameStart)
@@ -75,7 +77,7 @@ readSQL(file)
 	. . . . set columnNameAndType=$extract(line(i),columnNameAndTypeStart+1,columnNameAndTypeEnd-2)
 	. . . . set columnName=$piece(columnNameAndType," ")
 	. . . . set columnType=$piece(columnNameAndType," ",2)
-	. . . . set sqlInfo(tableName,columnName,columnType)="test"
+	. . . . set sqlInfo(tableName,i2,columnName,columnType)=""
 	. . . set tableNameEnd=columnNameAndTypeEnd
 	. . ; shifted over 1 to remove trailing ")"
 	. . set finalColumnAndTypeEnd=$find(line(i),")",columnNameAndTypeStart-1)
@@ -83,7 +85,7 @@ readSQL(file)
 	. . set finalColumnAndType=$extract(line(i),columnNameAndTypeStart+1,finalColumnAndTypeEnd-2)
 	. . set finalColumnName=$piece(finalColumnAndType," ")
 	. . set finalColumnType=$piece(finalColumnAndType," ",2)
-	. . set sqlInfo(tableName,finalColumnName,finalColumnType)=""
+	. . set sqlInfo(tableName,i2,finalColumnName,finalColumnType)=""
 	quit
 
 readZWR(file)
@@ -101,20 +103,20 @@ readZWR(file)
 	. . . if (i2=1) do
 	. . . . set table=$piece(line(i),"=""")
 	. . . . set firstData=$piece($piece(line(i),"=""",2),"|")
-	. . . . set data(table,firstData)=""
+	. . . . set data(table,i2,firstData)=""
 	. . . set holder=" "
 	. . . if (i2'=1) do
 	. . . . set holder=$piece(line(i),"|",i2)
 	. . . . ;If there is a " in the string, remove it
 	. . . . if (holder["""") set holder=$extract(holder,1,$find(holder,"""")-2)
-	. . . . if (holder'="") set data(table,holder)=""
+	. . . . if (holder'="") set data(table,i2,holder)=""
 	quit
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;Grammar Rules
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; https://ronsavage.github.io/SQL/sql-92.bnf.html#set%20quantifier
-setQuantifier()
+setQuantifier(curDepth)
 	if (initDone("setQuantifier")=0) do
 	. set initDone("setQuantifier")=1
 	. set quantifier=-1
@@ -125,33 +127,69 @@ setQuantifier()
 	quit quantifier($random(quantifier))
 
 ; https://ronsavage.github.io/SQL/sql-92.bnf.html#select%20list
-selectList()
+selectList(curDepth)
 	new randInt,result
 	;This function serves the same purpose as select sublist in the grammar rules for SQL.
-	;#FUTURE_TODO: Recursively generates a random select list.
-	;:returns result: A string representing a complete select list.
 
 	; Choose whether to use a wildcard or build a select list
 	set randInt=$random(4) ;25% chance to get an asterisk, 75% chance to continue further
+	if (randInt=0) do
+	. set table=$order(tableColumn(""))
+	. set tableColumn(table,"*")=""
 	if (randInt=0) quit "*"
 
 	set result=""
 	; Choose DerivedColumn or Qualifier
 	set randInt=$random(1)
-	set randInt=0 ; forced 0 until Qualifier/table.column notation is done
+	set randInt=0 ; forced 0 until Qualifier notation (table.column) is done
 	if randInt=0 set result=$$chooseColumn ;Chooses column
+	set column=result
 
-	; #FUTURE_TODO: change what this "returns" when ready
-	if randInt=1 do
-	. ;Call Qualifier (use dot)
-	. ;set result="table"_"."_"column"
-	. set result=$$chooseColumn
+	; #FUTURE_TODO: Enable when Qualifier notation (table.column) is to be used
+	;if randInt=1 do
+	;. ;Call Qualifier (use dot)
+	;. ;set result="table"_"."_"column"
 
-	; #FUTURE_TODO:
-	;if cur_depth > 0:
-	;. cur_depth=cur_depth-1 ; to drop down a "level" in depth
-	;. set result=result_", "
-	;. $$selectList
+	if curDepth>0 do
+	. if $increment(curDepth,-1) ; to drop down a "level" in depth
+	. if (curDepth'=0) set result=result_", "_$$selectList(curDepth)
+
+	; #FUTURE_TODO: Find a more elegant way to do this, maybe don't insert this comma at all
+	; Strip off trailing commas
+	if ($extract(result,$length(result)-2)=",")  set result=($extract(result,1,$length(result)-3))
+
+	quit result
+
+; https://ronsavage.github.io/SQL/sql-92.bnf.html#table%20expression
+tableExpression()
+	new randInt
+	set randInt=$random(2)
+
+	; From Clause should go here, but it needs to be decided on early as to
+	; allow for proper column(s) to be chosen
+
+	; #FUTURE_TODO: Change this structure such that each "clause" can be selected indepently
+	;               of each other. IE have a seperate random variable that is either 0 or 1
+	;               that controls whether a clause could be added. Similar to how the notString
+	;               is implemnted in the WHERE Clause
+	set result=""
+	; none
+	if (randInt=0) set result=""
+	; where
+	if (randInt=1) set result=$$whereClause
+	; #FUTURE_TODO: groupby
+	if (randInt=2) set result="this shouldn't be printed"
+	; #FUTURE_TODO: having
+	if (randInt=3) set result="this shouldn't be printed"
+	; #FUTURE_TODO: where groupby
+	if (randInt=4) set result="this shouldn't be printed"
+	; #FUTURE_TODO: where having
+	if (randInt=5) set result="this shouldn't be printed"
+	; #FUTURE_TODO: groupby having
+	if (randInt=6) set result="this shouldn't be printed"
+	; #FUTURE_TODO: where groupby having
+	if (randInt=7) set result="this shouldn't be printed"
+	; #FUTURE_TODO: orderby clause
 
 	quit result
 
@@ -166,25 +204,53 @@ fromClause()
 	; recursion depth. This limit is arbitrary, but should be reasonable
 	; provided a recursion depth < 10.
 	;randInt = random.randint(1, context.max_depth)
-	set randInt=$random(1)
+	;set randInt=$random(2)+1
 
-	; #FUTURE_TODO: Commented out until WHITELIST functionality is implemented
-	;whitelist = context.allTables
-	;for i in range(0, randInt-1):
-	;. result += TableReference(context.allTables, whitelist)
+	; #FUTURE_TODO: Commented out until JOIN functionality is implemented
+	;tableColumn = context.allTables
+	;for i=1:1:randInt do
+	;. set result=result_$$chooseTable
 	;. ; Isolate last call to exclude comma without additional string operations
-	;. if seed > 1:
-	;. . result += ", "
-	;. seed -= 1
-	;result += TableReference(context.allTables, whitelist)
+	;. if (randInt>1) do
+	;. . set result=result_", "
+	;. set randInt=$increment(randInt,-1)
+	;result += TableReference(context.allTables, tableColumn)
 
-	set result=result_$$chooseTable
-	quit result
+	set chosenTable=$$chooseTable
+	set tableColumn(chosenTable)=""
+
+	quit result_chosenTable
+
+; https://ronsavage.github.io/SQL/sql-92.bnf.html#where%20clause
+whereClause()
+	if (initDone("whereClause")=0) do
+	. set initDone("whereClause")=1
+	. set comparisonOperator=-1
+	. set comparisonOperator($increment(comparisonOperator))="="
+	. set comparisonOperator($increment(comparisonOperator))="!="
+	. set comparisonOperator($increment(comparisonOperator))="<"
+	. set comparisonOperator($increment(comparisonOperator))=">"
+	. set comparisonOperator($increment(comparisonOperator))="<="
+	. set comparisonOperator($increment(comparisonOperator))=">="
+	. ;set comparisonOperator($increment(comparisonOperator))="BETWEEN" ; #FUTURE_TODO: Not used currently, but is a valid option
+	. ;set comparisonOperator($increment(comparisonOperator))="LIKE" ; #FUTURE_TODO: Not used currently, but is a valid option
+	. ;set comparisonOperator($increment(comparisonOperator))="IN" ; #FUTURE_TODO: Not used currently, but is a valid option
+	. if $increment(number)
+
+	set notString=""
+	if $random(2) set notString="NOT "
+
+	; #FUTURE_TODO: operands can be literals/entries and column on either side
+	; #FUTURE_TODO: things like id + 1, column expressions
+	; #FUTURE_TODO: unary operations, NOT (id=2)
+	; #FUTURE_TODO: Involving boolean operators (expr1 AND expr2, expr1 OR expr2)
+	set chosenColumn=$$chooseColumn
+
+	quit " WHERE "_notString_chosenColumn_" "_comparisonOperator($random(comparisonOperator))_" "_$$chooseEntry(table,chosenColumn)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;Supporting Functions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; #FUTURE_TODO: This contains no logic, when WHITELIST is added more logic can be added
 chooseTable()
 	if (initDone("chooseTable")=0) do
 	. set initDone("chooseTable")=1
@@ -199,21 +265,50 @@ chooseTable()
 	set table=tables($random(tables))
 	quit table
 
-; #FUTURE_TODO: This contains no (very minimal) logic, when WHITELIST is added more logic can be added
 chooseColumn()
 	if (initDone("chooseColumn")=0) do
-	. new a
+	. new a,currentTable,column,columnCount,x,count
 	. for a=0:1:tables-1 do
 	. . set currentTable=tables(a)
 	. . set initDone("chooseColumn")=1
-	. . new column,x,count
 	. . set column=""
-	. . set columnCount=0
+	. . set columnCount=1
 	. . set x=""
 	. . for  set x=$order(sqlInfo(currentTable,x))  quit:x=""  if $increment(columnCount)
-	. . set count=0
+	. . set tableColumnCounts(currentTable)=columnCount
+	. . set count=1
 	. . set x=""
-	. . for  set x=$order(sqlInfo(currentTable,x)) do  quit:x=""  if $increment(count)
-	. . . set columns(currentTable,count)=x
+	. . for  set x=$order(sqlInfo(currentTable,count,x)) do  quit:(count=columnCount)  if $increment(count)
+	. . . set columns(currentTable,count,x)=""
+	. . . set x=""
+	. . kill columns(currentTable,count) ;kills empty last entry in this table
 	. set initDone("chooseColumn")=1
-	quit columns(table,$random(columnCount))
+	new table,CC,selectedColumn
+	set table=$order(tableColumn(""))
+	set CC=tableColumnCounts(table)
+	set selectedColumn=$order(columns(table,$random(CC-1)+1,$random(CC-1)+1))
+	set tableColumn(table,selectedColumn)=""
+	quit selectedColumn
+
+chooseEntry(tableName,columnName)
+	new index,formatted,CC,temp,type,entry,i,randInt,test
+
+	; Finds index of column
+	set index=1
+	for  quit:($order(columns(tableName,index,""))=columnName)  if $increment(index)
+
+	set formatted="^"_tableName_"("_index_")"
+
+	; Gets type of selected entry
+	set type=$order(sqlInfo(tableName,index,columnName,""))
+
+	; randomly selects an entry to be returned
+	set CC=tableColumnCounts(tableName)
+	set entry=$order(data(formatted,index,""))
+
+	; If selected entry is not of "INTEGER" type, pre/append a single quote (')
+	; This is done to match PSQL format rules, Octo does not care
+	; Depending on how Booleans are to be specified, this might need to be changed
+	if ($find(type,"INTEGER")=0) set entry="'"_entry_"'"
+
+	quit entry

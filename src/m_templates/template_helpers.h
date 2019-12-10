d79b6c5fe73c55e17f8e2d908cf2d9a0cfb5afa7
Narayanan Iyer <nars@yottadb.com>
2019-12-10 16:01:16 -0500

    [#397] ORDER BY does not work correctly if VARCHAR type (i.e. string) column stores numeric values
    * The issue is that M automatically converts strings that contain canonic numbers (e.g. "22125") to
      numeric type (i.e. 22125) whereas it keeps strings that do not contain canonic numbers (e.g. "02169")
      as a string type. This causes issues with ORDER BY ZIPCODE if zipcode is defined as a VARCHAR (which
      it would be because the leading 0 would not stay if it is defined as a NUMERIC or INTEGER type).
    
    * The main fix is to `src/m_templates/tmpl_column_list_combine.ctemplate` where we prefix a "Z" string
      to the ORDER BY expression if we determine that the type of the expression is a STRING_LITERAL.
      This meant changing the interface of `tmpl_column_list_combine` to pass an extra parameter `is_order_by`
      as TRUE from the place where we invoke this template function for the ORDER BY expression. And pass
      this as FALSE for all other invocations.
    
    * A new TOB12 subtest has been added in the test_order_by bats test to test this fix.
    
    * The reference file of an existing TWO04 subtest had to be fixed due to the "Z"_ prefix.

--- src/m_templates/template_helpers.h
+++ src/m_templates/template_helpers.h
@@ -69,7 +69,8 @@ TEMPLATE(tmpl_key_source, PhysicalPlan *pplan, SqlKey *key);
 TEMPLATE(tmpl_temp_key_advance, SqlKey *key);
 TEMPLATE(tmpl_print_expression, LogicalPlan *plan, PhysicalPlan *pplan);
 TEMPLATE(tmpl_column_reference, PhysicalPlan *pplan, SqlColumnAlias *column_alias);
-TEMPLATE(tmpl_column_list_combine, LogicalPlan *plan, PhysicalPlan *pplan, char *delim, int resume, int resume_length);
+TEMPLATE(tmpl_column_list_combine, LogicalPlan *plan, PhysicalPlan *pplan, char *delim,
+						int resume, int resume_length, boolean_t is_order_by);
 TEMPLATE(tmpl_emit_source, char *source, char *table_name, int unique_id, int keys_to_match, enum EmitSourceForm form);
 // Sets up trigger for a column reference, i.e. alias
 TEMPLATE(tmpl_column_reference_trigger, PhysicalPlan *pplan, SqlColumnAlias *column_alias);
---
+++ tests/fixtures/TOB12.sql
@@ -0,0 +1,16 @@
+#################################################################
+#								#
+# Copyright (c) 2019 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+-- TOB12 : ORDER BY does not work correctly if VARCHAR type (i.e. string) column stores numeric values
+
+select zipcode from customers order by zipcode;
+

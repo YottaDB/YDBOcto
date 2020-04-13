8bbbb8720052605e8af893243d885a056532bbf1
Narayanan Iyer <nars@yottadb.com>
2020-04-13 07:10:01 -0400

    Include UTF-8 in Northwind zwrite file header as data contains UTF-8 characters
    * Now that `tests/fixtures/northwind.zwr` contains data with UTF-8 characters in it (added by
      commit 0ada259eaec906710a6cd5dea07d06c69a55a6ad), the extract file header has been revised
      to indicate `UTF-8`.
    
    * Because of the change in the above bullet, `ydb_chset` env var had to be set to `UTF-8`
      or else `mupip load` would issue a `LOADINVCHSET` error. And because `ydb_chset` is set to `UTF-8`,
      locale had to be set to `en_US.UTF8` just for the `mupip load` (I tried setting the locale to
      `C.UTF-8` but that issued a `NONUTF8LOCALE` error so stuck with `en_US.UTF8`). And finally,
      `ydb_icu_version` env var had to be set to the correct value to avoid a `ICUSYMNOTFOUND` error.
      This is usually done by `ydb_env_set` when invoked after `ydb_dist` has been set to the `/utf8`
      subdirectory in `init_test()` function but we need this set even in the case where `utf8_path`
      is set to `""`. All these changes were done in `tests/test_helpers.bash.in`. Some debugging
      information about locales were also added to `init_test()`. Various typos (`en_US.UTF-8` should be
      `en_US.UTF8`) were fixed. And finally, an unnecessary set of `LC_ALL=C` in the `createdb()` function
      (as part of loading the postgres seed data) has been removed.
    
    * Various `.zwr` files in `tests/fixtures/` have been fixed to use `YottaDB` (instead of `GT.M`)
      in the mupip extract file header now that it is supported (due to YottaDB/DB/YDB#493).

--- tools/ci/install-tls.sh
+++ tools/ci/install-tls.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+#################################################################
+#								#
+# Copyright (c) 2019-2020 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
 
 source /opt/yottadb/current/ydb_env_set
 
-export gtm_icu_version=`pkg-config --modversion icu-io`
+export ydb_icu_version=`pkg-config --modversion icu-io`
 
 # Install TLS plugin
 pushd $ydb_dist/plugin/gtmcrypt

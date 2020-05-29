YottaDB/YDBOcto

commit a8234847cbce0c6f6c593734f50525fb4123f780

Reduce artifacts.zip size from 750Mb/150Mb to ~ 90Mb/90Mb in case pipeline tests fail/pass
* The main change is to `tools/ci/build.sh`. A prior commit fb3cf906dfd9be53114222e35712e930d7785351
  had reduced the pipeline artifact size.  But it did this only in the case the pipeline passes.
  Not if there are test failures. We want the size to be small even in the failing case. This commit
  addresses that by checking if the test fails. If so, it notes that down in a variable `exit_status`
  and still does the cleanup/rm commands before returning with the failure exit status from the test run.

All below changes reduce the size of the artifacts.zip file whether the pipeline tests pass or fail.
So even if the pipeline passes, the artifacts.zip file reduced from 150Mb to 90Mb due to the below changes.

* In addition the unit test case executables (e.g. `src/test_read_execute` etc.) occupy a majority of
  the space in the `src` directory and are not otherwise necessary in my understanding so they are removed
  as well.

* Executables created by `go` take up around 5Mb of space even in zipped form so those are also cleaned
  up right after the executable invocation in the test. This fix is in the `run_go()` function.

* The number of queries generated by the Query Generator has been reduced from 150 to 100. There are a
  lot of tests that invoke this (`run_query_generator()` function) and we have enough test coverage.
  Therefore it is considered okay to reduce the pipeline runtime as well as artifacts.zip file size
  by reducing the number of queries by 33%.

  Previously the number of queries (`150`) was hardcoded in multiple places in various subtests.
  All of that is now removed and is centralized in the `run_query_generator()` function (to `100`).
  The affected files are `tests/test_jdbc_connection.bats.in` and `tests/test_query_generator.bats.in`.

tools/ci/build.sh
@@ -172,35 +172,38 @@ $ydb_dist/mupip load ../../tests/fixtures/names.zwr
 popd
 
 # Run the tests
-${ctestCommand} -j `grep -c ^processor /proc/cpuinfo` || exit 1
+${ctestCommand} -j `grep -c ^processor /proc/cpuinfo`
+exit_status=$?
 
-# Rebuild Octo for packaging if it wasn't a RelWithDebInfo build
-if [[ $build_type != "RelWithDebInfo" ]]; then
-	${cmakeCommand} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=${ydb_dist}/plugin ..
-	make -j `grep -c ^processor /proc/cpuinfo`
+if [[ ! $exit_status ]]; then
+	# Rebuild Octo for packaging if it wasn't a RelWithDebInfo build
+	if [[ $build_type != "RelWithDebInfo" ]]; then
+		${cmakeCommand} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=${ydb_dist}/plugin ..
+		make -j `grep -c ^processor /proc/cpuinfo`
+	fi
+
+	# Ubuntu pipelines only: Copy installation files for use in Docker image construction
+	if [[ $cmakeCommand == "cmake" ]]; then
+		echo -n "Copying installation files for Docker image generation... "
+		cp ../tools/ci/install.sh .
+		# Create plugin directory structure for later reference by install.sh
+		mkdir -p plugin/r plugin/o/utf8 plugin/octo/
+		# Copy YDBPosix into build directory for later access by install.sh
+		cp $ydb_dist/plugin/libydbposix.so plugin
+		cp $ydb_dist/plugin/ydbposix.xc plugin
+		cp $ydb_dist/plugin/o/_ydbposix.so plugin/o
+		cp $ydb_dist/plugin/o/utf8/_ydbposix.so plugin/o/utf8
+		# Copy Octo-specific dependencies for later access by install.sh
+		cp ../src/aux/*.m plugin/r
+		cp ../tests/fixtures/postgres-seed.* plugin/octo
+		cp ../tests/fixtures/northwind.* plugin/octo
+		cp ../src/aux/octo.conf.default plugin/octo/octo.conf
+		echo "Done."
+	fi
+
+	# Build binary package
+	make package
 fi
-
-# Ubuntu pipelines only: Copy installation files for use in Docker image construction
-if [[ $cmakeCommand == "cmake" ]]; then
-	echo -n "Copying installation files for Docker image generation... "
-	cp ../tools/ci/install.sh .
-	# Create plugin directory structure for later reference by install.sh
-	mkdir -p plugin/r plugin/o/utf8 plugin/octo/
-	# Copy YDBPosix into build directory for later access by install.sh
-	cp $ydb_dist/plugin/libydbposix.so plugin
-	cp $ydb_dist/plugin/ydbposix.xc plugin
-	cp $ydb_dist/plugin/o/_ydbposix.so plugin/o
-	cp $ydb_dist/plugin/o/utf8/_ydbposix.so plugin/o/utf8
-	# Copy Octo-specific dependencies for later access by install.sh
-	cp ../src/aux/*.m plugin/r
-	cp ../tests/fixtures/postgres-seed.* plugin/octo
-	cp ../tests/fixtures/northwind.* plugin/octo
-	cp ../src/aux/octo.conf.default plugin/octo/octo.conf
-	echo "Done."
-fi
-
-# Build binary package
-make package
 
 # Cleanup files and directories that don't need to be included in the pipeline artifacts
 rm -rf CMakeFiles
@@ -208,3 +211,6 @@ rm -rf _CPack_Packages
 rm -rf bats-test.*/go
 rm -f postgresql*.jar
 rm -f *.cmake
+rm -f src/test_*	# these are the unit test case executables (should not be needed otherwise)
+
+exit $exit_status

tests/test_helpers.bash.in
@@ -329,6 +329,9 @@ run_go() {
 	go get $1
 	go build $1
 	$GOPATH/bin/$1 $2
+	# Executables created by go occupy a noticeable part of the artifacts.zip file created in the pipeline.
+	# They are not considered necessary at this point for test failure analysis so remove them.
+	rm -f $1
 }
 
 run_java() {
@@ -642,10 +645,10 @@ run_query_in_octo_and_postgres_and_crosscheck() {
 
 run_query_generator() {
   databaseName="$1"
-  numQueries="$2"
-  prefix="$3"
-  usejdbc="$4"
-  useextended="$5"
+  prefix="$2"
+  usejdbc="$3"
+  useextended="$4"
+  numQueries=100	# For now generate 100 queries by default (good balance of test runtime vs test coverage)
 
   cp @PROJECT_SOURCE_DIR@/tests/fixtures/QueryGenerator.m .
   load_fixture "$databaseName.sql"

tests/test_jdbc_connection.bats.in
@@ -62,19 +62,19 @@ teardown() {
 }
 
 @test "TJC004 : Test Query Generator against customers database" {
-  run_query_generator customers 150 TJC04- "" "" usejdbc
+  run_query_generator customers TJC04- "" "" usejdbc
 }
 
 @test "TJC005 : Test Query Generator against pastas database" {
-  run_query_generator pastas 150 TJC05- "" "" usejdbc
+  run_query_generator pastas TJC05- "" "" usejdbc
 }
 
 @test "TJC006 : Test Query Generator against names database" {
-  run_query_generator names 150 TJC06- "" "" usejdbc
+  run_query_generator names TJC06- "" "" usejdbc
 }
 
 @test "TJC007 : Test Query Generator against northwind database" {
-  run_query_generator northwind 150 TJC07- "" "" usejdbc
+  run_query_generator northwind TJC07- "" "" usejdbc
 }
 
 @test "TJC008 : Validate usage of Simple vs. Extended Query protocol using PrepareThreshold" {

tests/test_query_generator.bats.in
@@ -18,22 +18,22 @@ setup() {
 }
 
 @test "TQG01 : Test Query Generator against customers database" {
-  run_query_generator customers 150 TQG01-
+  run_query_generator customers TQG01-
 }
 
 @test "TQG02 : Test Query Generator against pastas database" {
-  run_query_generator pastas 150 TQG02-
+  run_query_generator pastas TQG02-
 }
 
 @test "TQG03 : Test Query Generator against names database" {
-  run_query_generator names 150 TQG03-
+  run_query_generator names TQG03-
 }
 
 @test "TQG04 : Test Query Generator against northwind database" {
-  run_query_generator northwind 150 TQG04-
+  run_query_generator northwind TQG04-
 }
 
 @test "TQG05 : Test Query Generator against boolean database" {
   skip #Skipped until issue 346 is resolved
-  run_query_generator boolean 150 TQG05-
+  run_query_generator boolean TQG05-
 }

commit c710c25f0e27597e1b7efc8ac59a5a801d4757cf

Fix master branch pipeline failure due to regression (wrong if check syntax in bash)
* The main issue is that an `if` check introduced in commit a8234847cbce0c6f6c593734f50525fb4123f780
  had the wrong bash syntax. `if [[ ! $exit_status ]];` should instead be `if [[ 0 == $exit_status ]];`.
  The wrong syntax caused a section of the `build.sh` code (that copied installation files for
  docker image construction) to not execute resulting in pipeline failures in the `octo` job in the
  master branch. Note that this pipeline happens only when a commit is merged and so it is not easily
  possible to test this out until this commit gets merged. Issues (if any) have to be fixed in a later
  commit.

* While debugging this, it was realized that the pipeline output from `build.sh` is very cryptic. And
  so it was enhanced to log each step as it happens and highlight values of key variables.

  Later I realized it is better to invoke `build.sh` with the `-v` so each step is printed by the shell
  as it reads it. So a `-v` was added to the first line of `build.sh` when `bash` is invoked.

  Similar changes were done to other `.sh` files `docs.sh`, `install-tls.sh` and `vistabuild.sh`.

* Additionally, if there are any differences in the expected vs actual warnings, the diff output will
  now show with `<` as the expected lines and `>` as the actual lines (it was the other way around
  all along and always confused me).

tools/ci/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -v
 #################################################################
 #								#
 # Copyright (c) 2019-2020 YottaDB LLC and/or its subsidiaries.	#
@@ -22,25 +22,27 @@ if [ -x "$(command -v cmake3)" ]; then
 else
   cmakeCommand="cmake"
 fi
+echo " -> cmakeCommand = $cmakeCommand"
 
 if [ -x "$(command -v ctest3)" ]; then
   ctestCommand="ctest3"
 else
   ctestCommand="ctest"
 fi
+echo " -> ctestCommand = $ctestCommand"
 
-# Install the YottaDB POSIX plugin
+echo "# Install the YottaDB POSIX plugin"
 pushd $start_dir
 ./tools/ci/install_posix.sh $cmakeCommand
 popd
 
-# Source the ENV script again to YottaDB environment variables after installing POSIX plugin
+echo "# Source the ENV script again to YottaDB environment variables after installing POSIX plugin"
 source /opt/yottadb/current/ydb_env_unset
 source /opt/yottadb/current/ydb_env_set
-echo "Done setting up POSIX plugin"
-echo "ydb_routines: $ydb_routines"
+echo " -> Done setting up POSIX plugin"
+echo " -> ydb_routines: $ydb_routines"
 
-# Download and Install BATS testing framework
+echo "# Download and Install BATS testing framework"
 cd $start_dir
 mkdir build
 cd build
@@ -49,89 +51,95 @@ cd bats-core
 ./install.sh /usr/local
 cd ..
 
-# Download PostgreSQL JDBC driver for testing
+echo "# Download PostgreSQL JDBC driver for testing"
 wget https://jdbc.postgresql.org/download/postgresql-$JDBC_VERSION.jar
 
-# Check repo for unused outref files
+echo "# Check repo for unused outref files"
 pushd ../tests
 unused_outrefs=$(../tools/ci/find_unused_outrefs.sh)
 if [ "$unused_outrefs" != "" ]; then
-  echo "Unused outrefs found!"
+  echo " -> Unused outrefs found!"
   echo "$unused_outrefs"
   exit 1
 fi
 popd
 
-# Check repo for unused test files
+echo "# Check repo for unused test files"
 pushd ../cmake
 unused_tests=$(../tools/ci/find_unused_tests.sh)
 if [ "$unused_tests" != "" ]; then
-  echo "Unused test files found!"
+  echo " -> Unused test files found!"
   echo "$unused_tests"
   exit 1
 fi
 popd
 
-# Randomly choose to test Debug or Release build
+echo "# Randomly choose to test Debug or Release build"
 if [[ $(( $RANDOM % 2)) -eq 0 ]]; then
 	build_type="Debug"
 else
 	build_type="RelWithDebInfo"
 fi
-# Randomly choose whether to use the full test suite or its limited version (prefer full version 3/4 times)
+echo " -> build_type = $build_type"
+
+echo "# Randomly choose whether to use the full test suite or its limited version (prefer full version 3/4 times)"
 if [[ $(( $RANDOM % 4)) -eq 0 ]]; then
 	full_test="OFF"
 else
 	full_test="ON"
 fi
-# Randomly choose whether to use to test installation or in build directory (prefer installation 3/4 times)
+echo " -> full_test = $full_test"
+
+echo "# Randomly choose whether to use to test installation or in build directory (prefer installation 3/4 times)"
 if [[ $(( $RANDOM % 4)) -eq 0 ]]; then
 	disable_install="ON"
 else
 	disable_install="OFF"
 fi
+echo " -> disable_install = $disable_install"
 
-# Configure the build system for Octo
+echo "# Configure the build system for Octo"
 ${cmakeCommand} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=${ydb_dist}/plugin -DCMAKE_BUILD_TYPE=$build_type -DFULL_TEST_SUITE=$full_test -DDISABLE_INSTALL=$disable_install ..
 if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 
-# Compile Octo
+echo "# Compile Octo"
 make -j `grep -c ^processor /proc/cpuinfo` 2> make_warnings.txt
 
-# Check for unexpected warnings and error/exit if unexpected errors are found
+echo "# Check for unexpected warnings and error/exit if unexpected errors are found"
 ../tools/ci/sort_warnings.sh
-echo -n "Checking for unexpected warning(s)... "
+echo " -> Checking for unexpected warning(s)... "
 if [ -x "$(command -v yum)" ]; then
 	if [[ $build_type == "Debug" ]]; then
-		diff sorted_warnings.txt ../tools/ci/expected_warnings-centos.ref &> differences.txt
+		diff ../tools/ci/expected_warnings-centos.ref sorted_warnings.txt &> differences.txt
 	else
-		diff sorted_warnings.txt ../tools/ci/expected_warnings-centos_release.ref &> differences.txt
+		diff ../tools/ci/expected_warnings-centos_release.ref sorted_warnings.txt &> differences.txt
 	fi
 else
 	if [[ $build_type == "Debug" ]]; then
-		diff sorted_warnings.txt ../tools/ci/expected_warnings.ref &> differences.txt
+		diff ../tools/ci/expected_warnings.ref sorted_warnings.txt &> differences.txt
 	else
-		diff sorted_warnings.txt ../tools/ci/expected_warnings-release.ref &> differences.txt
+		diff ../tools/ci/expected_warnings-release.ref sorted_warnings.txt &> differences.txt
 	fi
 fi
 
 if [ $(wc -l differences.txt | awk '{print $1}') -gt 0 ]; then
-  echo "Expected warnings differ from actual warnings!"
-  echo "note: '>' indicates an expected warning, '<' indicates an actual warning"
+  echo " -> Expected warnings differ from actual warnings! diff output follows"
+  echo " -> note: '<' indicates an expected warning, '>' indicates an actual warning"
   cat differences.txt
   exit 1
 fi
-echo "OK."
 
+echo "# make install"
 make install
 
 if [ -z $USER ]; then
+  echo " -> export USER=root"
   export USER=root
 fi
 
-# start PostgreSQL Server
+echo "# Start PostgreSQL Server"
 if [ -f /etc/init.d/postgresql ]; then
   /etc/init.d/postgresql start
 else
@@ -145,67 +153,67 @@ else
   sleep 2
 fi
 
-# Make the current user a superuser
+echo "# Make the current user a superuser"
 su - postgres -c psql <<PSQL
 create user $USER;
 alter user $USER SUPERUSER;
 PSQL
 
-# Setup for tests
+echo "# Setup for tests"
 pushd src
 $ydb_dist/mupip set -n=true -reg '*'
 
-# Source ydb_env_set after building and installing Octo
+echo "# Source ydb_env_set after building and installing Octo"
 if [[ $disable_install == "OFF" ]]; then
 	source /opt/yottadb/current/ydb_env_unset
 	source /opt/yottadb/current/ydb_env_set
-	echo "Done setting up Octo plugin"
-	echo "ydb_routines: $ydb_routines"
+	echo " -> Done setting up Octo plugin"
+	echo " -> ydb_routines: $ydb_routines"
 else
 	export ydb_routines="$(pwd)/_ydbocto.so $ydb_routines"
-	echo "ydb_routines: $ydb_routines"
+	echo " -> ydb_routines: $ydb_routines"
 fi
 
-# Load the data required for tests
+echo "# Load the data required for tests"
 ./octo -f ../../tests/fixtures/names.sql
 $ydb_dist/mupip load ../../tests/fixtures/names.zwr
 popd
 
-# Run the tests
+echo "# Run the tests"
 ${ctestCommand} -j `grep -c ^processor /proc/cpuinfo`
 exit_status=$?
+echo " -> exit_status from ${ctestCommand} = $exit_status"
 
-if [[ ! $exit_status ]]; then
-	# Rebuild Octo for packaging if it wasn't a RelWithDebInfo build
+if [[ 0 == $exit_status ]]; then
 	if [[ $build_type != "RelWithDebInfo" ]]; then
+		echo "# Rebuild Octo for packaging as it wasn't a RelWithDebInfo build"
 		${cmakeCommand} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=${ydb_dist}/plugin ..
 		make -j `grep -c ^processor /proc/cpuinfo`
 	fi
 
-	# Ubuntu pipelines only: Copy installation files for use in Docker image construction
 	if [[ $cmakeCommand == "cmake" ]]; then
-		echo -n "Copying installation files for Docker image generation... "
+		echo "# Ubuntu pipelines only: Copy installation files for use in Docker image construction"
+		echo " -> Copying installation files for Docker image generation... "
 		cp ../tools/ci/install.sh .
-		# Create plugin directory structure for later reference by install.sh
+		echo "# Create plugin directory structure for later reference by install.sh"
 		mkdir -p plugin/r plugin/o/utf8 plugin/octo/
-		# Copy YDBPosix into build directory for later access by install.sh
+		echo "# Copy YDBPosix into build directory for later access by install.sh"
 		cp $ydb_dist/plugin/libydbposix.so plugin
 		cp $ydb_dist/plugin/ydbposix.xc plugin
 		cp $ydb_dist/plugin/o/_ydbposix.so plugin/o
 		cp $ydb_dist/plugin/o/utf8/_ydbposix.so plugin/o/utf8
-		# Copy Octo-specific dependencies for later access by install.sh
+		echo "# Copy Octo-specific dependencies for later access by install.sh"
 		cp ../src/aux/*.m plugin/r
 		cp ../tests/fixtures/postgres-seed.* plugin/octo
 		cp ../tests/fixtures/northwind.* plugin/octo
 		cp ../src/aux/octo.conf.default plugin/octo/octo.conf
-		echo "Done."
 	fi
 
-	# Build binary package
+	echo "# Build binary package"
 	make package
 fi
 
-# Cleanup files and directories that don't need to be included in the pipeline artifacts
+echo "# Cleanup files and directories that don't need to be included in the pipeline artifacts"
 rm -rf CMakeFiles
 rm -rf _CPack_Packages
 rm -rf bats-test.*/go
@@ -213,4 +221,5 @@ rm -f postgresql*.jar
 rm -f *.cmake
 rm -f src/test_*	# these are the unit test case executables (should not be needed otherwise)
 
+echo " -> exit $exit_status"
 exit $exit_status

tools/ci/docs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -v
 #################################################################
 #								#
 # Copyright (c) 2019-2020 YottaDB LLC and/or its subsidiaries.	#

tools/ci/install-tls.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -v
 #################################################################
 #								#
 # Copyright (c) 2019-2020 YottaDB LLC and/or its subsidiaries.	#

tools/ci/vistabuild.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/sh -v
 #################################################################
 #								#
-# Copyright (c) 2019 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2019-2020 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
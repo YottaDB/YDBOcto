53a4b5adb06d87a64b89e11a8bb5a116614dc1ce
Joshua Nelson <joshua@yottadb.com>
2020-06-26 14:35:20 -0400

    [#314] Add support for named inserts and NULL in filter.py
    - Replace NULL with empty string
    - Don't require CREATE TABLE statements to be on multiple lines
    - Don't overwrite primary_key before saving it
    - Don't include '(' in the name of the table
    - If generating a primary key and inserting with named columns, add that key to the names

--- tests/fixtures/sqllogic/insert.py
+++ tests/fixtures/sqllogic/insert.py
@@ -50,7 +50,7 @@ with open(input_filename) as insert_file:
 # First write the parsed SQL statements to disk
 format = lambda query: ''.join(query).rstrip() + ';\n'
 
-# {table_name: has_primary_key}
+# {table_name: (has_primary_key, [column, ...])}
 tables = {}
 
 unique_id = 0
@@ -58,14 +58,24 @@ def add_unique_id(stmt):
     if "CREATE TABLE" in stmt[0]:
         # CREATE TABLE table_name(...)
         table_name = stmt[0].split()[2]
-        paren = table_name.index('(')
-        if paren != -1:
-            table_name = table_name[:paren]
+        table_name = table_name[:table_name.index('(')]
         assert table_name not in tables.keys(), "saw CREATE TABLE for table that has already been created"
         # Find the primary key for the table, if one exists
         primary_key = next((line for line in stmt if " PRIMARY KEY" in line), None)
-        # Store this so we know whether to generate an id for INSERT statements
-        tables[table_name] = primary_key is not None
+        has_primary_key = primary_key is not None
+        # Find the column names so we can support named columns in INSERTs
+        if stmt[0].find(')') == -1:
+            print("warning: currently named columns for `CREATE TABLE` statements must all be on one line")
+            print("warning: continuing, but named inserts will not work") # not work -> assertion failure, not data corruption
+            columns = []
+        else:
+            # stmt[0]: "... (a INTEGER, b INTEGER PRIMARY KEY, c VARCHAR)"
+            # [a INTEGER, ...]
+            create = stmt[0]
+            assert create.count(')') == 1, "`VARCHAR(30)` or any other types with nested parentheses are not supported"
+            columns = create[create.index('(') + 1:create.index(')')].split(',')
+            # Keep only the names: [a, b, c]
+            columns = list(map(lambda column: column.strip().split(' ')[0], columns))
         # Add our own primary key if one does not already exist.
         # This prevents catastrophically slow joins when there are many columns in a table.
         if primary_key is None:
@@ -79,15 +89,23 @@ def add_unique_id(stmt):
             # )
             else:
                 stmt[0] = stmt[0] + "  id INTEGER PRIMARY KEY,\n"
+            # Our generated ID will be the first column.
             primary_key = "id"
+            if columns:
+                columns = [primary_key] + columns
         else:
             # x INTEGER PRIMARY KEY -> x
             words = primary_key.split()
             primary_key = words[words.index("PRIMARY") - 2]
+        print(columns)
+
+        # Store the columns so we know whether to generate an id for INSERT statements and how to handle named columns
+        tables[table_name] = (has_primary_key, columns)
+
         # This is not a very smart script.
         assert stmt[-1].rstrip()[-1] == ')', \
            "Don't know how to handle CREATE TABLE statements not ending with ')' (got {})".format(stmt[-1])
-        stmt[-1] = ') GLOBAL "^{}(keys(""{}""))"'.format(table_name, primary_key)
+        stmt[-1] += ' GLOBAL "^{}(keys(""{}""))"'.format(table_name, primary_key)
     elif "INSERT INTO" in stmt[0]:
         # INSERT INTO table_name(...)
         words = stmt[0].split()
@@ -96,10 +114,13 @@ def add_unique_id(stmt):
         if paren != -1:
             table_name = table_name[:paren].strip()
         # Add our own primary key if there wasn't one in the table already
-        if not tables[table_name]:
+        if not tables[table_name][0]:
             global unique_id
             # INSERT INTO table_name(id, ...)
-            stmt[0] = stmt[0].replace("VALUES(", "VALUES({}, ".format(unique_id))
+            stmt[0] = stmt[0].replace(table_name + '(', table_name + '(id,')
+            stmt[0] = stmt[0].replace("VALUES(", "VALUES({},".format(unique_id))
+            print("inserting primary key")
+            print(stmt[0])
             unique_id += 1
     return stmt
 
@@ -112,8 +133,7 @@ with open("sqllogic{}.sql".format(file_num), 'w') as sql_file:
 # Now transform the SQL syntax into something MUPIP LOAD understands
 # This supports the following statements:
 # - INSERT INTO table VALUES (a, b, ...)
-# This will give an assertion error on the following statements:
-# - INSERT INTO table (named, columns) VALUES (a, b, ...)
+# - INSERT INTO table (named, columns) VALUES (b, a, ...)
 # All other statements are ignored.
 #
 # This assumes that there is no `PRIMARY KEY` column and generates its own internal unique ID to serve as a PRIMARY KEY.
@@ -127,14 +147,44 @@ with open("sqllogic{}.zwr".format(file_num), 'w') as zwr_file:
         assert len(stmt) == 1, "INSERT statements should only have one line"
         # Allow spaces inside the VALUES list
         table, rest = stmt[0][len("INSERT INTO "):].split(' ', 1)
-        # e.g. INSERT INTO table(b, a) VALUES (b, a)
-        assert '(' not in table, "named inserts are not supported (for insert statement {})".format(stmt[0])
         # NOTE: does not handle escaping at all
         values = list(map(lambda s: s.strip().replace("'", ''), rest.split(',')))
-        values[0] = values[0].replace('VALUES(', '').lstrip()
+        values[0] = values[0].split('VALUES(', 1)[1].lstrip()
         assert values[-1][-1] == ')', "INSERT INTO ... VALUES (...) should end with a ')', got {}".format(values[-1])
         # Python doesn't have a str.pop function
         values[-1] = values[-1][:-1]
+
+        # named inserts, e.g. INSERT INTO table(b, a) VALUES (b, a)
+        if '(' in table:
+            start, end = table.index('('), table.index(')')
+            insert_columns = table[start + 1:end].replace(' ', '').split(',')
+            table = table[:start]
+            schema_columns = tables[table][1]
+            assert schema_columns, "attempted a named insert, but the column names for table {} are unknown".format(table)
+            # column = (idx, name)
+            values = map(
+                lambda column: column[1],
+                # Sort the inserts by the index of the column in the schema.
+                # https://stackoverflow.com/a/6422808/7669110
+                # For example, given
+                # - schema_columns = (a, b, c)
+                # - insert_columns = (b, c, a)
+                # - values = (1, 2, 3)
+                # outputs (3, 1, 2)
+                sorted(enumerate(values), key=lambda column: schema_columns.index(insert_columns[column[0]]))
+            )
+            print(values)
+            #assert False, "named inserts are not supported (for insert statement {})".format(stmt[0])
+
+        # Now replace NULL with the empty string
+        def replace_null(val):
+            if val == 'NULL':
+                return ''
+            else:
+                return val
+        values = map(replace_null, values)
+
+        # Finally write out to disk.
         content = '^{}({})="{}"\n'.format(table, values[0], '|'.join(values[1:]))
         zwr_file.write(content)
 

YottaDB/YDBOcto

commit cea9d6a6d097e9447562c2d313381a717dc28e8d

[#90] Fix CREATE/DROP FUNCTION so pre-existing plans using the created/dropped function get regenerated the next time the corresponding query gets run
Overview
---------
* This is the first of a series of commits geared towards fixing #90.

  https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/90#note_372807043 lists a summary of 5 issues that needs
  to be fixed. This commit addresses bullet (3) (the simplest of the 5 issues) in that summary which is also
  pasted below.

  Fix CREATE or DROP FUNCTION to automatically
  --------------------------------------------
  - Delete all generated M plans that relied on the function being created/deleted
  - Delete all M global nodes in ^%ydboctoocto/^%ydboctoschema etc. that point to any of the above deleted objects

* Although the above summary talks about deleting the generated M plans, this commit keeps the generated M
  plans intact. Instead it deletes only the M global nodes that store the fact that a plan exists in the Octo
  internal database. This is enough to ensure the plan gets recreated the next time such a query is seen by
  Octo even if the plan already exists. This is done because deleting the .m file is not enough. One needs to
  also delete the .o file to ensure the plan is completely deleted and finding the .o file is not easy (it
  could be in a different directory than the .m file). Since deleting the .m/.o files is not necessary to ensure
  recreation of plans, we stick with just deleting the database nodes and verify that plans do get recreated
  even in that case.

Cleanup fixes
-------------
Since this is the first commit of #90, a lot of time was spent cleaning up the code to prepare for the later
commits. Related changes are the following.

* Remove unused M node in octo-seed.zwr.

* Rename string literals (mostly used in subscripts of lvns/gvns by Octo/Rocto) with OCTOLIT_ and PP_
  prefixes so their usage is centralized and consistent. For example instead of using `"functions"`

tests/outref/TCF023.ref
@@ -1,0 +1,43 @@
+# Run 8 queries from TCF023.sql that use ABS and REPLACE functions
+# Verify 8 _ydboctoP*.m plans have been generated
+# Verify ALL 8 queries created new plans
+[ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+[ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+[ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+[ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+[ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+[ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+[ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+[ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+# Verify db nodes correctly represent ABS and REPLACE functions and plans that rely on them
+^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","REPLACE","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","REPLACE","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","REPLACE","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","REPLACE","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","REPLACE","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","REPLACE","plan_metadata","$PWD/_ydboctoP*.m")=""
+# Rerun CREATE FUNCTION of REPLACE
+# Verify 8 _ydboctoP*.m plans still remain
+# Verify db nodes corresponding to REPLACE function have been deleted
+^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
+^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
+# Rerun 8 queries from TCF023.sql that use ABS and REPLACE functions
+# Verify 6 queries that used REPLACE function created new plans while rest reused existing plans
+[ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+[ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+[ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+[ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+[ INFO] PATH:LINENUM DATE TIME : Using already generated M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+[ INFO] PATH:LINENUM DATE TIME : Using already generated M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+[ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)
+[ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)

tests/test_create_function.bats.in
@@ -148,3 +148,31 @@ setup() {
 	load_fixture TCF022.sql subtest novv
 	verify_output TCF022 output.txt noinfo
 }
+
+@test "TCF023 : OCTO90 : Rerunning query after CREATE FUNCTION should recreate plans that relied on the recreated function" {
+	# Note this test is mostly the same as the "test_drop_function/TDF002" subtest
+	# Change to one will need to be done in both places.
+	echo "# Run 8 queries from TCF023.sql that use ABS and REPLACE functions" > output.txt
+	cp @PROJECT_SOURCE_DIR@/tests/fixtures/TCF023{,b}.sql .
+	octo -vv -f TCF023.sql > run1.txt 2>&1
+	echo '# Verify 8 _ydboctoP*.m plans have been generated' >> output.txt
+	[[ $(ls -l _ydboctoP*.m | wc -l) -eq 8 ]]
+	echo "# Verify ALL 8 queries created new plans" >> output.txt
+	grep "execute SQL query" run1.txt >> output.txt
+	echo "# Verify db nodes correctly represent ABS and REPLACE functions and plans that rely on them" >> output.txt
+	mupip extract run1.ext
+	grep plan_metadata run1.ext | grep functions >> output.txt
+	echo "# Rerun CREATE FUNCTION of REPLACE" >> output.txt
+	octo -f TCF023b.sql >> output.txt 2>&1
+	echo '# Verify 8 _ydboctoP*.m plans still remain' >> output.txt
+	[[ $(ls -l _ydboctoP*.m | wc -l) -eq 8 ]]
+	echo "# Verify db nodes corresponding to REPLACE function have been deleted" >> output.txt
+	mupip extract run2.ext
+	grep plan_metadata run2.ext | grep functions >> output.txt
+	echo "# Rerun 8 queries from TCF023.sql that use ABS and REPLACE functions" >> output.txt
+	octo -vv -f TCF023.sql > run2.txt 2>&1
+	echo "# Verify 6 queries that used REPLACE function created new plans while rest reused existing plans" >> output.txt
+	grep "execute SQL query" run2.txt >> output.txt
+	verify_output TCF023 output.txt
+}
+

src/cleanup_tables.c
@@ -27,11 +27,11 @@ void cleanup_tables() {
 	ydb_buffer_t result_b;
 
 	YDB_STRING_TO_BUFFER(config->global_names.loadedschemas, &loaded_schemas_b[0]);
-	YDB_STRING_TO_BUFFER("tables", &loaded_schemas_b[1]);
+	YDB_STRING_TO_BUFFER(OCTOLIT_TABLES, &loaded_schemas_b[1]);
 	loaded_schemas_b[2].buf_addr = table_name;
 	loaded_schemas_b[2].len_used = 0;
 	loaded_schemas_b[2].len_alloc = sizeof(table_name);
-	YDB_STRING_TO_BUFFER("chunk", &loaded_schemas_b[3]);
+	YDB_STRING_TO_BUFFER(OCTOLIT_CHUNK, &loaded_schemas_b[3]);
 
 	result_b.buf_addr = buffer;
 	result_b.len_alloc = result_b.len_used = sizeof(buffer);

src/delete_function_from_pg_proc.c
@@ -17,6 +17,9 @@
 
 /* Deletes all references to a function name from the catalog.
  * Undoes what was done by "store_function_in_pg_proc.c".
+ * Returns
+ *	0 for normal
+ *	1 for error
  */
 int delete_function_from_pg_proc(ydb_buffer_t *function_name_buffer) {
 	int	     status;
@@ -25,25 +28,26 @@ int delete_function_from_pg_proc(ydb_buffer_t *function_name_buffer) {
 	char	     oid_str[INT64_TO_STRING_MAX];
 
 	YDB_STRING_TO_BUFFER(config->global_names.octo, &pg_proc[0]);
-	YDB_STRING_TO_BUFFER("tables", &pg_proc[1]);
-	YDB_STRING_TO_BUFFER("pg_catalog", &pg_proc[2]);
+	YDB_STRING_TO_BUFFER(OCTOLIT_TABLES, &pg_proc[1]);
+	YDB_STRING_TO_BUFFER(OCTOLIT_PG_CATALOG, &pg_proc[2]);
 	YDB_STRING_TO_BUFFER("pg_proc", &pg_proc[3]);
 	pg_proc[4].buf_addr = oid_str;
 	pg_proc[4].len_alloc = sizeof(oid_str);
 
-	// Check OID for FUNCTIONNAME (usually stored as ^%ydboctoocto("functions",FUNCTIONNAME,"oid")=FUNCTIONOID)
+	// Check OID for FUNCTIONNAME (usually stored as ^%ydboctoocto(OCTOLIT_FUNCTIONS,FUNCTIONNAME,OCTOLIT_OID)=FUNCTIONOID)
 	YDB_STRING_TO_BUFFER(config->global_names.octo, &octo_functions[0]);
-	YDB_STRING_TO_BUFFER("functions", &octo_functions[1]);
+	YDB_STRING_TO_BUFFER(OCTOLIT_FUNCTIONS, &octo_functions[1]);
 	octo_functions[2] = *function_name_buffer;
-	YDB_STRING_TO_BUFFER("oid", &octo_functions[3]);
+	YDB_STRING_TO_BUFFER(OCTOLIT_OID, &octo_functions[3]);
 	status = ydb_get_s(&octo_functions[0], 3, &octo_functions[1], &pg_proc[4]);
 	if (YDB_ERR_GVUNDEF != status) {
 		YDB_ERROR_CHECK(status);
 		if (YDB_OK != status) {
 			return 1;
 		}
-		// Delete function OID node : i.e. KILL ^%ydboctoocto("tables","pg_catalog","pg_proc",FUNCTIONOID)
+		// Delete function OID node : i.e. KILL ^%ydboctoocto(OCTOLIT_TABLES,OCTOLIT_PG_CATALOG,"pg_proc",FUNCTIONOID)
 		status = ydb_delete_s(&pg_proc[0], 4, &pg_proc[1], YDB_DEL_NODE);
+		YDB_ERROR_CHECK(status);
 		if (YDB_OK != status) {
 			return 1;
 		}

src/delete_table_from_pg_class.c
@@ -41,6 +41,9 @@
 
 /* Deletes all references to a tablename and its columns from the catalog.
  * Undoes what was done by "store_table_in_pg_class.c".
+ * Returns
+ *	0 for normal
+ *	1 for error
  */
 int delete_table_from_pg_class(ydb_buffer_t *table_name_buffer) {
 	int	      status;
@@ -50,11 +53,11 @@ int delete_table_from_pg_class(ydb_buffer_t *table_name_buffer) {
 	ydb_buffer_t  schema_global;
 	ydb_buffer_t  oid_buffer;
 
-	pg_class = make_buffers(config->global_names.octo, 4, "tables", "pg_catalog", "pg_class", "");
-	pg_attribute = make_buffers(config->global_names.octo, 4, "tables", "pg_catalog", "pg_attribute", "");
+	pg_class = make_buffers(config->global_names.octo, 4, OCTOLIT_TABLES, OCTOLIT_PG_CATALOG, OCTOLIT_PG_CLASS, "");
+	pg_attribute = make_buffers(config->global_names.octo, 4, OCTOLIT_TABLES, OCTOLIT_PG_CATALOG, OCTOLIT_PG_ATTRIBUTE, "");
 	YDB_MALLOC_BUFFER(&oid_buffer, INT64_TO_STRING_MAX);
 	YDB_MALLOC_BUFFER(&pg_attribute_schema[2], MAX_STR_CONST);
-	/* Check OID for tablename (usually stored as ^%ydboctoschema(TABLENAME,"pg_class")=TABLEOID) */
+	/* Check OID for tablename (usually stored as ^%ydboctoschema(TABLENAME,OCTOLIT_PG_CLASS)=TABLEOID) */
 	YDB_STRING_TO_BUFFER(config->global_names.schema, &schema_global);
 	pg_class_schema[0] = *table_name_buffer;
 	pg_class_schema[1] = pg_class[3];
@@ -62,18 +65,19 @@ int delete_table_from_pg_class(ydb_buffer_t *table_name_buffer) {
 	if (YDB_ERR_GVUNDEF != status) {
 		CLEANUP_AND_RETURN_IF_NOT_YDB_OK(status, pg_class, pg_attribute, pg_attribute_schema, oid_buffer);
 		pg_class[4] = oid_buffer;
-		/* Delete table OID node : i.e. KILL ^%ydboctoocto("tables","pg_catalog","pg_class",TABLEOID) */
+		/* Delete table OID node : i.e. KILL ^%ydboctoocto(OCTOLIT_TABLES,OCTOLIT_PG_CATALOG,OCTOLIT_PG_CLASS,TABLEOID) */
 		status = ydb_delete_s(&pg_class[0], 4, &pg_class[1], YDB_DEL_NODE);
 		CLEANUP_AND_RETURN_IF_NOT_YDB_OK(status, pg_class, pg_attribute, pg_attribute_schema, oid_buffer);
 	} else {
 		/* OID for table does not exist. Move on to next step. */
 	}
-	/* Check OID for each column in table (usually stored as ^%ydboctoschema(TABLENAME,"pg_attribute",COLUMNNAME)=COLUMNOID) */
+	/* Check OID for each column in table (usually stored as
+	 * ^%ydboctoschema(TABLENAME,OCTOLIT_PG_ATTRIBUTE,COLUMNNAME)=COLUMNOID) */
 	pg_attribute_schema[0] = *table_name_buffer;
 	pg_attribute_schema[1] = pg_attribute[3];
 	assert(0 == pg_attribute_schema[2].len_used); /* should have been set by YDB_MALLOC_BUFFER above */
 	do {
-		/* Find COLUMNNAME such that ^%ydboctoschema(TABLENAME,"pg_attribute",COLUMNNAME) exists */
+		/* Find COLUMNNAME such that ^%ydboctoschema(TABLENAME,OCTOLIT_PG_ATTRIBUTE,COLUMNNAME) exists */
 		status = ydb_subscript_next_s(&schema_global, 3, pg_attribute_schema, &pg_attribute_schema[2]);
 		if (YDB_ERR_NODEEND == status) {
 			break;
@@ -82,7 +86,8 @@ int delete_table_from_pg_class(ydb_buffer_t *table_name_buffer) {
 		/* Get COLUMNOID corresponding to COLUMNNAME */
 		status = ydb_get_s(&schema_global, 3, pg_attribute_schema, &oid_buffer);
 		CLEANUP_AND_RETURN_IF_NOT_YDB_OK(status, pg_class, pg_attribute, pg_attribute_schema, oid_buffer);
-		/* Delete column OID node : i.e. KILL ^%ydboctoocto("tables","pg_catalog","pg_attribute",COLUMNOID) */
+		/* Delete column OID node : i.e. KILL
+		 * ^%ydboctoocto(OCTOLIT_TABLES,OCTOLIT_PG_CATALOG,OCTOLIT_PG_ATTRIBUTE,COLUMNOID) */
 		pg_attribute[4] = oid_buffer;
 		status = ydb_delete_s(&pg_attribute[0], 4, &pg_attribute[1], YDB_DEL_NODE);
 		CLEANUP_AND_RETURN_IF_NOT_YDB_OK(status, pg_class, pg_attribute, pg_attribute_schema, oid_buffer);

src/drop_schema_from_local_cache.c
@@ -24,16 +24,16 @@ int drop_schema_from_local_cache(ydb_buffer_t *name_buffer, SqlSchemaType schema
 	YDB_STRING_TO_BUFFER(config->global_names.loadedschemas, &schema_local);
 	/* Free up memory chunk noted down at the end of "src/find_function.c" or "src/find_table.c"
 	 * Those would be stored in one of the below 2 node pairs:
-	 *	%ydboctoloadedschemas("functions",FUNCTIONNAME)
-	 *	%ydboctoloadedschemas("functions",FUNCTIONNAME,"chunk")
+	 *	%ydboctoloadedschemas(OCTOLIT_FUNCTIONS,FUNCTIONNAME)
+	 *	%ydboctoloadedschemas(OCTOLIT_FUNCTIONS,FUNCTIONNAME,OCTOLIT_CHUNK)
 	 * OR
 	 *	%ydboctoloadedschemas(TABLENAME)
-	 *	%ydboctoloadedschemas(TABLENAME,"chunk")
+	 *	%ydboctoloadedschemas(TABLENAME,OCTOLIT_CHUNK)
 	 */
 	if (TableSchema == schema_type) {
-		YDB_STRING_TO_BUFFER("tables", &subs_array[0]);
+		YDB_STRING_TO_BUFFER(OCTOLIT_TABLES, &subs_array[0]);
 	} else if (FunctionSchema == schema_type) {
-		YDB_STRING_TO_BUFFER("functions", &subs_array[0]);
+		YDB_STRING_TO_BUFFER(OCTOLIT_FUNCTIONS, &subs_array[0]);
 	} else {
 		assert(FALSE);
 	}
@@ -52,7 +52,7 @@ int drop_schema_from_local_cache(ydb_buffer_t *name_buffer, SqlSchemaType schema
 		YDB_ERROR_CHECK(status);
 		return status;
 	}
-	YDB_LITERAL_TO_BUFFER("chunk", &subs_array[2]);
+	YDB_LITERAL_TO_BUFFER(OCTOLIT_CHUNK, &subs_array[2]);
 	status = ydb_get_s(&schema_local, 3, &subs_array[0], &ret);
 	if (YDB_OK == status) {
 		assert(sizeof(void *) == ret.len_used);
@@ -64,5 +64,6 @@ int drop_schema_from_local_cache(ydb_buffer_t *name_buffer, SqlSchemaType schema
 	}
 	/* Now that memory has been freed, delete those nodes */
 	status = ydb_delete_s(&schema_local, 1, &subs_array[0], YDB_DEL_TREE);
+	YDB_ERROR_CHECK(status);
 	return status;
 }

src/emit_select_statement.c
@@ -21,6 +21,7 @@
 #include "template_strings.h"
 #include "logical_plan.h"
 #include "physical_plan.h"
+#include "lp_verify_structure.h"
 #include "helpers.h"
 
 PSQL_TypeSize get_type_size_from_psql_type(PSQL_TypeOid type) {
@@ -57,7 +58,7 @@ PSQL_TypeSize get_type_size_from_psql_type(PSQL_TypeOid type) {
  */
 PhysicalPlan *emit_select_statement(SqlStatement *stmt, char *plan_filename) {
 	LPActionType	    set_oper_type;
-	LogicalPlan *	    plan, *cur_plan, *column_alias;
+	LogicalPlan *	    plan, *cur_plan, *column_alias, *function;
 	PhysicalPlan *	    pplan;
 	SqlValue *	    value;
 	char		    output_key[INT32_TO_STRING_MAX], valbuff[INT32_TO_STRING_MAX];
@@ -91,6 +92,11 @@ PhysicalPlan *emit_select_statement(SqlStatement *stmt, char *plan_filename) {
 	memset(&options, 0, sizeof(PhysicalPlanOptions));
 	pplan = NULL;
 	options.last_plan = &pplan;
+	function = NULL;
+	options.function = &function; /* Store pointer to "function" in options.function. "generate_physical_plan" call below
+				       * will update "function" to point to start of the linked list of LP_FUNCTION_CALL usages
+				       * (if any) in the entire query.
+				       */
 	pplan = generate_physical_plan(plan, &options);
 	if (NULL == pplan) {
 		ERROR(ERR_PLAN_NOT_GENERATED, "physical");
@@ -125,6 +131,37 @@ PhysicalPlan *emit_select_statement(SqlStatement *stmt, char *plan_filename) {
 		free(plan_meta);
 		return NULL;
 	}
+	if (NULL != function) {
+		/* "function" points to the start of the linked list of LP_FUNCTION_CALL usages in entire query.
+		 * Store link between plan and all function calls that plan uses so a later CREATE/DROP FUNCTION
+		 * of any of the functions in this plan knows to delete this stale plan.
+		 */
+		ydb_buffer_t func_buff[4];
+
+		YDB_LITERAL_TO_BUFFER(OCTOLIT_FUNCTIONS, &func_buff[0]);
+		func_buff[2] = plan_meta[1];
+		func_buff[3] = plan_meta[2];
+		do {
+			LogicalPlan *function_name;
+
+			/* Store function name in "func_buff[1]" */
+			GET_LP(function_name, function, 0, LP_VALUE);
+			value = function_name->v.lp_value.value;
+			assert(STRING_LITERAL == value->type);
+			YDB_STRING_TO_BUFFER(value->v.string_literal, &func_buff[1]);
+			/* Store gvn that links plan and this function */
+			status = ydb_set_s(&plan_meta[0], 4, func_buff, NULL);
+			YDB_ERROR_CHECK(status);
+			if (YDB_OK != status) {
+				break;
+			}
+			function = function->extra_detail.lp_function_call.next_function;
+		} while (LP_FUNCTION_CALL_LIST_END != function);
+		if (YDB_OK != status) {
+			free(plan_meta);
+			return NULL;
+		}
+	}
 	YDB_LITERAL_TO_BUFFER(OCTOLIT_OUTPUT_COLUMNS, &plan_meta[3]);
 	YDB_MALLOC_BUFFER(&plan_meta[4], INT16_TO_STRING_MAX); // Column ID
 
@@ -140,7 +177,7 @@ PhysicalPlan *emit_select_statement(SqlStatement *stmt, char *plan_filename) {
 		num_columns++;
 		OCTO_INT16_TO_BUFFER(num_columns, &plan_meta[4]);
 
-		YDB_LITERAL_TO_BUFFER("name", &plan_meta[5]);
+		YDB_LITERAL_TO_BUFFER(OCTOLIT_NAME, &plan_meta[5]);
 		value_buffer.buf_addr = value->v.string_literal;
 		value_buffer.len_used = value_buffer.len_alloc = strlen(value_buffer.buf_addr);
 		status = ydb_set_s(plan_meta, 5, &plan_meta[1], &value_buffer);
@@ -148,7 +185,7 @@ PhysicalPlan *emit_select_statement(SqlStatement *stmt, char *plan_filename) {
 		if (YDB_OK != status)
 			break;
 
-		YDB_LITERAL_TO_BUFFER("table_id", &plan_meta[5]);
+		YDB_LITERAL_TO_BUFFER(OCTOLIT_TABLE_ID, &plan_meta[5]);
 		value_buffer.buf_addr = "0";
 		value_buffer.len_used = value_buffer.len_alloc = strlen(value_buffer.buf_addr);
 		status = ydb_set_s(plan_meta, 5, &plan_meta[1], &value_buffer);
@@ -156,7 +193,7 @@ PhysicalPlan *emit_select_statement(SqlStatement *stmt, char *plan_filename) {
 		if (YDB_OK != status)
 			break;
 
-		YDB_LITERAL_TO_BUFFER("column_id", &plan_meta[5]);
+		YDB_LITERAL_TO_BUFFER(OCTOLIT_COLUMN_ID, &plan_meta[5]);
 		value_buffer.buf_addr = "0";
 		value_buffer.len_used = value_buffer.len_alloc = strlen(value_buffer.buf_addr);
 		status = ydb_set_s(plan_meta, 5, &plan_meta[1], &value_buffer);
@@ -164,7 +201,7 @@ PhysicalPlan *emit_select_statement(SqlStatement *stmt, char *plan_filename) {
 		if (YDB_OK != status)
 			break;
 
-		YDB_LITERAL_TO_BUFFER("data_type", &plan_meta[5]);
+		YDB_LITERAL_TO_BUFFER(OCTOLIT_DATA_TYPE, &plan_meta[5]);
 		value_buffer.buf_addr = valbuff;
 		value_buffer.len_alloc = INT32_TO_STRING_MAX;
 		column_type = get_psql_type_from_sqlvaluetype(column_alias->v.lp_column_list_alias.column_list_alias->type);
@@ -174,7 +211,7 @@ PhysicalPlan *emit_select_statement(SqlStatement *stmt, char *plan_filename) {
 		if (YDB_OK != status)
 			break;
 
-		YDB_LITERAL_TO_BUFFER("data_type_size", &plan_meta[5]);
+		YDB_LITERAL_TO_BUFFER(OCTOLIT_DATA_TYPE_SIZE, &plan_meta[5]);
 		value_buffer.len_alloc = INT16_TO_STRING_MAX;
 		type_size = get_type_size_from_psql_type(column_type);
 		// PostgreSQL protocol specifies a 16-bit integer to store each data type size
@@ -185,7 +222,7 @@ PhysicalPlan *emit_select_statement(SqlStatement *stmt, char *plan_filename) {
 		if (YDB_OK != status)
 			break;
 
-		YDB_LITERAL_TO_BUFFER("type_modifier", &plan_meta[5]);
+		YDB_LITERAL_TO_BUFFER(OCTOLIT_TYPE_MODIFIER, &plan_meta[5]);
 		value_buffer.buf_addr = "-1";
 		value_buffer.len_used = value_buffer.len_alloc = strlen(value_buffer.buf_addr);
 		status = ydb_set_s(plan_meta, 5, &plan_meta[1], &value_buffer);
@@ -193,7 +230,7 @@ PhysicalPlan *emit_select_statement(SqlStatement *stmt, char *plan_filename) {
 		if (YDB_OK != status)
 			break;
 
-		YDB_LITERAL_TO_BUFFER("format_code", &plan_meta[5]);
+		YDB_LITERAL_TO_BUFFER(OCTOLIT_FORMAT_CODE, &plan_meta[5]);
 		value_buffer.buf_addr = "0";
 		value_buffer.len_used = value_buffer.len_alloc = strlen(value_buffer.buf_addr);
 		status = ydb_set_s(plan_meta, 5, &plan_meta[1], &value_buffer);

commit 4aff1b430bd55a9f6cb585e2caaa1417017a5679

[#90] Fix random test_create_function/TCF023 subtest failure due to uninitialized memory (regression introduced in cea9d6a6)
* As part of a prior commit (cea9d6a6d097e9447562c2d313381a717dc28e8d), `src/run_query.c` used to
  invoke `DELETE_PLAN_METADATA_DB_NODE` with the null-terminated plan name string passed in as a
  parameter `filename`. But `filename` was not guaranteed to be null-terminated as it pointed to
  `function_name_buffers[3].buf_addr` whose contents were populated by `ydb_subscript_next_s()` call
  and the SimpleAPI call does not null terminate (instead it sets `len_used` to the appropriate length).

  This meant that depending on what garbage was there at the end of the valid `filename` string,
  the call to `ydb_delete_s()` inside the `DELETE_PLAN_METADATA_DB_NODE` macro could end up deleting
  a non-existent global variable node resulting in the node that we intended to delete not being
  deleted. This meant a later query which used this function that was dropped used to run without
  regenerating the plan which failed the `TCF023` subtest.

  Note that in-house testing never revealed the garbage. It was only in the pipeline and that too only
  in some runs and in some jobs that this failure was seen so it is a rare failure.

  This is now fixed by passing a `ydb_buffer_t` structure instead of a null-terminated `char *` to
  the `DELETE_PLAN_METADATA_DB_NODE` and related `GET_PLAN_METADATA_DB_NODE` macros. This way we
  avoid an unnecessary `YDB_STRING_TO_BUFFER` in the common `SETUP_PLAN_METADATA_DB_NODE` macro (which
  both of the above macros in turn invoke). This saves us an unnecessary call to `strlen()` since
  we already know the string length. As well as avoid the need for us to null-terminate the string
  (which we would have otherwise needed to do to avoid the garbage and the TCF023 subtest failure).

* The `TCF023` subtest is now enhanced to also verify that the `ydb_delete_s()` call did delete

src/run_query.c
@@ -68,7 +68,7 @@
 	{                                                                     \
 		YDB_STRING_TO_BUFFER(config->global_names.octo, &VARNAME);    \
 		YDB_LITERAL_TO_BUFFER(OCTOLIT_PLAN_METADATA, &SUBS_ARRAY[0]); \
-		YDB_STRING_TO_BUFFER(PLAN_FILENAME, &SUBS_ARRAY[1]);          \
+		SUBS_ARRAY[1] = PLAN_FILENAME;                                \
 	}
 
 #define GET_PLAN_METADATA_DB_NODE(PLAN_FILENAME, DB_NODE_FOUND, STATUS)          \
@@ -322,8 +322,11 @@ int run_query(callback_fnptr_t callback, void *parms, boolean_t send_row_descrip
 				 * an ERR_DATABASE_FILES_OOS error later.
 				 */
 				unsigned int db_node_found;
+				ydb_buffer_t filename_buffer;
 
-				GET_PLAN_METADATA_DB_NODE(filename, db_node_found, status); /* sets "db_node_found" and "status" */
+				YDB_STRING_TO_BUFFER(filename, &filename_buffer);
+				GET_PLAN_METADATA_DB_NODE(filename_buffer, db_node_found,
+							  status); /* sets "db_node_found" and "status" */
 				YDB_ERROR_CHECK(status);
 				if (YDB_OK != status) {
 					CLEANUP_FROM_PLAN_GENERATION(i, filename_lock, query_lock, memory_chunks);
@@ -599,7 +602,7 @@ int run_query(callback_fnptr_t callback, void *parms, boolean_t send_row_descrip
 			}
 			CLEANUP_AND_RETURN_IF_NOT_YDB_OK(status, memory_chunks, buffer, function_sub_buffer, function_buffer,
 							 query_lock);
-			DELETE_PLAN_METADATA_DB_NODE(filename, status);
+			DELETE_PLAN_METADATA_DB_NODE(function_name_buffers[3], status);
 			CLEANUP_AND_RETURN_IF_NOT_YDB_OK(status, memory_chunks, buffer, function_sub_buffer, function_buffer,
 							 query_lock);
 		}

tests/outref/TCF023.ref
@@ -22,6 +22,15 @@
 ^%ydboctoocto("functions","REPLACE","plan_metadata","$PWD/_ydboctoP*.m")=""
 ^%ydboctoocto("functions","REPLACE","plan_metadata","$PWD/_ydboctoP*.m")=""
 ^%ydboctoocto("functions","REPLACE","plan_metadata","$PWD/_ydboctoP*.m")=""
+# Verify db nodes correctly represent 8 plans that are reusable (i.e. do not need to be regenerated)
+^%ydboctoocto("plan_metadata","$PWD/_ydboctoP*.m","output_key")="3"
+^%ydboctoocto("plan_metadata","$PWD/_ydboctoP*.m","output_key")="3"
+^%ydboctoocto("plan_metadata","$PWD/_ydboctoP*.m","output_key")="3"
+^%ydboctoocto("plan_metadata","$PWD/_ydboctoP*.m","output_key")="3"
+^%ydboctoocto("plan_metadata","$PWD/_ydboctoP*.m","output_key")="3"
+^%ydboctoocto("plan_metadata","$PWD/_ydboctoP*.m","output_key")="3"
+^%ydboctoocto("plan_metadata","$PWD/_ydboctoP*.m","output_key")="3"
+^%ydboctoocto("plan_metadata","$PWD/_ydboctoP*.m","output_key")="3"
 # Rerun CREATE FUNCTION of REPLACE
 # Verify 8 _ydboctoP*.m plans still remain
 # Verify db nodes corresponding to REPLACE function have been deleted
@@ -31,6 +40,9 @@
 ^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
 ^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
 ^%ydboctoocto("functions","ABS","plan_metadata","$PWD/_ydboctoP*.m")=""
+# Verify db nodes correctly represent 2 plans that are reusable (i.e. do not need to be regenerated)
+^%ydboctoocto("plan_metadata","$PWD/_ydboctoP*.m","output_key")="3"
+^%ydboctoocto("plan_metadata","$PWD/_ydboctoP*.m","output_key")="3"
 # Rerun 8 queries from TCF023.sql that use ABS and REPLACE functions
 # Verify 6 queries that used REPLACE function created new plans while rest reused existing plans
 [ INFO] PATH:LINENUM DATE TIME : Generating M file [$PWD/_ydboctoP*.m] (to execute SQL query)

tests/test_create_function.bats.in
@@ -162,6 +162,8 @@ setup() {
 	echo "# Verify db nodes correctly represent ABS and REPLACE functions and plans that rely on them" >> output.txt
 	mupip extract run1.ext
 	grep plan_metadata run1.ext | grep functions >> output.txt
+	echo "# Verify db nodes correctly represent 8 plans that are reusable (i.e. do not need to be regenerated)" >> output.txt
+	grep plan_met run1.ext | grep output_key >> output.txt
 	echo "# Rerun CREATE FUNCTION of REPLACE" >> output.txt
 	octo -f TCF023b.sql >> output.txt 2>&1
 	echo '# Verify 8 _ydboctoP*.m plans still remain' >> output.txt
@@ -169,6 +171,8 @@ setup() {
 	echo "# Verify db nodes corresponding to REPLACE function have been deleted" >> output.txt
 	mupip extract run2.ext
 	grep plan_metadata run2.ext | grep functions >> output.txt
+	echo "# Verify db nodes correctly represent 2 plans that are reusable (i.e. do not need to be regenerated)" >> output.txt
+	grep plan_met run2.ext | grep output_key >> output.txt
 	echo "# Rerun 8 queries from TCF023.sql that use ABS and REPLACE functions" >> output.txt
 	octo -vv -f TCF023.sql > run2.txt 2>&1
 	echo "# Verify 6 queries that used REPLACE function created new plans while rest reused existing plans" >> output.txt
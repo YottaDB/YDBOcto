d664fffe3560d75e21173a0c44c8bb3946600e62
Narayanan Iyer <nars@yottadb.com>
2020-08-19 11:27:22 -0400

    [#576] Numeric literals in SELECT column list should be displayed as is (and not converted to equivalent canonical number)
    * The main change here is to `src/m_templates/tmpl_print_expression.ctemplate`. Previously, if we know
      a value is of type say `NUMERIC_LITERAL`, we would force it to a number using the `+` M operator.
      This is the cause of #576 since a numeric like `3.00` would be converted to `3` when used with the `+`
      operator. This is now fixed by removing those type coercions for the following types.
      - BOOLEAN_VALUE
      - INTEGER_LITERAL
      - NUMERIC_LITERAL
    
    * A new `test_select_columns_psql/TSCP07` bats subtest verifies this fix.
    
    * The above code change resulted in changes in the generated M plans which required various test reference
      files to be updated.

---
+++ tests/fixtures/TSCP07.sql
@@ -0,0 +1,18 @@
+#################################################################
+#								#
+# Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+-- TSCP07 : OCTO576 : Numeric literals in SELECT column list should be displayed as is
+
+SELECT 3;
+SELECT 3.0;
+SELECT 3.000;
+

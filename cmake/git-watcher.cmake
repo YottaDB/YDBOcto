c70275b4762468abc3c3ef1b425922bb400f9f5c
Joshua Nelson <joshua@yottadb.com>
2020-12-14 11:36:47 -0500

    [#641] Add copyright to git-watcher.cmake
    The MIT license should have been present originally but was never added.
    The YDB license does not go into effect until after this change, which
    is the first time it was modified from the original code Andrew Hardin
    wrote.

--- cmake/git-watcher.cmake
+++ cmake/git-watcher.cmake
@@ -2,6 +2,35 @@
 #
 # License: MIT
 # Source: https://raw.githubusercontent.com/andrew-hardin/cmake-git-version-tracking/master/git_watcher.cmake
+#
+# Copyright (c) 2019 Andrew Hardin
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.
+# All rights reserved.
+#
+#	This source code contains the intellectual property
+#	of its copyright holder(s), and is made available
+#	under a license.  If you do not know the terms of
+#	the license, please stop and do not read further.
+#################################################################
 
 
 # This file defines the functions and targets needed to monitor

4f04bf948e50fefd113e582b2a99a47edc595632
Ganesh Mahesh <ganesh@yottadb.com>
2020-12-15 11:39:41 -0500

    [#509] Support psql \d query
    Implements \d command which lists the schema of the current database. After this change rocto is able to process query that psql generates for the command.
    
    Original issue mentioned in the description of #509 i.e. double quotes not supported was implemented by !613 which revealed the hidden problem of `pg_table_is_visible(table oid)` and `pg_get_userbyid(role oid)` not being defined. This MR implements the mentioned functions.
    
    Octo implements `pg_get_userbyid(role oid)` in `src/aux/_ydboctopgGetUserbyid.m`. "octo" is returned as it is the only role present at this time.
    
    Octo implements `pg_table_is_visible(table oid)` in `src/aux/_ydboctopgTableIsVisible.m`. 1 is returned on invocation (complete usage is yet to be implemented).
    
    For more information regarding the implementation refer: https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/509#note_467351622
    
    New test TPC020 verifies **\d** commands output. It only checks the query results and not the query itself as it varies with platform as shown below.
    
    ```
    <   CASE c.relkind WHEN 'r' THEN 'table' WHEN 'v' THEN 'view' WHEN 'm' THEN 'materialized view' WHEN 'i' THEN 'index' WHEN 'S' THEN 'sequence' WHEN 's' THEN 'special' WHEN 'f' THEN 'foreign table' WHEN 'p' THEN 'table' END as "Type",
    ---
    >   CASE c.relkind WHEN 'r' THEN 'table' WHEN 'v' THEN 'view' WHEN 'm' THEN 'materialized view' WHEN 'i' THEN 'index' WHEN 'S' THEN 'sequence' WHEN 's' THEN 'special' WHEN 'f' THEN 'foreign table' WHEN 'p' THEN 'partitioned table' WHEN 'I' THEN 'partitioned index' END as "Type",
    ```
    
    `tests/outref/TSQ007.ref` now has query results instead of error information. The error is not seen as the required functions for the query have been implemented by this change.

---
+++ src/aux/_ydboctopgTableIsVisible.m
@@ -0,0 +1,19 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                                                               ;
+; Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.       ;
+; All rights reserved.                                          ;
+;                                                               ;
+;       This source code contains the intellectual property     ;
+;       of its copyright holder(s), and is made available       ;
+;       under a license.  If you do not know the terms of       ;
+;       the license, please stop and do not read further.       ;
+;                                                               ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+func(a) ; TODO this is just a placeholder
+	; This function returns true i.e 1 if a certain object
+	; is visible in the current schema search path.
+	; For example, a table is said to be visible if its containing
+	; schema is in the search path and no table of the same name appears earlier in the search path.
+        QUIT 1
+

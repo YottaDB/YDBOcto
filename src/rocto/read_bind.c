YottaDB/YDBOcto

commit 617893fc89dad8f7be9dbd5b35fcea4c896b1491

[#592] Fixed `doc_error_update.sh` issues
Previously, the `doc_error_updated.sh` script did not work correctly when included the `pre-commit` hook, resulting in duplicate error messages and erroneously modified files. These issues are corrected by this commit.

Specifically, the determination of the format string text was corrected to omit the double-quotes from the format string when it is extracted from `errors.hd`. Including these quotes causes messages to not be detected when they are present, as `errors.rst` does not include quotes in its message text.

Also, a list is now compiled that includes each format text string missing from `errors.rst` and the script fails if this list non-empty. Nonetheless, the previous behavior of updating `errors.rst` with these missing strings is retained. The result is that, these changes are applied, but the commit fails when format text is missing for an error message. This allows the commit author to review the automatically applied changes before committing.

Finally, a new check for duplicated error messages was added to `doc_error_update.sh`. To pass this new check, a couple revisions were made:

+ Removed duplicate `INFO_MEMORY_USAGE` from `errors.rst`
+ Revised `ERR_ROCTO_INVALID_CHAR_VALUE to `ERR_ROCTO_INVALID_ITEM_VALUE`
+ Revised `ERR_ROCTO_INVALID_INT_MULTI` to `ERR_ROCTO_INVALID_FORMAT_CODE`

src/rocto/read_bind.c
@@ -95,8 +95,7 @@ Bind *read_bind(BaseMessage *message) {
 	for (i = 0; i < ret->num_parm_format_codes; i++) {
 		ret->parm_format_codes[i] = ntohs(ret->parm_format_codes[i]);
 		if (0 != ret->parm_format_codes[i] && 1 != ret->parm_format_codes[i]) {
-			ERROR(ERR_ROCTO_INVALID_INT_VALUE_MULTI, "Bind", "parameter format code", ret->parm_format_codes[i],
-			      "0 (text) or 1 (binary)");
+			ERROR(ERR_ROCTO_INVALID_FORMAT_CODE, "parameter", ret->parm_format_codes[i]);
 			free(ret);
 			return NULL;
 		}
@@ -176,8 +175,7 @@ Bind *read_bind(BaseMessage *message) {
 		for (i = 0; i < ret->num_result_col_format_codes; i++) {
 			ret->result_col_format_codes[i] = ntohs(ret->result_col_format_codes[i]);
 			if (0 != ret->result_col_format_codes[i] && 1 != ret->result_col_format_codes[i]) {
-				ERROR(ERR_ROCTO_INVALID_INT_VALUE_MULTI, "Bind", "result column format code",
-				      ret->result_col_format_codes[i], "0 (text) or 1 (binary)");
+				ERROR(ERR_ROCTO_INVALID_FORMAT_CODE, "result column", ret->parm_format_codes[i]);
 				free(ret->parms);
 				free(ret);
 				return NULL;

src/errors.hd
@@ -196,9 +196,9 @@ ERROR_DEF(ERR_MIDENT_LENGTH, "Length %d too large for M identifier (max length %
 ERROR_DEF(ERR_INVALID_NUMBER, "%s: invalid number of %s: %d is out of range (min: %d, max: %d)", PSQL_Code_Numeric_Value_Out_Of_Range)
 ERROR_DEF(ERR_TOO_MANY_FUNCTION_ARGUMENTS, "Too many arguments passed for function %s (max: %d)", PSQL_Code_Numeric_Value_Out_Of_Range)
 // ROCTO //
-ERROR_DEF(ERR_ROCTO_INVALID_CHAR_VALUE, "%s: invalid %s value %c: must be %s", PSQL_Code_Data_Exception)
+ERROR_DEF(ERR_ROCTO_INVALID_ITEM_VALUE, "%s: invalid item value %c: must be %s", PSQL_Code_Data_Exception)
 ERROR_DEF(ERR_ROCTO_INVALID_INT_VALUE, "%s: invalid %s value %d: must be %d", PSQL_Code_Numeric_Value_Out_Of_Range)
-ERROR_DEF(ERR_ROCTO_INVALID_INT_VALUE_MULTI, "%s: invalid %s value %d: must be %s", PSQL_Code_Numeric_Value_Out_Of_Range)
+ERROR_DEF(ERR_ROCTO_INVALID_FORMAT_CODE, "Bind: invalid %s format code %d: must be 0 (text) or 1 (binary)", PSQL_Code_Numeric_Value_Out_Of_Range)
 ERROR_DEF(ERR_ROCTO_MISSING_DATA, "%s: missing %s", PSQL_Code_Data_Exception)
 ERROR_DEF(ERR_ROCTO_MISSING_NULL, "%s: %s missing null terminator", PSQL_Code_Unterminated_C_String)
 ERROR_DEF(ERR_ROCTO_TOO_FEW_VALUES, "%s: too few %s", PSQL_Code_Numeric_Value_Out_Of_Range)

src/rocto/read_close.c
@@ -44,7 +44,7 @@ Close *read_close(BaseMessage *message) {
 	}
 	// Ensure valid value for item field
 	if ('S' != ret->item && 'P' != ret->item) {
-		ERROR(ERR_ROCTO_INVALID_CHAR_VALUE, "Close", "item", ret->item, "'S' or 'P'");
+		ERROR(ERR_ROCTO_INVALID_ITEM_VALUE, "Close", ret->item, "'S' or 'P'");
 		free(ret);
 		return NULL;
 	}

src/rocto/read_describe.c
@@ -44,7 +44,7 @@ Describe *read_describe(BaseMessage *message) {
 	}
 	// Ensure valid value for item field
 	if (ret->item != 'S' && ret->item != 'P') {
-		ERROR(ERR_ROCTO_INVALID_CHAR_VALUE, "Describe", "item", ret->item, "'S' or 'P'");
+		ERROR(ERR_ROCTO_INVALID_ITEM_VALUE, "Describe", ret->item, "'S' or 'P'");
 		free(ret);
 		return NULL;
 	}

tools/ci/doc_error_update.sh
@@ -13,23 +13,31 @@
 
 # Note that this file is similar to doc_error_check.sh, so changes there may also need to be made here.
 missing_errors=""
+missing_text=""
+duplicated_text=""
 while read line; do
 	mnemonic=$(echo $line | sed 's/ERROR_DEF(\(ERR_.*\|INFO_.*\),/\1/' | cut -f 1 -d ',')
 	if [[ $(grep -cn "^$mnemonic$" doc/errors.rst) -eq 0 ]]; then
 		missing_errors="$mnemonic\n$missing_errors"
 	else
-		format_string=$(echo $line | sed 's/.*, \(".*"\), PSQL.*)/\1/' | sed 's/%s\|%d\|%c\|%x\|%ld\|%\.\*s/xxx/g')
-		if [[ $(grep -c "$format_string" doc/errors.rst) -eq 0 ]]; then
-			line_number=$(grep -n "^$mnemonic$" doc/errors.rst | cut -f 1 -d ':')
-			# line_number=$(grep -n $mnemonic errors.rst)
-			if [[ "" != $line_number ]]; then
-				line_number=$(( $line_number+2 ))
-				# Sed expects a newline here, hence the line break
-				temp=$(sed -i "${line_number}a\
-					$format_string\n" doc/errors.rst)
+		format_string=$(echo $line | sed 's/.*, "\(.*\)", PSQL.*)/\1/' | sed 's/%s\|%d\|%c\|%x\|%ld\|%\.\*s/xxx/g')
+		format_occurrences=$(grep -cn "^Text: $format_string$" doc/errors.rst)
+		if [[ $format_occurrences -ne 1 ]]; then
+			if [[ $format_occurrences -eq 0 ]]; then
+				missing_text="$mnemonic\n$missing_text"
+				line_number=$(grep -n "^$mnemonic$" doc/errors.rst | cut -f 1 -d ':')
+				# line_number=$(grep -n $mnemonic errors.rst)
+				if [[ "" != $line_number ]]; then
+					line_number=$(( $line_number+2 ))
+					# Sed expects a newline here, hence the line break
+					temp=$(sed -i "${line_number}a\
+						Text: $format_string\n" doc/errors.rst)
+				else
+					echo "-> Error message $mnemonic missing from errors.rst. Please add this mnemonic error name along with its message text, error code, and a description to doc/errors.rst."
+					exit 1
+				fi
 			else
-				echo "-> Error message $mnemonic missing from errors.rst. Please add this mnemonic error name along with its message text, error code, and a description to doc/errors.rst."
-				exit 1
+				duplicated_text="$mnemonic\n$duplicated_text"
 			fi
 		fi
 	fi
@@ -42,5 +50,13 @@ done < <(grep ERROR_DEF src/errors.hd | grep -v -f tools/ci/omitted_errors.ref)
 if [[ "" != $missing_errors ]]; then
 	echo "-> The following error message mnemonics are missing from errors.rst. Please add each mnemonic error name along with its message text, error code, and a description to doc/errors.rst:"
 	echo -e $missing_errors
+	exit 1
+elif [[ "" != $missing_text ]]; then
+	echo "-> The following error message mnemonics were found in errors.rst, but lacking error message text. This text has been automatically added for the following errors:"
+	echo -e $missing_text
+	exit 1
+elif [[ "" != $duplicated_text ]]; then
+	echo "-> The following error message mnemonics are duplicated in errors.rst. Please remove the duplicate instance(s) of each error along with its message text, error code, and description from doc/errors.rst:"
+	echo -e $duplicated_text
 	exit 1
 fi

commit d29790c1789f19d517d46b0786cde9c5c708736f

[#592] Corrected variable name in `read_bind` error message
Previously, an `ERROR` macro call for an `ERR_ROCTO_INVALID_FORMAT_CODE` case in `read_bind` was passed an incorrect variable. Specifically, `ret->parm_format_codes[i]` was passed in place of the correct `ret->result_col_format_codes[i]`. This is now fixed.

This regression was introduced with commit 617893fc89d.

src/rocto/read_bind.c
@@ -175,7 +175,7 @@ Bind *read_bind(BaseMessage *message) {
 		for (i = 0; i < ret->num_result_col_format_codes; i++) {
 			ret->result_col_format_codes[i] = ntohs(ret->result_col_format_codes[i]);
 			if (0 != ret->result_col_format_codes[i] && 1 != ret->result_col_format_codes[i]) {
-				ERROR(ERR_ROCTO_INVALID_FORMAT_CODE, "result column", ret->parm_format_codes[i]);
+				ERROR(ERR_ROCTO_INVALID_FORMAT_CODE, "result column", ret->result_col_format_codes[i]);
 				free(ret->parms);
 				free(ret);
 				return NULL;
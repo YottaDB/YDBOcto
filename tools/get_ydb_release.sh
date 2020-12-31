16e7ff295009a68daa2fd4ba6f362729e56a1d29
Joshua Nelson <joshua@yottadb.com>
2020-12-31 18:10:34 -0500

    [#659] Fix bug where Octo would strip trailing zeros from the YDB version
    Previously:
    
    ```
    YottaDB version: r1.3
    ```
    
    Fixed:
    
    ```
    YottaDB version: r1.30
    ```
    
    This uncovered another bug: constants.h will not be automatically
    regenerated when `get_ydb_release.sh` is changed. Since
    `YDB_RELEASE_VERSION` is set at configure time, not build time, I didn't
    see a simple way to fix this, so I judged that this happens infrequently
    enough not to be worth fixing.
    
    There was yet another bug: `tools/get_ydb_release.sh` should have caught
    that 1.3 < 1.30 and given an error. It was comparing the versions as
    floats, not as semantic versioning; the fix was to split on . and compare
    each component individually.

--- tools/get_ydb_release.sh
+++ tools/get_ydb_release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #################################################################
 #								#
-# Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -19,10 +19,17 @@ if [[ -f $ydb_dist/libyottadbutil.so ]]; then
 else
 	export ydb_routines="$ydb_dist"
 fi
-actual_release=$($ydb_dist/yottadb -run %XCMD 'write +$PIECE($ZYRELEASE," r",2)')
-release_accepted=$(awk -v expected=$1 -v actual=$actual_release 'BEGIN { printf (expected <= actual ? "1" : "0") }')
+actual_release=$($ydb_dist/yottadb -run %XCMD 'write $PIECE($ZYRELEASE," ",2)' | tr -d r)
 echo -n $actual_release
-if [[ "1" == $release_accepted ]]; then
+
+# Require an expected version
+if [ -z $1 ]; then
+	exit 1
+fi
+# https://stackoverflow.com/a/25731924/7669110
+# Split by `.`, then compare each field numerically.
+later_version=$(printf "$actual_release\n$1\n" | sort -t '.' -k 1,1 -k 2,2 -n | tail -n1)
+if [[ "$actual_release" == $later_version ]]; then
 	exit 0
 else
 	exit 1

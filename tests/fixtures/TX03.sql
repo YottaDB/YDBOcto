0fb6428d92c139c6fcd8c2971016322999478e22
Narayanan Iyer <nars@yottadb.com>
2021-01-20 10:33:05 -0500

    [#667] SQuirreL SQL Client Column Autocomplete hangs/freezes with VistA (TPNOTACID due to TP + LOCK)
    Background
    -----------
    * See discussion surrounding https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/667#note_489325929 for more details.
      But the core issue is a TP +_LOCK interaction in Octo that could cause indefinite restarts in some cases.
    
    * It is a timed lock usage in Octo (which has been there all along when building cross-references) along with
      not enough global buffers that caused indefinite transaction restarts due to `TPNOTACID` messages.
    
    * To ensure only one process builds the cross reference, Octo had a timed lock from day one. But as part of a
      recent commit (sha e38211ed), the execution of the query plans was wrapped around a `TSTART/TCOMMIT` (aka TP)
      fence. As part of that commit, the timed lock should have been removed. But it was not.
    
    * That is the cause of the indefinite restarts as TP + LOCK do not interact well.
    
    Fix
    ----
    * The fix is to remove the no-longer necessary `LOCK` from the generated xref plan now that we have `TP`.
      The changes are in `src/m_templates/tmpl_physical_plan.ctemplate`.
    
    * Since the generated M plans are different in this commit than before, the plan format (`FMT_PLAN_DEFINITION`)
      has been bumped in `src/octo.h`.
    
    * A new `test_xref/TX03` bats subtest verifies this fix by using a query that requires 2 cross references
      to be built and has a 1-region setup and few global buffers along with dummy M global nodes that are not
      mapped to any Octo table. All these conditions are required to reproduce the indefinite restart issue
      without the code fixes.

---
+++ tests/fixtures/TX03.sql
@@ -0,0 +1,18 @@
+#################################################################
+#								#
+# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+-- TX03 : OCTO667 : Query that builds more than one cross reference does not TP restart indefinitely
+
+CREATE TABLE names1 (id INTEGER PRIMARY KEY, firstName VARCHAR(30), lastName TEXT(30)) GLOBAL "^names1(keys(""id""))";
+CREATE TABLE names2 (id INTEGER PRIMARY KEY, firstName VARCHAR(30), lastName TEXT(30)) GLOBAL "^names2(keys(""id""))";
+SELECT COUNT(*) FROM names1 n1, names2 n2 WHERE n1.firstname = 'C' AND n2.lastname = 'D';
+

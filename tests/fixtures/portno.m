2d2bfc635f58c0cab0c84e3e9887a041df40c7a2
Narayanan Iyer <nars@yottadb.com>
2021-04-02 11:48:12 -0400

    [#420] Bats tests now run fine even when run concurrently (e.g. using "ctest -j")
    Background
    ----------
    * The Octo bats tests are started using `ctest`. They run fine if run serially (which is how they
      run in the pipeline). But if they are started using `ctest -j8` (to run with a parallelism of 8
      for example), they show random test failures with varied symptoms.
    
      Example symptoms that were seen in internal testing are pasted below.
    
      ```sh
      $ tail stderr.txt
      SQL State: 42P01
      ERROR: Unknown table: ORDERS
    
      $ cat tests-000.octo.out.err
      psql: ERROR:  handle_password_message: failed to retrieve user info for user ydb from database
      ```
    
    * Coincidentally, this issue started showing up after the YDBOcto!684, YDBOcto#420 commit
      (SHA da0fb7109b8c627293bd5e490923ee07247dc732).
    
    * Some investigation showed that the issue is in the logic that finds an open port in the test framework
      (i.e. the `find_open_port()` function in `tests/test_helpers.bash.in`). It currently does a check using
      `netstat` to see if a port is used or not and if not it returns that port as usable for a test when it
      needs to start `rocto`. But it is possible that when concurrent tests are started, multiple tests invoke
      the `find_open_port()` function at the very same time. If so multiple tests can see a particular port
      as not used (`netstat` does not see the port in `LISTEN` state because neither has started `rocto` using
      the port yet) and decide on the exact same port number as open/available at the same time. And this means
      the rocto process started from 2 subtests will end up trying to use the same port. And this can cause
      weird failures like seen above.
    
    Fixes
    -----
    * The `find_open_port()` function (invoked by `start_rocto()` function) now additionally checks whether a port
      has been allocated. This fact is stored in a YottaDB database in the parent directory (`@TEST_OUTPUT_DIR@`)
      in a file `find_open_port.dat`.  A global directory `find_open_port.gld` points to this database file. And a
      file `find_open_port.lock` serves as a lock file that ensures only one test can execute the port allocation
      code in `find_open_port()`.
    
    * `flock` is used to get an exclusive lock on the `find_open_port.lock` and then the actual port allocation
      happens inside a new M program `tests/fixtures/portno.m` in the `findopenport` entryref.
    
    * Later in `stop_rocto()` we invoke a `releaseport^portno` entryref to release the allocated port.
    
    * `tests/fixtures/portno.m` also has a `scavenge` entryref that helps clean up obsolete ports. It finds them
      by storing the bats test output directory name and later checks if the directory exists. If not, it is
      guaranteed to be an unused port and so it releases those.
    
    * All subtests in `tests/test_port_option.bats.in` now have a name added `TPO01` etc. to help with
      running a specific subtest if needed (like is the convention in other tests).

---
+++ tests/fixtures/portno.m
@@ -0,0 +1,50 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+portno	;
+	quit
+
+findopenport
+	;
+	; Finds an port that has not yet been allocated for Octo bats tests AND
+	;	is not currently in use by any other process in the system
+	;
+	new port,portisopen,portdir
+	set portisopen=0
+	for port=+$zcmdline:1 do  quit:portisopen
+	.	; Check if port has been already allocated. If so, move on to next port.
+	.	set portdir=$get(^portdir(port))
+	.	if (""=$zsearch(portdir)) do
+	.	.	do:(""'=portdir) scavenge
+	.	.	; Port has not been allocated. Check if it is in use by any process in system currently.
+	.	.	zsystem "netstat -tulpn | grep -q "":"_port_""""
+	.	.	set portisopen=$zsystem
+	set ^portdir(port)=$zdir
+	write port
+	quit
+
+releaseport
+	;
+	; Release port
+	;
+	new port
+	set port=+$zcmdline
+	write:'$data(^portdir(port)) "TEST-E-RELEASEPORT : port "_port_" expected to be allocated but is not",!
+	kill ^portdir(port)
+	quit
+
+scavenge
+	new port
+	set port="" for  set port=$order(^portdir(port)) quit:port=""  do
+	.	kill:(""=$zsearch(^portdir(port))) ^portdir(port)
+	quit
+

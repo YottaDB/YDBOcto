YottaDB/YDBOcto

commit 8587b12086666c88ea2c8a19b55a736629269907

[#712][YottaDB/DB/YDB#719][YottaDB/DB/YDB#661] Pipeline Failure
Avoid using %XCMD as $ydb_dist vs $ydb_chset could be mismatched or
$ydb_routines could point to Octo shared libraries that are yet to be
generated by CMake.

tools/get_ydb_release.sh
@@ -11,15 +11,10 @@
 #								#
 #################################################################
 
-# Set ydb_routines env var to ensure %XCMD usage below works fine. This avoids potential ZROSYNTAX errors in case the env var comes
-# in holding Octo shared libraries that are yet to be generated by CMake.
-# Note that _XCMD.o will be found in libyottadbutil.so if it exists, and in $ydb_dist otherwise.
-if [[ -f $ydb_dist/libyottadbutil.so ]]; then
-	export ydb_routines="$ydb_dist/libyottadbutil.so"
-else
-	export ydb_routines="$ydb_dist"
-fi
-actual_release=$($ydb_dist/yottadb -run %XCMD 'write $PIECE($ZYRELEASE," ",2)' | tr -d r)
+# Avoid using %XCMD as $ydb_dist vs $ydb_chset could be mismatched or
+# $ydb_routines could point to Octo shared libraries that are yet to be
+# generated by CMake.
+actual_release=$(echo 'write $PIECE($ZYRELEASE," ",2)' | $ydb_dist/yottadb -direct | grep r | tr -d r)
 echo -n $actual_release
 
 # Require an expected version

commit f3d2c21431eaddc208526085d6eeea87242a41c9

[#712] Fix tools/get_ydb_release.sh to correctly determine YottaDB release number even if ydb_prompt env var is set
* This fixes a regression in commit 8587b12086666c88ea2c8a19b55a736629269907.

* That commit assumed a default setup where `ydb_prompt` env var was unset or set to `YDB>`.
  In that case, there is only one line containing the letter `r` in the below output.
  And one can remove `r` from it to get the release number `1.30`.

  ```sh
  $ echo 'write $PIECE($ZYRELEASE," ",2)' | $ydb_dist/yottadb -direct

  YDB>
  r1.30
  YDB>
  ```

  And so the value of the YottaDB release determined by `tools/get_ydb_release.sh` ended up as follows.

  ```sh
  $ echo $(echo 'write $PIECE($ZYRELEASE," ",2)' | $ydb_dist/yottadb -direct | grep r | tr -d r)
  1.30
  ```

* But if the `ydb_prompt` (or `gtm_prompt`) env var had the letter `r` in it, the output would be the following.

  ```sh
  $ export ydb_prompt='Production-Octo>'
  $ echo 'write $PIECE($ZYRELEASE," ",2)' | $ydb_dist/yottadb -direct

  Production-Octo>
  r1.30
  Production-Octo>
  ```

  And because of this, searching for lines containing `r` would yield 3 lines instead of 1.

  ```sh
  $ echo $(echo 'write $PIECE($ZYRELEASE," ",2)' | $ydb_dist/yottadb -direct | grep r | tr -d r)
  Poduction-Octo>
  1.30
  Poduction-Octo>
  ```

  And so the value of the YottaDB release determined by `tools/get_ydb_release.sh` ended up as
  follows.

  ```sh
  $ echo $(echo 'write $PIECE($ZYRELEASE," ",2)' | $ydb_dist/yottadb -direct | grep r | tr -d r)
  Poduction-Octo> 1.30 Poduction-Octo>
  ```

  And this incorrect determination of the YottaDB release number caused the Octo cmake script to
  error out as follows

  ```sh
  CMake Error at CMakeLists.txt:38 (message):
    YottaDB version rPoduction-Octo> 1.30 Poduction-Octo> not supported.
    Minimum supported version: 1.29
  ```

* This is now fixed by using `yottadb -version` (instead of `yottadb -direct`) and filtering out

tools/get_ydb_release.sh
@@ -11,10 +11,8 @@
 #								#
 #################################################################
 
-# Avoid using %XCMD as $ydb_dist vs $ydb_chset could be mismatched or
-# $ydb_routines could point to Octo shared libraries that are yet to be
-# generated by CMake.
-actual_release=$(echo 'write $PIECE($ZYRELEASE," ",2)' | $ydb_dist/yottadb -direct | grep r | tr -d r)
+actual_release=$($ydb_dist/yottadb -version | grep "YottaDB release:" | awk '{print $3}' | tr -d r)
+
 echo -n $actual_release
 
 # Require an expected version
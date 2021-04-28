33a1194c56155e3032b66d41d0a2c77e9d10aeb3
Sam Habiel <sam@yottadb.com>
2021-04-28 14:22:46 -0400

    [#711] Add a VistA file of 500000 recs to speed test
    This is necessary because YDBAIM has a different performance profile for
    data that is stored with various fixed subscripts (e.g.
    ^ORD(100.01,:,0)).
    
    Misc changes:
    - `tests/test_helpers.bash.in` needs to have DEFAULT region key size set
      to maximum. Comments explain why.
    - `tests/test_speed.bats.in` mixed tabs and spaces. Sorry! Now all
      converted into spaces.

---
+++ tests/fixtures/vista-file-129_2.sql
@@ -0,0 +1,26 @@
+#################################################################
+#								#
+# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+CREATE TABLE `RXNORM_CONC_NAMES_AND_SOUR`(
+ `RXNORM_CONC_NAMES_AND_SOUR_ID` NUMERIC PRIMARY KEY START 0 END "'(keys(""RXNORM_CONC_NAMES_AND_SOUR_ID""))!(keys(""RXNORM_CONC_NAMES_AND_SOUR_ID"")="""")",
+ `RXCUI` CHARACTER(8) NOT NULL GLOBAL "^ETSRXN(129.2,keys(""RXNORM_CONC_NAMES_AND_SOUR_ID""),0)" PIECE 1,
+ `SOURCE_SAB` CHARACTER(20) GLOBAL "^ETSRXN(129.2,keys(""RXNORM_CONC_NAMES_AND_SOUR_ID""),0)" PIECE 2,
+ `TERM_TYPE_TTY` CHARACTER(20) GLOBAL "^ETSRXN(129.2,keys(""RXNORM_CONC_NAMES_AND_SOUR_ID""),0)" PIECE 3,
+ `CODE` CHARACTER(50) GLOBAL "^ETSRXN(129.2,keys(""RXNORM_CONC_NAMES_AND_SOUR_ID""),0)" PIECE 4,
+ `SUPPRESSION_FLAG_SUPPRESS` CHARACTER(1) GLOBAL "^ETSRXN(129.2,keys(""RXNORM_CONC_NAMES_AND_SOUR_ID""),0)" PIECE 5,
+ `CONTENT_VIEW_FLAG_CVF` CHARACTER(50) GLOBAL "^ETSRXN(129.2,keys(""RXNORM_CONC_NAMES_AND_SOUR_ID""),0)" PIECE 6,
+ `RXAUI` CHARACTER(8) GLOBAL "^ETSRXN(129.2,keys(""RXNORM_CONC_NAMES_AND_SOUR_ID""),0)" PIECE 7,
+ `TEXT_STR` CHARACTER(3000) GLOBAL "^ETSRXN(129.2,keys(""RXNORM_CONC_NAMES_AND_SOUR_ID""),1)" PIECE 1,
+ `ACTIVATION_DATE` NUMERIC GLOBAL "^ETSRXN(129.2,keys(""RXNORM_CONC_NAMES_AND_SOUR_ID""),""VA"")" PIECE 1
+)
+GLOBAL "^ETSRXN(129.2,keys(""RXNORM_CONC_NAMES_AND_SOUR_ID""))"
+DELIM "^";
+

YottaDB/YDBOcto

commit f8c73bd5779dca5f85a68ea2056fd76759db241f

[#642] Fix warnings from shellcheck
This adds `shellcheck` to the CI pipeline and fixes the resulting
warnings.

Apart from all the fishy code this turned up, this caught several real
bugs:

- `old_plan_name` was missing a `$` before the variable
- Because the script had `set -e` at the top, `cleanup_before_exit`
would never run if cmake failed
- build.sh used `[[ -z Testing/Temporary/LastTest.log ]]`, which is
  always false. It meant to look at the file contents instead.
- clang-tidy would never be run on CentOS, even if it was installed.

  The first bug was that `which` doesn't exist on CentOS, so `[ -x $(which
  rpm) ]` would always be false. Previously this didn't fail the pipeline
  because the check was `if [ -x $(which rpm) ] && ! grep ...; then exit
  1; fi`, which doesn't exit with an error when the check fails.  Now that
  the check is `if [ $is_centos = 0 ]`, it will unconditionally exit with
  an error.

  The second bug was that the condition on grep was incorrect - grep
  returns 0 if there was a match, not 1.

This also fixes some unrelated bugs:

- Silenced the output from `grep /etc/os-release`

I started by fixing *all* warnings, but quickly gave up after seeing how
many there were. This fixes all warnings excluding
`SC1091,SC2154,SC1090,SC2086,SC2053,SC2046`:

- 1091: "Not following: (error message here)": this is not a bug, just a
    diagnostic that the file isn't being checked. Not checking is usually
    the right behavior (e.g. `ydbinstall.sh` should be checked in the YDB
    pipeline if at all).
- 2154: "var is referenced but not assigned": this is most commonly used
    with $ydb_dist or other $ydb_* variables, which must be set before
    running the script.
- 1090: "Can't follow non-constant source. Use a directive to specify location.": same reason as 1091
- 2086: "Double quote to prevent globbing and word splitting". This
    warning is *extremely* verbose, and only affects variables that have

tools/ci/clang-format-all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #################################################################
 #								#
-# Copyright (c) 2019-2020 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2019-2021 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -10,14 +10,14 @@
 #	the license, please stop and do not read further.	#
 #								#
 #################################################################
-if [ -x "$(which "$1")" ]; then
+if [ -x "$(command -v "$1")" ]; then
 	CLANG_FORMAT="$1"
 else
 	CLANG_FORMAT=clang-format
 fi
 # NOTE: does not format .ctemplate files, because clang-format will change `%{}` to `% {}`, breaking the preprocessor.
-find ../src -name '*.c' -o -name '*.h' | xargs "$CLANG_FORMAT" -i
-if ! [ $(git diff --stat | wc -l) = 0 ]; then
+find ../src -printf0 -name '*.c' -o -name '*.h' | xargs -0 "$CLANG_FORMAT" -i
+if ! [ "$(git diff --stat | wc -l)" = 0 ]; then
   echo " -> Formatting differences found!"
   git diff
   exit 1

.gitlab-ci.yml
@@ -43,7 +43,7 @@ before_script:
     - ln -fs /usr/share/zoneinfo/US/Eastern /etc/localtime
     - apt-get update -qq && apt-get install -y tzdata
     - dpkg-reconfigure --frontend noninteractive tzdata
-    - apt-get install -y -qq --no-install-recommends build-essential clang clang-tidy clang-format-9 cmake bison flex libcmocka-dev python3 python3-pip python3-setuptools python3-wheel libreadline-dev git libconfig-dev libssl-dev postgresql-client postgresql xxd wget libgcrypt20-dev libgpgme-dev expect default-jdk ninja-build valgrind unixodbc odbc-postgresql tcsh
+    - apt-get install -y -qq --no-install-recommends build-essential clang clang-tidy clang-format-9 cmake bison flex libcmocka-dev python3 python3-pip python3-setuptools python3-wheel libreadline-dev git libconfig-dev libssl-dev postgresql-client postgresql xxd wget libgcrypt20-dev libgpgme-dev expect default-jdk ninja-build valgrind unixodbc odbc-postgresql tcsh shellcheck
     - pip3 install sphinxcontrib-fulltoc sphinx sphinx_rtd_theme
     - export PATH=/usr/local/go/bin:$PATH
     - wget -O go.tgz -q https://golang.org/dl/go${GOLANG_VERSION}.linux-amd64.tar.gz

tests/fixtures/report_memory_usage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #################################################################
 #								#
-# Copyright (c) 2019 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2019-2021 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -15,7 +15,7 @@ while [[ $(grep -c "with pid" rocto.log) -lt 1 ]]; do
     sleep .1
 done
 
-grep "with pid" rocto.log | sed 's/^.*rocto server process forked with pid \([0-9]*\)/\1/' | while read pid; do
+grep "with pid" rocto.log | sed 's/^.*rocto server process forked with pid \([0-9]*\)/\1/' | while read -r pid; do
     ps -p $pid -o vsize | cut -d ' ' -f 2 | tr -d '\n' >> mem_usage.log
 done
 echo >> mem_usage.log

tools/ci/build.sh
@@ -40,7 +40,7 @@ compile_octo() {
 	set +e
 	# Use Ninja by default, but allow overriding it to use Make.
 	if [ "$USE_MAKE" = 1 ]; then
-		make -j `grep -c ^processor /proc/cpuinfo` 2> build_warnings.txt
+		make -j $(grep -c ^processor /proc/cpuinfo) 2> build_warnings.txt
 	else
 		# Only show warnings in the GitLab UI. Show the full output in `build_warnings.txt`.
 		# See https://ninja-build.org/manual.html#_environment_variables for the syntax of NINJA_STATUS.
@@ -141,6 +141,12 @@ if [[ "test-auto-upgrade" != $jobname ]]; then
 	fi
 	popd
 
+	if [ -x "$(command -v rpm)" ] && grep -q 'VERSION_ID=.*7' /etc/os-release; then
+		is_centos7=1
+	else
+		is_centos7=0
+	fi
+
 	# If we found a recent enough version, run clang-format
 	if CLANG_FORMAT="$(../tools/ci/find-llvm-tool.sh clang-format 9)"; then
 		echo "# Check code style using clang-format"
@@ -148,9 +154,16 @@ if [[ "test-auto-upgrade" != $jobname ]]; then
 		../tools/ci/clang-format-all.sh $CLANG_FORMAT
 	# RHEL/CentOS 7 has an outdated version of clang-format, but we run it in pipelines.
 	# Ignore failures only on this platform.
-	elif [ -x "$(which rpm)" ] && ! grep 'VERSION_ID=.*7' /etc/os-release; then
+	elif [ $is_centos7 = 0 ]; then
 		# Otherwise, fail the pipeline.
 		echo " -> A recent enough version of clang-format was not found!"
+		exit 1
+	fi
+
+	if [ -x "$(command -v shellcheck)" ]; then
+		find .. -name build -prune -o -name '*.sh' -print0 | xargs -0 shellcheck -e SC1091,SC2154,SC1090,SC2086,SC2053,SC2046
+	elif [ $is_centos7 = 0 ]; then
+		echo " -> Shellcheck not found!"
 		exit 1
 	fi
 fi
@@ -158,13 +171,10 @@ fi
 # Confirm all error message mnemonics and text are included in the documentation
 pushd ..
 ./tools/ci/doc_error_update.sh "check"
-if [[ $? -ne 0 ]]; then
-	exit 1
-fi
 popd
 
 echo "# Randomly choose to test Debug or Release build"
-if [[ $(( $RANDOM % 2)) -eq 0 ]]; then
+if [[ $(( RANDOM % 2)) -eq 0 ]]; then
 	build_type="Debug"
 else
 	build_type="RelWithDebInfo"
@@ -173,13 +183,13 @@ echo " -> build_type = $build_type"
 
 if [[ "test-auto-upgrade" != $jobname ]]; then
 	echo "# Randomly choose whether to use the full test suite or its limited version (prefer full version 3/4 times)"
-	if [[ $(( $RANDOM % 4)) -eq 0 ]]; then
+	if [[ $(( RANDOM % 4)) -eq 0 ]]; then
 		full_test="OFF"
 	else
 		full_test="ON"
 	fi
 	echo "# Randomly choose whether to test from installed directory OR from build directory (prefer install 3/4 times)"
-	if [[ $(( $RANDOM % 4)) -eq 0 ]]; then
+	if [[ $(( RANDOM % 4)) -eq 0 ]]; then
 		disable_install="ON"
 	else
 		disable_install="OFF"
@@ -222,16 +232,16 @@ if [[ ("test-auto-upgrade" == $jobname) && ("force" != $subtaskname) ]]; then
 		git remote add upstream_repo "$upstream_URL"
 		git fetch upstream_repo
 	fi
-	stopcommit=`git merge-base HEAD upstream_repo/master`
+	stopcommit=$(git merge-base HEAD upstream_repo/master)
 	# Find HEAD commit to verify its not the same as $stopcommit
-	HEAD_COMMIT_ID=`git rev-list HEAD~1..HEAD`
+	HEAD_COMMIT_ID=$(git rev-list HEAD~1..HEAD)
 	if [[ "$stopcommit" == "$HEAD_COMMIT_ID" ]]; then
 		echo "INFO : HEAD commit and stopcommit is the same. No in between commits to choose from."
 		echo "INFO : Back off stopcommit by 1 commit"
-		stopcommit=`git rev-list HEAD~2..HEAD~1`
+		stopcommit=$(git rev-list HEAD~2..HEAD~1)
 	fi
 	# Find common ancestor of $hardstopcommit and $stopcommit. Verify it is $hardstopcommit. If not, we cannot test.
-	startcommit=`git merge-base $hardstopcommit $stopcommit`
+	startcommit=$(git merge-base $hardstopcommit $stopcommit)
 	if [[ "$startcommit" != "$hardstopcommit" ]]; then
 		echo "INFO : Ancestor commit of $hardstopcommit and $stopcommit was not the former but instead is [$startcommit]"
 		echo "INFO : Cannot run the test-auto-upgrade test in this case. Exiting with success."
@@ -243,9 +253,9 @@ if [[ ("test-auto-upgrade" == $jobname) && ("force" != $subtaskname) ]]; then
 	git log --graph --oneline $startcommit~1..$stopcommit > gitlogmaster.txt
 	# Note: The awk usage below is needed to only skip commits that branch off an otherwise linear commit history.
 	awk '($1 == "*") && ($2 != "|") {print $0;}' gitlogmaster.txt > commit_history.txt
-	numcommits=`wc -l commit_history.txt | awk '{print $1}'`
-	commitnumber=`shuf -i 1-$numcommits -n 1`
-	commitsha=`head -$commitnumber commit_history.txt | tail -1 | awk '{print $2}'`
+	numcommits=$(wc -l commit_history.txt | awk '{print $1}')
+	commitnumber=$(shuf -i 1-$numcommits -n 1)
+	commitsha=$(head -$commitnumber commit_history.txt | tail -1 | awk '{print $2}')
 	echo $commitsha > commit_picked.txt
 	echo "# Random older commit picked = $commitsha"
 	echo "# Checkout the older commit"
@@ -258,7 +268,7 @@ if [[ ("test-auto-upgrade" == $jobname) && ("force" != $subtaskname) ]]; then
 	# Temporarily switch ydb_routines for running M program (testAutoUpgrade.m)
 	saveydbroutines="$ydb_routines"
 	export ydb_routines="."	# so testAutoUpgrade.o gets created in current directory
-	cat bats-tests.cmake.orig | $ydb_dist/yottadb -run batsTestsChooseRandom^testAutoUpgrade > bats-tests.cmake.new
+	$ydb_dist/yottadb -run batsTestsChooseRandom^testAutoUpgrade < bats-tests.cmake.orig > bats-tests.cmake.new
 	export ydb_routines="$saveydbroutines"	# Switch back to original ydb_routines
 	cp bats-tests.cmake.new ../cmake/bats-tests.cmake
 else
@@ -271,13 +281,14 @@ fi
 cleanup_before_exit() {
 	echo "# Cleanup files and directories that don't need to be included in the pipeline artifacts"
 	rm -rf CMakeFiles _CPack_Packages bats-test.*/go src/CMakeFiles || true
-	rm -f postgresql*.jar *.cmake || true
+	rm -f postgresql*.jar ./*.cmake || true
 	rm -f src/test_* || true	# these are the unit test case executables (should not be needed otherwise)
 }
 
 echo "# Configure the build system for Octo"
-${cmakeCommand} -G "$generator" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=${ydb_dist}/plugin -DCMAKE_BUILD_TYPE=$build_type -DFULL_TEST_SUITE=$full_test -DDISABLE_INSTALL=$disable_install ..
-if [[ $? -ne 0 ]]; then
+
+if ! ${cmakeCommand} -G "$generator" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=${ydb_dist}/plugin -DCMAKE_BUILD_TYPE=$build_type -DFULL_TEST_SUITE=$full_test -DDISABLE_INSTALL=$disable_install ..
+then
 	cleanup_before_exit
 	exit 1
 fi
@@ -388,7 +399,7 @@ if [[ "test-auto-upgrade" != $jobname ]]; then
 	create_tarball
 
 	echo "# Randomly choose to install from tarball or via make/ninja install"
-	if [[ $(( $RANDOM % 2)) -eq 0 ]]; then
+	if [[ $(( RANDOM % 2)) -eq 0 ]]; then
 		echo "# install from tarball"
 		cd $tarball_name
 		./octoinstall.sh
@@ -447,10 +458,11 @@ else
 	# In case DISABLE_INSTALL = ON, we need to set the correct UTF-8 directory for $ZROUTINES
 	# In case DISABLE_INSTALL = OFF, ydb_env_set takes care of that for us.
 	if [ "$ydb_chset" = "UTF-8" ]; then
-		export ydb_routines="$(pwd)/utf8/_ydbocto.so $ydb_routines"
+		ydb_routines="$(pwd)/utf8/_ydbocto.so $ydb_routines"
 	else
-		export ydb_routines="$(pwd)/_ydbocto.so $ydb_routines"
+		ydb_routines="$(pwd)/_ydbocto.so $ydb_routines"
 	fi
+	export ydb_routines
 	echo " -> ydb_routines: $ydb_routines"
 fi
 
@@ -475,7 +487,7 @@ PSQL
 	# We do not want any failures in "ctest" to exit the script (need to do some cleanup so the artifacts
 	# are not that huge etc.). So disable the "set -e" setting temporarily for this step.
 	set +e
-	${ctestCommand} -j `grep -c ^processor /proc/cpuinfo`
+	${ctestCommand} -j $(grep -c ^processor /proc/cpuinfo)
 	exit_status=$?
 	echo " -> exit_status from ${ctestCommand} = $exit_status"
 
@@ -491,12 +503,12 @@ PSQL
 	set +v
 	set +x
 	# Find out list of passed bats dirs. Need to sort for later use by "join"
-	ls -1d bats-test.*// | sed 's,/.*,,g' | sort > all_bats_dirs.txt
+	find . -maxdepth 1 -type d | sed 's#^\./##' | grep '^bats-test' | sort > all_bats_dirs.txt
 	# Find out list of failed bats dirs. Need to sort for later use by "join"
 	grep "Temporary files in" Testing/Temporary/LastTest.log | awk '{print $NF}' | sed 's,.*/,,g' | sort > failed_bats_dirs.txt
 	# Note down list of bats test directory names and corresponding subtest name in one file
-	cat */bats_test.out > all_bats_test.out
-	ls -lart */bats_test.out > lslart_bats_test.out	# this is to note down time stamp of the bats_test.out files
+	cat ./*/bats_test.out > all_bats_test.out
+	ls -lart ./*/bats_test.out > lslart_bats_test.out	# this is to note down time stamp of the bats_test.out files
 	grep '^ok' Testing/Temporary/LastTest.log > passed_bats_subtests.txt || true
 	grep '^not ok' Testing/Temporary/LastTest.log > failed_bats_subtests.txt || true
 	touch summary_bats_dirs.txt passed_bats_dirs.txt
@@ -510,7 +522,7 @@ PSQL
 			continue
 		fi
 		cd $tstdir
-		subtest=`sed 's/.*subtest \[//;s/].*//;' bats_test.out`
+		subtest=$(sed 's/.*subtest \[//;s/].*//;' bats_test.out)
 		# Need -F below in case there are any special characters in the subtest name (e.g. '*')
 		# We do not want to treat those as regex in the grep.
 		if [[ $(grep -F -c "$subtest" ../passed_bats_subtests.txt) -eq 1 ]]; then
@@ -537,7 +549,7 @@ if [[ "test-auto-upgrade" != $jobname ]]; then
 		# and pollute the pipeline console output
 		set +v
 		set +x
-		rm -rf `cat passed_bats_dirs.txt`
+		rm -rf $(cat passed_bats_dirs.txt)
 		# Restore verbose output now that for long line of output is done
 		set -v
 		set -x
@@ -588,10 +600,10 @@ else
 	cmakeflags="$cmakeflags -DCMAKE_BUILD_TYPE=$build_type -DFULL_TEST_SUITE=$full_test"
 	cmakeflags="$cmakeflags -DDISABLE_INSTALL=$disable_install"
 	# Randomly select a power of two to use for altering the size of OCTO_INIT_BUFFER_LEN to test for regressions
-	new_buffer_size=$(( 2 ** ($RANDOM % 11) ))
+	new_buffer_size=$(( 2 ** (RANDOM % 11) ))
 	sed -i "s/OCTO_INIT_BUFFER_LEN [0-9]*/OCTO_INIT_BUFFER_LEN $new_buffer_size/" ../src/octo.h
-	${cmakeCommand} -G "$generator" $cmakeflags ..
-	if [[ $? -ne 0 ]]; then
+
+	if ! ${cmakeCommand} -G "$generator" $cmakeflags ..; then
 		cleanup_before_exit
 		exit 1
 	fi
@@ -609,7 +621,7 @@ else
 	set +v
 	if [[ "force" != $subtaskname ]]; then
 		echo '# Remove "bats-test*" directories corresponding to failed subtests (if any)'
-		rm -rf `cat failed_bats_dirs.txt`
+		rm -rf $(cat failed_bats_dirs.txt)
 		env > env.out
 		echo '# Do auto-upgrade tests on the leftover "bats-test*" directories.'
 		gldfile="yottadb.gld"
@@ -617,7 +629,8 @@ else
 		defaultdat="mumps.dat"
 		octodat="octo.dat"
 		touch skip_bats_test.txt gde_change_segment.txt
-		export ydb_icu_version=`pkg-config --modversion icu-io`	# needed for UTF-8 chset in for loop below
+		ydb_icu_version=$(pkg-config --modversion icu-io)	# needed for UTF-8 chset in for loop below
+		export ydb_icu_version
 		# Note down if older commit is prior to the YDBOcto#275 commit when NULL and empty string began to be
 		# treated the same. This will be used later to skip a few tests.
 		pre_octo275_commit="babc2e2e78eb00813cb5d76a8f2bbda66742c1b7"	# 1 commit before the #275 commit
@@ -639,14 +652,14 @@ else
 				rm -rf $tstdir
 				continue
 			fi
-			if ! ls *.sql 1> /dev/null 2>&1; then
+			if ! ls ./*.sql 1> /dev/null 2>&1; then
 				# This test directory does not contain any "*.sql" files. Skip auto-upgrade test.
 				echo "SKIPPED : $tstdir : Does not contain *.sql files" >> ../bats_test.txt
 				cd ..
 				rm -rf $tstdir
 				continue
 			fi
-			subtest=`sed 's/.*subtest \[//;s/].*//;' bats_test.out`
+			subtest=$(sed 's/.*subtest \[//;s/].*//;' bats_test.out)
 			# ----------------------------------------------------------------------------
 			# Exclude specific set of subtests (reasons explained below)
 			# ----------------------------------------------------------------------------
@@ -693,7 +706,7 @@ else
 			echo "# Running *.sql files in $tstdir : [subtest : $subtest]" | tee -a ../errors.log
 			echo "INCLUDE : $tstdir" >> ../include_bats_test.txt
 			# Check if subtest ran in M or UTF-8 mode and switch ydb_chset and ydb_routines accordingly
-			is_utf8=`grep "ydb_chset=UTF-8" env.out | wc -l` || true
+			is_utf8=$(grep -c "ydb_chset=UTF-8" env.out) || true
 			if [[ $is_utf8 == 0 ]]; then
 				export ydb_chset=M
 				utf8_path="."
@@ -752,13 +765,14 @@ FILE
 					octooutfile="$sqlfile.octo.out"
 					usedjdbcdriver=1
 				else
-					basename=`echo $sqlfile | sed 's/\.sql//g'`
+					# shellcheck disable=SC2001
+					basename=$(echo $sqlfile | sed 's/\.sql//g')
 					if [[ -e $basename ]]; then
 						octooutfile=$basename
 					fi
 					usedjdbcdriver=0
 				fi
-				if [[ ($subtest =~ ^"TQG") && (! -z $octooutfile) ]]; then
+				if [[ ($subtest =~ ^"TQG") && (-n $octooutfile) ]]; then
 					# a) If the random older commit predates the YDBOcto#649 commit, then the octo output
 					#    would not contain the row-header and row summary line at the head and tail of the octo output.
 					#    So filter that out from the newer Octo build output.
@@ -829,10 +843,10 @@ FILE
 		# Find out all "CREATE TABLE" queries in tests/fixtures/*.sql. Generate one query file for each.
 		# Filter out lines like "\set ON_ERROR_STOP on" that are in tests/fixtures/postgres-*.sql files
 		# as they confuse split_queries.py. Also filter out queries with errors that are in TERR*.sql files.
-		cat `grep -l "CREATE TABLE" ../tests/fixtures/*.sql | grep -v TERR` | grep -v ON_ERROR_STOP > create_table.sql
+		grep -l "CREATE TABLE" ../tests/fixtures/*.sql | grep -v TERR | xargs cat | grep -v ON_ERROR_STOP > create_table.sql
 		../tests/fixtures/sqllogic/split_queries.py create_table.sql "CREATE TABLE"
 		# Create *.gld and *.dat files
-		rm -f *.gld *.dat || true
+		rm -f ./*.gld ./*.dat || true
 		# Point src to newsrc so GDE works fine or else ZROSYNTAX error would be issued.
 		rm -f src || true; ln -s newsrc src
 		export ydb_gbldir="yottadb.gld"
@@ -844,7 +858,7 @@ FILE
 		add -segment OCTOSEG -file=octo.dat
 		change -region OCTOREG -null_subscripts=true -key_size=1019 -record_size=1048576
 FILE
-		rm *.dat || true
+		rm ./*.dat || true
 		$ydb_dist/mupip create
 		touch errors.log
 		filediff() {
@@ -869,7 +883,7 @@ FILE
 			if [[ -s $filename2.diff ]]; then
 				echo "ERROR : [diff oldsrc/$filename1 newsrc/$filename2] returned non-zero diff" | tee -a ../errors.log
 				echo "[cat $filename2.diff] output follows" | tee -a ../errors.log
-				cat $filename2.diff | tee -a ../errors.log
+				tee -a < $filename2.diff ../errors.log
 				exit_status=1
 			fi
 		}
@@ -886,7 +900,7 @@ FILE
 					echo "ERROR : [octo -f $dir/$queryfile] returned non-zero exit status : $?" | tee -a ../errors.log
 					exit_status=1
 				fi
-				plan_name=`grep _ydboctoP $queryfile.vv.out | sed 's/.*_ydboctoP/_ydboctoP/;s/].*//'`
+				plan_name=$(grep _ydboctoP $queryfile.vv.out | sed 's/.*_ydboctoP/_ydboctoP/;s/].*//')
 			fi
 			./octo -f $queryfile >& $queryfile.out	# Run without -vv to get actual output (minus INFO/LP_ etc. output)
 								# This will be used for filediff as it is deterministic.
@@ -896,7 +910,7 @@ FILE
 		cd oldsrc
 		export ydb_routines=". _ydbocto.so $ydb_routines $ydb_dist/libyottadbutil.so"
 		cp ../*.gld ../{mumps,octo}.dat ../create_table-*.sql .
-		cp *.gld {mumps,octo}.dat create_table-*.sql ../newsrc
+		cp ./*.gld {mumps,octo}.dat create_table-*.sql ../newsrc
 		for queryfile in create_table-*.sql
 		do
 			echo " --> Processing $queryfile"
@@ -904,7 +918,7 @@ FILE
 			cd ../oldsrc; rm -f ../src || true; ln -s oldsrc ../src
 			run_octo $queryfile oldsrc novv
 			# Determine table name and generate a query that selects all columns from that table
-			tablename=`grep -n "CREATE TABLE" $queryfile | grep -v "^--" | sed 's/.*CREATE TABLE //;s/(.*//;' | awk '{print $1}'`
+			tablename=$(grep -n "CREATE TABLE" $queryfile | grep -v "^--" | sed 's/.*CREATE TABLE //;s/(.*//;' | awk '{print $1}')
 			echo "select * from $tablename;" > $queryfile.2
 			# Copy over database files from oldsrc to newsrc
 			cp {mumps,octo}.dat $queryfile.2 ../newsrc
@@ -926,17 +940,17 @@ FILE
 				echo "ERROR : $queryfile : Plan name in oldsrc [$old_plan_name] differs from newsrc [$new_plan_name]" | tee -a ../errors.log
 				exit_status=1
 			fi
-			if [[ ("" != $new_plan_name) && ("" != old_plan_name) ]]; then
+			if [[ ("" != "$new_plan_name") && ("" != "$old_plan_name") ]]; then
 				# "$new_plan_name" can be "" for example if "CREATE TABLE" occurs in a comment in the query
 				# file and the actual query is something else like a "DROP TABLE". In that case, skip this check.
-				filediff $old_plan_name $new_plan_name
+				filediff "$old_plan_name" "$new_plan_name"
 			fi
 			# Need to DROP TABLE the current table in "oldsrc" directory to avoid reusing this plan for future
 			# CREATE TABLE queries that point to the same table name (e.g. `Customers` vs `customers`) as that
 			# can cause a failure in the pipeline job (see commit message for details).
 			cd ../oldsrc; rm -f ../src || true; ln -s oldsrc ../src
-			echo "DROP TABLE $tablename;" > $queryfile.drop
-			run_octo $queryfile.drop oldsrc novv
+			echo "DROP TABLE $tablename;" > "$queryfile.drop"
+			run_octo "$queryfile.drop" oldsrc novv
 			cd ../newsrc; rm -f ../src || true; ln -s newsrc ../src
 		done
 		cd ..
@@ -952,12 +966,12 @@ echo " -> exit $exit_status"
 set +x
 set +v
 
-if [[ 0 != $exit_status ]]; then
-	if [[ "test-auto-upgrade" != $jobname ]]; then
+if [[ 0 != "$exit_status" ]]; then
+	if [[ "test-auto-upgrade" != "$jobname" ]]; then
 		echo "# ----------------------------------------------------------"
 		echo "# List of failed tests/subtests and their output directories"
 		echo "# ----------------------------------------------------------"
-		if [[ -z Testing/Temporary/LastTest.log ]]; then
+		if ! [[ -s Testing/Temporary/LastTest.log ]]; then
 			echo "# Detected script failure prior to BATS test execution. Please review script output to determine source."
 		else
 			grep -A 6 -E "not ok|Test: " Testing/Temporary/LastTest.log | grep -E "not ok|# Temporary|Test: " | grep -C 1 "not ok" | sed "s/^not/  &/;s/^#/  &/"

tools/ci/clang-tidy-all.sh
@@ -2,7 +2,7 @@
 
 #################################################################
 #								#
-# Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2020-2021 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -17,10 +17,10 @@ ignored_warnings="\
 -clang-diagnostic-gnu-zero-variadic-macro-arguments,\
 -clang-analyzer-security.insecureAPI.strcpy"
 
-if ! clang_tidy=$($(git rev-parse --show-toplevel)/tools/ci/find-llvm-tool.sh clang-tidy 8); then
+if ! clang_tidy=$("$(git rev-parse --show-toplevel)"/tools/ci/find-llvm-tool.sh clang-tidy 8); then
 	echo "error: clang-tidy-8 or greater is required"
 	exit 1
 fi
 
 find ../src -name '*.c' | grep -v '/test_.*\.c' \
-	| xargs $clang_tidy --checks="$ignored_warnings" "$@"
+	| xargs "$clang_tidy" --checks="$ignored_warnings" "$@"

tools/ci/doc_error_update.sh
@@ -15,7 +15,7 @@ set -e
 
 topleveldir=$(git rev-parse --show-toplevel)
 cwd=$(pwd)
-if [[ $topleveldir != $cwd ]]; then
+if [[ $topleveldir != "$cwd" ]]; then
 	echo "ERROR: doc_error_update.sh run from $cwd (expected: $topleveldir)"
 	exit 1
 fi
@@ -24,12 +24,12 @@ missing_mnemonics=""
 missing_messages=""
 missing_text=""
 duplicated_text=""
-while read line; do
-	mnemonic=$(echo $line | sed 's/ERROR_DEF(\(ERR_.*\|INFO_.*\|WARN_.*\),/\1/' | cut -f 1 -d ',')
+while read -r line; do
+	mnemonic=$(echo "$line" | sed 's/ERROR_DEF(\(ERR_.*\|INFO_.*\|WARN_.*\),/\1/' | cut -f 1 -d ',')
 	if [[ $(grep -cn "^$mnemonic$" doc/errors.rst) -eq 0 ]]; then
 		missing_mnemonics="$mnemonic\n$missing_mnemonics"
 	else
-		format_string=$(echo $line | sed 's/.*, "\(.*\)", PSQL.*)/\1/' | sed 's/%s\|%d\|%c\|%x\|%ld\|%\.\*s/xxx/g')
+		format_string=$(echo "$line" | sed 's/.*, "\(.*\)", PSQL.*)/\1/' | sed 's/%s\|%d\|%c\|%x\|%ld\|%\.\*s/xxx/g')
 		if [[ $1 == "check" ]]; then
 			if [[ $(grep -c "$format_string" doc/errors.rst) -eq 0 ]]; then
 				missing_messages="$mnemonic\n$missing_messages"
@@ -41,11 +41,11 @@ while read line; do
 					missing_text="$mnemonic\n$missing_text"
 					line_number=$(grep -n "^$mnemonic$" doc/errors.rst | cut -f 1 -d ':')
 					# line_number=$(grep -n $mnemonic errors.rst)
-					if [[ "" != $line_number ]]; then
-						line_number=$(( $line_number+2 ))
+					if [[ "" != "$line_number" ]]; then
+						line_number=$(( line_number+2 ))
 						# Sed expects a newline here, hence the line break
-						temp=$(sed -i "${line_number}a\
-							Text: $format_string\n" doc/errors.rst)
+						sed -i "${line_number}a\
+							Text: $format_string\n" doc/errors.rst
 					else
 						echo "-> Error message $mnemonic missing from errors.rst. Please add this mnemonic error name along with its message text, error code, and a description to doc/errors.rst."
 						exit 1
@@ -66,15 +66,15 @@ while read line; do
 	# not being currently documented in "doc/errors.rst". Hence the grep for '^//' below.
 done < <(grep ERROR_DEF src/errors.hd | grep -v '^//' | grep -v -f tools/ci/omitted_errors.ref)
 result=0
-if [[ "" != $missing_mnemonics ]]; then
+if [[ "" != "$missing_mnemonics" ]]; then
 	echo "-> The following error message mnemonics are missing from errors.rst. Please add each mnemonic error name along with its message text, error code, and a description to doc/errors.rst:"
-	echo -e $missing_mnemonics
+	echo -e "$missing_mnemonics"
 	result=1
 fi
 if [[ $1 == "check" ]]; then
-	if [[ "" != $missing_messages ]]; then
+	if [[ "" != "$missing_messages" ]]; then
 		echo "-> Error message text for the following mnemonics is missing from errors.rst. Please add the error message text for each to doc/errors.rst:"
-		echo -e $missing_messages
+		echo -e "$missing_messages"
 		result=1
 	fi
 	exit $result
@@ -82,13 +82,13 @@ else
 	if [[ $result -eq 1 ]]; then
 		# Error already message issued for this case, so just exit
 		exit 1
-	elif [[ "" != $missing_text ]]; then
+	elif [[ "" != "$missing_text" ]]; then
 		echo "-> The following error message mnemonics were found in errors.rst, but lacking error message text. This text has been automatically added for the following errors:"
-		echo -e $missing_text
+		echo -e "$missing_text"
 		exit 1
-	elif [[ "" != $duplicated_text ]]; then
+	elif [[ "" != "$duplicated_text" ]]; then
 		echo "-> The following error message mnemonics are duplicated in errors.rst. Please remove the duplicate instance(s) of each error along with its message text, error code, and description from doc/errors.rst:"
-		echo -e $duplicated_text
+		echo -e "$duplicated_text"
 		exit 1
 	fi
 fi

commit 607eec76cf6e68b644b187f61b9c95c579066321

Fix two issues in tools/ci/clang-format-all.sh (introduced in f8c73bd5779dca5f85a68ea2056fd76759db241f)
* The first issue is a typo that was introduced in commit f8c73bd5779dca5f85a68ea2056fd76759db241f.
  That commit addressed various `shellcheck` warnings. And it addressed the following warning in
  `tools/ci/clang-format-all.sh` by trying to use the `-print0` predicate in the `find` command.

  ```
  2038: "Use -print0/-0 or find -exec + to allow for non-alphanumeric filenames."
  ```

  But ended up using the incorrect `printf0` predicate instead. This is now fixed in the current commit.

* The second issue is a more subtle one. And that is the placement of the `-print0` predicate.
  It was placed first before the `-name` predicates that filtered only `*.c` and `*.h` files.
  This meant all files that were found under `../src` (e.g. `*.ctemplate`, `*.y` files etc.) were printed
  too and passed to `clang-format` instead of only `*.c` and `*.h` files. This is fixed by moving the
  `-print0` predicate to AFTER the `-name` predicates. Also had to group the operands of the `-o` predicate
  using parentheses (need a backslash before the open and close paren to avoid shell from parsing it).

tools/ci/clang-format-all.sh
@@ -10,13 +10,14 @@
 #	the license, please stop and do not read further.	#
 #								#
 #################################################################
+
 if [ -x "$(command -v "$1")" ]; then
 	CLANG_FORMAT="$1"
 else
 	CLANG_FORMAT=clang-format
 fi
 # NOTE: does not format .ctemplate files, because clang-format will change `%{}` to `% {}`, breaking the preprocessor.
-find ../src -printf0 -name '*.c' -o -name '*.h' | xargs -0 "$CLANG_FORMAT" -i
+find ../src \( -name '*.c' -o -name '*.h' \) -print0 | xargs -0 "$CLANG_FORMAT" -i
 if ! [ "$(git diff --stat | wc -l)" = 0 ]; then
   echo " -> Formatting differences found!"
   git diff
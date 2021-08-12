47b18a2fceca7c2d31ec01363f932fd7ed9d7c7e
Sam Habiel <sam@yottadb.com>
2021-08-12 14:17:24 -0600

    [#657] Test for Octo-AIM regression
    See
    https://gitlab.com/YottaDB/DBMS/YDBOcto/-/merge_requests/898#note_647885316
    for detailed discussion.
    
    The summary of the issue is that AIM currently does not cross reference
    nodes that do not exist, resulting in queries that are looking for NULLs
    as a cross-reference value to possibly return incorrect results.
    
    This test is here to be part of the regression test suite to ensure that
    this doesn't happen.

---
+++ tests/fixtures/TX05.sql
@@ -0,0 +1,15 @@
+#################################################################
+#								#
+# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+-- TX05 : OCTO657 : Octo-AIM regression where NULLs are missed because data that doesn't exist isn't indexed
+-- See https://gitlab.com/YottaDB/DBMS/YDBOcto/-/merge_requests/898#note_647885316 for details
+CREATE TABLE x (id INTEGER PRIMARY KEY, val VARCHAR GLOBAL "^x(keys(""ID""),1)") GLOBAL "^x";
+select id, val from x where val is NULL;

a2ddd41f0b03468d784f9c031d2077d1ba5c77bd
Sam Habiel <sam@yottadb.com>
2021-10-08 13:13:01 -0600

    [#755] Add realistic sample queries to the test-vista pipeline
    Queries supplied by Chris Combs. Thank you. Queries are in TVA0003 and
    TVA0004.
    
    Various changes in `tests/test_vista.bats.in`:
    - Set-up `ydb_xc_ydbposix` for Posix plug-in call-outs; also add `_ydbposix.so`
    to `ydb_routines`.
    - Set-up VistA specific octo functions by getting both
    _YDBOCTOVISTAF.{m,sql} and put them in the `primary_routine_directory`
    and load into Octo respectively.
    - Add TVA0003 and TVA0004 to the test_vista suite.

---
+++ tests/fixtures/TVA0003.sql
@@ -0,0 +1,33 @@
+#################################################################
+#								#
+# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2021 Chris Combs
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+# TVA0003 : Test Realistic Sample Query 1
+SELECT P.NAME AS PATIENT_NAME, P.PATIENT_ID as PATIENT_ID,
+       P.WARD_LOCATION,
+       TOKEN(REPLACE(TOKEN(REPLACE(P.WARD_LOCATION,"WARD ",""),"-",1),"WARD ","")," ",2) AS PCU,
+       CONCAT(TOKEN(REPLACE(TOKEN(REPLACE(P.WARD_LOCATION,"WARD ",""),"-",2),"WARD ","")," ",1)," ",TOKEN(P.WARD_LOCATION,"-",3)) AS UNIT,
+       P.ROOM_BED as ROOM_BED,
+       REPLACE(P.DIVISION,"VEHU","") as FACILTY,
+       P.SEX as SEX,
+       P.CURRENT_ADMISSION as CURRENT_ADMISSION,
+       P.CURRENT_MOVEMENT as CURRENT_MOVEMENT,
+       DATEFORMAT(P.DATE_OF_BIRTH,"5Z") as DATE_OF_BIRTH,
+       P.Age,
+       PM.PATIENT_MOVEMENT_ID as Current_Patient_Movement,
+       PM.TYPE_OF_MOVEMENT as Current_Movement_Type,
+       AM.PATIENT_MOVEMENT_ID as Admission_Movement,
+       AM.TYPE_OF_MOVEMENT as Admission_Type
+FROM PATIENT P
+left join patient_movement PM on P.CURRENT_MOVEMENT=PM.PATIENT_MOVEMENT_ID
+left join patient_movement AM on P.CURRENT_ADMISSION=AM.PATIENT_MOVEMENT_ID
+where P.CURRENT_MOVEMENT is not null
+and P.ward_location not like "ZZ%" and P.NAME not like "ZZ%";
---
+++ tests/fixtures/TVA0004.sql
@@ -0,0 +1,38 @@
+#################################################################
+#								#
+# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2021 Chris Combs
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+# TVA0004 : Test Realistic Sample Query 2
+SELECT A.ORDER1_ID,
+E.NAME,
+A.CURRENT_ACTION,
+F.DISPLAY_GROUP_ID,
+F.NAME as Order_Category,
+G.ORDER_ORDER_ACT_ORDER_TEXT_ID as ORDER_TEXT_ID,
+G.ORDER_TEXT
+FROM ORDER1 A
+LEFT JOIN PATIENT E ON NUMBER(A.OBJECT_OF_ORDER)=E.PATIENT_ID
+LEFT JOIN ORDER_STATUS C ON (A.STATUS=C.ORDER_STATUS_ID)
+LEFT JOIN ORDER_ORDER_ACTIONS B ON
+(
+   A.ORDER1_ID = B.ORDER1_ID
+   AND A.CURRENT_ACTION=B.ORDER_ORDER_ACTIONS_ID
+)
+LEFT join ORDER_ORDER_ACT_ORDER_TEXT G on (A.ORDER1_ID= G.ORDER1_ID and B.ORDER_ORDER_ACTIONS_ID=G.ORDER_ORDER_ACTIONS_ID)
+LEFT JOIN NEW_PERSON D ON (B.SIGNED_BY = D.NEW_PERSON_ID)
+LEFT JOIN DISPLAY_GROUP F ON (A.TO=F.DISPLAY_GROUP_ID)
+WHERE
+   A.CURRENT_ACTION is not null
+   AND A.STATUS in (3,4,5,6,8,9,11,15)
+   AND (A.STOP_DATE>=CURRTIMESTAMP("V") OR A.STOP_DATE IS NULL)
+   AND (A.OBJECT_OF_ORDER is not null and E.CURRENT_MOVEMENT is not null AND E.NAME NOT LIKE "ZZ%")
+   AND C.NAME="ACTIVE"
+order by e.patient_id,a.order1_id,G.ORDER_ORDER_ACT_ORDER_TEXT_ID

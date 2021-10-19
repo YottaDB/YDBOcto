9be5ca334eefa0ba97c989873200139120120453
Sam Habiel <sam@yottadb.com>
2021-10-19 12:53:37 -0600

    [YDBOctoVistA#7] Add test for Word Processing fields using DELIM ""
    This test is being added to the `test_vista_database` suite in order for
    the DDL changes to be validated when it changes how Word Processing
    fields are processed as part of YDBOctoVistA#7.

---
+++ tests/fixtures/TVD0006.sql
@@ -0,0 +1,14 @@
+#################################################################
+#								#
+# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+# YDBOctoVistA#7 Refactoring Word Processing Extract Fields to use DELIM ""
+# The field whose definition got changed from EXTRACT to DELIM "" is TIU_DOCUMENT_REPORT_TEXT.REPORT_TEXT
+SELECT P.NAME, T.PATIENT, X.REPORT_TEXT from PATIENT P, TIU_DOCUMENT T, TIU_DOCUMENT_REPORT_TEXT X WHERE T.PATIENT = P.PATIENT_ID and T.TIU_DOCUMENT_ID = X.TIU_DOCUMENT_ID LIMIT 100;

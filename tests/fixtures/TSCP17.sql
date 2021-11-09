YottaDB/YDBOcto

commit 853ce79e3d4bf97789fbde851b2f683ef4c977d4

[#386] Enables TABLENAME.ASTERISK usage in Group by clause and fix ORDER BY usage bug
**Parent Query table reference usage fix for ORDER BY and SELECT**

ORDER BY and SELECT column list usage fix when table.* referred to parent query table. Some queries where ORDER BY/SELECT column list referred to parent query column failed with previous implementation. This is because table.* expansion occurred at query_specification() and parent query information is not available at the time. This would result in an error being raised by `populate_data_type()` as encountering table.* . Now with process_table_asterisk_cla() taking care of table.* expansions at qualify_statement() this problem is resolved as parent query related information exists at the time of expansion.

Example query which would fail is :`select * from names n1 where exists (select id from names where id=n1.id order by n1.*);`

Also queries with select column list referring to parent query encountered same problem. Example query: `select id from names n1 where exists (select n1.*);`

With this change both errors are fixed.

**Asterisk processing is also deferred to qualify_query()**

Explained later on in the `ASTERISK and TABLE_ASTERISK usage` section of description.

**Implementation of TABLENAME.ASTERISK in GROUP BY and aggregate functions**

Usage of `TABLENAME.ASTERISK`:
* In AGGREGATE FUNCTION
  * Usage of COUNT
    * select count(n1.*) from names n1;
  * Usage of AVG, SUM, MIN and MAX in similar manner is also valid with the condition that the table has to have a single column and the data type needs to be compatible with the function.
  * Both DISTINCT and ALL can be applied
    * select DISTINCT count(n1.*) from names n1;
    * select ALL count(n1.*) from names n1;
    * select count(DISTINCT n1.*) from names n1;
    * select count(ALL n1.*) from names n1;
    * select SUM(DISTINCT n1.*) from names1col n1;
    * select MIN(DISTINCT n2.*) from (values (1), (2), (NULL)) n2;

* In SELECT COLUMN LIST

src/qualify_query.c
@@ -1,6 +1,6 @@
 /****************************************************************
  *								*
- * Copyright (c) 2019-2020 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2019-2021 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -47,6 +47,43 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 		assert(NULL == parent_table_alias);
 		assert(NULL == ret->ret_cla);
 		result |= qualify_query(insert->src_table_alias_stmt, NULL, NULL, ret);
+		/* Check if insert->columns to src_table_alias_stmt columns mapping is valid. If not issue error.
+		 * Deferred till this point as we need the expanded column list in case of * or table.* usage.
+		 * TODO: avoid deferring the following processing in cases where its not required. Refer:
+		 * https://gitlab.com/YottaDB/DBMS/YDBOcto/-/merge_requests/816#note_583771101
+		 */
+		if (NULL != insert->columns) {
+			SqlColumnList *	    start_cl, *cur_cl;
+			SqlTableAlias *	    src_table_alias;
+			SqlColumnListAlias *start_cla, *cur_cla;
+			SqlStatement *	    table_alias_stmt;
+
+			table_alias_stmt = drill_to_table_alias(insert->src_table_alias_stmt);
+			UNPACK_SQL_STATEMENT(src_table_alias, table_alias_stmt, table_alias);
+			UNPACK_SQL_STATEMENT(start_cla, src_table_alias->column_list, column_list_alias);
+			cur_cla = start_cla;
+			UNPACK_SQL_STATEMENT(start_cl, insert->columns, column_list);
+			cur_cl = start_cl;
+			do {
+				if (NULL == cur_cla) {
+					ERROR(ERR_INSERT_TOO_MANY_COLUMNS, NULL);
+					yyerror(NULL, NULL, &cur_cl->value, NULL, NULL, NULL);
+					return 1;
+				}
+				cur_cl = cur_cl->next;
+				if (NULL != cur_cla) {
+					cur_cla = cur_cla->next;
+					if (start_cla == cur_cla) {
+						cur_cla = NULL;
+					}
+				}
+			} while (cur_cl != start_cl);
+			if (NULL != cur_cla) {
+				ERROR(ERR_INSERT_TOO_MANY_EXPRESSIONS, NULL);
+				yyerror(NULL, NULL, &cur_cla->column_list, NULL, NULL, NULL);
+				return 1;
+			}
+		}
 		/* There is nothing to qualify in "insert->dst_table_alias" and "insert->columns" */
 		return result;
 	}
@@ -99,10 +136,44 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 	 * Do not use any tables from the current query level FROM list for this qualification.
 	 */
 	do {
+		SqlStatement *stmt = join->value;
+
 		/* Qualify sub-queries involved in the join. Note that it is possible a `table` is involved in the join instead
 		 * of a `sub-query` in which case the below `qualify_query` call will return right away.
 		 */
 		result |= qualify_query(cur_join->value, parent_join, table_alias, ret);
+		/* The following code block needs to be after above call to qualify_query() because we need
+		 * any asterisk usage in cur_join->value to be expanded and qualified for the later natural_join_condition() to work
+		 * correctly.
+		 * TODO: avoid deferring the following processing in cases where its not required. Refer:
+		 * https://gitlab.com/YottaDB/DBMS/YDBOcto/-/merge_requests/816#note_583771101
+		 */
+		if (NATURAL_JOIN == cur_join->type) {
+			/* cur_join->condition would not yet have been filled in (deferred in parser). Do that here and
+			 * at the same time do some qualification checks too (errors will be returned as a non-zero value).
+			 * Its possible to defer this logic till this point because asterisk and table.* are processed in
+			 * qualify_query() itself. The asterisk processing logic requires the below call to set duplicates columns
+			 * correctly.
+			 */
+			if (natural_join_condition(start_join, cur_join)) {
+				return 1;
+			}
+		}
+		/* Copy correlation specification i.e. aliases. We defer it till this point as asterisk and table.* expansion has to
+		 * be complete before we can determine if the correlation specification is valid or not.
+		 * TODO: avoid deferring the following processing in cases where its not required. Refer:
+		 * https://gitlab.com/YottaDB/DBMS/YDBOcto/-/merge_requests/816#note_583771101
+		 */
+		stmt = drill_to_table_alias(stmt);
+		if (table_alias_STATEMENT == stmt->type) {
+			SqlTableAlias *join_table_alias;
+
+			UNPACK_SQL_STATEMENT(join_table_alias, stmt, table_alias);
+			if (join_table_alias->correlation_specification != NULL) {
+				if (copy_correlation_specification_aliases(join_table_alias))
+					return 1;
+			}
+		}
 		cur_join = cur_join->next;
 	} while (cur_join != start_join);
 	/* Now that FROM clause has been qualified, qualify the JOIN conditions etc. in the FROM clause.
@@ -204,7 +275,8 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 				 */
 				assert(result);
 				assert((value_STATEMENT == col_list->value->type)
-				       && (COLUMN_REFERENCE == col_list->value->v.value->type));
+				       && ((COLUMN_REFERENCE == col_list->value->v.value->type)
+					   || (TABLE_ASTERISK == col_list->value->v.value->type)));
 			}
 			cur_cla = cur_cla->next;
 			if (cur_cla == start_cla) {
@@ -229,6 +301,62 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 		// Qualify HAVING clause
 		result |= qualify_statement(select->having_expression, start_join, table_alias_stmt, 0, NULL);
 		// Qualify SELECT column list next
+		/* Expand ASTERISK here using prev_start and prev_end
+		 * Deferred till this point as we might come across a table.* usage in the join and that might be referring to a
+		 * parent query table.
+		 * TODO: avoid deferring the following processing in cases where its not required. Refer:
+		 * https://gitlab.com/YottaDB/DBMS/YDBOcto/-/merge_requests/816#note_583771101
+		 */
+		if ((!table_alias->do_group_by_checks) && (NULL != select->select_list)
+		    && (column_list_alias_STATEMENT == select->select_list->type)) {
+			SqlStatement *	    select_list;
+			SqlColumnListAlias *cla_cur, *cla_head, *asterisk_list;
+
+			select_list = select->select_list;
+			cla_cur = cla_head = select->select_list->v.column_list_alias;
+			asterisk_list = NULL;
+			/* Go through the select column list (`select n1.id,*,n2.id ...`) to find/process ASTERISK */
+			do {
+				if (NULL == cla_cur->column_list) {
+					/* Came across an ASTERISK in the select column list */
+					if (NULL == asterisk_list) {
+						SqlJoin *next_join = NULL;
+
+						if (prev_end != start_join->prev) {
+							/* Parent join exists. Remove them temporarily so process_asterisk() expands
+							 * * to only all current query level columns and not parent query columns */
+							next_join = prev_end->next;
+							start_join->prev->next = next_join;
+							next_join->prev = start_join->prev;
+							start_join->prev = prev_end;
+							prev_end->next = start_join;
+						}
+						asterisk_list = process_asterisk(start_join, select_list->loc);
+						if (NULL != next_join) {
+							/* Add back the parent join nodes */
+							prev_end->next = next_join;
+							start_join->prev = next_join->prev;
+							next_join->prev->next = start_join;
+							next_join->prev = prev_end;
+						}
+					}
+					if (cla_cur->next == cla_cur) {
+						/* cla_cur is the only member of select column list (`select * from ..`) */
+						select_list->v.column_list_alias = asterisk_list;
+					} else {
+						SqlColumnListAlias *cla_alias;
+
+						cla_alias = copy_column_list_alias_list(asterisk_list, NULL, NULL, NULL);
+						/* ASTERISK is present among other columns `select n1.id,*,n2.id from ..`
+						 * Replace dummy node (current cla_cur) with column alias list corresponding
+						 * to ASTERISK in the position where it was seen in select column list.
+						 */
+						REPLACE_COLUMNLISTALIAS(cla_cur, cla_alias, cla_head, select_list);
+					}
+				}
+				cla_cur = cla_cur->next;
+			} while (cla_cur != cla_head);
+		}
 		result |= qualify_statement(select->select_list, start_join, table_alias_stmt, 0, NULL);
 		// Qualify ORDER BY clause next
 		/* Now that all column names used in the query have been qualified, allow columns specified in

tests/test_select_columns_psql.bats.in
@@ -90,8 +90,11 @@ setup() {
 	run_query_in_octo_and_postgres_and_crosscheck_multiple_queries names TSCP07.sql
 }
 
-@test "TSCP08 : OCTO385 : Verify different asterisk usage in select column list" {
+@test "TSCP08 : OCTO385/OCTO386 : Verify different asterisk and TABLENAME.ASTERISK usage in select column list and ORDER BY" {
   run_query_in_octo_and_postgres_and_crosscheck_multiple_queries names TSCP08.sql
+  load_fixture customers.sql
+  load_fixture customers.zwr
+  run_query_in_octo_and_postgres_and_crosscheck_multiple_queries customers TSCP08_customers.sql
   load_fixture TSCP08_errors.sql subtest novv
   verify_output TSCP08_errors output.txt
 }
@@ -102,5 +105,50 @@ setup() {
 
 @test "TSCP10 : OCTO628 : RPARENMISSING error when trying to use || operator with hundreds of operands" {
 	run_query_in_octo_and_postgres_and_crosscheck_multiple_queries names TSCP10.sql
+}
+
+@test "TSCP11 : OCTO386 : Verify different TABLENAME.ASTERISK usage with GROUP BY and aggregate functions" {
+  run_query_in_octo_and_postgres_and_crosscheck_multiple_queries names TSCP11.sql
+  load_fixture customers.sql
+  load_fixture customers.zwr
+  run_query_in_octo_and_postgres_and_crosscheck_multiple_queries customers TSCP11_customers.sql
+  load_fixture TSCP11_errors.sql subtest novv
+  verify_output TSCP11_errors output.txt
+}
+
+@test "TSCP12 : OCTO386 : Verify count(DISTINCT n1.id) and count(DISTINCT n1.*) creates two different physical plans" {
+  run_query_in_octo_and_postgres_and_crosscheck_multiple_queries names TSCP12_column.sql
+  run_query_in_octo_and_postgres_and_crosscheck_multiple_queries names TSCP12_table_asterisk.sql
+  [[ $(ls -l _ydboctoP*.m | wc -l) -eq 2 ]]
+}
+
+@test "TSCP13 : OCTO386 : Verify count(DISTINCT n1.*) and count(n1.*) behavior with single column table created through DDL" {
+  load_fixture names1col.sql
+  load_fixture names1col.zwr
+  run_query_in_octo_and_postgres_and_crosscheck_multiple_queries names1col TSCP13.sql
+  load_fixture TSCP13_errors.sql subtest novv
+  verify_output TSCP13_errors output.txt
+}
+
+@test "TSCP14 : OCTO386 : Verify AVG, SUM, MIN & MAX work with table.* when table has only one column" {
+  # Aggregate functions other than COUNT also work with table.*. But, table must have only one column.
+  # MIN and MAX work with tables having single column of VARCHAR or INTEGER
+  # AVG and SUM work with tables having single column of INT
+  # Postgres doesn't allow table.* syntax for all aggregate functions so we can't cross check here with postgres.
+  load_fixture names1col.sql
+  load_fixture names1col.zwr
+  load_fixture TSCP14_table_asterisk.sql subtest novv
+  mv output.txt table_asterisk_output.txt
+  load_fixture TSCP14_single_column.sql subtest novv
+  diff -I "^select " output.txt table_asterisk_output.txt
+}
+
+@test "TSCP15 : OCTO386 : Verify queries with TABLENAME.ASTERISK as a column in GROUP BY and select column list" {
+  load_fixture names1col.sql
+  load_fixture names1col.zwr
+  load_fixture customers.sql
+  load_fixture customers.zwr
+  load_fixture TSCP15.sql subtest novv
+  verify_output TSCP15 output.txt
 }
 

commit e2f38b60117969f388630cdc98232e90f0bc82b8

[#779] [#386] Fix incorrect ERR_UNKNOWN_COLUMN_NAME error when GROUP BY is used on a VALUES table in the JOIN list
Background
----------
* Below is a simple example illustrating the issue. In this case, I define the `values ('Joey')` table
  with an alias `alias1` and column name `name` but the `GROUP BY alias1.name` incorrectly issues an
  `Unknown column` error in Octo. The same query runs fine in Postgres as can be seen by the later `psql`
  output below.

  ```sql
  $ cat test.sql
  select count(*) from names, (values ('Joey')) AS alias1(name) GROUP BY alias1.name;

  $ src/octo -f test.sql
  [ERROR]src/qualify_column_name.c:267 2021-11-09 10:46:09 : ERR_UNKNOWN_COLUMN_NAME : Unknown column: ALIAS1.NAME
  Error with syntax near (line 1, column 81):

  select count(*) from names cross join (values ('Joey')) AS alias1(name) GROUP BY alias1.name;
                                                                                   ^^^^^^^^^^^
  [ERROR]src/parse_line.c:45 2021-11-09 10:46:09 : ERR_PARSING_COMMAND : Error parsing statement: select count(*) from names cross join (values ('Joey')) AS alias1(name) GROUP BY alias1.name;

  $ psql names -f test.sql
  count
  6
  (1 row)
  ```

Issue
-----
* This is a regression introduced in 853ce79e (!816 as part of fixing #386).

* Line 8 of the `git show` output below was added inside a `do/while` loop but used a loop-invariant
  variable `join`. The loop variable was `cur_join`.

  ```diff
      1 $ git show 853ce79e src/qualify_query.c
      2 .
      3 .
      4 @@ -99,10 +136,44 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
      5          * Do not use any tables from the current query level FROM list for this qualification.
      6          */
      7         do {
      8 +               SqlStatement *stmt = join->value;
      9 +
     10                 /* Qualify sub-queries involved in the join. Note that it is possible a `table` is involved in the join instead

src/qualify_query.c
@@ -190,7 +190,7 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 	 * FROM/JOIN list. Do not use any tables from the current query level FROM/JOIN list for this qualification.
 	 */
 	do {
-		SqlStatement *stmt = join->value;
+		SqlStatement *stmt = cur_join->value;
 
 		/* Qualify sub-queries involved in the join. Note that it is possible a `table` is involved in the join instead
 		 * of a `sub-query` in which case the below `qualify_query` call will return right away.

tests/fixtures/TSCP17.sql
@@ -1,0 +1,21 @@
+#################################################################
+#								#
+# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+-- TSCP17 : OCTO779 : Incorrect ERR_UNKNOWN_COLUMN_NAME error when GROUP BY is used on a VALUES table in the JOIN list
+
+SELECT 1 FROM names, (values ('Joey')) AS alias1(name) GROUP BY alias1.name;
+SELECT COUNT(*) FROM names, (values ('Joey')) AS alias1(name) GROUP BY alias1.name;
+SELECT COUNT(DISTINCT alias1.*) FROM names, (values ('Joey')) AS alias1(name) GROUP BY alias1.name;
+SELECT COUNT(alias1.name) FROM names, (values ('Joey')) AS alias1(name) GROUP BY alias1.name;
+SELECT COUNT(alias1.name) FROM names, (values ('Joey')) AS alias1(name) GROUP BY name;
+SELECT name FROM names, (values ('Joey')) AS alias1(name) GROUP BY name;
+

tests/test_select_columns_psql.bats.in
@@ -160,3 +160,8 @@ setup() {
   load_fixture TSCP16.sql subtest novv
   verify_output TSCP16 output.txt
 }
+
+@test "TSCP17 : OCTO779 : Incorrect ERR_UNKNOWN_COLUMN_NAME error when GROUP BY is used on a VALUES table in the JOIN list" {
+	run_query_in_octo_and_postgres_and_crosscheck_multiple_queries names TSCP17.sql
+}
+
YottaDB/YDBOcto

commit b4fd3618c9495522af993d3720614b655cc5b57a

[#601] Reduce query runtime by moving parts of WHERE clause to ON clause
* The high level change here is to examine all parts of the WHERE clause and see if any of them can be
  moved to the ON clause as that improves query runtimes.

  For example, the below query

  ```sql
  select * from names n1 inner join names n2 on true inner join names n3 on true where n1.id < 2;
  ```

  can be better written as

  ```sql
  select * from names n1 inner join names n2 on n2.id < 2 inner join names n3 on true;
  ```

  The second version executes the n2 loop only for `n2.id < 2` whereas the first version executes the
  n2 loop for all values of n2.id. The second version therefore avoids executing the n3 for loop for
  n2.id values greater than 1. And this can add up to a lot of iterations if the number of rows in
  the `names` table is huge (`O(N^2)` where `N` is the number of rows). If more number of joins
  are involved, the difference could be `O(N^3)` or higher.

* `src/move_where_clause_to_on_clause.c` : This new file implements the core logic of moving parts of
  expressions from the WHERE clause to the ON clause of the JOIN if safely possible.

  To help with determining if it is safe to move, a new field `max_unique_id` in the `SqlJoin` structure
  (in `src/octo_types.h`) is introduced. This field is initialized to a copy of `config->plan_id` in the
  following files.
  - src/parser/select.y
  - src/parser/derived_table.c
  - src/parser/table_reference.c

  Since `config->plan_id` is a constantly increasing id that we assign to tables encountered in the query
  as we parse it, this one number is a good idea of all table ids that are valid as of the current JOIN.
  Any id that is greater than this is guaranteed to have been encountered AFTER this JOIN and so any
  column reference corresponding to a table with that id cannot be safely moved from the WHERE clause
  (where all column references are valid) to this JOIN ON clause (where this column reference is not valid).

tests/test_where.bats.in
@@ -250,3 +250,11 @@ OCTO
   verify_output TW24 output.txt
 }
 
+@test "TW25 : OCTO601 : Reduce query runtime by moving parts of WHERE clause to ON clause" {
+  load_fixture TW25.sql subtest novv
+  echo "# ----------------------------------------------------------------------------" >> output.txt
+  echo "# Include generated M code in reference file to verify all optimizations there" >> output.txt
+  echo "# ----------------------------------------------------------------------------" >> output.txt
+  cat _ydboctoP*.m >> output.txt
+  verify_output TW25 output.txt
+}

src/CMakeLists.txt
@@ -186,6 +186,7 @@ add_library(libocto OBJECT
   match_column_list_alias_in_select_column_list.c
   match_sql_statement.c
   memory_chunk.c
+  move_where_clause_to_on_clause.c
   octo_init.c
   optimization_transforms/generate_logical_plan.c
   optimization_transforms/get_new_plan_unique_id.c
@@ -226,6 +227,7 @@ add_library(libocto OBJECT
   optimization_transforms/optimize_logical_plan.c
   parse_line.c
   parse_startup_flags.c
+  parse_tree_optimize.c
   parser/sql_set_statement.c
   parser/aggregate_function.c
   parser/as_name.c
@@ -260,6 +262,7 @@ add_library(libocto OBJECT
   store_table_in_pg_class.c
   store_function_in_pg_proc.c
   tables_equal.c
+  traverse_where_clause.c
   values_equal.c
   ydb_error_check.c
   ydb_hash_to_string.c

src/copy_sql_statement.c
@@ -162,6 +162,7 @@ SqlStatement *copy_sql_statement(SqlStatement *stmt) {
 		do {
 			OCTO_CMALLOC_STRUCT(new_join, SqlJoin);
 			dqinit(new_join);
+			new_join->max_unique_id = cur_join->max_unique_id;
 			new_join->type = cur_join->type;
 			new_join->value = copy_sql_statement(cur_join->value);
 			new_join->condition = copy_sql_statement(cur_join->condition);

src/move_where_clause_to_on_clause.c
@@ -1,0 +1,90 @@
+/****************************************************************
+ *								*
+ * Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+
+#include <assert.h>
+
+#include "octo.h"
+
+/* This is an optimization function that tries to determine if it is safe to move a portion of the WHERE clause
+ * (whose address is passed in as "stmt_ptr") to the ON clause of some JOIN in the query. If so, it moves it.
+ * This can significantly reduce query runtime in case the query has lots of joins (the ON clause in one join
+ * noticeably reduces the number of iterations a nested join executes compared to the same condition in the
+ * WHERE clause which gets executed in the innermost loop for all nested join FOR loops) (YDBOcto#601).
+ */
+void move_where_clause_to_on_clause(SqlStatement **stmt_ptr, SqlJoin *start_join) {
+	int	      max_unique_id;
+	SqlJoin *     cur_join, *noted_join;
+	boolean_t     outer_join_seen, right_or_full_join_seen;
+	SqlStatement *stmt;
+
+	stmt = *stmt_ptr;
+	max_unique_id = stmt->hash_canonical_query_cycle;
+	cur_join = start_join;
+	outer_join_seen = FALSE;
+	right_or_full_join_seen = FALSE;
+	noted_join = NULL; /* This notes down the JOIN whose ON clause/condition will inherit the WHERE clause portion */
+	do {
+		enum SqlJoinType join_type;
+		SqlJoin *	 next_join;
+
+		next_join = cur_join->next;
+		join_type = cur_join->type;
+		if (IS_OUTER_JOIN(join_type)) {
+			outer_join_seen = TRUE;
+			if ((RIGHT_JOIN == join_type) || (FULL_JOIN == join_type)) {
+				right_or_full_join_seen = TRUE;
+			}
+		} else if ((NULL == noted_join) && (CROSS_JOIN != join_type) && (CROSS_JOIN != next_join->type)
+			   && (next_join != start_join) && (max_unique_id <= cur_join->max_unique_id)) {
+			/* CROSS JOIN does not currently expect an ON clause ("lp_optimize_cross_join()" relies on this).
+			 * Hence the "CROSS_JOIN != join_type" and "CROSS_JOIN != next_join->type" check above. Also, if the
+			 * join we are going to move the WHERE clause to is the last in the join list, we might as well keep
+			 * it inside the WHERE clause (no savings in runtime) hence the check for "next_join != start_join" above.
+			 * The "max_unique_id <= cur_join->max_unique_id" check is the key check. It confirms that it is safe
+			 * to move the WHERE clause to the ON clause because the part of the WHERE clause we are considering
+			 * moving has references to columns all of which correspond to tables that are known at the time the
+			 * JOIN is parsed in the query.
+			 */
+			noted_join
+			    = cur_join; /* Note: We still need to process rest of JOIN list to see if there is any OUTER JOIN */
+		}
+		cur_join = next_join;
+	} while (cur_join != start_join);
+	/* If we saw a RIGHT or FULL JOIN, the move from a WHERE clause to the ON clause cannot be safely done in all
+	 * cases (TOJ03 bats subtest generates random queries which will fail otherwise). But moving in the presence of
+	 * LEFT JOINs (the most frequently used outer join) are okay hence the check for "!right_or_full_join_seen" below.
+	 * If RIGHT/FULL JOIN cases become more common and need to be optimized, this code needs to be revisited.
+	 */
+	if ((NULL != noted_join) && !right_or_full_join_seen) {
+		/* Move operand of BOOLEAN_AND from WHERE clause to ON clause of this JOIN */
+		if (NULL != noted_join->condition) {
+			SqlStatement *join_condition;
+
+			SQL_STATEMENT(join_condition, binary_STATEMENT);
+			MALLOC_STATEMENT(join_condition, binary, SqlBinaryOperation);
+			join_condition->v.binary->operation = BOOLEAN_AND;
+			join_condition->v.binary->operands[0] = noted_join->condition;
+			join_condition->v.binary->operands[1] = stmt;
+			noted_join->condition = join_condition;
+		} else {
+			noted_join->condition = stmt;
+		}
+		/* If OUTER JOIN has been seen, then we cannot safely remove this from the WHERE clause
+		 * (might return incorrect results). Adding the same condition to the ON clause does help
+		 * speed up so we will do that but will also retain it in the WHERE clause.
+		 */
+		if (!outer_join_seen) {
+			*stmt_ptr = NULL;
+		}
+	}
+	return;
+}

src/octo.h
@@ -369,6 +369,8 @@ typedef enum RegexType {
 		input_buffer_combined[cur_input_index + QUERY_LENGTH + NEWLINE_NEEDED] = '\0';                     \
 	}
 
+#define IS_STMT_BOOLEAN_AND(STMT) ((binary_STATEMENT == STMT->type) && (BOOLEAN_AND == STMT->v.binary->operation))
+
 // Convenience type definition for run_query callback function
 typedef int (*callback_fnptr_t)(SqlStatement *, int, void *, char *, boolean_t);
 
@@ -402,8 +404,12 @@ SqlFunction * find_function(const char *function_name, const char *function_hash
 int	   drop_schema_from_local_cache(ydb_buffer_t *name_buffer, SqlSchemaType schema_type, ydb_buffer_t *function_hash_buffer);
 SqlColumn *find_column(char *column_name, SqlTable *table);
 SqlStatement *find_column_alias_name(SqlStatement *stmt);
-int	      qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTableAlias *parent_table_alias);
-int qualify_statement(SqlStatement *stmt, SqlJoin *tables, SqlStatement *table_alias_stmt, int depth, SqlColumnListAlias **ret_cla);
+void	      parse_tree_optimize(SqlSelectStatement *select);
+void	      move_where_clause_to_on_clause(SqlStatement **stmt_ptr, SqlJoin *start_join);
+SqlStatement *traverse_where_clause(SqlStatement *binary_stmt, SqlJoin *start_join);
+int	      qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTableAlias *parent_table_alias,
+			    QualifyStatementParms *ret);
+int qualify_statement(SqlStatement *stmt, SqlJoin *tables, SqlStatement *table_alias_stmt, int depth, QualifyStatementParms *ret);
 SqlColumnAlias *    qualify_column_name(SqlValue *column_value, SqlJoin *tables, SqlStatement *table_alias_stmt, int depth,
 					SqlColumnListAlias **ret_cla);
 SqlColumnListAlias *match_column_in_table(SqlTableAlias *table, char *column_name, int column_name_len, boolean_t *ambiguous,

src/octo_types.h
@@ -454,8 +454,18 @@ typedef struct SqlJoin {
 	struct SqlStatement *value;
 	// SqlValue
 	struct SqlStatement *condition;
+	enum SqlJoinType     type;
+	/* The below field stores the value of "config->plan_id" at the time this join was parsed in the query.
+	 * That gives us a picture of the maximum number of tables (and in turn table_alias->unique_id) that
+	 * were processed till now. To elaborate, since "config->plan_id" is a constantly increasing id that we
+	 * assign to tables encountered in the query as we parse it, this one number is a good idea of all table
+	 * ids that are valid as of the current JOIN. Any id that is greater than this is guaranteed to have been
+	 * encountered AFTER this JOIN and so any column reference corresponding to a table with that id cannot be
+	 * safely moved from the WHERE clause (where all column references are valid) to this JOIN ON clause (where
+	 * this column reference is not valid).
+	 */
+	int max_unique_id;
 	dqcreate(SqlJoin);
-	enum SqlJoinType type;
 } SqlJoin;
 
 /**
@@ -739,8 +749,22 @@ typedef struct SqlStatement {
 		 *	invalid_STATEMENT
 		 */
 	} v;
-	uint64_t hash_canonical_query_cycle; // used during "hash_canonical_query" to avoid
-					     // multiple traversals of same node.
+	/* The below is used during "hash_canonical_query" to avoid multiple traversals of same node.
+	 * Before "hash_canonical_query()" is reached, this field is used to store a 4-byte "max_unique_id"
+	 * if this SqlStatement structure falls inside the subtree of a WHERE or ON clause. In that case, it
+	 * represents the MAXIMUM value of "table_alias->unique_id" where "table_alias" is the table alias corresponding
+	 * to any/all column references inside that subtree. For example if the WHERE clause had "(n1.id = 3)" then
+	 * this field will represent the "unique_id" corresponding to the table_alias for "n1". This is used to compare
+	 * against "SqlJoin.max_unique_id" field to see if a subtree of the WHERE clause can be moved over to the
+	 * ON clause of the JOIN.
+	 */
+	uint64_t hash_canonical_query_cycle;
 } SqlStatement;
+
+/* The below is used by qualify_statement.c */
+typedef struct {
+	SqlColumnListAlias **ret_cla;
+	int *		     max_unique_id;
+} QualifyStatementParms;
 
 #endif

src/optimization_transforms/generate_logical_plan.c
@@ -107,13 +107,7 @@ LogicalPlan *generate_logical_plan(SqlStatement *stmt) {
 		if (cur_join->condition) {
 			MALLOC_LP_2ARGS(t_join_condition, LP_WHERE);
 			LP_GENERATE_WHERE(cur_join->condition, stmt, t_join_condition->v.lp_default.operand[0], error_encountered);
-			if (num_outer_joins) {
-				join_right->extra_detail.lp_table_join.join_on_condition = t_join_condition;
-			} else { /* No OUTER JOINs. We can safely add the ON clause in the join condition to the
-				  * WHERE clause without risk of correctness issues.
-				  */
-				start_join_condition = lp_join_where(start_join_condition, t_join_condition);
-			}
+			join_right->extra_detail.lp_table_join.join_on_condition = t_join_condition;
 		}
 		cur_join = cur_join->next;
 	} while (cur_join != start_join);

src/optimization_transforms/lp_make_normal_disjunctive_form.c
@@ -157,19 +157,19 @@ LogicalPlan *lp_make_normal_disjunctive_form(LogicalPlan *root) {
 	LogicalPlan *left = lp_make_normal_disjunctive_form(root->v.lp_default.operand[0]);
 	LogicalPlan *right = lp_make_normal_disjunctive_form(root->v.lp_default.operand[1]);
 
+	// If this case is an OR, we don't need to combine things, so return early
+	if (LP_BOOLEAN_OR == root->type) {
+		root->v.lp_default.operand[0] = left;
+		root->v.lp_default.operand[1] = right;
+		return root;
+	}
+	assert(LP_BOOLEAN_AND == root->type);
 	// Algorithm:
 	// 1. Process left and right children into normal form
 	// 2. For each each clause on the left:
 	//  2.1 For each clause on the right:
 	//   2.1.1 If clause on left is OR, and clause on right is OR, construct new AND of left and right
 	//   2.1.2 If clause of left is AND, and clause on right is OR, join with
-
-	// If this case is not an AND, we don't need to combine things, so return early
-	if (LP_BOOLEAN_AND != root->type) {
-		root->v.lp_default.operand[0] = left;
-		root->v.lp_default.operand[1] = right;
-		return root;
-	}
 
 	// Else, walk through the left list, right list, and form a new series
 	LogicalPlan *ret = NULL, *cur = NULL;

commit 6f76eab3e2bf1a3fce246993c262d9bd6ef63118

[#790] Fix rare incorrect query results or incorrect errors (e.g. ERR_SUBQUERY_MULTIPLE_ROWS error)
Background
----------
* Below is a simple example that demonstrates the issue.
  - I run 4 queries using the `names` database.
  - Query 2 is a query where we do expect an `ERR_SUBQUERY_MULTIPLE_ROWS` error.
  - But Query 1, 3 and 4 are identical and we do not expect an error from them.
  - Query 1 and Query 4 produce identical output and no error as expected.
  - But Query 3 incorrectly produces an `ERR_SUBQUERY_MULTIPLE_ROWS` error (instead of the same output as Query 1 and Query 4).

  ```sql
  $ rm -f _ydbocto*
  $ src/octo
  OCTO> SELECT * FROM names a WHERE a.firstName != (SELECT b.firstName FROM names b WHERE a.id = (b.id + 2));
  ID|FIRSTNAME|LASTNAME
  2|Cereal|Killer
  3|Lord|Nikon
  4|Joey|
  5|Zero|Cool
  (4 rows)
  OCTO> SELECT * FROM names a WHERE a.firstName != (SELECT NULL from names limit 2);
  [ERROR] src/run_query.c:257 2021-11-29 13:53:54 : ERR_SUBQUERY_MULTIPLE_ROWS : More than one row returned by a subquery used as an expression
  OCTO> SELECT * FROM names a WHERE a.firstName != (SELECT b.firstName FROM names b WHERE a.id = (b.id + 2));
  [ERROR] src/run_query.c:257 2021-11-29 13:53:54 : ERR_SUBQUERY_MULTIPLE_ROWS : More than one row returned by a subquery used as an expression
  OCTO> SELECT * FROM names a WHERE a.firstName != (SELECT b.firstName FROM names b WHERE a.id = (b.id + 2));
  ID|FIRSTNAME|LASTNAME
  2|Cereal|Killer
  3|Lord|Nikon
  4|Joey|
  5|Zero|Cool
  (4 rows)
  ```

Issue
-----
* This is a regression introduced in b4fd3618c9495522af993d3720614b655cc5b57a (!788 which fixes #601).

* Pasted below is relevant text from that commit message.

  _Additionally, an existing `hash_canonical_query_cycle` field in the `SqlStatement` structure
  (in `src/octo_types.h`) is overloaded to store the `max_unique_id` under a subtree in the WHERE clause.

src/octo_init.c
@@ -857,6 +857,15 @@ int octo_init(int argc, char **argv) {
 				ydb_release_number += atoi(ch2);
 			}
 		}
+		/* Set global variables to non-default values if necessary (default value is 0) */
+		hash_canonical_query_cycle = (uint64_t)INT32_MAX;
+		/* This global variable is incremented before every outermost call to "hash_canonical_query".
+		 * It is initialized to INT32_MAX instead of 0 (default for most globals) since the field that this is
+		 * checked for equality (in "hash_canonical_query()") is "stmt->hash_canonical_query_cycle" and the latter
+		 * can take on an overloaded meaning of a 4-byte signed integer "max_unique_id" (see comments in
+		 * "SqlStatement" in "octo_types.h" for more details). Initializing this to INT32_MAX avoids this field
+		 * from incorrectly/coincidentally being set to the exact same value as "max_unique_id" (YDBOcto#790).
+		 */
 		config->config_file = config_file;
 		/* "argc" can be 0 in case of cmocka unit tests. Do not attempt auto upgrade/load in that case. */
 		if (argc) {

tests/fixtures/TW26.sql
@@ -1,0 +1,21 @@
+#################################################################
+#								#
+# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+-- TW26 : OCTO790 : Octo incorrectly issues ERR_SUBQUERY_MULTIPLE_ROWS error
+-- Below are queries pasted from https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/790#description.
+-- Without the code fixes for YDBOcto#790, the 3rd query below used to incorrectly issue an ERR_SUBQUERY_MULTIPLE_ROWS error.
+
+SELECT * FROM names a WHERE a.firstName != (SELECT b.firstName FROM names b WHERE a.id = (b.id + 2));
+SELECT * FROM names a WHERE a.firstName != (SELECT NULL from names limit 2);
+SELECT * FROM names a WHERE a.firstName != (SELECT b.firstName FROM names b WHERE a.id = (b.id + 2));
+SELECT * FROM names a WHERE a.firstName != (SELECT b.firstName FROM names b WHERE a.id = (b.id + 2));
+

tests/outref/TW26.ref
@@ -1,0 +1,29 @@
+
+-- TW26 : OCTO790 : Octo incorrectly issues ERR_SUBQUERY_MULTIPLE_ROWS error
+-- Below are queries pasted from https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/790#description.
+-- Without the code fixes for YDBOcto#790, the 3rd query below used to incorrectly issue an ERR_SUBQUERY_MULTIPLE_ROWS error.
+
+SELECT * FROM names a WHERE a.firstName != (SELECT b.firstName FROM names b WHERE a.id = (b.id + 2));
+SELECT * FROM names a WHERE a.firstName != (SELECT NULL from names limit 2);
+SELECT * FROM names a WHERE a.firstName != (SELECT b.firstName FROM names b WHERE a.id = (b.id + 2));
+SELECT * FROM names a WHERE a.firstName != (SELECT b.firstName FROM names b WHERE a.id = (b.id + 2));
+
+ID|FIRSTNAME|LASTNAME
+2|Cereal|Killer
+3|Lord|Nikon
+4|Joey|
+5|Zero|Cool
+(4 rows)
+[ERROR] PATH:LINENUM DATE TIME : ERR_SUBQUERY_MULTIPLE_ROWS : More than one row returned by a subquery used as an expression
+ID|FIRSTNAME|LASTNAME
+2|Cereal|Killer
+3|Lord|Nikon
+4|Joey|
+5|Zero|Cool
+(4 rows)
+ID|FIRSTNAME|LASTNAME
+2|Cereal|Killer
+3|Lord|Nikon
+4|Joey|
+5|Zero|Cool
+(4 rows)

tests/test_where.bats.in
@@ -1,6 +1,6 @@
 #################################################################
 #								#
-# Copyright (c) 2019-2020 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2019-2021 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -257,3 +257,9 @@ OCTO
   cat _ydboctoP*.m >> output.txt
   verify_output TW25 output.txt
 }
+
+@test "TW26 : OCTO790 : Octo incorrectly issues ERR_SUBQUERY_MULTIPLE_ROWS error" {
+	load_fixture TW26.sql subtest novv
+	verify_output TW26 output.txt
+}
+
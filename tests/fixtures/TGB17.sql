fecc433cce9f384bdbb52beef6dffb2a9359b8a4
Narayanan Iyer <nars@yottadb.com>
2022-03-10 14:40:38 -0500

    [#813] Fix query with no GROUP BY or aggregate function but HAVING when WHERE returns no rows
    Background
    ----------
    * Below is a query from the #796 issue description with a slight change to add a `where FALSE`.
    
    * Notice the output of Octo is different from Postgres.
    
      **Octo output**
      ```sql
      OCTO> select 1 from names where FALSE having 1=1;
      ???
      (0 rows)
      ```
    
      **Postgres output**
      ```sql
      names=> select 1 from names where FALSE having 1=1;
      ?column?
      1
      (1 row)
      ```
    
    * Below is from the Postgres documentation (https://www.postgresql.org/docs/current/sql-select.html#SQL-GROUPBY)
    
      _The presence of HAVING turns a query into a grouped query even if there is no GROUP BY clause. This is the
      same as what happens when the query contains aggregate functions but no GROUP BY clause. **All the selected
      rows are considered to form a single group**, and the SELECT list and HAVING clause can only reference table
      columns from within aggregate functions. Such a query will emit a single row if the HAVING condition is true,
      zero rows if it is not true._
    
    * Based on the above, I believe that if a GROUP BY is not specified, a HAVING clause is specified and no aggregate
      functions are specified, we should treat even an empty set of rows from the WHERE clause as 1 group and output 1
      row of query output. Whereas Octo currently treats it as 0 group and therefore produces 0 rows of query output.
    
    * This is the same way we do when a GROUP BY is not specified and aggregate functions are specified (see
      https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/455#note_321309198 for details on how this was addressed).
    
    Fix
    ---
    * `src/m_templates/tmpl_group_by.ctemplate` already took care of the case where GROUP BY was not specified
      but aggregate functions were used by emitting special M code to ensure an empty query result from the
      WHERE clause gets counted as 1 group instead of 0 groups.
    
    * A similar fix is now done for the case where HAVING is specified but GROUP BY is not specified and no
      aggregate functions are specified either.
    
    Test
    ----
    *  A new `test_group_by/TGB17` bats subtest verifies this fix.

---
+++ tests/fixtures/TGB17.sql
@@ -0,0 +1,16 @@
+#################################################################
+#								#
+# Copyright (c) 2022 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+-- TGB17: OCTO813 : Test query with no GROUP BY or aggregate function but HAVING when WHERE returns no rows
+
+select 1 from names where FALSE having 1=1;
+

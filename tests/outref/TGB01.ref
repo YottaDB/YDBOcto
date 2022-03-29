YottaDB/YDBOcto

commit d32d2fee781072d3393612a7c1a33507d6156cbe

[#786] Issue ERR_GROUP_BY_OR_AGGREGATE_FUNCTION error if ungrouped column is used in HAVING clause
Background
----------
* This is a day-one issue in the `HAVING` clause. If one uses an ungrouped column in that clause
  outside of an aggregate function, Octo currently does not issue an error whereas it should.
  See https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/786#description for more details.

* See simple query output below for example.

  **Octo output**
  ```sql
  OCTO> select 1 from names n1 having n1.firstname = 'Acid';
  ???
  1
  1
  1
  1
  1
  1
  (6 rows)
  ```

  **Postgres output**
  ```sql
  names=> select 1 from names n1 having n1.firstname = 'Acid';
  ERROR:  column "n1.firstname" must appear in the GROUP BY clause or be used in an aggregate function
  LINE 1: select 1 from names n1 having n1.firstname = 'Acid';
                                        ^
  ```

Fix
---
* The first change was to identify in `src/qualify_query.c` that we are qualifying the `HAVING` clause.
  This is done by noting down `table_alias->aggregate_depth` as a new `AGGREGATE_DEPTH_HAVING_CLAUSE`
  enum value. This way `src/qualify_statement.c` knows we are inside the `HAVING` clause.

* Next change was to notice that the `HAVING` clause cannot have `column_list_alias_STATEMENT` structures.
  All column references there would end up as `column_alias_STATEMENT` structures. This means that the
  `ERR_GROUP_BY_OR_AGGREGATE_FUNCTION` error that is currently issued in `src/qualify_statement.c` can
  never be issued for column references inside the `HAVING` clause. Therefore, it is unnecessary to
  keep the `qualify_statement(select->having_expression)` invocation in `src/qualify_query.c` inside
  the `for` loop (where the second iteration of the loop is to issue the `ERR_GROUP_BY_OR_AGGREGATE_FUNCTION`
  error. Therefore, this was moved out of the `for` loop.

* The next change was in `src/qualify_statement.c` inside the `COLUMN_REFERENCE` case. Here we now check

src/qualify_query.c
@@ -338,6 +338,11 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 			select->group_by_expression = NULL;
 		}
 	}
+
+	/* Qualify HAVING clause */
+	table_alias->aggregate_depth = AGGREGATE_DEPTH_HAVING_CLAUSE;
+	result |= qualify_statement(select->having_expression, start_join, table_alias_stmt, 0, NULL);
+
 	/* Expand "*" usage in SELECT column list here. This was not done in "query_specification.c" when the "*" usage was
 	 * first encountered because the FROM/JOIN list of that query could in turn contain a "TABLENAME.*" usage that refers
 	 * to a parent query table. In that case "query_specification.c" does not have access to the parent query context.
@@ -409,8 +414,6 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 	 */
 	for (;;) {
 		assert(0 == table_alias->aggregate_depth);
-		// Qualify HAVING clause
-		result |= qualify_statement(select->having_expression, start_join, table_alias_stmt, 0, NULL);
 		// Qualify SELECT column list next
 		result |= qualify_statement(select->select_list, start_join, table_alias_stmt, 0, NULL);
 		// Qualify ORDER BY clause next (see comment above for why "&lcl_ret" is passed only for ORDER BY).

src/qualify_statement.c
@@ -18,6 +18,18 @@
 #include "octo.h"
 #include "octo_types.h"
 
+#define ISSUE_GROUP_BY_OR_AGGREGATE_FUNCTION_ERROR(COLUMN_ALIAS_STMT)               \
+	{                                                                           \
+		SqlStatement *column_name;                                          \
+		SqlValue *    value;                                                \
+                                                                                    \
+		column_name = find_column_alias_name(COLUMN_ALIAS_STMT);            \
+		assert(NULL != column_name);                                        \
+		UNPACK_SQL_STATEMENT(value, column_name, value);                    \
+		ERROR(ERR_GROUP_BY_OR_AGGREGATE_FUNCTION, value->v.string_literal); \
+		yyerror(NULL, NULL, &COLUMN_ALIAS_STMT, NULL, NULL, NULL);          \
+	}
+
 /* Note: The code in "qualify_check_constraint.c" is modeled on the below so it is possible changes here might need to be
  *       made there too. And vice versa (i.e. changes to "qualify_statement.c" might need to be made here too).
  *       An automated tool "tools/ci/check_code_base_assertions.csh" alerts us (through the pre-commit script and/or
@@ -69,14 +81,7 @@ int qualify_statement(SqlStatement *stmt, SqlJoin *tables, SqlStatement *table_a
 			 * 3) The current column reference is not in the GROUP BY clause.
 			 * Issue an error.
 			 */
-			SqlStatement *column_name;
-			SqlValue *    value;
-
-			column_name = find_column_alias_name(stmt);
-			assert(NULL != column_name);
-			UNPACK_SQL_STATEMENT(value, column_name, value);
-			ERROR(ERR_GROUP_BY_OR_AGGREGATE_FUNCTION, value->v.string_literal);
-			yyerror(NULL, NULL, &stmt, NULL, NULL, NULL);
+			ISSUE_GROUP_BY_OR_AGGREGATE_FUNCTION_ERROR(stmt);
 			result = 1;
 		}
 		break;
@@ -113,23 +118,31 @@ int qualify_statement(SqlStatement *stmt, SqlJoin *tables, SqlStatement *table_a
 						}
 					}
 					parent_table_alias = column_table_alias->parent_table_alias;
-					if (parent_table_alias == table_alias) {
-						if (0 < parent_table_alias->aggregate_depth) {
+
+					if ((parent_table_alias == table_alias) && parent_table_alias->aggregate_depth) {
+						int aggregate_depth;
+
+						aggregate_depth = parent_table_alias->aggregate_depth;
+						if (0 < aggregate_depth) {
 							parent_table_alias->aggregate_function_or_group_by_specified = TRUE;
-						} else if (AGGREGATE_DEPTH_GROUP_BY_CLAUSE == parent_table_alias->aggregate_depth) {
+						} else if (AGGREGATE_DEPTH_GROUP_BY_CLAUSE == aggregate_depth) {
 							/* Update `group_by_column_count` and `group_by_column_number` */
 							new_column_alias->group_by_column_number
 							    = ++parent_table_alias->group_by_column_count;
-						} else if (0 != parent_table_alias->aggregate_depth) {
-							assert((AGGREGATE_DEPTH_WHERE_CLAUSE == parent_table_alias->aggregate_depth)
-							       || (AGGREGATE_DEPTH_FROM_CLAUSE
-								   == parent_table_alias->aggregate_depth));
-							/* AGGREGATE_DEPTH_WHERE_CLAUSE case is inside a WHERE clause where
-							 * aggregate function use is disallowed so do not record anything in that
-							 * case as that will otherwise confuse non-aggregated use of the same column
-							 * in say a SELECT column list. Same reasoning for
-							 * AGGREGATE_DEPTH_FROM_CLAUSE.
+						} else if (AGGREGATE_DEPTH_HAVING_CLAUSE == aggregate_depth) {
+							if (!new_column_alias->group_by_column_number) {
+								/* We are inside a HAVING clause while making a non-grouped column
+								 * reference outside of an aggregate function. Issue error.
+								 */
+								ISSUE_GROUP_BY_OR_AGGREGATE_FUNCTION_ERROR(stmt);
+								result = 1;
+							}
+						} else {
+							/* We are inside a WHERE or FROM/JOIN clause where  aggregate function
+							 * use is disallowed so no need to record anything related to GROUP BY.
 							 */
+							assert((AGGREGATE_DEPTH_WHERE_CLAUSE == aggregate_depth)
+							       || (AGGREGATE_DEPTH_FROM_CLAUSE == aggregate_depth));
 						}
 					}
 				} else {
@@ -229,9 +242,11 @@ int qualify_statement(SqlStatement *stmt, SqlJoin *tables, SqlStatement *table_a
 		break;
 	case aggregate_function_STATEMENT:;
 		SqlAggregateFunction *af;
+		boolean_t	      depth_adjusted;
 
 		UNPACK_SQL_STATEMENT(af, stmt, aggregate_function);
 		UNPACK_SQL_STATEMENT(table_alias, table_alias_stmt, table_alias);
+		depth_adjusted = FALSE;
 		if (table_alias->aggregate_depth) {
 			if (table_alias->do_group_by_checks) {
 				/* This is the second pass. Any appropriate error has already been issued so skip this. */
@@ -240,6 +255,7 @@ int qualify_statement(SqlStatement *stmt, SqlJoin *tables, SqlStatement *table_a
 			if (0 < table_alias->aggregate_depth) {
 				/* Nesting of aggregate functions at the same query level is not allowed */
 				ERROR(ERR_AGGREGATE_FUNCTION_NESTED, "");
+				result = 1;
 			} else {
 				/* Note: AGGREGATE_DEPTH_GROUP_BY_CLAUSE is also negative but we should never get here in that
 				 *       case as the parser for GROUP BY would have issued an error if ever GROUP BY is
@@ -249,19 +265,29 @@ int qualify_statement(SqlStatement *stmt, SqlJoin *tables, SqlStatement *table_a
 				if (AGGREGATE_DEPTH_WHERE_CLAUSE == table_alias->aggregate_depth) {
 					/* Aggregate functions are not allowed inside a WHERE clause */
 					ERROR(ERR_AGGREGATE_FUNCTION_WHERE, "");
+					result = 1;
 				} else if (AGGREGATE_DEPTH_FROM_CLAUSE == table_alias->aggregate_depth) {
 					/* Aggregate functions are not allowed inside a FROM clause.
 					 * Since the only way to use aggregate functions inside a FROM clause is through
 					 * JOIN conditions, issue a JOIN related error (not a FROM related error).
 					 */
 					ERROR(ERR_AGGREGATE_FUNCTION_JOIN, "");
+					result = 1;
+				} else if (AGGREGATE_DEPTH_HAVING_CLAUSE == table_alias->aggregate_depth) {
+					/* Aggregate functions are allowed inside a HAVING clause.
+					 * Temporarily reset depth to 0 before going inside the aggregate function call.
+					 */
+					table_alias->aggregate_depth = 0;
+					depth_adjusted = TRUE;
 				} else {
 					assert(FALSE);
 				}
 			}
-			yyerror(&af->parameter->loc, NULL, NULL, NULL, NULL, NULL);
-			result = 1;
-		} else {
+			if (result) {
+				yyerror(&af->parameter->loc, NULL, NULL, NULL, NULL, NULL);
+			}
+		}
+		if (!result) {
 			assert(!table_alias->do_group_by_checks || table_alias->aggregate_function_or_group_by_specified);
 			table_alias->aggregate_function_or_group_by_specified = TRUE;
 			table_alias->aggregate_depth++;
@@ -278,6 +304,10 @@ int qualify_statement(SqlStatement *stmt, SqlJoin *tables, SqlStatement *table_a
 				}
 			}
 			table_alias->aggregate_depth--;
+		}
+		if (depth_adjusted) {
+			/* Undo temporary reset of depth now that we have finished qualifying the aggregate function call */
+			table_alias->aggregate_depth = AGGREGATE_DEPTH_HAVING_CLAUSE;
 		}
 		break;
 	case cas_STATEMENT:

tests/fixtures/TGB01.sql
@@ -112,3 +112,8 @@ SELECT (SELECT n3.id FROM names n3 ORDER BY COUNT(n1.id)),(SELECT n2.id FROM nam
 SELECT (SELECT n3.id FROM names n3 WHERE n3.id = n1.id),(SELECT n2.id FROM names n2 ORDER BY COUNT(n1.id)) FROM names n1;
 SELECT (SELECT n3.id FROM names n3 WHERE n3.id = n1.id),(SELECT n2.id FROM names n2 ORDER BY COUNT(n1.id)) FROM names n1 GROUP BY n1.id;
 
+--> Below should error because non-grouped columns cannot be outside of aggregate functions in HAVING clause
+select 1 from names n1 having n1.firstname = 'Acid';
+select 1 from names n1 having n1.id is null;
+select 1 from names n1 having n1.* is null;
+

tests/outref/TGB01.ref
@@ -101,6 +101,11 @@ SELECT (SELECT n3.id FROM names n3 ORDER BY COUNT(n1.id)),(SELECT n2.id FROM nam
 SELECT (SELECT n3.id FROM names n3 WHERE n3.id = n1.id),(SELECT n2.id FROM names n2 ORDER BY COUNT(n1.id)) FROM names n1;
 SELECT (SELECT n3.id FROM names n3 WHERE n3.id = n1.id),(SELECT n2.id FROM names n2 ORDER BY COUNT(n1.id)) FROM names n1 GROUP BY n1.id;
 
+--> Below should error because non-grouped columns cannot be outside of aggregate functions in HAVING clause
+select 1 from names n1 having n1.firstname = 'Acid';
+select 1 from names n1 having n1.id is null;
+select 1 from names n1 having n1.* is null;
+
 [ERROR] PATH:LINENUM DATE TIME : ERR_GROUP_BY_ONLY_COLUMN_NAME : Only column names are valid in GROUP BY (not expressions or constants)
 Error with syntax near (line 2, column 30):
 
@@ -392,12 +397,6 @@ Error with syntax near (line 2, column 108):
 --> Below should error out because GROUP BY is needed for EmployeeID if used in the HAVING clause without aggregate function
 SELECT COUNT(OrderID),CustomerID FROM Orders GROUP BY CustomerID HAVING COUNT(OrderID) BETWEEN 3 AND 6 AND EmployeeID BETWEEN 2 and 5 ORDER BY COUNT(OrderID) desc, CustomerID;
                                                                                                            ^^^^^^^^^^
-[ERROR] PATH:LINENUM DATE TIME : ERR_GROUP_BY_OR_AGGREGATE_FUNCTION : Column EMPLOYEEID must appear in the GROUP BY clause or be used in an aggregate function
-Error with syntax near (line 2, column 108):
-
---> Below should error out because GROUP BY is needed for EmployeeID if used in the HAVING clause without aggregate function
-SELECT COUNT(OrderID),CustomerID FROM Orders GROUP BY CustomerID HAVING COUNT(OrderID) BETWEEN 3 AND 6 AND EmployeeID BETWEEN 2 and 5 ORDER BY COUNT(OrderID) desc, CustomerID;
-                                                                                                           ^^^^^^^^^^
 [ERROR] PATH:LINENUM DATE TIME : ERR_PARSING_COMMAND : Error parsing statement: --> Below should error out because GROUP BY is needed for EmployeeID if used in the HAVING clause without aggregate function
 SELECT COUNT(OrderID),CustomerID FROM Orders GROUP BY CustomerID HAVING COUNT(OrderID) BETWEEN 3 AND 6 AND EmployeeID BETWEEN 2 and 5 ORDER BY COUNT(OrderID) desc, CustomerID;
 [ERROR] PATH:LINENUM DATE TIME : ERR_GROUP_BY_OR_AGGREGATE_FUNCTION : Column EMPLOYEEID must appear in the GROUP BY clause or be used in an aggregate function
@@ -435,3 +434,23 @@ Error with syntax near (line 1, column 65):
 SELECT (SELECT n3.id FROM names n3 WHERE n3.id = n1.id),(SELECT n2.id FROM names n2 ORDER BY COUNT(n1.id)) FROM names n1 GROUP BY n1.id;
                                                                 ^^^^^
 [ERROR] PATH:LINENUM DATE TIME : ERR_PARSING_COMMAND : Error parsing statement: SELECT (SELECT n3.id FROM names n3 WHERE n3.id = n1.id),(SELECT n2.id FROM names n2 ORDER BY COUNT(n1.id)) FROM names n1 GROUP BY n1.id;
+[ERROR] PATH:LINENUM DATE TIME : ERR_GROUP_BY_OR_AGGREGATE_FUNCTION : Column FIRSTNAME must appear in the GROUP BY clause or be used in an aggregate function
+Error with syntax near (line 2, column 31):
+
+--> Below should error because non-grouped columns cannot be outside of aggregate functions in HAVING clause
+select 1 from names n1 having n1.firstname = 'Acid';
+                              ^^^^^^^^^^^^
+[ERROR] PATH:LINENUM DATE TIME : ERR_PARSING_COMMAND : Error parsing statement: --> Below should error because non-grouped columns cannot be outside of aggregate functions in HAVING clause
+select 1 from names n1 having n1.firstname = 'Acid';
+[ERROR] PATH:LINENUM DATE TIME : ERR_GROUP_BY_OR_AGGREGATE_FUNCTION : Column ID must appear in the GROUP BY clause or be used in an aggregate function
+Error with syntax near (line 1, column 31):
+
+select 1 from names n1 having n1.id is null;
+                              ^^^^^
+[ERROR] PATH:LINENUM DATE TIME : ERR_PARSING_COMMAND : Error parsing statement: select 1 from names n1 having n1.id is null;
+[ERROR] PATH:LINENUM DATE TIME : ERR_GROUP_BY_OR_AGGREGATE_FUNCTION : Column N1.* must appear in the GROUP BY clause or be used in an aggregate function
+Error with syntax near (line 1, column 31):
+
+select 1 from names n1 having n1.* is null;
+                              ^^
+[ERROR] PATH:LINENUM DATE TIME : ERR_PARSING_COMMAND : Error parsing statement: select 1 from names n1 having n1.* is null;

src/octo.h
@@ -581,6 +581,7 @@ typedef enum RegexType {
 #define AGGREGATE_DEPTH_FROM_CLAUSE	-1
 #define AGGREGATE_DEPTH_WHERE_CLAUSE	-2
 #define AGGREGATE_DEPTH_GROUP_BY_CLAUSE -3
+#define AGGREGATE_DEPTH_HAVING_CLAUSE	-4
 
 /* Timeouts used in various ydb_lock_incr_s() function calls */
 #define TIMEOUT_1_SEC		  ((unsigned long long)1000000000)

commit e34e5eb42bd2cc0e39b7937b1eacbff7405d8fbb

[#786] Issue ERR_GROUP_BY_OR_AGGREGATE_FUNCTION if sub-query in HAVING clause uses ungrouped outer query column (regression in d32d2fee)
Background
----------
* Below is pasted from https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/786#note_893116624.

* Below is a simple query that returns the following incorrect output.

  ```sql
  OCTO> select count(firstname) from names n1 HAVING EXISTS (select n1.firstname);
  COUNT
  6
  (1 row)
  ```

* The expected output is the following.

  ```sql
  OCTO> select count(firstname) from names n1 HAVING EXISTS (select n1.firstname);
  [ERROR] src/qualify_statement.c:78 2022-03-29 10:35:50 : ERR_GROUP_BY_OR_AGGREGATE_FUNCTION : Column FIRSTNAME must appear in the GROUP BY clause or be used in an aggregate function
  Error with syntax near (line 1, column 60):

  select count(firstname) from names n1 HAVING EXISTS (select n1.firstname);
                                                              ^^^^^^^^^^^^
  ```

* This is a regression in d32d2fee.

Issue
-----
* As part of d32d2fee, the following change was done (pasted from the respective commit message).

  Next change was to notice that the `HAVING` clause cannot have `column_list_alias_STATEMENT` structures.
  All column references there would end up as `column_alias_STATEMENT` structures. This means that the
  `ERR_GROUP_BY_OR_AGGREGATE_FUNCTION` error that is currently issued in `src/qualify_statement.c` can
  never be issued for column references inside the `HAVING` clause. Therefore, it is unnecessary to
  keep the `qualify_statement(select->having_expression)` invocation in `src/qualify_query.c` inside
  the `for` loop (where the second iteration of the loop is to issue the `ERR_GROUP_BY_OR_AGGREGATE_FUNCTION`
  error. Therefore, this was moved out of the `for` loop.

* While that change is the right thing to have done, it did not account for the fact that the code was

src/qualify_query.c
@@ -416,7 +416,7 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 	lcl_ret.ret_cla = &ret_cla;
 	lcl_ret.max_unique_id = ((NULL != ret) ? ret->max_unique_id : NULL);
 	table_alias->aggregate_depth = 0;
-	/* We qualify HAVING, SELECT and ORDER BY clauses TWICE below. Hence the "for" loop. This is because we want to
+	/* We qualify SELECT and ORDER BY clauses TWICE below. Hence the "for" loop. This is because we want to
 	 * check if any aggregate functions were used anywhere even if a GROUP BY was not used. In that case, we want to
 	 * ensure there are no non-grouped column references in the remainder of the query. See comment where
 	 * "do_group_by_checks" member is defined in the "SqlTableAlias" structure in "octo_types.h" for more details.

src/qualify_statement.c
@@ -131,8 +131,7 @@ int qualify_statement(SqlStatement *stmt, SqlJoin *tables, SqlStatement *table_a
 						}
 					}
 					parent_table_alias = column_table_alias->parent_table_alias;
-
-					if ((parent_table_alias == table_alias) && parent_table_alias->aggregate_depth) {
+					if ((NULL != parent_table_alias) && (parent_table_alias->aggregate_depth)) {
 						int aggregate_depth;
 
 						aggregate_depth = parent_table_alias->aggregate_depth;

tests/fixtures/TGB01.sql
@@ -1,6 +1,6 @@
 #################################################################
 #								#
-# Copyright (c) 2019-2021 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2019-2022 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -112,8 +112,14 @@ SELECT (SELECT n3.id FROM names n3 ORDER BY COUNT(n1.id)),(SELECT n2.id FROM nam
 SELECT (SELECT n3.id FROM names n3 WHERE n3.id = n1.id),(SELECT n2.id FROM names n2 ORDER BY COUNT(n1.id)) FROM names n1;
 SELECT (SELECT n3.id FROM names n3 WHERE n3.id = n1.id),(SELECT n2.id FROM names n2 ORDER BY COUNT(n1.id)) FROM names n1 GROUP BY n1.id;
 
---> Below should error because non-grouped columns cannot be outside of aggregate functions in HAVING clause
+--> OCTO786 : Below should error because non-grouped columns cannot be outside of aggregate functions in HAVING clause
 select 1 from names n1 having n1.firstname = 'Acid';
 select 1 from names n1 having n1.id is null;
 select 1 from names n1 having n1.* is null;
+
+-- OCTO786 : Below should error because non-grouped column n1.firstname is used inside HAVING clause in a sub-query
+-- Below tests https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/786#note_893116624
+SELECT COUNT(firstname) FROM names n1 HAVING EXISTS (select n1.firstname);
+-- Below is a slightly fancier version of the previous query using deeper sub-queries.
+SELECT COUNT(firstname) FROM names n1 HAVING EXISTS (SELECT COUNT(firstname) FROM names n2 WHERE 'Zero' IN (select n1.firstname));
 

tests/outref/TGB01.ref
@@ -101,10 +101,16 @@ SELECT (SELECT n3.id FROM names n3 ORDER BY COUNT(n1.id)),(SELECT n2.id FROM nam
 SELECT (SELECT n3.id FROM names n3 WHERE n3.id = n1.id),(SELECT n2.id FROM names n2 ORDER BY COUNT(n1.id)) FROM names n1;
 SELECT (SELECT n3.id FROM names n3 WHERE n3.id = n1.id),(SELECT n2.id FROM names n2 ORDER BY COUNT(n1.id)) FROM names n1 GROUP BY n1.id;
 
---> Below should error because non-grouped columns cannot be outside of aggregate functions in HAVING clause
+--> OCTO786 : Below should error because non-grouped columns cannot be outside of aggregate functions in HAVING clause
 select 1 from names n1 having n1.firstname = 'Acid';
 select 1 from names n1 having n1.id is null;
 select 1 from names n1 having n1.* is null;
+
+-- OCTO786 : Below should error because non-grouped column n1.firstname is used inside HAVING clause in a sub-query
+-- Below tests https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/786#note_893116624
+SELECT COUNT(firstname) FROM names n1 HAVING EXISTS (select n1.firstname);
+-- Below is a slightly fancier version of the previous query using deeper sub-queries.
+SELECT COUNT(firstname) FROM names n1 HAVING EXISTS (SELECT COUNT(firstname) FROM names n2 WHERE 'Zero' IN (select n1.firstname));
 
 [ERROR] PATH:LINENUM DATE TIME : ERR_GROUP_BY_ONLY_COLUMN_NAME : Only column names are valid in GROUP BY (not expressions or constants)
 Error with syntax near (line 2, column 30):
@@ -437,10 +443,10 @@ SELECT (SELECT n3.id FROM names n3 WHERE n3.id = n1.id),(SELECT n2.id FROM names
 [ERROR] PATH:LINENUM DATE TIME : ERR_GROUP_BY_OR_AGGREGATE_FUNCTION : Column FIRSTNAME must appear in the GROUP BY clause or be used in an aggregate function
 Error with syntax near (line 2, column 31):
 
---> Below should error because non-grouped columns cannot be outside of aggregate functions in HAVING clause
+--> OCTO786 : Below should error because non-grouped columns cannot be outside of aggregate functions in HAVING clause
 select 1 from names n1 having n1.firstname = 'Acid';
                               ^^^^^^^^^^^^
-[ERROR] PATH:LINENUM DATE TIME : ERR_PARSING_COMMAND : Error parsing statement: --> Below should error because non-grouped columns cannot be outside of aggregate functions in HAVING clause
+[ERROR] PATH:LINENUM DATE TIME : ERR_PARSING_COMMAND : Error parsing statement: --> OCTO786 : Below should error because non-grouped columns cannot be outside of aggregate functions in HAVING clause
 select 1 from names n1 having n1.firstname = 'Acid';
 [ERROR] PATH:LINENUM DATE TIME : ERR_GROUP_BY_OR_AGGREGATE_FUNCTION : Column ID must appear in the GROUP BY clause or be used in an aggregate function
 Error with syntax near (line 1, column 31):
@@ -454,3 +460,15 @@ Error with syntax near (line 1, column 31):
 select 1 from names n1 having n1.* is null;
                               ^^
 [ERROR] PATH:LINENUM DATE TIME : ERR_PARSING_COMMAND : Error parsing statement: select 1 from names n1 having n1.* is null;
+[ERROR] PATH:LINENUM DATE TIME : ERR_GROUP_BY_OR_AGGREGATE_FUNCTION : Column FIRSTNAME must appear in the GROUP BY clause or be used in an aggregate function
+Error with syntax near (line 1, column 61):
+
+SELECT COUNT(firstname) FROM names n1 HAVING EXISTS (select n1.firstname);
+                                                            ^^^^^^^^^^^^
+[ERROR] PATH:LINENUM DATE TIME : ERR_PARSING_COMMAND : Error parsing statement: SELECT COUNT(firstname) FROM names n1 HAVING EXISTS (select n1.firstname);
+[ERROR] PATH:LINENUM DATE TIME : ERR_GROUP_BY_OR_AGGREGATE_FUNCTION : Column FIRSTNAME must appear in the GROUP BY clause or be used in an aggregate function
+Error with syntax near (line 1, column 116):
+
+SELECT COUNT(firstname) FROM names n1 HAVING EXISTS (SELECT COUNT(firstname) FROM names n2 WHERE 'Zero' IN (select n1.firstname));
+                                                                                                                   ^^^^^^^^^^^^
+[ERROR] PATH:LINENUM DATE TIME : ERR_PARSING_COMMAND : Error parsing statement: SELECT COUNT(firstname) FROM names n1 HAVING EXISTS (SELECT COUNT(firstname) FROM names n2 WHERE 'Zero' IN (select n1.firstname));

tests/outref/TGB19_errors.ref
@@ -13,6 +13,11 @@ SELECT id,firstname FROM names n1 WHERE id IN (SELECT n2.id FROM names n2 group 
                                                       ^^^^^
 [ERROR] PATH:LINENUM DATE TIME : ERR_PARSING_COMMAND : Error parsing statement: SELECT id,firstname FROM names n1 WHERE id IN (SELECT n2.id FROM names n2 group by n1.id);
 [ERROR] PATH:LINENUM DATE TIME : ERR_GROUP_BY_OR_AGGREGATE_FUNCTION : Column ID must appear in the GROUP BY clause or be used in an aggregate function
+Error with syntax near (line 1, column 84):
+
+SELECT id,firstname FROM names n1 HAVING 1 IN (SELECT n2.id FROM names n2 group by n1.id);
+                                                                                   ^^^^^
+[ERROR] PATH:LINENUM DATE TIME : ERR_GROUP_BY_OR_AGGREGATE_FUNCTION : Column ID must appear in the GROUP BY clause or be used in an aggregate function
 Error with syntax near (line 1, column 55):
 
 SELECT id,firstname FROM names n1 HAVING 1 IN (SELECT n2.id FROM names n2 group by n1.id);
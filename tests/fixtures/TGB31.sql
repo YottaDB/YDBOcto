YottaDB/YDBOcto

commit b4fd3618c9495522af993d3720614b655cc5b57a

[#601] Reduce query runtime by moving parts of WHERE clause to ON clause
* The high level change here is to examine all parts of the WHERE clause and see if any of them can be
  moved to the ON clause as that improves query runtimes.

  For example, the below query

  ```sql
  select * from names n1 inner join names n2 on true inner join names n3 on true where n1.id < 2;
  ```

  can be better written as

  ```sql
  select * from names n1 inner join names n2 on n2.id < 2 inner join names n3 on true;
  ```

  The second version executes the n2 loop only for `n2.id < 2` whereas the first version executes the
  n2 loop for all values of n2.id. The second version therefore avoids executing the n3 for loop for
  n2.id values greater than 1. And this can add up to a lot of iterations if the number of rows in
  the `names` table is huge (`O(N^2)` where `N` is the number of rows). If more number of joins
  are involved, the difference could be `O(N^3)` or higher.

* `src/move_where_clause_to_on_clause.c` : This new file implements the core logic of moving parts of
  expressions from the WHERE clause to the ON clause of the JOIN if safely possible.

  To help with determining if it is safe to move, a new field `max_unique_id` in the `SqlJoin` structure
  (in `src/octo_types.h`) is introduced. This field is initialized to a copy of `config->plan_id` in the
  following files.
  - src/parser/select.y
  - src/parser/derived_table.c
  - src/parser/table_reference.c

  Since `config->plan_id` is a constantly increasing id that we assign to tables encountered in the query
  as we parse it, this one number is a good idea of all table ids that are valid as of the current JOIN.
  Any id that is greater than this is guaranteed to have been encountered AFTER this JOIN and so any
  column reference corresponding to a table with that id cannot be safely moved from the WHERE clause
  (where all column references are valid) to this JOIN ON clause (where this column reference is not valid).

src/qualify_query.c
@@ -22,7 +22,8 @@
  *	0 if query is successfully qualified.
  *	1 if query had errors during qualification.
  */
-int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTableAlias *parent_table_alias) {
+int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTableAlias *parent_table_alias,
+		  QualifyStatementParms *ret) {
 	SqlColumnListAlias *ret_cla;
 	SqlJoin *	    join;
 	SqlJoin *	    prev_start, *prev_end;
@@ -36,8 +37,8 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 		SqlSetOperation *set_opr;
 
 		UNPACK_SQL_STATEMENT(set_opr, table_alias_stmt, set_operation);
-		result |= qualify_query(set_opr->operand[0], parent_join, parent_table_alias);
-		result |= qualify_query(set_opr->operand[1], parent_join, parent_table_alias);
+		result |= qualify_query(set_opr->operand[0], parent_join, parent_table_alias, ret);
+		result |= qualify_query(set_opr->operand[1], parent_join, parent_table_alias, ret);
 		return result;
 	}
 	assert(table_alias_STATEMENT == table_alias_stmt->type);
@@ -65,7 +66,7 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 		/* Qualify sub-queries involved in the join. Note that it is possible a `table` is involved in the join instead
 		 * of a `sub-query` in which case the below `qualify_query` call will return right away.
 		 */
-		result |= qualify_query(cur_join->value, parent_join, table_alias);
+		result |= qualify_query(cur_join->value, parent_join, table_alias, ret);
 		cur_join = cur_join->next;
 	} while (cur_join != start_join);
 	/* Now that FROM clause has been qualified, qualify the JOIN conditions etc. in the FROM clause.
@@ -103,7 +104,11 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 	} while ((cur_join != start_join) && (cur_join != parent_join));
 	// Qualify WHERE clause next
 	table_alias->aggregate_depth = AGGREGATE_DEPTH_WHERE_CLAUSE;
-	result |= qualify_statement(select->where_expression, start_join, table_alias_stmt, 0, NULL);
+	if (NULL != ret) {
+		ret->ret_cla = NULL;
+		/* Note: Inherit ret.max_unique_id from caller (could be parent/outer query in case this is a sub-query) as is */
+	}
+	result |= qualify_statement(select->where_expression, start_join, table_alias_stmt, 0, ret);
 	// Qualify GROUP BY clause next
 	group_by_expression = select->group_by_expression;
 	/* Note that while table_alias->aggregate_function_or_group_by_specified will mostly be FALSE at this point, it is
@@ -184,6 +189,8 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 	ret_cla = NULL;
 	table_alias->aggregate_depth = 0;
 	for (;;) {
+		QualifyStatementParms lcl_ret;
+
 		assert(0 == table_alias->aggregate_depth);
 		// Qualify HAVING clause
 		result |= qualify_statement(select->having_expression, start_join, table_alias_stmt, 0, NULL);
@@ -192,9 +199,11 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 		// Qualify ORDER BY clause next
 		/* Now that all column names used in the query have been qualified, allow columns specified in
 		 * ORDER BY to be qualified against any column names specified till now without any strict checking.
-		 * Hence the use of a non-NULL value (`&ret_cla`) as the last parameter to `qualify_statement()` below.
+		 * Hence the use of a non-NULL value ("&ret_cla") for "lcl_ret->ret_cla".
 		 */
-		result |= qualify_statement(select->order_by_expression, start_join, table_alias_stmt, 0, &ret_cla);
+		lcl_ret.ret_cla = &ret_cla;
+		lcl_ret.max_unique_id = ((NULL != ret) ? ret->max_unique_id : NULL);
+		result |= qualify_statement(select->order_by_expression, start_join, table_alias_stmt, 0, &lcl_ret);
 		if (!table_alias->aggregate_function_or_group_by_specified) {
 			/* GROUP BY or AGGREGATE function was never used in the query. No need to do GROUP BY validation checks. */
 			break;

src/CMakeLists.txt
@@ -186,6 +186,7 @@ add_library(libocto OBJECT
   match_column_list_alias_in_select_column_list.c
   match_sql_statement.c
   memory_chunk.c
+  move_where_clause_to_on_clause.c
   octo_init.c
   optimization_transforms/generate_logical_plan.c
   optimization_transforms/get_new_plan_unique_id.c
@@ -226,6 +227,7 @@ add_library(libocto OBJECT
   optimization_transforms/optimize_logical_plan.c
   parse_line.c
   parse_startup_flags.c
+  parse_tree_optimize.c
   parser/sql_set_statement.c
   parser/aggregate_function.c
   parser/as_name.c
@@ -260,6 +262,7 @@ add_library(libocto OBJECT
   store_table_in_pg_class.c
   store_function_in_pg_proc.c
   tables_equal.c
+  traverse_where_clause.c
   values_equal.c
   ydb_error_check.c
   ydb_hash_to_string.c

src/copy_sql_statement.c
@@ -162,6 +162,7 @@ SqlStatement *copy_sql_statement(SqlStatement *stmt) {
 		do {
 			OCTO_CMALLOC_STRUCT(new_join, SqlJoin);
 			dqinit(new_join);
+			new_join->max_unique_id = cur_join->max_unique_id;
 			new_join->type = cur_join->type;
 			new_join->value = copy_sql_statement(cur_join->value);
 			new_join->condition = copy_sql_statement(cur_join->condition);

src/move_where_clause_to_on_clause.c
@@ -1,0 +1,90 @@
+/****************************************************************
+ *								*
+ * Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+
+#include <assert.h>
+
+#include "octo.h"
+
+/* This is an optimization function that tries to determine if it is safe to move a portion of the WHERE clause
+ * (whose address is passed in as "stmt_ptr") to the ON clause of some JOIN in the query. If so, it moves it.
+ * This can significantly reduce query runtime in case the query has lots of joins (the ON clause in one join
+ * noticeably reduces the number of iterations a nested join executes compared to the same condition in the
+ * WHERE clause which gets executed in the innermost loop for all nested join FOR loops) (YDBOcto#601).
+ */
+void move_where_clause_to_on_clause(SqlStatement **stmt_ptr, SqlJoin *start_join) {
+	int	      max_unique_id;
+	SqlJoin *     cur_join, *noted_join;
+	boolean_t     outer_join_seen, right_or_full_join_seen;
+	SqlStatement *stmt;
+
+	stmt = *stmt_ptr;
+	max_unique_id = stmt->hash_canonical_query_cycle;
+	cur_join = start_join;
+	outer_join_seen = FALSE;
+	right_or_full_join_seen = FALSE;
+	noted_join = NULL; /* This notes down the JOIN whose ON clause/condition will inherit the WHERE clause portion */
+	do {
+		enum SqlJoinType join_type;
+		SqlJoin *	 next_join;
+
+		next_join = cur_join->next;
+		join_type = cur_join->type;
+		if (IS_OUTER_JOIN(join_type)) {
+			outer_join_seen = TRUE;
+			if ((RIGHT_JOIN == join_type) || (FULL_JOIN == join_type)) {
+				right_or_full_join_seen = TRUE;
+			}
+		} else if ((NULL == noted_join) && (CROSS_JOIN != join_type) && (CROSS_JOIN != next_join->type)
+			   && (next_join != start_join) && (max_unique_id <= cur_join->max_unique_id)) {
+			/* CROSS JOIN does not currently expect an ON clause ("lp_optimize_cross_join()" relies on this).
+			 * Hence the "CROSS_JOIN != join_type" and "CROSS_JOIN != next_join->type" check above. Also, if the
+			 * join we are going to move the WHERE clause to is the last in the join list, we might as well keep
+			 * it inside the WHERE clause (no savings in runtime) hence the check for "next_join != start_join" above.
+			 * The "max_unique_id <= cur_join->max_unique_id" check is the key check. It confirms that it is safe
+			 * to move the WHERE clause to the ON clause because the part of the WHERE clause we are considering
+			 * moving has references to columns all of which correspond to tables that are known at the time the
+			 * JOIN is parsed in the query.
+			 */
+			noted_join
+			    = cur_join; /* Note: We still need to process rest of JOIN list to see if there is any OUTER JOIN */
+		}
+		cur_join = next_join;
+	} while (cur_join != start_join);
+	/* If we saw a RIGHT or FULL JOIN, the move from a WHERE clause to the ON clause cannot be safely done in all
+	 * cases (TOJ03 bats subtest generates random queries which will fail otherwise). But moving in the presence of
+	 * LEFT JOINs (the most frequently used outer join) are okay hence the check for "!right_or_full_join_seen" below.
+	 * If RIGHT/FULL JOIN cases become more common and need to be optimized, this code needs to be revisited.
+	 */
+	if ((NULL != noted_join) && !right_or_full_join_seen) {
+		/* Move operand of BOOLEAN_AND from WHERE clause to ON clause of this JOIN */
+		if (NULL != noted_join->condition) {
+			SqlStatement *join_condition;
+
+			SQL_STATEMENT(join_condition, binary_STATEMENT);
+			MALLOC_STATEMENT(join_condition, binary, SqlBinaryOperation);
+			join_condition->v.binary->operation = BOOLEAN_AND;
+			join_condition->v.binary->operands[0] = noted_join->condition;
+			join_condition->v.binary->operands[1] = stmt;
+			noted_join->condition = join_condition;
+		} else {
+			noted_join->condition = stmt;
+		}
+		/* If OUTER JOIN has been seen, then we cannot safely remove this from the WHERE clause
+		 * (might return incorrect results). Adding the same condition to the ON clause does help
+		 * speed up so we will do that but will also retain it in the WHERE clause.
+		 */
+		if (!outer_join_seen) {
+			*stmt_ptr = NULL;
+		}
+	}
+	return;
+}

src/octo.h
@@ -369,6 +369,8 @@ typedef enum RegexType {
 		input_buffer_combined[cur_input_index + QUERY_LENGTH + NEWLINE_NEEDED] = '\0';                     \
 	}
 
+#define IS_STMT_BOOLEAN_AND(STMT) ((binary_STATEMENT == STMT->type) && (BOOLEAN_AND == STMT->v.binary->operation))
+
 // Convenience type definition for run_query callback function
 typedef int (*callback_fnptr_t)(SqlStatement *, int, void *, char *, boolean_t);
 
@@ -402,8 +404,12 @@ SqlFunction * find_function(const char *function_name, const char *function_hash
 int	   drop_schema_from_local_cache(ydb_buffer_t *name_buffer, SqlSchemaType schema_type, ydb_buffer_t *function_hash_buffer);
 SqlColumn *find_column(char *column_name, SqlTable *table);
 SqlStatement *find_column_alias_name(SqlStatement *stmt);
-int	      qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTableAlias *parent_table_alias);
-int qualify_statement(SqlStatement *stmt, SqlJoin *tables, SqlStatement *table_alias_stmt, int depth, SqlColumnListAlias **ret_cla);
+void	      parse_tree_optimize(SqlSelectStatement *select);
+void	      move_where_clause_to_on_clause(SqlStatement **stmt_ptr, SqlJoin *start_join);
+SqlStatement *traverse_where_clause(SqlStatement *binary_stmt, SqlJoin *start_join);
+int	      qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTableAlias *parent_table_alias,
+			    QualifyStatementParms *ret);
+int qualify_statement(SqlStatement *stmt, SqlJoin *tables, SqlStatement *table_alias_stmt, int depth, QualifyStatementParms *ret);
 SqlColumnAlias *    qualify_column_name(SqlValue *column_value, SqlJoin *tables, SqlStatement *table_alias_stmt, int depth,
 					SqlColumnListAlias **ret_cla);
 SqlColumnListAlias *match_column_in_table(SqlTableAlias *table, char *column_name, int column_name_len, boolean_t *ambiguous,

src/octo_types.h
@@ -454,8 +454,18 @@ typedef struct SqlJoin {
 	struct SqlStatement *value;
 	// SqlValue
 	struct SqlStatement *condition;
+	enum SqlJoinType     type;
+	/* The below field stores the value of "config->plan_id" at the time this join was parsed in the query.
+	 * That gives us a picture of the maximum number of tables (and in turn table_alias->unique_id) that
+	 * were processed till now. To elaborate, since "config->plan_id" is a constantly increasing id that we
+	 * assign to tables encountered in the query as we parse it, this one number is a good idea of all table
+	 * ids that are valid as of the current JOIN. Any id that is greater than this is guaranteed to have been
+	 * encountered AFTER this JOIN and so any column reference corresponding to a table with that id cannot be
+	 * safely moved from the WHERE clause (where all column references are valid) to this JOIN ON clause (where
+	 * this column reference is not valid).
+	 */
+	int max_unique_id;
 	dqcreate(SqlJoin);
-	enum SqlJoinType type;
 } SqlJoin;
 
 /**
@@ -739,8 +749,22 @@ typedef struct SqlStatement {
 		 *	invalid_STATEMENT
 		 */
 	} v;
-	uint64_t hash_canonical_query_cycle; // used during "hash_canonical_query" to avoid
-					     // multiple traversals of same node.
+	/* The below is used during "hash_canonical_query" to avoid multiple traversals of same node.
+	 * Before "hash_canonical_query()" is reached, this field is used to store a 4-byte "max_unique_id"
+	 * if this SqlStatement structure falls inside the subtree of a WHERE or ON clause. In that case, it
+	 * represents the MAXIMUM value of "table_alias->unique_id" where "table_alias" is the table alias corresponding
+	 * to any/all column references inside that subtree. For example if the WHERE clause had "(n1.id = 3)" then
+	 * this field will represent the "unique_id" corresponding to the table_alias for "n1". This is used to compare
+	 * against "SqlJoin.max_unique_id" field to see if a subtree of the WHERE clause can be moved over to the
+	 * ON clause of the JOIN.
+	 */
+	uint64_t hash_canonical_query_cycle;
 } SqlStatement;
+
+/* The below is used by qualify_statement.c */
+typedef struct {
+	SqlColumnListAlias **ret_cla;
+	int *		     max_unique_id;
+} QualifyStatementParms;
 
 #endif

src/optimization_transforms/generate_logical_plan.c
@@ -107,13 +107,7 @@ LogicalPlan *generate_logical_plan(SqlStatement *stmt) {
 		if (cur_join->condition) {
 			MALLOC_LP_2ARGS(t_join_condition, LP_WHERE);
 			LP_GENERATE_WHERE(cur_join->condition, stmt, t_join_condition->v.lp_default.operand[0], error_encountered);
-			if (num_outer_joins) {
-				join_right->extra_detail.lp_table_join.join_on_condition = t_join_condition;
-			} else { /* No OUTER JOINs. We can safely add the ON clause in the join condition to the
-				  * WHERE clause without risk of correctness issues.
-				  */
-				start_join_condition = lp_join_where(start_join_condition, t_join_condition);
-			}
+			join_right->extra_detail.lp_table_join.join_on_condition = t_join_condition;
 		}
 		cur_join = cur_join->next;
 	} while (cur_join != start_join);

src/optimization_transforms/lp_make_normal_disjunctive_form.c
@@ -157,19 +157,19 @@ LogicalPlan *lp_make_normal_disjunctive_form(LogicalPlan *root) {
 	LogicalPlan *left = lp_make_normal_disjunctive_form(root->v.lp_default.operand[0]);
 	LogicalPlan *right = lp_make_normal_disjunctive_form(root->v.lp_default.operand[1]);
 
+	// If this case is an OR, we don't need to combine things, so return early
+	if (LP_BOOLEAN_OR == root->type) {
+		root->v.lp_default.operand[0] = left;
+		root->v.lp_default.operand[1] = right;
+		return root;
+	}
+	assert(LP_BOOLEAN_AND == root->type);
 	// Algorithm:
 	// 1. Process left and right children into normal form
 	// 2. For each each clause on the left:
 	//  2.1 For each clause on the right:
 	//   2.1.1 If clause on left is OR, and clause on right is OR, construct new AND of left and right
 	//   2.1.2 If clause of left is AND, and clause on right is OR, join with
-
-	// If this case is not an AND, we don't need to combine things, so return early
-	if (LP_BOOLEAN_AND != root->type) {
-		root->v.lp_default.operand[0] = left;
-		root->v.lp_default.operand[1] = right;
-		return root;
-	}
 
 	// Else, walk through the left list, right list, and form a new series
 	LogicalPlan *ret = NULL, *cur = NULL;

commit a2e01bd4c869bce2416a478de75dcb481ead4149

[#850] Fix incorrect result for query with any JOIN being done in a subquery of HAVING clause
Background
----------
* The below is pasted from https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/850#note_1025637531.

* Below is a simple query that runs fine with EXISTS inside the WHERE clause (1st query below)
  but does not when the same EXISTS is inside the HAVING clause (2nd query). Both queries are
  expected to return 1 row containing the value 1.

  ```sql
  OCTO> SELECT 1 WHERE  EXISTS(SELECT 1 FROM names INNER JOIN (SELECT id FROM names) t2 ON TRUE WHERE t2.id = 2);
  ???
  1
  (1 row)
  OCTO> SELECT 1 HAVING EXISTS(SELECT 1 FROM names INNER JOIN (SELECT id FROM names) t2 ON TRUE WHERE t2.id = 2);
  ???
  (0 rows)
  ```

Issue
-----
* As part of 1ac5f501, the `HAVING` clause processing was changed in `qualify_query.c` to invoke
  `qualify_statement()` twice. As described in a comment in `qualify_query.c`, this second iteration
  was necessary to perform expression matching inside the HAVING clause.

* Doing the qualification twice is okay but in the second iteration, we skip processing parts of the
  HAVING clause under certain conditions (see SET_GROUP_BY_EXPRESSION_COLUMN_NUMBER_AND_BREAK in
  `qualify_statement.c`) to serve as an optimization.

* But this created a subtle issue with a pre-existing optimization introduced in b4fd3618 (which moved
  parts of WHERE clause to the ON clause using `src/move_where_clause_to_on_clause.c`).

* That optimization relied on examining `ALL` parts of the `WHERE` clause to ensure we have an idea of
  the column references encountered and whether a part of the WHERE clause can be moved into the ON
  condition (based on the `unique_id` of the table_aliases encountered in the WHERE clause and coming
  up with a `max_unique_id` value).

* Examining only parts of the WHERE clause caused this `max_unique_id` computation to be much lower

src/qualify_query.c
@@ -317,14 +317,15 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 	} while (cla_cur != cla_head);
 
 	SqlColumnListAlias *  ret_cla;
-	QualifyStatementParms lcl_ret;
+	QualifyStatementParms lcl_ret, *lcl_ret_ptr;
 	ret_cla = NULL;
-	/* Initialize "lcl_ret->ret_cla" to a non-NULL value (&ret_cla) and pass "&lcl_ret" in case of qualifying ORDER BY and GROUP
-	 * BY. This allows columns specified in an these clauses to be qualified against any column names specified till now
-	 * (including aliases specified after an AS) without any strict checking.
+	/* Initialize "lcl_ret->ret_cla" to a non-NULL value (&ret_cla) and pass "&lcl_ret" (through the "lcl_ret_ptr" variable)
+	 * in case of qualifying ORDER BY and GROUP BY. This allows columns specified in an these clauses to be qualified against
+	 * any column names specified till now (including aliases specified after an AS) without any strict checking.
 	 */
 	lcl_ret.ret_cla = &ret_cla;
 	lcl_ret.max_unique_id = ((NULL != ret) ? ret->max_unique_id : NULL);
+	lcl_ret_ptr = &lcl_ret;
 	/* We qualify SELECT, HAVING and ORDER BY clauses TWICE below. Hence the "for" loop.
 	 * This is because
 	 * 1) In case of SELECT, HAVING and ORDER BY we want to check if any aggregate functions were used anywhere even if a
@@ -398,7 +399,7 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 				 * o.CustomerID GROUP BY c.CustomerID;
 				 */
 				gb_result = 0;
-				gb_result |= qualify_statement(group_by_expression, start_join, table_alias_stmt, 0, &lcl_ret);
+				gb_result |= qualify_statement(group_by_expression, start_join, table_alias_stmt, 0, lcl_ret_ptr);
 				ret_cla = NULL; // Re-set the variable so that ORDER BY can re-use it
 				// Ensure error is forwarded to result as that is the one returned at the end of this function
 				result |= gb_result;
@@ -504,10 +505,10 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 			result |= qualify_statement(select->having_expression, start_join, table_alias_stmt, 0, ret);
 			table_alias->aggregate_function_or_group_by_or_having_specified |= HAVING_SPECIFIED;
 		}
-		// Qualify ORDER BY clause next (see comment above for why "&lcl_ret" is passed for ORDER BY).
+		// Qualify ORDER BY clause next (see comment above for why "lcl_ret_ptr" is passed for ORDER BY).
 		table_alias->qualify_query_stage = QualifyQuery_ORDER_BY;
 		table_alias->aggregate_depth = 0;
-		result |= qualify_statement(select->order_by_expression, start_join, table_alias_stmt, 0, &lcl_ret);
+		result |= qualify_statement(select->order_by_expression, start_join, table_alias_stmt, 0, lcl_ret_ptr);
 		/* Expansion of table.* in ORDER BY is done here so that expansion can be skipped when GROUP BY/HAVING/Aggregate
 		 * exists. At this point we can safely rely on `aggregate_function_or_group_by_or_having_specified` to indicate if
 		 * the entire query has any form of grouping or not.
@@ -568,6 +569,16 @@ int qualify_query(SqlStatement *table_alias_stmt, SqlJoin *parent_join, SqlTable
 		}
 		/* GROUP BY or AGGREGATE function or HAVING was used in the query. Assign GROUP BY number and validate */
 		table_alias->do_group_by_checks = TRUE;
+		/* Disable "max_unique_id" computation (using "stmt->hash_canonical_query_cycle") in "qualify_statement()"
+		 * in the 2nd iteration of this for loop as that takes some shortcuts using the
+		 * SET_GROUP_BY_EXPRESSION_COLUMN_NUMBER_AND_BREAK macro in "qualify_statement.c" and that could result
+		 * in incorrect values of "*ret->max_unique_id" which would then break the "move_where_clause_to_on_clause()"
+		 * optimization and cause incorrect move of WHERE clause conditions to the ON clause. This is achieved by
+		 * setting the below 2 variables (the only choices possible as the last parameter to "qualify_statement()"
+		 * calls in this for loop) to NULL (YDBOcto#850).
+		 */
+		lcl_ret_ptr = NULL;
+		ret = NULL;
 	}
 	table_alias->do_group_by_checks = FALSE;
 

tests/fixtures/TGB31.sql
@@ -1,0 +1,25 @@
+#################################################################
+#								#
+# Copyright (c) 2022 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+-- Below test queries are from https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/850#description
+SELECT 1 FROM names x HAVING EXISTS(SELECT t1.id, MAX(t1.lastName) FROM names t1 LEFT JOIN (SELECT lastName FROM names group by lastname) AS t2 ON (t1.firstName <= 'Acid') WHERE t1.id <= 1 and t2.lastName IS NULL GROUP BY t2.*, t1.id having 2 IN (SELECT t1.id FROM names y ORDER BY t2.*,t1.id limit 1));
+-- Below 2 queries are commented out because they need YDBOcto#870 to also be fixed (not just YDBOcto#850).
+-- SELECT 1 FROM names x HAVING EXISTS(SELECT t1.id, MAX(t1.lastName) FROM names t1 LEFT JOIN (SELECT lastName FROM names group by lastname) AS t2 ON (t1.firstName <= 'Acid') WHERE t1.id <= 1 and t2.lastName IS NULL GROUP BY t2.*, t1.id having 0 IN (SELECT t1.id FROM names y ORDER BY t2.*,t1.id limit 1));
+-- SELECT 1 FROM names x HAVING EXISTS(SELECT t1.id, MAX(t1.lastName) FROM names t1 LEFT JOIN (SELECT lastName FROM names group by lastname) AS t2 ON (t1.firstName <= 'Acid') WHERE t1.id <= 1 and t2.lastName IS NULL GROUP BY t2.*, t1.id having 1 IN (SELECT t1.id FROM names y ORDER BY t2.*,t1.id limit 1));
+
+-- Below test queries are simplified versions of the above (from https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/850#description)
+SELECT 1 FROM names x HAVING EXISTS(SELECT 1 FROM names t1 LEFT JOIN (SELECT lastName FROM names group by lastname) AS t2 ON (t1.firstName <= 'Acid') WHERE t2.lastName IS NULL);
+SELECT 1 FROM names x HAVING EXISTS(SELECT 1 FROM names t1 INNER JOIN (SELECT lastName FROM names group by lastname) AS t2 ON (t1.firstName <= 'Acid') WHERE t2.lastName IS NULL);
+
+-- Below test queries are from https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/850#note_1025637531
+SELECT 1 WHERE  EXISTS(SELECT 1 FROM names INNER JOIN (SELECT id FROM names) t2 ON TRUE WHERE t2.id = 2);
+

tests/test_group_by.bats.in
@@ -199,4 +199,7 @@ setup() {
 	verify_output TGB30_errors output.txt
 }
 
+@test "TGB31 : OCTO850 : Test query with any JOIN being done in a subquery of HAVING clause returns correct results" {
+	run_query_in_octo_and_postgres_and_crosscheck_multiple_queries names TGB31.sql
+}
 
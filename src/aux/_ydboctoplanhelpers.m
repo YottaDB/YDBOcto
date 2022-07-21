7aa8156fc1b506db970c4a6a8b6f80d349fb27e3
Sam Habiel <sam@yottadb.com>
2022-07-21 10:58:17 -0400

    Revert "[VALGRIND] Add missing regfree in ^%ydboctoplanhelpers"
    This reverts commit a56525775ae874b0febf571598bf28e377c70513.
    
    We found out that implementing this causes a 2x slowdown on a simple
    RegEx query (`WHERE NAME LIKE 'IBU%'`). So this commit will be discarded
    as the memory leak is not big and is not important enough to justify the
    slowdown.
    
    Tracked in issue https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/873.

--- src/aux/_ydboctoplanhelpers.m
+++ src/aux/_ydboctoplanhelpers.m
@@ -844,12 +844,10 @@ regexmatch(str,regexstr,regextype,regexflags)
 	SET pvrs=$GET(%ydboctoregex("result"))
 	SET pvpln=$GET(%ydboctoregex("plan"))
 	IF ((regexstr=pvstr)&(regextype=pvpln)) DO  QUIT ret
-	. IF ("error"=pvrs) DO throwregexerr(regexstr) QUIT
-	. IF (".*"=pvrs) SET ret=1 QUIT
-	. ;
-	. IF (0=$DATA(regexflags)) SET ret=$$regmatch^%ydbposix(str,pvrs) IF 1
+	. IF ("error"=pvrs) DO throwregexerr(regexstr)
+	. ELSE  IF (".*"=pvrs) SET ret=1
+	. ELSE  IF (0=$DATA(regexflags)) SET ret=$$regmatch^%ydbposix(str,pvrs)
 	. ELSE  SET ret=$$regmatch^%ydbposix(str,pvrs,regexflags)
-	. IF ret>=0 DO regfree(pvrs,.regexflags) ; Negative values means pattern didn't compile, no memory to free
 	SET %ydboctoregex("regexstr")=regexstr
 	SET %ydboctoregex("plan")=regextype
 	DO regexinit
@@ -868,14 +866,9 @@ regexmatch(str,regexstr,regextype,regexflags)
 	. IF (".*"=result) SET %ydboctoregex("result")=result,ret=1
 	QUIT:1=ret 1
 	SET %ydboctoregex("result")=result
-	IF (0=$DATA(regexflags)) SET ret=$$regmatch^%ydbposix(str,result) IF 1
+	IF (0=$DATA(regexflags)) SET ret=$$regmatch^%ydbposix(str,result)
 	ELSE  SET ret=$$regmatch^%ydbposix(str,result,regexflags)
-	IF ret>=0 DO regfree(result,.regexflags) ; Negative values means pattern didn't compile, no memory to free
 	QUIT ret
-
-regfree:(regex,regexflags) ; Private call to free RegEx memory
-	DO regfree^%ydbposix($NAME(%ydbposix("regmatch",regex,$GET(regexflags,0))))
-	QUIT
 
 Cast2VARCHAR(string,size)
 	; This function helps implement the typecast operator where the target type is VARCHAR

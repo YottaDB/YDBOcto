YottaDB/YDBOcto

commit 3831a9bb3819cc01d248fadd7479672b2f7eed17

[#772] \d tablename now displays CHECK constraints (if defined)
* 74f64658 implemented `\d tablename` functionality.

* This commit enhances that functionality to display CHECK constraints if any exist for the table.
  The output format is kept as close to that of `psql` as possible.

* The code changes are in `src/describe_tablename.c`.

* The existing `test_constraint_table_column/TCTC005` bats subtest has been enhanced to verify
  that `\d tablename` lists the CHECK constraints too.

src/describe_tablename.c
@@ -45,7 +45,10 @@ int describe_tablename(SqlStatement *table_name) {
 		return -1;
 	}
 	UNPACK_SQL_STATEMENT(value, table->tableName, value);
-	/* The below output is more or less the same as what \d tablename outputs at the psql prompt */
+
+	/* Note: The below output is more or less the same as what \d tablename outputs at the psql prompt */
+
+	/* First output Column names, types etc. */
 	fprintf(stdout, "Table \"%s\"\n", value->v.reference);
 	fprintf(stdout, "Column|Type|Collation|Nullable|Default\n");
 	UNPACK_SQL_STATEMENT(start_column, table->columns, column);
@@ -75,6 +78,52 @@ int describe_tablename(SqlStatement *table_name) {
 			/* fprintf(stdout, ""); "Default" column is empty till YDBOcto#555 is implemented hence commented */
 			fprintf(stdout, "\n");
 		}
+		cur_column = cur_column->next;
+	} while (cur_column != start_column);
+
+	/* Next output constraints (if any) */
+	boolean_t first_check_constraint;
+	char *	  buffer, *buffer_orig, **buff_ptr;
+	int	  buffer_size, status;
+
+	first_check_constraint = TRUE;
+	cur_column = start_column;
+	do {
+		SqlOptionalKeyword *cur_keyword, *start_keyword;
+
+		UNPACK_SQL_STATEMENT(start_keyword, cur_column->keywords, keyword);
+		cur_keyword = start_keyword;
+		do {
+			switch (cur_keyword->keyword) {
+			case OPTIONAL_CHECK_CONSTRAINT:;
+				if (first_check_constraint) {
+					fprintf(stdout, "Check constraints:\n");
+					buffer_size = OCTO_INIT_BUFFER_LEN;
+					buffer = (char *)malloc(sizeof(char) * buffer_size);
+					buffer_orig = buffer;
+					first_check_constraint = FALSE;
+				}
+
+				SqlConstraint *constraint;
+				SqlValue *     value;
+				UNPACK_SQL_STATEMENT(constraint, cur_keyword->v, constraint);
+				UNPACK_SQL_STATEMENT(value, constraint->name, value);
+				fprintf(stdout, "    \"%s\" CHECK (", value->v.string_literal);
+				buffer = buffer_orig;
+				buff_ptr = &buffer;
+				status = emit_check_constraint(&buffer, &buffer_size, buff_ptr, constraint->definition);
+				if (0 > status) {
+					assert(FALSE);
+					free(buffer);
+					return -1;
+				}
+				fprintf(stdout, "%s)\n", buffer_orig);
+				break;
+			default:
+				break;
+			}
+			cur_keyword = cur_keyword->next;
+		} while (cur_keyword != start_keyword);
 		cur_column = cur_column->next;
 	} while (cur_column != start_column);
 	return 0;

tests/fixtures/TCTC005.sql
@@ -57,3 +57,16 @@ create table tmp28 (id integer, check(id not in (2,3)));
 -- -- create table tmp39 (id integer, check(id <= all (2,3)));
 -- -- create table tmp40 (id integer, check(id >= all (2,3)));
 
+-- Test CHECK column constraint using a column ("price" below) that is going to be defined in the table in a later line
+create table tmp29 (product_no integer CHECK (price > product_no), name text, price numeric);
+
+-- Test that CONSTRAINT name is accepted but ignored for NOT NULL (proved below because same constraint name is accepted
+-- for a different constraint without errors like it normally would if it was a real constraint name collision).
+create table tmp30 (id integer unique, name varchar constraint name1 not null, firstname varchar constraint name1 check (firstname is not NULL));
+
+-- Test that \d tablename in Octo matches that in Postgres for the below test case
+-- This is a case where the constraint name is derived from an empty column name.
+-- At one point, Octo used to create a constraint named PRODUCTS__CHECK whereas we expect it to be PRODUCTS_CHECK
+-- (i.e. no double underscore).
+create table tmp31 (id integer, check (1 + 2 = 3), check (2 + 3 = 5));
+

tests/outref/TCTC005.ref
@@ -1,3 +1,4 @@
+# Run CREATE TABLE commands in TCTC005.sql that tests various types of CHECK constraints
 
 -- TCTC005 : OCTO772 : Test CHECK constraint shows up correctly in CREATE TABLE text definition
 -- Test all possible CHECK constraint expressions (i.e. all "case" code paths in "src/emit_check_constraint.c")
@@ -46,6 +47,19 @@ create table tmp28 (id integer, check(id not in (2,3)));
 -- -- create table tmp39 (id integer, check(id <= all (2,3)));
 -- -- create table tmp40 (id integer, check(id >= all (2,3)));
 
+-- Test CHECK column constraint using a column ("price" below) that is going to be defined in the table in a later line
+create table tmp29 (product_no integer CHECK (price > product_no), name text, price numeric);
+
+-- Test that CONSTRAINT name is accepted but ignored for NOT NULL (proved below because same constraint name is accepted
+-- for a different constraint without errors like it normally would if it was a real constraint name collision).
+create table tmp30 (id integer unique, name varchar constraint name1 not null, firstname varchar constraint name1 check (firstname is not NULL));
+
+-- Test that \d tablename in Octo matches that in Postgres for the below test case
+-- This is a case where the constraint name is derived from an empty column name.
+-- At one point, Octo used to create a constraint named PRODUCTS__CHECK whereas we expect it to be PRODUCTS_CHECK
+-- (i.e. no double underscore).
+create table tmp31 (id integer, check (1 + 2 = 3), check (2 + 3 = 5));
+
 CREATE TABLE
 CREATE TABLE
 CREATE TABLE
@@ -74,6 +88,10 @@ CREATE TABLE
 CREATE TABLE
 CREATE TABLE
 CREATE TABLE
+CREATE TABLE
+CREATE TABLE
+CREATE TABLE
+# Generate and Run query file select.sql that does a SELECT * of all tables created in TCTC005.sql
 ID
 (0 rows)
 ID
@@ -130,6 +148,13 @@ ID
 (0 rows)
 ID
 (0 rows)
+PRODUCT_NO|NAME|PRICE
+(0 rows)
+ID|NAME|FIRSTNAME
+(0 rows)
+ID
+(0 rows)
+# Verify CHECK constraints are correctly stored in the CREATE TABLE text definition (in global variable nodes)
 `TMP01` (`ID` INTEGER DELIM """",  CONSTRAINT TMP01_CHECK CHECK (((TRUE IS UNKNOWN) AND (FALSE IS NULL))))
 `TMP02` (`ID` INTEGER CONSTRAINT TMP02_ID_CHECK CHECK (((('abcd' != 'efgh') AND (1 <= ID)) AND (1.5 > ABS((- 2.35))))) DELIM """")
 `TMP03` (`ID` INTEGER DELIM """",  CONSTRAINT TMP03_CHECK CHECK ((NOT (NULLIF(1.0,1) IS NOT NULL))))
@@ -158,3 +183,177 @@ ID
 `TMP26` (`LASTNAME` VARCHAR CONSTRAINT TMP26_LASTNAME_CHECK CHECK (((LASTNAME SIMILAR TO 'ab%cd') AND (NOT (LASTNAME SIMILAR TO 'ab%cd')))) DELIM """")
 `TMP27` (`ID` INTEGER CONSTRAINT TMP27_ID_CHECK CHECK ((ID IN (2,3))) DELIM """")
 `TMP28` (`ID` INTEGER CONSTRAINT TMP28_ID_CHECK CHECK ((ID NOT IN (2,3))) DELIM """")
+`TMP29` (`PRODUCT_NO` INTEGER PIECE 1, `NAME` VARCHAR PIECE 2, `PRICE` NUMERIC PIECE 3,  CONSTRAINT TMP29_CHECK CHECK ((PRICE > PRODUCT_NO)))
+`TMP30` (`ID` INTEGER UNIQUE PIECE 1, `NAME` VARCHAR NOT NULL PIECE 2, `FIRSTNAME` VARCHAR CONSTRAINT NAME1 CHECK ((FIRSTNAME IS NOT NULL)) PIECE 3)
+`TMP31` (`ID` INTEGER DELIM """",  CONSTRAINT TMP31_CHECK CHECK (((1 + 2) = 3)),  CONSTRAINT TMP31_CHECK1 CHECK (((2 + 3) = 5)))
+# Generate and Run query file slashd.sql that does a \d of all tables created in TCTC005.sql
+# Also verify that constraint names are created correctly
+# 1) Constraints that use only one column in the table end up as column-level constraints
+#    and have that column name in their name.
+# 2) Constraints that use 0 or more than 1 column in the table end up as table-level constraints
+#    and have no column name in their name.
+Table "TMP01"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP01_CHECK" CHECK (((TRUE IS UNKNOWN) AND (FALSE IS NULL)))
+Table "TMP02"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP02_ID_CHECK" CHECK (((('abcd' != 'efgh') AND (1 <= ID)) AND (1.5 > ABS((- 2.35)))))
+Table "TMP03"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP03_CHECK" CHECK ((NOT (NULLIF(1.0,1) IS NOT NULL)))
+Table "TMP04"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+FIRSTNAME|VARCHAR|||
+Check constraints:
+    "TMP04_FIRSTNAME_CHECK" CHECK ((GREATEST(FIRSTNAME,'abcd',NULL,'zzz') IS NULL))
+Table "TMP05"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+FIRSTNAME|VARCHAR|||
+LASTNAME|VARCHAR|||
+Check constraints:
+    "TMP05_CHECK" CHECK ((LEAST((FIRSTNAME || 'efgh'),LASTNAME,(FIRSTNAME || LASTNAME)) IS NULL))
+Table "TMP06"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+LASTNAME|VARCHAR|||
+Check constraints:
+    "TMP06_LASTNAME_CHECK" CHECK ((COALESCE(NULL,LASTNAME) IS NULL))
+Table "TMP07"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+LASTNAME|VARCHAR|||
+Check constraints:
+    "TMP07_CHECK" CHECK ((NULLIF(NULL,2) IS NULL))
+Table "TMP08"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP08_ID_CHECK" CHECK (CASE ID WHEN 2 THEN TRUE END)
+Table "TMP09"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP09_ID_CHECK" CHECK (CASE ID WHEN 2 THEN TRUE ELSE FALSE END)
+Table "TMP10"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP10_ID_CHECK" CHECK (CASE WHEN (ID IS NULL) THEN TRUE END)
+Table "TMP11"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP11_ID_CHECK" CHECK (CASE WHEN (ID IS NULL) THEN TRUE ELSE FALSE END)
+Table "TMP12"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP12_ID_CHECK" CHECK (ID::BOOLEAN)
+Table "TMP13"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP13_ID_CHECK" CHECK ((ID::NUMERIC(3) = 3.0))
+Table "TMP14"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP14_ID_CHECK" CHECK ((ID::NUMERIC(4,1) != 4.5))
+Table "TMP15"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP15_ID_CHECK" CHECK ((ID::INTEGER < 4))
+Table "TMP16"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP16_ID_CHECK" CHECK ((ID::VARCHAR(10) > 'abcd'))
+Table "TMP17"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP17_ID_CHECK" CHECK (((+ ID) >= (- 2)))
+Table "TMP18"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP18_ID_CHECK" CHECK ((NOT ID::BOOLEAN))
+Table "TMP19"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP19_ID_CHECK" CHECK ((((ID + (ID % 3)) - (ID * 2)) <= (ID / 20)))
+Table "TMP20"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP20_ID_CHECK" CHECK ((ID::BOOLEAN OR (NOT ID::BOOLEAN)))
+Table "TMP21"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP21_ID_CHECK" CHECK ((ID::BOOLEAN AND (NOT ID::BOOLEAN)))
+Table "TMP22"
+Column|Type|Collation|Nullable|Default
+LASTNAME|VARCHAR|||
+Check constraints:
+    "TMP22_LASTNAME_CHECK" CHECK ((LASTNAME ~ 'abcd'))
+Table "TMP23"
+Column|Type|Collation|Nullable|Default
+LASTNAME|VARCHAR|||
+Check constraints:
+    "TMP23_LASTNAME_CHECK" CHECK ((LASTNAME ~* 'abcd'))
+Table "TMP24"
+Column|Type|Collation|Nullable|Default
+LASTNAME|VARCHAR|||
+Check constraints:
+    "TMP24_LASTNAME_CHECK" CHECK (((LASTNAME LIKE 'abcd%') OR (NOT (LASTNAME LIKE 'abcd%'))))
+Table "TMP25"
+Column|Type|Collation|Nullable|Default
+LASTNAME|VARCHAR|||
+Check constraints:
+    "TMP25_LASTNAME_CHECK" CHECK (((LASTNAME ILIKE 'ab%cd') OR (NOT (LASTNAME ILIKE 'ab%cd'))))
+Table "TMP26"
+Column|Type|Collation|Nullable|Default
+LASTNAME|VARCHAR|||
+Check constraints:
+    "TMP26_LASTNAME_CHECK" CHECK (((LASTNAME SIMILAR TO 'ab%cd') AND (NOT (LASTNAME SIMILAR TO 'ab%cd'))))
+Table "TMP27"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP27_ID_CHECK" CHECK ((ID IN (2,3)))
+Table "TMP28"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP28_ID_CHECK" CHECK ((ID NOT IN (2,3)))
+Table "TMP29"
+Column|Type|Collation|Nullable|Default
+PRODUCT_NO|INTEGER|||
+NAME|VARCHAR|||
+PRICE|NUMERIC|||
+Check constraints:
+    "TMP29_CHECK" CHECK ((PRICE > PRODUCT_NO))
+Table "TMP30"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+NAME|VARCHAR||NOT NULL|
+FIRSTNAME|VARCHAR|||
+Check constraints:
+    "NAME1" CHECK ((FIRSTNAME IS NOT NULL))
+Table "TMP31"
+Column|Type|Collation|Nullable|Default
+ID|INTEGER|||
+Check constraints:
+    "TMP31_CHECK" CHECK (((1 + 2) = 3))
+    "TMP31_CHECK1" CHECK (((2 + 3) = 5))

tests/test_constraint_table_column.bats.in
@@ -60,14 +60,22 @@ setup() {
 	skip	# TODO : YDBOcto#772 : Remove this line once CHECK CONSTRAINT functionality is fully supported
 	testname="TCTC005"
 	cp @PROJECT_SOURCE_DIR@/tests/fixtures/$testname.sql .
-	grep '^create table' $testname.sql | awk '{printf "select * from %s;\n", $3}' >> select.sql
-	echo "# Run lots of CREATE TABLE commands that test various syntax for CHECK constraints"
+	echo "# Run CREATE TABLE commands in $testname.sql that tests various types of CHECK constraints" >> output.txt
 	load_fixture $testname.sql subtest novv
-	echo "# Execute SELECT * queries for all the tables created above"
+	echo "# Generate and Run query file select.sql that does a SELECT * of all tables created in $testname.sql" >> output.txt
+	grep '^create table' $testname.sql | awk '{printf "select * from %s;\n", $3}' >> select.sql
 	octo -f select.sql >> output.txt
-	echo "# Verify CHECK constraints are correctly stored in the CREATE TABLE text definition (in global variable nodes)"
+	echo "# Verify CHECK constraints are correctly stored in the CREATE TABLE text definition (in global variable nodes)" >> output.txt
 	$ydb_dist/mupip extract $testname.ext
 	grep -w "text" $testname.ext | grep TMP | sed 's/ GLOBAL .*//g;s/.*CREATE TABLE //g;' >> output.txt
+	echo "# Generate and Run query file slashd.sql that does a \d of all tables created in $testname.sql" >> output.txt
+	echo "# Also verify that constraint names are created correctly" >> output.txt
+	echo "# 1) Constraints that use only one column in the table end up as column-level constraints" >> output.txt
+	echo "#    and have that column name in their name." >> output.txt
+	echo "# 2) Constraints that use 0 or more than 1 column in the table end up as table-level constraints" >> output.txt
+	echo "#    and have no column name in their name." >> output.txt
+	grep '^create table' $testname.sql | awk '{printf "\\d %s;\n", $3}' >> slashd.sql
+	octo -f slashd.sql >> output.txt
 	verify_output $testname output.txt
 }
 

commit f0d51dd180d3f95c3d181320ebfde1692b68eb44

[#772] Fix memory leak in \d tablename when CHECK constraints are defined
Background
----------
* This is a pipeline failure that we noticed in the `asan-ubuntu` and `asan-rocky` pipeline jobs
  where the `test_constraint_table_column/TCTC005` and `test_constraint_table_column/TCTC006` subtests
  failed.

* Both failures are similar. The `TCTC006` failure log is pasted below as it is smaller.

  ```c
  $ cat output.txt
  .
  .
  Direct leak of 7168 byte(s) in 7 object(s) allocated from:
      #0 0x7fce4371f808 in __interceptor_malloc ../../../../src/libsanitizer/asan/asan_malloc_linux.cc:144
      #1 0x559d8114c37c in describe_tablename ../src/describe_tablename.c:102
      #2 0x559d81255370 in run_query ../src/run_query.c:228
      #3 0x559d8138f51b in main ../src/octo.c:197
      #4 0x7fce42b31082 in __libc_start_main (/lib/x86_64-linux-gnu/libc.so.6+0x24082)
  ```

Issue
-----
* We do a `malloc()` in `describe_tablename.c` but never do a `free()` of that buffer in the normal case
  (we do a `free()` in an error code path).

Fix
---
* The fix is to do the `free()` of the malloced buffer in the normal code path too.

Note
----
* While the issue was introduced in 3831a9bb (merged May 27, 2022), it did not get noticed till today
  because the `CHECK` constraint code was enabled only yesterday (merged Aug 10, 2022) as part of 4a37a642.
  And that was needed in order to exercise the memory-leak code path in `describe_tablename.c`.

src/describe_tablename.c
@@ -86,6 +86,7 @@ int describe_tablename(SqlStatement *table_name) {
 	char *	  buffer, *buffer_orig, **buff_ptr;
 	int	  buffer_size, status;
 
+	buffer_orig = NULL;
 	first_check_constraint = TRUE;
 	cur_column = start_column;
 	do {
@@ -130,5 +131,8 @@ int describe_tablename(SqlStatement *table_name) {
 		} while (cur_keyword != start_keyword);
 		cur_column = cur_column->next;
 	} while (cur_column != start_column);
+	if (NULL != buffer_orig) {
+		free(buffer_orig);
+	}
 	return 0;
 }
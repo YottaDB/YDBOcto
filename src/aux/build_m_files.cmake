a0e366f84780aae6eff8df14c8be88140d4ca0f5
Sam Habiel <sam@yottadb.com>
2022-08-24 14:46:14 -0400

    [YottaDB/Tools/YDBCMake#5] Implement YDBCMake#5 changes + misc fixes
    - Grab latest version of YDBCMake, which provides `add_ydb_library`,
      `install_ydb_library`, and `install_ydb_sources`.
    - Create `src/aux/build_m_files.cmake` that implements the compilation
      of M sources per the scheme in YDBCMake#5. The file is structure so
      that it can be used with `add_subdirectory()`, but using that will add a
      extra directory `aux` to our $ydb_routines, and we don't want to
      change our scripts for this. Therefore, we `include()` the file so
      that the target get created in the `src` directory. The objects are
      `_ydboctoM` and `_ydboctoutf8`.
    - Remove the now unneeded UTF-8 logicl in `src/CMakeLists.txt' for
      compiling UTF-8 objects, as that is taken care of in YDBCMake#5.
    - Removed unused CMake variables
    	- OCTO_TOOLS_DIR
    	- MAX_EXPRESSION_LENGTH
    	- MAX_KEY_COUNT
    	- ENABLE_INSTALL_TESTS (this is usable, but was not documented
    	  and used anywhere, so it was removed)
    - Set C Standard to C11 globally, not library by library.
    - Rename `libcocto` to `cocto`. lib is automatically added by CMake and
      the C toolchain. We were actively suppressing it.
    - Per a comment that is now removed, use `add_compile_definitions`
      instead of `add_definitions` when all platforms can support CMake
      3.12. Our minimum version now is 3.14.

---
+++ src/aux/build_m_files.cmake
@@ -0,0 +1,46 @@
+#################################################################
+#								#
+# Copyright (c) 2022 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+# This file has to be used with include(), instead of the preferred
+# add_subdirectory(), because we want to keep the created objects in the same
+# directory (src) that is including this file.
+#
+# Create a shared library of M routines
+set(aux_files
+  aux/_ydboctoAdmin.m
+  aux/_ydboctoCleanup.m
+  aux/_ydboctoDiscard.m
+  aux/_ydboctofCONCAT.m
+  aux/_ydboctofCURRENTTIME.m
+  aux/_ydboctofCURRENTTIMESTAMP.m
+  aux/_ydboctofDATEFORMAT.m
+  aux/_ydboctofLOCALTIME.m
+  aux/_ydboctofLPAD.m
+  aux/_ydboctofTRUNCATE.m
+  aux/_ydboctoInit.m
+  aux/_ydboctoUtils.m
+  aux/_ydboctopgfunctions.m
+  aux/_ydboctoZinterrupt.m
+  aux/_ydboctoplanhelpers.m
+  aux/_ydboctoselect.m
+  aux/_ydboctosqlfunctions.m
+  aux/_ydboctoxrefupdate.m
+)
+
+add_ydb_library(_ydbocto SOURCES ${aux_files})
+add_dependencies(_ydboctoM _ydboctoutf8 check_git)
+
+if(NOT ${DISABLE_INSTALL})
+	# Library
+	install_ydb_library(_ydbocto)
+	# M Routines
+	install_ydb_sources(SOURCES ${aux_files})
+endif()

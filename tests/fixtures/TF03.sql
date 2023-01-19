c109f5e1d7f58a25065c95055d22a9b316d29604
Ganesh Mahesh <ganesh@yottadb.com>
2023-01-19 16:15:57 -0500

    [pipeline] Fix regex bug (introduced with c581a95e) in cross check function sed command used to match error messages
    Issue
    -------
    The pipelines `asan-ubuntu` and `make-tls-ubuntu` both failed due to a problem in `test_query_generator`. It fails because there is a diff between psql and Octo. A sample diff is:
    
    ```
    < ERROR:  NULL value in column SALARY" OF RELATION "NULLNAMESBTQG06 violates NOT NULL constraint
    ---
    > ERROR:  NULL value in column SALARY violates NOT NULL constraint
    ```
    Discussions on the issue: https://gitlab.com/YottaDB/DBMS/YDBOcto/-/merge_requests/1203#note_1246455143.
    
    Fix
    ----
    * test_helpers.bats.in uses a `sed` commands to convert Postgres results to a comparable format in case of update queries.
    * The regex in these `sed` commands didn't match the `of relation "table name"` parts of the error message as expected
      * Specifically the regex for NOT NULL constraint error message matching didn't work as expected
      * The original regex itself had the bug (https://gitlab.com/YottaDB/DBMS/YDBOcto/-/merge_requests/1203)
      * `.*` in `ERROR: null value in column \"\(.*\)\"` ended up matching more that what was expected and thus resulting in the rest of the regex not matching the additional text introduced in newer version of Postgres.
    * The output from Postgres started having ` of relation "xyz01" ` only from psql client version -> `psql (PostgreSQL) 14.5 (Ubuntu 14.5-1ubuntu1)`. Version `psql (PostgreSQL) 14.5 (Ubuntu 14.5-0ubuntu0.22.04.1)` doesn't have the additional text.
    * The regex is fixed to have `[^"]*` instead of the `.*` and this fixed the issue.
    * Additionally, the information of whether "write" was passed to the cross check function is now noted down in dbg_env.out along with other cross check function parameters. This helps debugging.
    
    Test
    -----
    * TF03 subtest is added to test_framework.bats.in to validate that this issue is fixed. The query in TF03 is copied from test failure seen at https://gitlab.com/YottaDB/DBMS/YDBOcto/-/jobs/3632250490. It failed because of the regex issue discussed earlier.

---
+++ tests/fixtures/TF03.sql
@@ -0,0 +1,18 @@
+#################################################################
+#								#
+# Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+DROP TABLE IF EXISTS customersTQG01;
+CREATE TABLE customersTQG01 (customer_id INTEGER PRIMARY KEY,first_name VARCHAR(8),last_name VARCHAR(10),email VARCHAR(20),address VARCHAR(26),city VARCHAR(16),state VARCHAR(2),zipcode VARCHAR(5));
+INSERT INTO customersTQG01 (SELECT * FROM customers);
+-- The following query results in a NOT NULL constraint failure and it is expected to be processed by the sed in test_helpers.bats.in responsible to convert them to a comparable format works fine
+-- It failed in pipeline couple of times at one time
+INSERT INTO customersTQG01  (last_name,email) (VALUES('Madison','jadams@usa.gov'));

YottaDB/YDBOcto

commit 0893983f41cfff0c051700e35e14585503257a19

[pipeline] Fix test-auto-upgrade job failure due to c581a95e commit
Issue
-----
* @nars1 identified that test-auto-upgrade job in nightly cron job failed because of c581a95e changes
* The failure seen is
  ```sql
  [ERROR]: ERR_UNKNOWN_TABLE: Unknown table: SUPPLIERSTQG04
  LINE 1:1: ...STINCT alias3.Phone FROM SuppliersTQG04 alias3 WHERE alias3.Supp...
                                        ^^^^^^^^^^^^^^
  ```
* The table definition wasn't available for `SuppliersTQG04` at the time of execution which is not expected as query generated test is expected to first execute table definition queries and then the test queries like the one in the error snippet

Analysis
--------
* The failure is caused by the third point below, others are redundancy in test execution found in test-auto-upgrade job
  * When JDBC client is selected two types of query files are created. Comment and no-comment sql files.
    * Both are being run by test-auto-upgrade job, but, only the later is expected to be run.
  * QueryGenerator generates a single .sql file with all the test queries. The cross check function in test_helper then splits the queries into separate files such that we have 1 query per file. The test_auto_upgrade job right now runs the first query file (which has all the test queries) and then the individual query files. Running the first query file is redundant.
  * When the QueryGenerator chooses to generated DML statements, definition of the table is generated in a separate .sql file. This definition is executed at the beginning of the test. The test-auto-upgrade job script on the other hand fetches all sql files using `for sqlfile in *.sql` and because of ASCII based sorting, the definition files which are in lower case are run after all the test queries. This results in the failure noticed.

tools/ci/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #################################################################
 #								#
-# Copyright (c) 2019-2022 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2019-2023 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -820,12 +820,43 @@ AIM
 			change -segment OCTOSEG -file_name=$octodat
 			change -segment AIMSEG -file_name=$aimdat
 FILE
-
+			tabledefsqlfile=""
+			# Check if this is a test_query_generator test. If so, run table definition queries prior to running
+			# other queries as other queries will depend on the tables created in this definition.
+			if [[ ($subtest =~ ^"TQG") ]]; then
+				qgquery=1
+				# Disable the "set -e" setting temporarily as below command can return exit status 0 or 1
+				set +e
+				tabledefsqlfile=$(compgen -G "tabledefinition-TQG[0-9]*\.sql")
+				set -e
+			elif [[ ($subtest =~ ^TJC00[4-7]) ]]; then
+				qgquery=1
+				# Disable the "set -e" setting temporarily as below command can return exit status 0 or 1
+				set +e
+				tabledefsqlfile=$(compgen -G "tabledefinition-TJC00[4-7]\.sql")
+				set -e
+			else
+				qgquery=0
+			fi
+			if [[ "" != $tabledefsqlfile ]]; then
+				outfile="autoupgrade.$tabledefsqlfile.out"
+				../newsrc/octo -f $tabledefsqlfile > $outfile 2>&1
+			fi
 			# TEST1 and TEST2 below together test that Octo automatically recreates any
 			# binary-definitions/plans/xrefs/triggers as needed thereby testing YDBOcto#90.
 			errors_found=0
 			for sqlfile in *.sql
 			do
+				# .sql files with _comment- are only seen when JDBC client is used and they are not part of test queries
+				if [[ ($sqlfile =~ "_comment-") ]]; then
+					continue
+				fi
+				if [[ (1 -eq $qgquery) ]]; then
+					# .sql files with tabledefintion are run prior to this loop so skip them here
+					if [[ ($sqlfile =~ "tabledefinition") ]]; then
+						continue
+					fi
+				fi
 				# TEST1
 				# We do not want any failures in the "octo" invocation below to exit the script.
 				# So disable the "set -e" setting temporarily for this step.

commit 70cf892e698cbbc596bfa293dc3017cc413e8cde

[pipeline] Fix test-auto-upgrade job failure due to row-header and row summary line removal code and a bug introduced by 0893983f
Issue
------
* The test-auto-upgrade nightly cron job failed with the following diff
  ```
  # cat autoupgrade.TQG04_nocomment-001.sql.diff
  1,2d0
  < ERROR: NULL value in column EMPLOYEEID violates NOT NULL constraint
  < SQL State: 23502
  ```
* It was found that autoupgrade.TQG04_nocomment-001.sql.out was empty

Analysis
---------
* Since the test executed using JDBC driver, following code from tools/ci/build.sh is processed
  ```
  950                                         if [[ (0 == $is_post_octo649_commit) || (0 != $usedjdbcdriver) ]]; then
  951                                                 mv $outfile $outfile.tmp
  952                                                 tail -n +2 $outfile.tmp | head -n -1 > $outfile
  ```
* The above code is intended to be run only for SELECT queries as they only are expected to have row-header and row summary lines in the result
* Line 952 removes the additional lines described above
* In the test failure case, line 952 resulted in the removal of the entire contents of the output file. This is because the output was an error message which had just 1 line in the file.

Fix
-----
* A test generated by QueryGenerator.m can have INSERT and UPDATE statements in addition to SELECT statements. INSERT/UPDATE statements will not have the additional rows described in the previous section. So, the processing done by 950-952 should be avoided in such a case.
* `querytype` which contains whether the query being executed is `select` or `insert` or `update` is used to prevent execution of the lines 950-952
* Additionally,
  * it was found that `querytype` was being set by executing an `awk` command on output file instead of the query file. It has been corrected to use the query file.
    * The code which needed change is -> `querytype=$(awk 'NR==1 {print tolower($1);}' $outfile)`

tools/ci/build.sh
@@ -889,10 +889,10 @@ FILE
 				# Re-enable "set -e" now that "octo" invocation is done.
 				set -e
 				if [[ 1 -eq $qgquery ]]; then
-					querytype=$(awk 'NR==1 {print tolower($1);}' $outfile)
-					# Ignore any errors in insert or update query execution as it will be validated
+					querytype=$(awk 'NR==1 {print tolower($1);}' $sqlfile)
+					# Ignore any errors in insert or update or delete query execution as it will be validated
 					# against previous commit output error message down below
-					if [[ ("insert" == $querytype || "update" == $querytype) ]]; then
+					if [[ (("insert" == $querytype) || ("update" == $querytype) || ("delete" == $querytype)) ]]; then
 						ret_status=0
 					fi
 				fi
@@ -948,11 +948,16 @@ FILE
 					# Re-enable "set -e" now that "git merge-base" invocation is done.
 					set -e
 					if [[ (0 == $is_post_octo649_commit) || (0 != $usedjdbcdriver) ]]; then
-						mv $outfile $outfile.tmp
-						tail -n +2 $outfile.tmp | head -n -1 > $outfile
+						# If QueryGenerator is used and has generated a tabledefinition file, and the query is of
+						# type insert/update/delete, then output of these queries will not have row-header or row
+						# summary lines. Remove the row-header and row-summary lines for SELECT query results only.
+						if ! [[ ((1 -eq $qgquery) && (("insert" == $querytype) || ("update" == $querytype) || ("delete" == $querytype))) ]]; then
+							mv $outfile $outfile.tmp
+							tail -n +2 $outfile.tmp | head -n -1 > $outfile
+						fi
 					fi
 					# Error formatting in case of query generator with update queries
-					if [[ (1 -eq $qgquery) && ("insert" == $querytype || "update" == $querytype) ]]; then
+					if [[ (1 -eq $qgquery) && (("insert" == $querytype) || ("update" == $querytype) || ("delete" == $querytype)) ]]; then
 						# Below sed statements are similar to the sed statements in test_helper.bats.in
 						# run_query_in_octo_and_postgres_and_crosscheck(). Any change here must be done
 						# there as well.
YottaDB/YDBOcto

commit e10ddc44c4b8af5a53d1c0efb8815f12718e6361

[#592] Added mnemonic error name to error output and error messages to docs
Previously, Octo error messages omitted the mnemonic error name used to identify the error internally and in the documentation, though it would be more user-friendly to include the former rather than omit it. Accordingly, Octo now includes the mnemonic error name in each error message.

This was accomplished by revising `errors.c` to include the mnemonic error name in the error message prefix strings specified for Octo and Rocto there. Similarly, a new `err_name_str` array is now automatically generated in this file from the error definitions in `errors.hd`, similar to how the error message format strings themselves are generated.

Additionally, a new `INFO_ENV_VAR` error message was added in replacement of the `CUSTOM_ERROR` used in `octo_init.c` for outputting environment variable information.

Also, as requested in #592, error format messages were added to `errors.rst`. To make it possible to update this by a single script and to prevent confusion with `ERR_SEND_MESSAGE`, `ERR_ROCTO_SEND_MESSAGE` was renamed to `ERR_ROCTO_SEND_FAILED`. The usages of this message and its documentation were updated accordingly.

To accommodate the new error message format, a large number of outrefs were updated to include the mnemonic name for each recorded error message.

Some other test updates were also required, namely:
+ Revised `verify_output` helper function to include `INFO_ENV_VAR` when stripping environment variable output
+ Revised `test_psql_authentication.bats` to use username other than 'jon' to prevent conflicts with developer environment
+ Corrected test number in `test_index_statement.bats.in` to `TIS001`
+ Revised `cut` command in `TMU02` to account for additional spaces in output due to mnemonic inclusion
+ Revised `grep` calls in several test cases in `test_squirrel_sql_connect_queries.bats.in` to account for mnemonic error names

src/rocto.c
@@ -40,7 +40,6 @@ void handle_sigint(int sig, siginfo_t *info, void *context) {
 // Currently unused. May need to be used for CancelRequest handling in the future.
 // NOTE: This code has been disabled due to the lack of signal forwarding support for SIGUSR1 to YDB
 // void handle_sigusr1(int sig) {
-// INFO(CUSTOM_ERROR, "SIGUSR1 RECEIVED");
 // }
 
 #if YDB_TLS_AVAILABLE
@@ -148,7 +147,7 @@ int main(int argc, char **argv) {
 		status = inet_pton(AF_INET6, config->rocto_config.address, &addressv6.sin6_addr);
 		switch (status) {
 		case 0:
-			FATAL(ERR_BAD_ADDRESS, config->rocto_config.address);
+			FATAL(ERR_ROCTO_BAD_ADDRESS, config->rocto_config.address);
 			break;
 		case 1:
 			break;
@@ -272,7 +271,7 @@ int main(int argc, char **argv) {
 		}
 		rocto_session.ip = host_buf;
 		rocto_session.port = serv_buf;
-		LOG_LOCAL_ONLY(INFO, ERR_CLIENT_CONNECTED, NULL);
+		LOG_LOCAL_ONLY(INFO, INFO_CLIENT_CONNECTED, NULL);
 
 		status = ydb_init(); // YDB init needed by gtm_tls_init call below */
 		YDB_ERROR_CHECK(status);
@@ -327,7 +326,7 @@ int main(int argc, char **argv) {
 						if (-1 == tls_errno) {
 							ERROR(ERR_ROCTO_TLS_ACCEPT, gtm_tls_get_error());
 						} else {
-							ERROR(CUSTOM_ERROR, "unknown", tls_errno, strerror(tls_errno));
+							ERROR(ERR_ROCTO_TLS_UNNAMED, tls_errno, strerror(tls_errno));
 						}
 						break;
 					} else if (GTMTLS_WANT_READ == status) {

commit 1fd387c01eb2d7ec57c509f151c86e95ac85b048

Fix SIG-11 in rocto while issuing ERR_ROCTO_TLS_UNNAMED error (regression in e10ddc44)
Background
----------
* While running Octo tests on a Debian 11 system in the foreground from a terminal, I noticed
  a `Enter PEM pass phrase: ` prompt in the terminal. Not sure what in the test required it.
  I hit `Enter` without entering any pass phrase and later found that `rocto` had generated
  a core file due to a SIG-11.

* Below is the stack trace of the SIG-11.

  ```c
  (gdb) where
  #0  __pthread_kill (threadid=<optimized out>, signo=signo@entry=3) at ../sysdeps/unix/sysv/linux/pthread_kill.c:56
  #1  gtm_dump_core () at sr_unix/gtm_dump_core.c:74
  #2  gtm_fork_n_core () at sr_unix/gtm_fork_n_core.c:163
  #3  generic_signal_handler (sig=11, info=<optimized out>, context=0x7f894a6f2cc8 <stapi_signal_handler_oscontext+4552>, is_os_signal_handler=<optimized out>) at sr_unix/generic_signal_handler.c:492
  #4  <signal handler called>
  #5  __strlen_avx2 () at ../sysdeps/x86_64/multiarch/strlen-avx2.S:74
  #6  __vfprintf_internal (s=s@entry=0x7ffe2ed7cb80, format=format@entry=0x5640ee1a8b78 "Unnamed failure in ydb_tls_accept: %s (tls_errno: %d)", ap=ap@entry=0x7ffe2ed7cd30, mode_flags=mode_flags@entry=0) at vfprintf-internal.c:1688
  #7  __vsnprintf_internal (string=0x5640efd6d860 "Unnamed failure in ydb_tls_accept: ", maxlen=<optimized out>, format=0x5640ee1a8b78 "Unnamed failure in ydb_tls_accept: %s (tls_errno: %d)", args=0x7ffe2ed7cd30, mode_flags=0) at vsnprintf.c:114
  #8  populate_and_print_full_err_str (error=ERR_ROCTO_TLS_UNNAMED, full_err_str=0x7ffe2ed7cd88, full_err_len=0x7ffe2ed7d19c, err_prefix=0x7ffe2ed7cd90 "[127.0.0.1:59042] [ERROR] src/rocto.c:350 2023-01-26 15:05:45: ERR_ROCTO_TLS_UNNAMED: ", orig_args=0x7ffe2ed7d1e8) at src/errors.c:103
  #9  octo_log (line=350, file=0x5640ee1a7068 "src/rocto.c", level=DEBUG, severity=ERROR_Severity, error=ERR_ROCTO_TLS_UNNAMED) at src/errors.c:193
  #10 main (argc=5, argv=0x7ffe2ed7db78) at src/rocto.c:350
  ```

Issue

src/rocto.c
@@ -347,7 +347,7 @@ int main(int argc, char **argv) {
 						if (-1 == tls_errno) {
 							ERROR(ERR_ROCTO_TLS_ACCEPT, gtm_tls_get_error());
 						} else {
-							ERROR(ERR_ROCTO_TLS_UNNAMED, tls_errno, strerror(tls_errno));
+							ERROR(ERR_ROCTO_TLS_UNNAMED, strerror(tls_errno), tls_errno);
 						}
 						break;
 					} else if (GTMTLS_WANT_READ == status) {

tests/test_log_split.bats.in
@@ -1,6 +1,6 @@
 #################################################################
 #								#
-# Copyright (c) 2019-2022 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2019-2023 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -32,7 +32,7 @@ teardown() {
   stop_rocto
 }
 
-@test "ensure log output split across multiple lines" {
+@test "TLS01 : ensure log output split across multiple lines" {
   # Spawn several client-server process pairs to create potential for log interleaving
   for i in {1..10}; do
     # Account for TLS and non-TLS pipelines
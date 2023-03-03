YottaDB/YDBOcto

commit 7fa4406a38eff621bc908654ab2746257ab4bbc8

[#519] Double-quoted SQL strings are handled in closer conformance to SQL standard
Background
----------

Previously, Octo treated double-quoted SQL strings as string literals. However, the SQL standard defines double-quoted strings to be treated as identifiers, not string literals. Accordingly, there is a non-trivial discrepancy between Octo's behavior and SQL '92 standard behavior (see the `delimited_identifier` rule of the grammar at https://ronsavage.github.io/SQL/sql-92.bnf.html). To resolve this discrepancy, Octo was enhanced to treat double-quoted SQL strings as identifiers.

The above change also made it necessary to implement case sensitive identifiers in the lexer and parser. This was necessary to ensure PostgreSQL compatible behavior in the handling of double-quoted strings. Specifically, PostgreSQL treats double-quoted strings as identifiers, not as string literals, whereas Octo previously treated all quoted strings as string literals.

Consequently, Octo did not support double-quoted identifiers like PostgreSQL and, as a result, did not support case sensitive identifiers. Rather, all identifiers had to be specified without quotes and were implicitly converted to upper case in all cases. Now, Octo behavior mirrors PostgreSQL behavior, such that single-quoted strings are treated as string literals, whereas double-quoted strings are treated as case-sensitive identifiers.

Code changes
------------

To implement this enhancement, a number of changes were required. First, the lexer and parser were updated to distinguish single-quote strings from double-quote strings by adding a new `DOUBLE_QUOTE_LITERAL` token, which is used for processing double-quote strings only. The pre-existing `LITERAL` token continues to be used as before.

tools/ci/build.sh
@@ -338,6 +338,25 @@ if [[ ("test-auto-upgrade" == $jobname) && ("force" != $subtaskname) ]]; then
 	# Add line to verify_output helper function to remove `..` from test output when Octo is built with Ninja
 	# See https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/861 for more details.
 	sed -i "s,# Filter rule #s and line #s printed by flex in TRACE verbosity level output,sed -i 's/\\\.\\\.PATH/PATH/' clean_output\.txt," ../tests/test_helpers.bash.in
+	# Check whether the chosen commit is prior to b1acb27f. If so, then the DDLs in various `*.sql` files must be updated to
+	# accommodate the case sensitivity changes introduced to resolve #519. Do this by removing all references to
+	# `keys(..)` to bring this file into conformity with the changes made to it in b1acb27f.
+	if git merge-base --is-ancestor HEAD b1acb27f; then
+		echo "# Commit $commitsha is ancestor of b1acb27f, deleting deprecated 'keys(..)' expressions from fixtures"
+		# Exclude any VistA fixtures from the search and replace, since the `keys(..)`
+		# expressions there use uppercase column names are thus safe.
+		shopt -s extglob # Temporarily set the extglob shell option to allow exclusion of patterns from glob
+		fixtures="$(ls ../tests/fixtures/!(vista-mini).sql)"
+		sed -i 's/(keys("".*""))*//' $fixtures
+		# Some `keys(..)` expressions are contained in test files themselves and belong to the functionality under test, and
+		# so cannot simply be removed as in the case of those in SQL files above. Rather, they must be modified to use
+		# uppercase table names to match the case convention for case insensitive identifiers. So, do that here.
+		fixtures="$(ls ../tests/*.bats.in)"
+		sed -i 's/keys(\(""[A-Za-z0-9]*""\))/keys(\U\1\E)/g' $fixtures
+		shopt -u extglob # Unset the extglob shell option to return the environment to its previous state
+	else
+		echo "# Commit $commitsha is descendant of b1acb27f, retaining 'keys(..)' expressions from fixtures"
+	fi
 	if [ "ALL" != "$autoupgrade_test_to_troubleshoot" ]; then
 		# Run only a random fraction of the bats tests as we will be running an auto upgrade test on the same queries
 		# once more a little later.

commit f13aff88fef422a1b507720323b8a4d96e54da64

[#519] Fix various issues with keys(...) upper casing in tools/ci/build.sh (regression in 7fa4406a)
Background
----------
Below is pasted from https://gitlab.com/YottaDB/DBMS/YDBOcto/-/merge_requests/1182#note_1300945558

* https://gitlab.com/YottaDB/DBMS/YDBOcto/-/merge_requests/1182#note_1239215788 mentioned `upper casing`
  all `keys(...)` usages. But what ended up being done is `removing` the lower case `keys(...)` usages
  from `tests/fixtures/*.sql`. An example change is shown below.

   **tests/fixtures/TC004.sql**
   ```diff
   -CREATE TABLE t1 (a int primary key) GLOBAL "^T1(keys(""a""))" READONLY;
   +CREATE TABLE t1 (a int primary key) GLOBAL "^T1" READONLY;
   ```

   Changes like this happened to the `names4` and `pastas` tables in `tests/fixtures/pastas.sql` too (if the
   older commit chosen was predated b1acb27f). But this caused a simple query like `select * from names4;`
   to run in an infinite loop in that older commit. And this caused the `test-auto-upgrade` job to timeout
   on a lot of the tests as well as fail a lot of the tests.

   https://gitlab.com/nars1/YDBOcto/-/jobs/3873064606 is a job failure that I noticed because of the above issue.

* Additionally, when the older commit was newer than b1acb27f, I noticed a different failure
  (https://gitlab.com/nars1/YDBOcto/-/jobs/3873382464). In this case, there was a `CREATE TABLE` command in
  `TC019.sql` which used `keys("id")` but was not converted to upper case by `tools/ci/build.sh` because this
  was a newer commit than b1acb27f. And that caused assert failures in M code emit time using the latest commit
  because the lower cased column was not found in the table.

Fix
---
To fix the above issues, I am making the following changes.

* Revert the code that `removed` the `keys(...)` usages.

* Make all `keys(...)` usages upper case. The only exceptions are the test fixtures introduced in 7fa4406a
  (i.e. !1182) which are TC058, TC058, TC060.

tools/ci/build.sh
@@ -338,25 +338,17 @@ if [[ ("test-auto-upgrade" == $jobname) && ("force" != $subtaskname) ]]; then
 	# Add line to verify_output helper function to remove `..` from test output when Octo is built with Ninja
 	# See https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/861 for more details.
 	sed -i "s,# Filter rule #s and line #s printed by flex in TRACE verbosity level output,sed -i 's/\\\.\\\.PATH/PATH/' clean_output\.txt," ../tests/test_helpers.bash.in
-	# Check whether the chosen commit is prior to b1acb27f. If so, then the DDLs in various `*.sql` files must be updated to
-	# accommodate the case sensitivity changes introduced to resolve #519. Do this by removing all references to
-	# `keys(..)` to bring this file into conformity with the changes made to it in b1acb27f.
-	if git merge-base --is-ancestor HEAD b1acb27f; then
-		echo "# Commit $commitsha is ancestor of b1acb27f, deleting deprecated 'keys(..)' expressions from fixtures"
-		# Exclude any VistA fixtures from the search and replace, since the `keys(..)`
-		# expressions there use uppercase column names are thus safe.
-		shopt -s extglob # Temporarily set the extglob shell option to allow exclusion of patterns from glob
-		fixtures="$(ls ../tests/fixtures/!(vista-mini).sql)"
-		sed -i 's/(keys("".*""))*//' $fixtures
-		# Some `keys(..)` expressions are contained in test files themselves and belong to the functionality under test, and
-		# so cannot simply be removed as in the case of those in SQL files above. Rather, they must be modified to use
-		# uppercase table names to match the case convention for case insensitive identifiers. So, do that here.
-		fixtures="$(ls ../tests/*.bats.in)"
-		sed -i 's/keys(\(""[A-Za-z0-9]*""\))/keys(\U\1\E)/g' $fixtures
-		shopt -u extglob # Unset the extglob shell option to return the environment to its previous state
-	else
-		echo "# Commit $commitsha is descendant of b1acb27f, retaining 'keys(..)' expressions from fixtures"
-	fi
+	# If the chosen older commit has "*.sql" or "*.bats.in" files with CREATE TABLE commands containing the GLOBAL
+	# keyword with "keys(..)" expressions using a lower case column name, those will no longer work with the master
+	# after 7fa4406a (YDBOcto#519) as we expect "keys(..)" to use upper case column names for case insensitive column
+	# names and case sensitive column names otherwise. The only tests that use case sensitive column names should be
+	# those that were newly introduced in 7fa4406a and they are TC058, TCO59 and TCO60. So excepting those *.sql files,
+	# all other "*.sql" or "*.bats.in" files are better updated to ensure upper case column names are used inside "keys(..)"
+	# expressions. The following logic takes care of that.
+	shopt -s extglob	# enable extended pattern matching feature (we use ! syntax below)
+	fixtures="$(ls ../tests/*.bats.in ../tests/fixtures/!(TC058*|TC059*|TC060*).sql)"
+	shopt -u extglob	# reset now that extended pattern matching feature need is done
+	sed -i 's/keys(\(""[A-Za-z0-9]*""\))/keys(\U\1\E)/g' $fixtures
 	if [ "ALL" != "$autoupgrade_test_to_troubleshoot" ]; then
 		# Run only a random fraction of the bats tests as we will be running an auto upgrade test on the same queries
 		# once more a little later.
YottaDB/YDBOcto

commit 0939090a9a04d99063dec7772afc40379b15388b

[#940] Avoid deletion of dependencies between functions (defined in octo-seed) and tables during octo-seed auto load
Issue
--------
* Octo on startup auto loads `octo-seed` if `YDBOCTO_GIT_COMMIT_VERSION` has changed
* It is possible for tables to have dependencies to functions which are defined in the `octo-seed`
* When such dependencies exist, following types of gvns are created at the time of table creation to track
  function and table dependency.
  ```m
   ^%ydboctoocto("functions","SAMEVALUE","%ydboctoFN0uUSDY6E7G9VcjaOGNP9G","check_constraint","NAMES","NAME1")=""
  ```
* Auto load (which happens when executing octo_init()) code issues a `KILL` on ^%ydboctoocto("functions","function_name")
  gvn. This operation deletes the dependency nodes like the one shown in previous point.
* A `DROP FUNCTION` after such an auto load will be allowed which is incorrect as the table still depends on the function
* Following is the exact behavior seen in Octo before this change. Refer to the comments in the below code for further
  details.
  ```sql
  -- Create a table which has dependency to ABS which is function defined in `octo-seed`.
  -- This creates the dependency gvns between `abs` and `test`.
  OCTO> create table test (id integer check(abs(id)<3));
  CREATE TABLE
  OCTO> insert into test values(1),(2);
  INSERT 0 2
  OCTO> insert into test values(3);
  [ERROR]: ERR_CHECK_CONSTRAINT_VIOLATION: New row for table TEST violates CHECK constraint TEST_ID_CHECK : Failing row contains (3)
  OCTO> select * from test;
  ID
  1
  2
  (4 rows)
  -- The following DROP FUNCTION utilizes the dependency gvn and prevents the DROP
  OCTO> drop function abs(integer);
  [ERROR]: ERR_DROP_FUNCTION_DEPENDS: Cannot DROP function 'ABS(INTEGER)' because other objects (constraint TEST_ID_CHECK on table TEST) depend on it
  OCTO> \q

  -- Build Octo with a different git SHA (Note we only want SHA to change not other parameters used to auto load
  -- binary definition and plan).

tools/ci/build.sh
@@ -740,6 +740,25 @@ else
 				continue
 			fi
 			subtest=$(sed 's/.*subtest \[//;s/].*//;' bats_test.out)
+			if [[ ($subtest =~ ^"TAU") ]]; then
+				subtestName=$(echo $subtest | cut -d " " -f1)
+				# Run auto upgrade specific test
+				# Disable the "set -e" setting temporarily as we want to check the error reported by Octo
+				mv output.txt "$subtestName"_1_output.txt
+				[[ -e "$subtestName"_2.sql ]]
+				[[ -e "$subtestName"_2.ref ]]
+				set +e
+				./newsrc/octo -f "$subtestName"_2.sql > output.txt 2>&1
+				diff "$subtestName"_2.ref output.txt > "$subtestName"_2_output.diff
+				set -e
+				if [[ -s "$subtestName"_2_output.diff ]]; then
+					echo "ERROR : [diff $subtestName_2.ref output.txt] returned non-zero diff" | tee -a ../errors.log
+					echo "[cat $subtestName_2_output.diff] output follows" | tee -a ../errors.log
+					tee -a < $subtestName_2_output.diff ../errors.log
+					exit_status=1
+				fi
+				continue
+			fi
 			# ----------------------------------------------------------------------------
 			# Exclude specific set of subtests (reasons explained below)
 			# ----------------------------------------------------------------------------

cmake/bats-tests.cmake
@@ -1,6 +1,6 @@
 #################################################################
 #								#
-# Copyright (c) 2019-2022 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2019-2023 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -172,6 +172,7 @@ if("${FULL_TEST_SUITE}")
 	ADD_BATS_TEST_DML(test_constraint_table_column)
 	ADD_BATS_TEST_DML(test_tablename_asterisk)
 	ADD_BATS_TEST_DML(test_select_columns_psql)
+	ADD_BATS_TEST_DML(test_auto_upgrade)
 
 	# The following test requires MySQL and PostgreSQL
 	ADD_BATS_TEST_DML(test_date_time_functions)

pre-commit
@@ -200,4 +200,30 @@ foreach file ($filelist)
 	endif
 end
 
+#--------------------------------------------------------------------------------------------------
+# 8) Verify that FMT_SEED_DEFINITION is bumped if there is a change to tests/fixtures/octo-seed.sql
+#--------------------------------------------------------------------------------------------------
+
+set tipcommit = "HEAD"
+set octo_seed_change = `git diff --cached $tipcommit -- tests/fixtures/octo-seed.sql`
+if ("" != "$octo_seed_change")  then
+	set octo_header_change=`git diff --cached $tipcommit -- src/octo.h`
+	if ("" != "$octo_header_change") then
+		# Get the FMT_SEED_DEFINITION value in the latest commit
+		set current_seed_fmt_value=`git diff --cached -G "\#define\ FMT\_SEED\_DEFINITION" $tipcommit -- src/octo.h | grep -o "\+\#define\ FMT\_SEED\_DEFINITION\ [0-9]*" | awk '{print $3}'`
+		# Get the previous FMT_SEED_DEFINITION value
+		set prior_seed_fmt_value=`git diff --cached -G "\#define\ FMT\_SEED\_DEFINITION" $tipcommit -- src/octo.h | grep -o "\-\#define\ FMT\_SEED\_DEFINITION\ [0-9]*" | awk '{print $3}'`
+		# Check if FMT_SEED_DEFINITION value is bumped
+		@ incr_prior_seed_fmt_value = $prior_seed_fmt_value + 1
+		if ("$incr_prior_seed_fmt_value" != "$current_seed_fmt_value") then
+			# octo-seed has changed but FMT_SEED_DEFINITION is not issue an error
+			echo "--> HOOK-E-FAIL : File octo-seed.sql has changed but no update to FMT_SEED_DEFINITION. FMT_SEED_DEFINITION must be bumped."
+			@ exitstatus = 1
+		endif
+	else
+		# octo-seed has changed but FMT_SEED_DEFINITION is not issue an error
+		echo "--> HOOK-E-FAIL : File octo-seed.sql has changed but no update to FMT_SEED_DEFINITION. FMT_SEED_DEFINITION must be bumped."
+		@ exitstatus = 1
+	endif
+endif
 exit $exitstatus

src/auto_load_octo_seed_if_needed.c
@@ -1,6 +1,6 @@
 /****************************************************************
  *								*
- * Copyright (c) 2021-2022 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2021-2023 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -15,6 +15,7 @@
 #include "octo.h"
 #include "git_hashes.h"
 
+#define OCTO_LIT_SEEDFMT "seedfmt"
 #define CLEANUP_AND_RETURN(STATUS, RELEASE_DDL_LOCK, OCTO_GLOBAL, LOCKSUB)      \
 	{                                                                       \
 		if (RELEASE_DDL_LOCK) {                                         \
@@ -34,6 +35,24 @@
 		}                                                                           \
 	}
 
+/* Previous to YDBOcto#940, ^%ydboctoocto("seedfmt") was used to hold git SHA
+ * and if this value was different than the current git SHA octo-seed was reloaded.
+ * After YDBOcto#940, ^%ydboctoocto("seeddfnfmt") is used to hold `FMT_SEED_DEFINITION`
+ * value and if the stored value is different from current `FMT_SEED_DEFINITION` then octo-seed is reloaded.
+ * Since there is a change in the way octo-seed reload was tracked, following macro is used to remove
+ * older gvn(^%ydboctoocto("seedfmt") which tracked octo-seed reload if it exists.
+ */
+#define DELETE_SEED_FMT_GVN()                                                               \
+	{                                                                                   \
+		ydb_buffer_t seed_fmt_buf, lcl_octo_global;                                 \
+		YDB_STRING_TO_BUFFER(OCTO_LIT_SEEDFMT, &seed_fmt_buf);                      \
+		int dlqStatus;                                                              \
+                                                                                            \
+		YDB_STRING_TO_BUFFER(config->global_names.octo, &lcl_octo_global);          \
+		dlqStatus = ydb_delete_s(&lcl_octo_global, 1, &seed_fmt_buf, YDB_DEL_TREE); \
+		YDB_ERROR_CHECK(dlqStatus);                                                 \
+	}
+
 /* Checks if the octo seed file needs to be reloaded due to a new Octo build opening this database for the first time.
  * If so, it auto loads the octo-seed.sql and octo-seed.zwr files.
  * Returns YDB_OK on success and a non-zero (YDB_ERR_* status code or 1) on errors.
@@ -41,48 +60,84 @@
  */
 int auto_load_octo_seed_if_needed(void) {
 	ydb_buffer_t octo_global, subs, fmt;
-	char	     fmt_buff[sizeof(YDBOCTO_GIT_COMMIT_VERSION)];
+	char	     fmt_buff[INT32_TO_STRING_MAX];
 	int	     status;
-	boolean_t    auto_load_needed, release_ddl_lock;
+	boolean_t    full_auto_load_needed, partial_auto_load_needed, release_ddl_lock;
 	ydb_buffer_t locksub;
 	boolean_t    save_allow_schema_changes;
 
-	/* We have had instances where YDBOCTO_GIT_COMMIT_VERSION does not get generated properly (GIT-NOTFOUND)
-	 * rather than the proper 40 hex digit SHA1 hash. If this happens, and we are getting data from
-	 * ^%ydboctoocto(OCTOLIT_SEEDFMT), we will fail with this error:
-	 *
-	 * YDB-E-INVSTRLEN, Invalid string length 40: max 13
-	 *
-	 * ... because the size of fmt_buff (now = "GIT-NOTFOUND") is not large enough to get the 40 characters
-	 * originally stored in YottaDB. As a precaution, assert that the hash is 40 characters long + 1 NULL.
-	 */
-	assert(sizeof(fmt_buff) == 41);
-
+	full_auto_load_needed = FALSE;
+	partial_auto_load_needed = FALSE;
 	/* Check if binary definitions (tables or functions) need to be auto upgraded. They need to be if
-	 * ^%ydboctoocto(OCTOLIT_SEEDFMT) is different from YDBOCTO_GIT_COMMIT_VERSION.
+	 * ^%ydboctoocto(OCTOLIT_SEEDDEFFMT) is different from FMT_SEED_DEFINITION.
 	 */
 	YDB_STRING_TO_BUFFER(config->global_names.octo, &octo_global);
-	YDB_STRING_TO_BUFFER(OCTOLIT_SEEDFMT, &subs);
+	YDB_STRING_TO_BUFFER(OCTOLIT_SEEDDFNFMT, &subs);
 	YDB_LITERAL_TO_BUFFER(OCTOLIT_DDL, &locksub);
 	fmt.buf_addr = &fmt_buff[0];
 	fmt.len_alloc = sizeof(fmt_buff);
 	for (release_ddl_lock = FALSE;; release_ddl_lock = TRUE) {
 		status = ydb_get_s(&octo_global, 1, &subs, &fmt);
 		switch (status) {
-		case YDB_ERR_GVUNDEF:
-			auto_load_needed = TRUE;
+		case YDB_ERR_GVUNDEF:;
+			ydb_buffer_t lcl_sub, lcl_fmt;
+			char	     lcl_fmt_buff[sizeof(YDBOCTO_GIT_COMMIT_VERSION)];
+			/* We have had instances where YDBOCTO_GIT_COMMIT_VERSION does not get generated properly (GIT-NOTFOUND)
+			 * rather than the proper 40 hex digit SHA1 hash. If this happens, and we are getting data from
+			 * ^%ydboctoocto(OCTOLIT_SEEDFMT), we will fail with this error:
+			 *
+			 * YDB-E-INVSTRLEN, Invalid string length 40: max 13
+			 *
+			 * ... because the size of fmt_buff (now = "GIT-NOTFOUND") is not large enough to get the 40 characters
+			 * originally stored in YottaDB. As a precaution, assert that the hash is 40 characters long + 1 NULL.
+			 */
+			assert(sizeof(lcl_fmt_buff) == 41);
+			lcl_fmt.buf_addr = &lcl_fmt_buff[0];
+			lcl_fmt.len_alloc = sizeof(lcl_fmt_buff);
+			YDB_STRING_TO_BUFFER(OCTO_LIT_SEEDFMT, &lcl_sub);
+			status = ydb_get_s(&octo_global, 1, &lcl_sub, &lcl_fmt);
+			switch (status) {
+			case YDB_ERR_GVUNDEF:
+				/* Both OCTOLIT_SEEDDFNFMT and OCTOLIT_SEEDFMT gvns are not found.
+				 * Only load octo-seed as we are populating an empty DB
+				 */
+				partial_auto_load_needed = TRUE;
+				break;
+			case YDB_OK:
+				/* Only OCTOLIT_SEEDFMT gvn is found.
+				 * Load octo-seed and upgrade binaries as the build has changed, and dependencies if they
+				 * exist have to reflect the latest state of the binary definition.
+				 */
+				full_auto_load_needed = TRUE;
+				DELETE_SEED_FMT_GVN();
+				break;
+			default:
+				YDB_ERROR_CHECK(status);
+				CLEANUP_AND_RETURN(status, release_ddl_lock, octo_global, locksub);
+				assert(FALSE);
+				break;
+			}
+#ifndef NDEBUG
+			// Force auto load if config->seedreload is TRUE
+			full_auto_load_needed = (config->seedreload) ? TRUE : full_auto_load_needed;
+#endif
 			break;
-		case YDB_OK:
+		case YDB_OK:;
 			assert(fmt.len_used < sizeof(fmt_buff));
 			fmt.buf_addr[fmt.len_used] = '\0';
-			auto_load_needed = memcmp(fmt.buf_addr, YDBOCTO_GIT_COMMIT_VERSION, fmt.len_used + 1);
+			int seed_definition_fmt = atoi(fmt.buf_addr);
+			full_auto_load_needed = (FMT_SEED_DEFINITION != seed_definition_fmt);
+#ifndef NDEBUG
+			// Force auto load if config->seedreload is TRUE
+			full_auto_load_needed = (config->seedreload) ? TRUE : full_auto_load_needed;
+#endif
 			break;
 		default:
 			YDB_ERROR_CHECK(status);
 			CLEANUP_AND_RETURN(status, release_ddl_lock, octo_global, locksub);
 			assert(FALSE);
 		}
-		if (!auto_load_needed) {
+		if (!full_auto_load_needed && !partial_auto_load_needed) {
 			/* No auto upgrade needed. Return after releasing ddl exclusive lock (if applicable). */
 			CLEANUP_AND_RETURN_IF_NOT_YDB_OK(status, release_ddl_lock, octo_global, locksub);
 			return YDB_OK;
@@ -101,6 +156,7 @@ int auto_load_octo_seed_if_needed(void) {
 	/* In case this is a rocto process, it is not allowed to do schema changes by default. But allow the auto upgrade
 	 * for this process. Hence the temporary modification to "config->allow_schema_changes" below.
 	 */
+	assert(full_auto_load_needed || partial_auto_load_needed);
 	save_allow_schema_changes = config->allow_schema_changes;
 	config->allow_schema_changes = TRUE;
 	/* Set a global variable to indicate this is the small window where auto load of octo-seed.sql runs. This lets
@@ -114,10 +170,27 @@ int auto_load_octo_seed_if_needed(void) {
 	if (0 != status) {
 		CLEANUP_AND_RETURN(status, release_ddl_lock, octo_global, locksub);
 	}
-	/* Now that auto load is complete, indicate that (so other processes do not attempt the auto load)
-	 * by setting ^%ydboctoocto(OCTOLIT_SEEDFMT) to YDBOCTO_GIT_COMMIT_VERSION.
+	if (full_auto_load_needed) {
+		/* Following call ensures that those tables which depend on functions in octo-seed.sql refer to the latest function
+		 * format.
+		 */
+		UPGRADE_BINARY_DEFINITIONS_AND_RETURN_IF_NOT_YDB_OK(status, release_ddl_lock, octo_global, locksub);
+		assert(YDB_OK == status); // Ensure above call succeeded
+	} else {
+		assert(partial_auto_load_needed);
+		/* Write the current FMT_BINARY_DEFINITION value to ^%ydboctoocto(OCTOLIT_BINFMT). So other processes do not
+		 * attempt the auto upgrade as its an empty database and there is nothing to upgrade at this point.
+		 */
+		SET_OCTOLIT_BINFMT_GVN_AND_RETURN_IF_NOT_YDB_OK(status, release_ddl_lock, octo_global, locksub);
+	}
+	/* Following assert ensures that the operations in the above if/else block have succeeded.
+	 * If it hasn't we should not reach this part of the code.
 	 */
-	fmt.len_used = snprintf(fmt.buf_addr, fmt.len_alloc, "%s", YDBOCTO_GIT_COMMIT_VERSION);
+	assert(YDB_OK == status);
+	/* Now that auto load is complete, indicate that (so other processes do not attempt the auto load)
+	 * by setting ^%ydboctoocto(OCTOLIT_SEEDDEFFMT) to FMT_SEED_DEFINITION.
+	 */
+	fmt.len_used = snprintf(fmt.buf_addr, fmt.len_alloc, "%d", FMT_SEED_DEFINITION);
 	status = ydb_set_s(&octo_global, 1, &subs, &fmt);
 	CLEANUP_AND_RETURN_IF_NOT_YDB_OK(status, release_ddl_lock, octo_global, locksub);
 

src/auto_upgrade_binary_definition_if_needed.c
@@ -1,6 +1,6 @@
 /****************************************************************
  *								*
- * Copyright (c) 2020-2021 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2020-2023 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -47,7 +47,6 @@ int auto_upgrade_binary_definition_if_needed(void) {
 #endif
 	boolean_t    auto_upgrade_needed, release_ddl_lock;
 	ydb_buffer_t locksub;
-	boolean_t    save_allow_schema_changes;
 
 #ifdef FORCE_BINARY_DEFINITION_AUTO_UPGRADE
 	if (NULL != getenv("disable_auto_upgrade")) {
@@ -99,38 +98,8 @@ int auto_upgrade_binary_definition_if_needed(void) {
 			break;
 		}
 	}
-	/* In case this is a rocto process, it is not allowed to do schema changes by default. But allow the auto upgrade
-	 * for this process. Hence the temporary modification to "config->allow_schema_changes" below.
-	 */
-	save_allow_schema_changes = config->allow_schema_changes;
-	config->allow_schema_changes = TRUE;
-	/* Do the actual auto upgrade of the binary table definition.
-	 * Set a global variable to indicate this is the small window where auto upgrade of binary table definitions happens.
-	 * This lets "table_definition.c" know to do some special processing (use different logic to calculate whether
-	 * a table should be considered READONLY or READWRITE).
-	 */
-	assert(FALSE == config->in_auto_upgrade_binary_table_definition);
-	config->in_auto_upgrade_binary_table_definition = TRUE;
-	status = auto_upgrade_binary_table_definition();
-	config->in_auto_upgrade_binary_table_definition = FALSE;
-	if (YDB_OK != status) {
-		config->allow_schema_changes = save_allow_schema_changes;
-		CLEANUP_AND_RETURN(status, release_ddl_lock, octo_global, locksub);
-	}
-	/* Do the actual auto upgrade of the binary function definition */
-	status = auto_upgrade_binary_function_definition();
-	if (YDB_OK != status) {
-		config->allow_schema_changes = save_allow_schema_changes;
-		CLEANUP_AND_RETURN(status, release_ddl_lock, octo_global, locksub);
-	}
-	config->allow_schema_changes = save_allow_schema_changes;
-	/* Now that auto upgrade is complete, indicate that (so other processes do not attempt the auto upgrade)
-	 * by setting ^%ydboctoocto(OCTOLIT_BINFMT) to FMT_BINARY_DEFINITION.
-	 */
-	fmt.len_used = snprintf(fmt.buf_addr, fmt.len_alloc, "%d", FMT_BINARY_DEFINITION);
-	status = ydb_set_s(&octo_global, 1, &subs, &fmt);
-	CLEANUP_AND_RETURN_IF_NOT_YDB_OK(status, release_ddl_lock, octo_global, locksub);
-
+	UPGRADE_BINARY_DEFINITIONS_AND_RETURN_IF_NOT_YDB_OK(status, release_ddl_lock, octo_global, locksub);
+	assert(YDB_OK == status); // Ensure above call succeeded
 	/* Now that auto upgrade is complete, release exclusive lock */
 	status = ydb_lock_decr_s(&octo_global, 1, &locksub);
 	release_ddl_lock = FALSE; /* needed so the below macro invocation does not try to release the lock again */

src/aux/_ydboctoDiscard.m
@@ -1,6 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;								;
-; Copyright (c) 2020-2022 YottaDB LLC and/or its subsidiaries.	;
+; Copyright (c) 2020-2023 YottaDB LLC and/or its subsidiaries.	;
 ; All rights reserved.						;
 ;								;
 ;	This source code contains the intellectual property	;
@@ -111,14 +111,23 @@ discardTable(tableName,tableGVNAME)	;
 	. . KILL ^%ydboctoschema(tableName,"primary_key_name")
 	QUIT
 
-discardFunction(functionName,functionHash)	;
+discardFunction(functionName,functionHash,isAutoLoad)	;
 	; --------------------------------------------------------------------------------------------------
 	; Discards all generated plans associated with a function whose name and hash are provided as input.
 	; --------------------------------------------------------------------------------------------------
 	NEW planName
 	SET planName="" FOR  DO  QUIT:""=planName  DO discardPlan(planName)
 	.  SET planName=$ORDER(^%ydboctoocto("functions",functionName,functionHash,"plan_metadata",planName))
-	KILL ^%ydboctoocto("functions",functionName,functionHash);
+	; If Octo is performing an auto load of octo-seed, discardFunction() is called with `isAutoLoad` set to 1.
+	; In such a case avoid deleting dependency nodes as these are only created during CREATE command execution
+	; for the entity which depends on this function and It is not guranteed that the entity will go through CREATE
+	; during auto load of octo-seed.
+	NEW sub
+	IF (isAutoLoad) DO
+	. SET sub="" FOR  DO  QUIT:""=sub
+	. . SET sub=$ORDER(^%ydboctoocto("functions",functionName,functionHash,sub))
+	. . KILL:("check_constraint"'=sub) ^%ydboctoocto("functions",functionName,functionHash,sub)
+	ELSE  KILL ^%ydboctoocto("functions",functionName,functionHash);
 	QUIT
 
 discardPlan(srcPlan)

src/aux/ydbocto.ci
@@ -2,7 +2,7 @@ _ydboctoselect          :void        run^%ydboctoselect(I:ydb_ulong_t,I:ydb_stri
 _ydboctoInit            :ydb_long_t* ^%ydboctoInit(I:ydb_int_t)
 _ydboctoNodeDump        :void        ydboctoNodeDump^%ydboctoUtils()
 _ydboctoDiscardTable    :void        discardTable^%ydboctoDiscard(I:ydb_string_t*,I:ydb_string_t*)
-_ydboctoDiscardFunction :void        discardFunction^%ydboctoDiscard(I:ydb_string_t*,I:ydb_string_t*)
+_ydboctoDiscardFunction :void        discardFunction^%ydboctoDiscard(I:ydb_string_t*,I:ydb_string_t*,I:ydb_int_t)
 _ydboctoDiscardAll      :void        discardALL^%ydboctoDiscard()
 _ydboctoxrefupdate      :void        handleSetKill^%ydboctoxrefupdate(I:ydb_string_t*,I:ydb_string_t*)
 _ydboctoxrefMetadata    :void        xrefMetadata^%ydboctoselect(I:ydb_string_t*)

src/config.h
@@ -1,6 +1,6 @@
 /****************************************************************
  *								*
- * Copyright (c) 2019-2022 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2019-2023 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -87,6 +87,9 @@ typedef struct OctoConfig {
 							    * This helps us to know how many entries we need to append
 							    * when we quit.
 							    */
+#ifndef NDEBUG
+	boolean_t seedreload; /* Only used for testing autoload */
+#endif
 } OctoConfig;
 
 extern OctoConfig *config;

commit 277345c57850fed4648571b55a0659a2acb72871

Fix test_auto_upgrade job failure due to issues in tools/ci/build.sh (regression in 0939090a)
Background
----------
* The nightly test_auto_upgrade cron job that runs in-house (uses `master~1`, `master~2`, till `master~7`
  as random older commits and runs the `test-auto-upgrade` pipeline job locally) failed with the following
  error.

  ```
  tools/ci/build.sh: line 774: subtestName_2: unbound variable
  ```

Issue
-----
* As part of 0939090a, the following code was added.

  **tools/ci/build.sh**
  ```sh
    724     for tstdir in bats-test.*
    725     do
    726             cd $tstdir
      .
      .
    750                     set +e
    751                     ./newsrc/octo -f "$subtestName"_2.sql > output.txt 2>&1
    752                     diff "$subtestName"_2.ref output.txt > "$subtestName"_2_output.diff
    753                     set -e
    754                     if [[ -s "$subtestName"_2_output.diff ]]; then
    755                             echo "ERROR : [diff $subtestName_2.ref output.txt] returned non-zero diff" | tee -a ../error
    756                             echo "[cat $subtestName_2_output.diff] output follows" | tee -a ../errors.log
    757                             tee -a < $subtestName_2_output.diff ../errors.log
    758                             exit_status=1
    759                     fi
    760                     continue
  ```

* The first issue is that line 751 uses `./newsrc/octo`. This should have been `../newsrc/octo`. Because of the
  wrong file path, `output.txt` contained an error message from the octo invocation and not the expected output.

* This caused line 752 to generate a non-zero diff file. And because of this we went down the `if` at line 754.

  Notice how lines 751-754 used `"$subtestName"_2` to access the variable `subtestName` and then append `_2` to
  its value.

  But lines 755-757 access it as `$subtestName_2` or `$subtestName_2_output`. This results in bash trying to

tools/ci/build.sh
@@ -763,19 +763,20 @@ else
 				subtestName=$(echo $subtest | cut -d " " -f1)
 				# Run auto upgrade specific test
 				# Disable the "set -e" setting temporarily as we want to check the error reported by Octo
-				mv output.txt "$subtestName"_1_output.txt
-				[[ -e "$subtestName"_2.sql ]]
-				[[ -e "$subtestName"_2.ref ]]
+				mv output.txt ${subtestName}_1_output.txt
+				[[ -e ${subtestName}_2.sql ]]
+				[[ -e ${subtestName}_2.ref ]]
 				set +e
-				./newsrc/octo -f "$subtestName"_2.sql > output.txt 2>&1
-				diff "$subtestName"_2.ref output.txt > "$subtestName"_2_output.diff
+				../newsrc/octo -f ${subtestName}_2.sql > output.txt 2>&1
+				diff ${subtestName}_2.ref output.txt > ${subtestName}_2_output.diff
 				set -e
-				if [[ -s "$subtestName"_2_output.diff ]]; then
-					echo "ERROR : [diff $subtestName_2.ref output.txt] returned non-zero diff" | tee -a ../errors.log
-					echo "[cat $subtestName_2_output.diff] output follows" | tee -a ../errors.log
-					tee -a < $subtestName_2_output.diff ../errors.log
+				if [[ -s ${subtestName}_2_output.diff ]]; then
+					echo "ERROR : [diff ${subtestName}_2.ref output.txt] returned non-zero diff" | tee -a ../errors.log
+					echo "[cat ${subtestName}_2_output.diff] output follows" | tee -a ../errors.log
+					tee -a < ${subtestName}_2_output.diff ../errors.log
 					exit_status=1
 				fi
+				cd ..
 				continue
 			fi
 			# ----------------------------------------------------------------------------
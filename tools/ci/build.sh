YottaDB/YDBOcto

commit 3f853ec552e01d06f7eb4fe8221f6aa5e3ca6429

Fix confusing [Multiple subtests with same name found] message when test-vista pipeline job fails
Background
----------
* The `test-vista` pipeline job failed at https://gitlab.com/YottaDB/DBMS/YDBOcto/-/jobs/4188389574
  with the following error in a rare failure.

  ```
  361 4: not ok 1 setup_file failed
  362 4: # (from function `setup_file' in test file /builds/YottaDB/DBMS/YDBOcto/build/bats_tests/test_vista_database.bats, line 72)
  363 4: #   `curl -sLO https://gitlab.com/YottaDB/DBMS/ydbvistaocto/raw/master/_YDBOCTOVISTAF.m' failed with status 7
      .
      .
  506 # Temporary files in: /builds/YottaDB/DBMS/YDBOcto/build/bats-test.RnRDG7
  507  --> Multiple subtests with name [] found in passed_bats_subtests.txt and/or failed_bats_subtests.txt
  508  --> Please first fix ambiguity by giving the subtests unique names. Exiting.
  ```

Issue
-----
* The original failure was the `curl` failure at line 363 above.

* But the end of the job output had a confusing message about `Multiple subtests with same name found`.
  And the name was the `empty string` as line 507 indicates.

* The issue is that the `setup_file()` function of the `test-vista` job creates a temporary directory
  of the form `bats-test*` but it is a temporary directory and not a real test output directory.
  And if one tries to derive a subtest name from the `bats_test.out` file in this directory, one would
  get an empty string.

Fix
---
* The fix is simple and is to check if the subtest name is the empty string and if so skip doing the
  passed/failed subtest check that is otherwise done for valid subtest names. This will avoid giving
  any confusing error messages.

  The main fix is to `tools/ci/vistatest.sh`.

  While at that, I noticed similar logic in `tools/ci/build.sh`. Although there is no need to do a similar
  fix there, it is better to have it there (keeps the code robust) so added the check there as well.

tools/ci/build.sh
@@ -632,6 +632,16 @@ PSQL
 			fi
 			cd $tstdir
 			subtest=$(sed 's/.*subtest \[//;s/].*//;' bats_test.out)
+			if [ -z $subtest ]; then
+				# See similar code in "tools/ci/vistatest.sh" for why this check is present.
+				# We don't know of any such need here but it might happen in the future so better
+				# to avoid such a situation and so have this safety check as a precaution.
+
+				# $tstdir is a directory that does not contain a valid subtest name in bats_test.out.
+				# Cannot determine if this is a failed or passed or timedout bats test.
+				echo "SUSPECT : $tstdir" >> summary_bats_dirs.txt
+				continue
+			fi
 			# Need -F below in case there are any special characters in the subtest name (e.g. '*')
 			# We do not want to treat those as regex in the grep.
 			passed=$(grep -F -c "$subtest" ../passed_bats_subtests.txt) || true

tools/ci/vistatest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #################################################################
 #								#
-# Copyright (c) 2021-2022 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2021-2023 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -94,6 +94,18 @@ do
 	fi
 	cd $tstdir
 	subtest=$(sed 's/.*subtest \[//;s/].*//;' bats_test.out)
+	if [ -z $subtest ]; then
+		# If a failure happens in the "setup_file()" function in "tests/test_vista_database.bats.in",
+		# we would end up with a temporary directory in the "bats-test*" format but one that does not
+		# have a valid "bats_test.out". And so we would end up with a subtest name that is an empty
+		# string. Skip this directory as we don't want to be doing a grep of an empty string (which
+		# would match all passed and failed subtests) and produce confusing messages.
+
+		# $tstdir is a directory that does not contain a valid subtest name in bats_test.out.
+		# Cannot determine if this is a failed or passed or timedout bats test.
+		echo "SUSPECT : $tstdir" >> summary_bats_dirs.txt
+		continue
+	fi
 	# Need -F below in case there are any special characters in the subtest name (e.g. '*')
 	# We do not want to treat those as regex in the grep.
 	passed=$(grep -F -c "$subtest" ../passed_bats_subtests.txt) || true

commit 6b24314a85dd511136bb083076069f730df2b805

Fix [too many arguments] syntax error in tools/ci/build.sh (regression in 3f853ec5)
Background
----------
* While watching the `make-tls-ubuntu` job in the pipeline at https://gitlab.com/nars1/YDBOcto/-/jobs/4198334372,
  I noticed the following error.

  ```
  ./tools/ci/build.sh: line 635: [: too many arguments
  ```

  The job succeeded inspite of this error. But it would be nice to fix this error.

Issue
-----
* This is a regression introduced in 3f853ec5. A `if [ -z $subtest ];` check can generate a
  `too many arguments` error from bash if `$subtest` evaluates to a list of values.

* See below simple example.

  ```sh
  $ subtest="a b c d"
  $ if [ -z $subtest ]; then echo "abcd"; fi
  bash: [: too many arguments
  $ if [ -z "$subtest" ]; then echo "abcd"; fi
  $
  ```

Fix
---
* As can be seen above surrounding `$subtest` with double-quotes fixes the issue so that is
  what is done in this commit.

* 2 files were similarly modified in 3f853ec5 so both files are fixed in this commit.

tools/ci/build.sh
@@ -632,7 +632,7 @@ PSQL
 			fi
 			cd $tstdir
 			subtest=$(sed 's/.*subtest \[//;s/].*//;' bats_test.out)
-			if [ -z $subtest ]; then
+			if [ -z "$subtest" ]; then
 				# See similar code in "tools/ci/vistatest.sh" for why this check is present.
 				# We don't know of any such need here but it might happen in the future so better
 				# to avoid such a situation and so have this safety check as a precaution.

tools/ci/vistatest.sh
@@ -94,7 +94,7 @@ do
 	fi
 	cd $tstdir
 	subtest=$(sed 's/.*subtest \[//;s/].*//;' bats_test.out)
-	if [ -z $subtest ]; then
+	if [ -z "$subtest" ]; then
 		# If a failure happens in the "setup_file()" function in "tests/test_vista_database.bats.in",
 		# we would end up with a temporary directory in the "bats-test*" format but one that does not
 		# have a valid "bats_test.out". And so we would end up with a subtest name that is an empty
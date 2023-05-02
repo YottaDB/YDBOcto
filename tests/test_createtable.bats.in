YottaDB/YDBOcto

commit 046a3194f91f73ae6cf3334457f66d865313b166

[#913] Error messages identify line numbers in file
Background
----------

Previously, Octo error messages indicated the line number of query errors relative to the start of the query, but omitted line number information relative to the full input, whether received from a file or via `stdin`. Now, Octo issues line numbers both relative to the query start and relative to the start of the input file or full input string received via `stdin`.

Code changes
------------

This change was accomplished by adding a new `line_in_file` variable to `yyerror.c` and storing the line number of the failing query relative to the file beginning before overwriting its value with the line number relative to the query beginning only. This additional value is then included in a subsequent `snprintf()` call later in the file.

Also, logic was added to `yyerror.c` to conditionally revert `old_input_line_begin`, `cur_column`, and `old_input_line_num` to the beginning of current query before outputting second error from line. This is done to prevent incorrect output when multiple errors are present on a single query line, which became possible with the above revisions. This entailed the addition of a new `prev_input_line_num` C global variable for tracking line numbers across calls to `yyerror.c`.

Additionally, a `do` loop in `readline_get_more()` was revised to no longer reset `old_input_index` on buffer resize, which implicitly resets the line number also, leading to erroneous line number output in `yyerror.c`. As part of this revision, the logic of the `do` loop was simplified to make the control flow more clear.

Also, `leading_spaces` is now set to `0` in `octo.c` on buffer reset to prevent off-by-one issues with syntax highlighting when multi-query lines are submitted in succession.

src/octo.h
@@ -631,6 +631,9 @@ typedef enum RegexType {
 		cur_input_max = INIT_QUERY_SIZE - 1;                                         \
 		input_buffer_combined = calloc(1, INIT_QUERY_SIZE);                          \
 		old_input_index = 0;                                                         \
+		old_input_line_num = 0;                                                      \
+		prev_input_line_num = 0;                                                     \
+		old_input_line_begin = input_buffer_combined;                                \
 		cur_input_index = 0;                                                         \
 		cur_input_more = &no_more;                                                   \
 		leading_spaces = 0;                                                          \
@@ -1104,7 +1107,10 @@ extern int	cur_input_index;	    // Current index of input_buffer_combined the pa
 					    // current query.
 extern int old_input_index;		    // The previous value of cur_input_index before the parser modifies it.
 					    // Effectively marks the start of the current query.
-extern int leading_spaces;		    // leading spaces in the current query it needs to be stored somewhere
+extern int   old_input_line_num;	    // The line number pointed to by old_input_index
+extern int   prev_input_line_num;	    // The line number pointed to by the previous value of old_input_index
+extern char *old_input_line_begin;	    // Pointer to the beginning of the line pointed to by old_input_index
+extern int   leading_spaces;		    // leading spaces in the current query it needs to be stored somewhere
 					    // accessible but should be ignored, except by the lexer and yyerror
 extern int   cur_input_max;
 extern int   eof_hit;

commit ff46e5154f6a901690ca34e80f23a963b09a9ff9

[#913] Fix old_input_line_begin maintenance (caused assert failure in readline_get_more.c during seed-reload)
Background
----------
* As part of https://gitlab.com/YottaDB/DBMS/YDBOcto/-/merge_requests/1244#note_1375473876, I was trying out
  some experiments with `seedreload = "TRUE"` in `octo.conf`.

* When I ran the bats test suite, I noticed the `TPK01` subtest fail with a core file in that case.

  ```
  $ cat startinclude_vista-mini.out
  .
  .
  octo: src/readline_get_more.c:73: readline_get_more: Assertion `old_input_line_begin >= input_buffer_combined' failed.
  .
  .
  ```

Issue
-----
* It turns out to be a regression that was introduced by 046a3194.

* That commit introduced a global variable `old_input_line_begin` that is maintained in sync with
  another global variable `input_buffer_combined`.

* This commit did the keep-in-sync part in `src/readline_get_more.c`. But it forgot to do the same
  thing in the `COPY_QUERY_TO_INPUT_BUFFER` macro in `src/octo.h`. That is the issue which caused
  the two to go out of sync when an auto-upgrade (initiated by the `seedreload = "TRUE"` setting in
  octo.conf) recreated the tables based on a long table text definition (the norm in vista-mini.sql).

Fix
---
* As part of this commit, the keep-in-sync code in `src/readline_get_more.c` is now incorporated
  into the `COPY_QUERY_TO_INPUT_BUFFER` macro in `src/octo.h`.

* In addition, in `src/get_display_relation_query_stmt.c`, we save/restore the global variable
  `input_buffer_combined` and so we do the same with the global variable `old_input_line_begin`.

Test
----
* A new `test_createtable/TC077` subtest verifies this fix. Also verified that this test fails the
  assert without the code fixes in this commit.

src/get_display_relation_query_stmt.c
@@ -1,6 +1,6 @@
 /****************************************************************
  *								*
- * Copyright (c) 2022 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2022-2023 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -52,6 +52,7 @@ SqlStatement *get_display_relation_query_stmt(ParseContext *parse_context) {
 	int	  save_cur_input_index = cur_input_index;
 	int	  save_old_input_index = old_input_index;
 	char *	  save_input_buffer_combined = input_buffer_combined;
+	char *	  save_old_input_line_begin = old_input_line_begin;
 	int	  save_leading_spaces = leading_spaces;
 	int (*save_cur_input_more)();
 	assert(cur_input_more == &readline_get_more);
@@ -69,6 +70,7 @@ SqlStatement *get_display_relation_query_stmt(ParseContext *parse_context) {
 	cur_input_index = 0;
 	old_input_index = 0;
 	input_buffer_combined = display_all_relation_query_str;
+	old_input_line_begin = input_buffer_combined;
 	cur_input_more = no_more;
 
 	// Invoke parse_line
@@ -79,6 +81,7 @@ SqlStatement *get_display_relation_query_stmt(ParseContext *parse_context) {
 	cur_input_index = save_cur_input_index;
 	old_input_index = save_old_input_index;
 	input_buffer_combined = save_input_buffer_combined;
+	old_input_line_begin = save_old_input_line_begin;
 	cur_input_more = save_cur_input_more;
 	leading_spaces = save_leading_spaces;
 	eof_hit = save_eof_hit;

src/octo.h
@@ -691,14 +691,18 @@ typedef enum DDLDependencyType {
 		 * by min(cur_input_max * 2, QUERY_LENGTH) + padding (for the \n\0)                                \
 		 */                                                                                                \
 		if (QUERY_LENGTH >= (cur_input_max - cur_input_index - padding)) {                                 \
-			int   resize_amt;                                                                          \
-			char *tmp;                                                                                 \
+			int    resize_amt;                                                                         \
+			size_t old_begin_index;                                                                    \
+			char * tmp;                                                                                \
                                                                                                                    \
 			resize_amt = ((QUERY_LENGTH > (cur_input_max * 2)) ? QUERY_LENGTH : (cur_input_max * 2));  \
 			tmp = malloc(resize_amt + NEWLINE_NEEDED + 1);                                             \
 			memcpy(tmp, input_buffer_combined, cur_input_index);                                       \
+			assert(old_input_line_begin >= input_buffer_combined);                                     \
+			old_begin_index = old_input_line_begin - input_buffer_combined;                            \
 			free(input_buffer_combined);                                                               \
 			input_buffer_combined = tmp;                                                               \
+			old_input_line_begin = &input_buffer_combined[old_begin_index];                            \
 			cur_input_max = resize_amt;                                                                \
 		}                                                                                                  \
 		memcpy(&input_buffer_combined[cur_input_index], QUERY, QUERY_LENGTH);                              \

tests/test_createtable.bats.in
@@ -923,3 +923,16 @@ OCTO
 	verify_output TC076_errors output.txt
 }
 
+@test "TC077 : OCTO913 : Test old_input_line_begin maintenance in readline_get_more.c (used to asert fail)" {
+	# Run only if build is of type "Debug", `seedreload` config is only available in "Debug" build
+	if [[ "Debug" != @CMAKE_BUILD_TYPE@ ]]; then
+		skip
+	fi
+	# This test used to previously assert fail in the second "load_fixture" call below as follows.
+	#	octo: src/readline_get_more.c:73: readline_get_more: Assertion `old_input_line_begin >= input_buffer_combined' failed.
+	#	%YDB-F-KILLBYSIGSINFO1, YottaDB process 93300 has been killed by a signal 6 at address 0x00007F5CC3C8BA7C (vaddr 0x000000E400016C74)
+	echo 'seedreload = "TRUE"' > octo.conf
+	load_fixture vista-mini.sql || true
+	load_fixture vista-mini.sql || true
+}
+
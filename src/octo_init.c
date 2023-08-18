YottaDB/YDBOcto

commit 4db864fa12542b71e0044a01dbac80e40bebe695

[YottaDB/Lang/YDBPython#32] Fix ipc leak in bats tests due to mupip stop of rocto when built with ASAN
Background
----------
* While running bats tests using an Octo built with ASAN, I noticed the `ipcs -a | wc -l` count steadily
  increasing as tests ran/finished. It eventually reached the 1000s at which point various tests started
  failing with the following error.

  ```
  (null):(null)] [ERROR]: ERR_YOTTADB: YottaDB error: 150372546,%PEEKBYNAME+71^%PEEKBYNAME,
  %YDB-E-DBFILERR, Error with database file gtmhelp.dat,
  %YDB-I-TEXT, Error with database control semget,
  %SYSTEM-E-ENO28, No space left on device
  ```

Issue
-----
* After some investigation, found that any test that uses `stop_rocto()` was leaking ipc resources.

  Eventually traced it to the `stop_rocto` function using a `mupip stop` of the rocto server process.
  That sends a SIGTERM signal. And because of recent changes in the `YDB` project side (YottaDB/DB/YDB#1019
  and commit YottaDB/DB/YDB@ae2721d8), the SIGTERM signal immediately terminates the rocto process (as it
  is a Simple API application and building with ASAN had established a default signal handler for SIGTERM
  using the SIG_DFL flag).

  This immediate termination meant that the database files which rocto had opened were not cleanly
  rundown and so the associated ipcs (semaphores and shared memory segments) were not cleaned up.

  Each test that used `stop_rocto()` added to the ipc leak eventually leading to the system running
  out of its ipc limit.

* Experimentation revealed that it is not just `rocto` but `octo` as well that suffers from this issue.
  Having an `octo` process at the `OCTO>` prompt and sending it a SIGTERM also causes an ipc leak.

Fix
---
* The fix in YottaDB/DB/YDB@ae2721d8 was to check if `SIG_DFL` is the signal disposition for SIGTERM
  before the `ydb_init()` call and if so immediately terminate the application on receipt of SIGTERM.

src/octo_init.c
@@ -740,6 +740,23 @@ int octo_init(int argc, char **argv) {
 	// Search for config file octo.conf (OCTO_CONF_FILE_NAME) in directories ".", "~", and "$ydb_dist/plugin/octo" in that order
 	config_init(config_file);
 
+#ifdef ENABLE_ASAN
+	/* If ASAN is enabled, it will establish the default signal handler (SIG_DFL) for SIGTERM which would cause a MUPIP STOP
+	 * that is sent to an octo/rocto process to terminate the process immediately (due to YDB@ae2721d8). That would
+	 * leave semaphores/shared-memory-segments resulting in an ipc leak which eventually builds up and results in
+	 * the system running out of ipc space (e.g. if one runs the entire bats test with an ASAN build of Octo).
+	 * To avoid such a situation, change SIG_DFL to SIG_IGN that way we ensure when the YottaDB SIGTERM signal handler
+	 * gets control, it does not immediately terminate the process but waits for a logical point to do it (and more
+	 * importantly takes care of running down the databases and cleaning up the ipcs).
+	 */
+	struct sigaction act;
+
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	act.sa_handler = SIG_IGN;
+	sigaction(SIGTERM, &act, NULL);
+#endif
+
 	// Note: This loop is only ever executed once. Structured this way to make it easy to break out in case of error code paths.
 	for (;;) {
 		ydb_buffer_t zyrelease, zyrel_value;

src/CMakeLists.txt
@@ -569,12 +569,12 @@ if (ENABLE_ASAN)
 		# If we don't do that, the tests stop, and we cannot proceed any further until we fix the issue
 		# -fsanitize-recover must be combined with ASAN_OPTIONS="halt_on_error=0" to achieve the desired effect.
 		# This feature is not currently used, but is kept here as it doesn't harm anything.
-		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=address -fsanitize-recover=address")
+		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=address -fsanitize-recover=address -DENABLE_ASAN")
 	else()
 		# We had some issues with Clang Sanitizer getting stuck when using psql and TLS with Rocto
 		# Created an issue here: https://github.com/google/sanitizers/issues/1477
 		message(WARNING "-fsanitize=address has some issues in tests for Octo for compiler ${CMAKE_C_COMPILER_ID} ${CMAKE_C_COMPILER_VERSION}")
-		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=address -fsanitize-recover=address")
+		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=address -fsanitize-recover=address -DENABLE_ASAN")
 	endif()
 
 endif()

tests/test_psql_connection.bats.in
@@ -137,6 +137,8 @@ OCTO
 }
 
 @test "TPC00K : ensure socket cleanup when rocto process is killed" {
+  # Note that because of the "kill -9" below, database rundown will not happen so ipcs (semaphores, shared memory segments)
+  # would be left over by this test.
   kill -9 $(cat rocto.pid)
   start_rocto $test_port
   log=$(cat rocto.log)

commit df7f4fd48ff8f20225f8e4561cf2427ca17acf71

[YottaDB/Lang/YDBPython#32] [YottaDB/DB/YDB#1019] Fix SIGTERM of octo/rocto to not leave ipcs around
* 4db864fa fixed an issue observed a few weeks ago when Octo was built using ASAN. See that commit
  message for more details. But the essence is that the default signal handler for SIGTERM was
  thought to be established using the `SIG_DFL` flag only when using Octo built with ASAN.

* The same issue was observed in the past few days by @shabiel even though Octo was built without
  using ASAN. Now that I think about it, the default disposition of the SIGTERM signal should be set to
  `SIG_DFL` at process startup, irrespective of whether it is built with ASAN or not. Not sure why
  during 4db864fa we did not see the issue when Octo was not built with ASAN. I suspect it was because
  we did not try that combination then.

* In any case, given we found the same issue even without ASAN, this commit fixes the issue by
  removing `#ifdef ASAN` introduced in 4db864fa but otherwise keeping the code introduced in that
  commit which establishes a signal handler for SIGTERM to be `SIG_IGN` (instead of `SIG_DFL`).
  With this change, the YottaDB signal handler for SIGTERM will not immediately terminate the
  octo/rocto process on receipt of SIGTERM. But instead will wait for a logical point to be reached
  and invoke the exit handlers which will take care of cleaning the ipcs. And avoid the issue
  where running the entire bats test causes a lot of ipcs to be left around.

src/octo_init.c
@@ -740,14 +740,13 @@ int octo_init(int argc, char **argv) {
 	// Search for config file octo.conf (OCTO_CONF_FILE_NAME) in directories ".", "~", and "$ydb_dist/plugin/octo" in that order
 	config_init(config_file);
 
-#ifdef ENABLE_ASAN
-	/* If ASAN is enabled, it will establish the default signal handler (SIG_DFL) for SIGTERM which would cause a MUPIP STOP
-	 * that is sent to an octo/rocto process to terminate the process immediately (due to YDB@ae2721d8). That would
-	 * leave semaphores/shared-memory-segments resulting in an ipc leak which eventually builds up and results in
-	 * the system running out of ipc space (e.g. if one runs the entire bats test with an ASAN build of Octo).
-	 * To avoid such a situation, change SIG_DFL to SIG_IGN that way we ensure when the YottaDB SIGTERM signal handler
-	 * gets control, it does not immediately terminate the process but waits for a logical point to do it (and more
-	 * importantly takes care of running down the databases and cleaning up the ipcs).
+	/* At octo/rocto process startup, the default signal handler (SIG_DFL) for SIGTERM would be established which would
+	 * cause a MUPIP STOP that is sent to an octo/rocto process to terminate the process immediately (due to YDB@ae2721d8).
+	 * That would leave semaphores/shared-memory-segments resulting in an ipc leak which eventually builds up and results in
+	 * the system running out of ipc space (e.g. if one runs the entire bats test). To avoid such a situation, change SIG_DFL
+	 * to SIG_IGN that way we ensure when the YottaDB SIGTERM signal handler gets control, it does not immediately terminate
+	 * the process but waits for a logical point to do it (and more importantly takes care of running down the databases and
+	 * cleaning up the ipcs).
 	 */
 	struct sigaction act;
 
@@ -755,7 +754,6 @@ int octo_init(int argc, char **argv) {
 	act.sa_flags = 0;
 	act.sa_handler = SIG_IGN;
 	sigaction(SIGTERM, &act, NULL);
-#endif
 
 	// Note: This loop is only ever executed once. Structured this way to make it easy to break out in case of error code paths.
 	for (;;) {
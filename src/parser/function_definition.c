YottaDB/YDBOcto

commit 358c9a393be10d79c73920ae1366f0d66bd47b10

[#929] Fix auto upgrade (of uppercase to lowercase name tables/functions/views) to work even if rerun
Background
----------
* A user reported seeing an `ERR_DUPLICATE_PRIMARY_KEY_CONSTRAINT` error after installing the latest
  Octo version (master branch) and attempting to enter the `OCTO>` prompt.

* If they come out of Octo and try to run it again, they kept getting the same error at startup.

* It is not clear what caused the original error.

* But we know that this error is happening in the auto upgrade step which kicks in at Octo startup.
  There was logic introduced as part of 23b2d60c to do a special auto upgrade to convert table/function/view
  names from upper case to lower case by default.

* The error is issued somewhere in there.

Issue 1
=======
* Although the cause of the original error is not known, I was able to reproduce the error by doing
  the following (to effectively force an auto upgrade even though there is no need of one).

  ```sh
  $ octo
  OCTO> create table tmp (id integer primary key);
  CREATE TABLE
  OCTO> exit

  $ yottadb -run %XCMD 'set ^%ydboctoocto("seeddfnfmt")=6'

  $ octo
  [ERROR]: ERR_DUPLICATE_PRIMARY_KEY_CONSTRAINT: PRIMARY KEY constraint name "tmp_PKEY" already exists in table "tmp"
  ```

* The issue is that the auto upgrade process checks if the table name in the `CREATE TABLE` command (stored
  in the table text definition in global nodes) is inside double quotes or not. If not, it assumes that the
  table name would be stored internally in upper case and so generates a `DROP TABLE` command using the
  upper case table name. And then generates a `CREATE TABLE` command to recreate the table using the lower
  case table name.

* But if the auto upgrade has already happened, the table name is stored internally in lower case. Therefore
  the `DROP TABLE` command that is generated by the forced auto upgrade does nothing because only the lower

src/parser/function_definition.c
@@ -50,90 +50,122 @@ SqlStatement *function_definition(SqlStatement *identifier, SqlStatement *functi
 	UNPACK_SQL_STATEMENT(extrinsic_function_value, function->extrinsic_function, value);
 	extrinsic_function_value->type = FUNCTION_NAME;
 
-	boolean_t octo929_drop_function = FALSE;
-	if (config->is_auto_upgrade_octo929) {
-		SqlValue *value;
-		char *	  function_name, *orig_name, uppercase_name[OCTO_MAX_IDENT + 1];
-		UNPACK_SQL_STATEMENT(value, identifier, value);
-		orig_name = value->v.string_literal;
-		if (!value->is_double_quoted) {
-			char *start, *end, *dst, *dst_end;
+	/* See comment in "src/parser/table_definition.c" for why "for" loop that runs 2 iterations is needed */
+	int i;
+	for (i = 0; i < 2; i++) {
+		boolean_t octo929_drop_function = FALSE;
+		boolean_t need_iteration_two;
 
-			start = orig_name;
-			end = start + strlen(start);
-			dst = uppercase_name;
-			dst_end = dst + sizeof(uppercase_name);
-			TOUPPER(dst, dst_end, start, end);
-			function_name = uppercase_name;
-		} else {
-			function_name = orig_name;
-		}
+		need_iteration_two = FALSE;
+		if (config->is_auto_upgrade_octo929) {
+			SqlValue *value;
+			char *	  function_name, *orig_name, upper_or_lower_case_name[OCTO_MAX_IDENT + 1];
+			UNPACK_SQL_STATEMENT(value, identifier, value);
+			orig_name = value->v.string_literal;
+			if (!value->is_double_quoted) {
+				char *start, *end, *dst, *dst_end;
 
-		hash128_state_t state;
-		char		function_hash[MAX_ROUTINE_LEN + 1];
-		int		status;
-		value->v.string_literal = function_name;	 /* temporarily tamper function name for hash computation */
-		INVOKE_HASH_CANONICAL_QUERY(state, ret, status); /* "state" holds final hash */
-		if (0 != status) {
-			return NULL;
-		}
-		generate_name_type(FunctionHash, &state, 0, function_hash, sizeof(function_hash));
-		value->v.string_literal = orig_name; /* Restore original function name */
+				start = orig_name;
+				end = start + strlen(start);
+				dst = upper_or_lower_case_name;
+				dst_end = dst + sizeof(upper_or_lower_case_name);
+				if (0 == i) {
+					TOUPPER(dst, dst_end, start, end);
+					need_iteration_two = TRUE; /* for lower case DROP FUNCTION command */
+				} else {
+					TOLOWER(dst, dst_end, start, end);
+				}
+				function_name = upper_or_lower_case_name;
+			} else {
+				function_name = orig_name;
+				need_iteration_two = FALSE;
+			}
 
-		ydb_buffer_t ydbocto929, func_subs[2];
-		char	     subs0_buff[INT32_TO_STRING_MAX];
-		unsigned int data_ret;
+			hash128_state_t state;
+			char		function_hash[MAX_ROUTINE_LEN + 1];
+			int		status;
+			value->v.string_literal = function_name;	 /* temporarily tamper function name for hash computation */
+			INVOKE_HASH_CANONICAL_QUERY(state, ret, status); /* "state" holds final hash */
+			if (0 != status) {
+				return NULL;
+			}
+			generate_name_type(FunctionHash, &state, 0, function_hash, sizeof(function_hash));
+			value->v.string_literal = orig_name; /* Restore original function name */
 
-		YDB_LITERAL_TO_BUFFER(OCTOLIT_YDBOCTO929, &ydbocto929);
-		func_subs[0].buf_addr = subs0_buff;
-		func_subs[0].len_alloc = sizeof(subs0_buff);
-		func_subs[0].len_used = snprintf(func_subs[0].buf_addr, func_subs[0].len_alloc, "%d", drop_function_STATEMENT);
-		YDB_STRING_TO_BUFFER(function_hash, &func_subs[1]);
-		status = ydb_data_s(&ydbocto929, 2, &func_subs[0], &data_ret);
-		if (YDB_OK != status) {
-			YDB_ERROR_CHECK(status);
-			return NULL;
-		}
-		if (0 == data_ret) {
-			status = ydb_set_s(&ydbocto929, 2, &func_subs[0], NULL);
+			ydb_buffer_t ydbocto929, func_subs[2];
+			char	     subs0_buff[INT32_TO_STRING_MAX];
+			unsigned int data_ret;
+
+			YDB_LITERAL_TO_BUFFER(OCTOLIT_YDBOCTO929, &ydbocto929);
+			func_subs[0].buf_addr = subs0_buff;
+			func_subs[0].len_alloc = sizeof(subs0_buff);
+			func_subs[0].len_used
+			    = snprintf(func_subs[0].buf_addr, func_subs[0].len_alloc, "%d", drop_function_STATEMENT);
+			YDB_STRING_TO_BUFFER(function_hash, &func_subs[1]);
+			status = ydb_data_s(&ydbocto929, 2, &func_subs[0], &data_ret);
 			if (YDB_OK != status) {
 				YDB_ERROR_CHECK(status);
 				return NULL;
 			}
-			fprintf(config->octo929_sqlfile_stream, "DROP FUNCTION IF EXISTS \"%s\"(", function_name);
-			octo929_drop_function = TRUE;
-		}
-	}
-
-	if (NULL != function->parameter_type_list) {
-		SqlParameterTypeList *start_parameter_type, *cur_parameter_type;
-		UNPACK_SQL_STATEMENT(start_parameter_type, function->parameter_type_list, parameter_type_list);
-		cur_parameter_type = start_parameter_type;
-
-		// Count the number of arguments for later storage in `pg_proc`, issue error if it exceeds YDB_MAX_PARMS
-		function->num_args = 0;
-		do {
-			function->num_args++;
-			if (YDB_MAX_PARMS < function->num_args) {
-				ERROR(ERR_TOO_MANY_FUNCTION_ARGUMENTS, function_name_value->v.string_literal, YDB_MAX_PARMS);
-				return NULL;
-			}
-			if (octo929_drop_function) {
-				char typestr[64]; /* this should be more than enough to hold the type name */
-
-				get_user_visible_data_type_string(&cur_parameter_type->data_type_struct->v.data_type_struct,
-								  typestr, sizeof(typestr));
-				fprintf(config->octo929_sqlfile_stream, "%s", typestr);
-				if (start_parameter_type != cur_parameter_type->next) {
-					fprintf(config->octo929_sqlfile_stream, ",");
+			if (0 == data_ret) {
+				status = ydb_set_s(&ydbocto929, 2, &func_subs[0], NULL);
+				if (YDB_OK != status) {
+					YDB_ERROR_CHECK(status);
+					return NULL;
 				}
+				/* If we are loading "octo-seed.sql" and are encountering "CREATE FUNCTION" commands there,
+				 * we don't need to worry about deleting the upper case version function as such
+				 * commands are included in "octo-seed.sql" itself at the start so skip generating the
+				 * "DROP FUNCTION" command in that case.
+				 */
+				if (!config->in_auto_load_octo_seed) {
+					fprintf(config->octo929_sqlfile_stream, "DROP FUNCTION IF EXISTS \"%s\"(", function_name);
+					octo929_drop_function = TRUE;
+				}
+			} else {
+				/* We already did a DROP FUNCTION of the upper case name. In that case, don't do a DROP FUNCTION
+				 * of the lower case name as it can incorrectly lead to permanently deleting a valid lower
+				 * case table name at the end of the auto upgrade.
+				 */
+				assert(0 == i);
+				need_iteration_two = FALSE;
 			}
-			cur_parameter_type = cur_parameter_type->next;
-		} while (start_parameter_type != cur_parameter_type);
-	}
+		}
 
-	if (octo929_drop_function) {
-		fprintf(config->octo929_sqlfile_stream, ");\n");
+		if (NULL != function->parameter_type_list) {
+			SqlParameterTypeList *start_parameter_type, *cur_parameter_type;
+			UNPACK_SQL_STATEMENT(start_parameter_type, function->parameter_type_list, parameter_type_list);
+			cur_parameter_type = start_parameter_type;
+
+			// Count the number of arguments for later storage in `pg_proc`, issue error if it exceeds YDB_MAX_PARMS
+			function->num_args = 0;
+			do {
+				function->num_args++;
+				if (YDB_MAX_PARMS < function->num_args) {
+					ERROR(ERR_TOO_MANY_FUNCTION_ARGUMENTS, function_name_value->v.string_literal,
+					      YDB_MAX_PARMS);
+					return NULL;
+				}
+				if (octo929_drop_function) {
+					char typestr[64]; /* this should be more than enough to hold the type name */
+
+					get_user_visible_data_type_string(&cur_parameter_type->data_type_struct->v.data_type_struct,
+									  typestr, sizeof(typestr));
+					fprintf(config->octo929_sqlfile_stream, "%s", typestr);
+					if (start_parameter_type != cur_parameter_type->next) {
+						fprintf(config->octo929_sqlfile_stream, ",");
+					}
+				}
+				cur_parameter_type = cur_parameter_type->next;
+			} while (start_parameter_type != cur_parameter_type);
+		}
+
+		if (octo929_drop_function) {
+			fprintf(config->octo929_sqlfile_stream, ");\n");
+		}
+		if (!need_iteration_two) {
+			break;
+		}
 	}
 	return ret;
 }

src/parser/table_definition.c
@@ -158,45 +158,85 @@ SqlStatement *table_definition(SqlStatement *tableName, SqlStatement *table_elem
 	table->tableName = tableName;
 	if (config->is_auto_upgrade_octo929) {
 		SqlValue *value;
-		char *	  table_name, uppercase_name[OCTO_MAX_IDENT + 1];
+		char *	  table_name, upper_or_lower_case_name[OCTO_MAX_IDENT + 1];
 
 		UNPACK_SQL_STATEMENT(value, tableName, value);
-		if (!value->is_double_quoted) {
-			char *start, *end, *dst, *dst_end;
 
-			start = value->v.string_literal;
-			end = start + strlen(start);
-			dst = uppercase_name;
-			dst_end = dst + sizeof(uppercase_name);
-			TOUPPER(dst, dst_end, start, end);
-			table_name = uppercase_name;
-		} else {
-			table_name = value->v.string_literal;
-		}
+		/* If table name was not double quoted, that means it would have been stored internally as an upper case
+		 * name before YDBOcto#929. Therefore generate a DROP TABLE command to drop the upper case table name.
+		 * But it is possible there is an error during the very first auto upgrade due to some operational issues
+		 * which later get fixed. In that case, the table binary/text definition would have been recreated using a
+		 * lower case table name at the start of the aborted auto upgrade. That would mean the second auto upgrade
+		 * would generate a DROP TABLE command on a non-existent upper case table name. And that would mean the
+		 * CREATE TABLE command that is generated immediately after the DROP TABLE command would issue an error
+		 * (e.g. ERR_DUPLICATE_PRIMARY_KEY_CONSTRAINT) and again abort the later auto upgrade attempts. To avoid
+		 * such errors, generate a DROP TABLE command to drop the lower case table name as well. Hence the "for" loop
+		 * that runs twice in case "value->is_double_quoted" is FALSE.
+		 */
+		int i;
+		for (i = 0; i < 2; i++) {
+			if (!value->is_double_quoted) {
+				char *start, *end, *dst, *dst_end;
 
-		ydb_buffer_t ydbocto929, table_subs[2];
-		char	     subs0_buff[INT32_TO_STRING_MAX];
-		unsigned int data_ret;
+				start = value->v.string_literal;
+				end = start + strlen(start);
+				/* Generate upper or lower case table name depending on for loop iteration number */
+				dst = upper_or_lower_case_name;
+				dst_end = dst + sizeof(upper_or_lower_case_name);
+				if (0 == i) {
+					TOUPPER(dst, dst_end, start, end);
+				} else {
+					TOLOWER(dst, dst_end, start, end);
+				}
+				table_name = upper_or_lower_case_name;
+			} else {
+				assert(0 == i);
+				table_name = value->v.string_literal;
+			}
 
-		YDB_LITERAL_TO_BUFFER(OCTOLIT_YDBOCTO929, &ydbocto929);
-		table_subs[0].buf_addr = subs0_buff;
-		table_subs[0].len_alloc = sizeof(subs0_buff);
-		table_subs[0].len_used = snprintf(table_subs[0].buf_addr, table_subs[0].len_alloc, "%d", drop_table_STATEMENT);
-		YDB_STRING_TO_BUFFER(table_name, &table_subs[1]);
+			ydb_buffer_t ydbocto929, table_subs[2];
+			char	     subs0_buff[INT32_TO_STRING_MAX];
+			unsigned int data_ret;
 
-		int status;
-		status = ydb_data_s(&ydbocto929, 2, &table_subs[0], &data_ret);
-		if (YDB_OK != status) {
-			YDB_ERROR_CHECK(status);
-			return NULL;
-		}
-		if (0 == data_ret) {
-			status = ydb_set_s(&ydbocto929, 2, &table_subs[0], NULL);
+			YDB_LITERAL_TO_BUFFER(OCTOLIT_YDBOCTO929, &ydbocto929);
+			table_subs[0].buf_addr = subs0_buff;
+			table_subs[0].len_alloc = sizeof(subs0_buff);
+			table_subs[0].len_used
+			    = snprintf(table_subs[0].buf_addr, table_subs[0].len_alloc, "%d", drop_table_STATEMENT);
+			YDB_STRING_TO_BUFFER(table_name, &table_subs[1]);
+
+			int status;
+			status = ydb_data_s(&ydbocto929, 2, &table_subs[0], &data_ret);
 			if (YDB_OK != status) {
 				YDB_ERROR_CHECK(status);
 				return NULL;
 			}
-			fprintf(config->octo929_sqlfile_stream, "DROP TABLE IF EXISTS \"%s\" KEEPDATA;\n", table_name);
+			if (0 == data_ret) {
+				status = ydb_set_s(&ydbocto929, 2, &table_subs[0], NULL);
+				if (YDB_OK != status) {
+					YDB_ERROR_CHECK(status);
+					return NULL;
+				}
+				/* If we are loading "octo-seed.sql" and are encountering "CREATE TABLE" commands there,
+				 * we don't need to worry about deleting the upper case version table as such
+				 * commands are included in "octo-seed.sql" itself at the start so skip generating the
+				 * "DROP TABLE" command in that case.
+				 */
+				if (!config->in_auto_load_octo_seed) {
+					fprintf(config->octo929_sqlfile_stream, "DROP TABLE IF EXISTS \"%s\" KEEPDATA;\n",
+						table_name);
+				}
+				if (value->is_double_quoted) {
+					break;
+				}
+			} else {
+				/* We already did a DROP TABLE of the upper case name. In that case, don't do a DROP TABLE
+				 * of the lower case name as it can incorrectly lead to permanently deleting a valid lower
+				 * case table name at the end of the auto upgrade.
+				 */
+				assert(0 == i);
+				break;
+			}
 		}
 	}
 	table->columns = table_element_list;

src/octo.h
@@ -398,13 +398,13 @@
  */
 #define TOLOWER(DEST, DEST_END, START, END)                                                 \
 	{                                                                                   \
-		char *start;                                                                \
+		char *sTART;                                                                \
                                                                                             \
-		start = (START);                                                            \
-		while (start < END) {                                                       \
-			(*DEST) = tolower(*start);                                          \
+		sTART = (START);                                                            \
+		while (sTART < END) {                                                       \
+			(*DEST) = tolower(*sTART);                                          \
 			(DEST)++;                                                           \
-			start++;                                                            \
+			sTART++;                                                            \
 		}                                                                           \
 		(*DEST) = '\0';                                                             \
 		(DEST)++;                                                                   \

src/view_definition.c
@@ -30,22 +30,38 @@ SqlStatement *view_definition(SqlStatement *create_view_stmt, ParseContext *pars
 	SqlStatement *viewName = view->viewName;
 	if (config->is_auto_upgrade_octo929) {
 		SqlValue *value;
-		char *	  view_name, uppercase_name[OCTO_MAX_IDENT + 1];
+		char *	  view_name, upper_or_lower_case_name[OCTO_MAX_IDENT + 1];
 
-		UNPACK_SQL_STATEMENT(value, viewName, value);
-		if (!value->is_double_quoted) {
-			char *start, *end, *dst, *dst_end;
+		/* See comment in "src/parser/table_definition.c" for why "for" loop that runs 2 iterations is needed */
+		int i;
+		for (i = 0; i < 2; i++) {
+			UNPACK_SQL_STATEMENT(value, viewName, value);
+			if (!value->is_double_quoted) {
+				char *start, *end, *dst, *dst_end;
 
-			start = value->v.string_literal;
-			end = start + strlen(start);
-			dst = uppercase_name;
-			dst_end = dst + sizeof(uppercase_name);
-			TOUPPER(dst, dst_end, start, end);
-			view_name = uppercase_name;
-		} else {
-			view_name = value->v.string_literal;
+				start = value->v.string_literal;
+				end = start + strlen(start);
+				dst = upper_or_lower_case_name;
+				dst_end = dst + sizeof(upper_or_lower_case_name);
+				if (0 == i) {
+					TOUPPER(dst, dst_end, start, end);
+				} else {
+					TOLOWER(dst, dst_end, start, end);
+				}
+				view_name = upper_or_lower_case_name;
+			} else {
+				view_name = value->v.string_literal;
+			}
+			/* octo-seed.sql only has CREATE TABLE and CREATE FUNCTION commands, no CREATE VIEW commands so
+			 * assert that as we don't do "OCTOLIT_YDBOCTO929" maintenance and "config->in_auto_load_octo_seed"
+			 * checks like we do in "table_definition.c" and "function_definition.c".
+			 */
+			assert(!config->in_auto_load_octo_seed);
+			fprintf(config->octo929_sqlfile_stream, "DROP VIEW IF EXISTS \"%s\";\n", view_name);
+			if (value->is_double_quoted) {
+				break;
+			}
 		}
-		fprintf(config->octo929_sqlfile_stream, "DROP VIEW IF EXISTS \"%s\";\n", view_name);
 	}
 	SqlStatement *column_name_list = view->column_name_list;
 	SqlStatement *query_expression = view->src_table_alias_stmt;

tests/fixtures/TC080.sql
@@ -1,0 +1,19 @@
+#################################################################
+#								#
+# Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+-- TC080 : OCTO929 : Test that repeated auto upgrade runs work fine with user defined tables
+
+-- Below queries verify that tables/function that were added to octo-seed.sql after YDBOcto#929 (23b2d60c) are still there
+-- We take one representative function and table.
+select pg_tablespace_location(1);
+select * from pg_depend;
+

tests/fixtures/TCF038.sql
@@ -1,0 +1,18 @@
+#################################################################
+#								#
+# Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+-- TCF038 : OCTO929 : Test that repeated auto upgrade runs work fine with user defined functions
+
+CREATE FUNCTION tmpFunc(integer) returns integer as $$^func;
+CREATE TABLE tmpTable (id integer);
+CREATE VIEW tmpView as select * from names;
+

tests/fixtures/TCV056.sql
@@ -1,0 +1,24 @@
+#################################################################
+#								#
+# Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+-- TCV056 : OCTO929 : Test that repeated auto upgrade runs work fine with user defined views
+
+-- Remove user defined tables created by test "setup()" function
+-- This is needed to ensure we see a ERR_CANNOT_CREATE_VIEW error (without the code fixes).
+-- Or else we would see a ERR_DUPLICATE_PRIMARY_KEY_CONSTRAINT error due to these tables.
+DROP TABLE names;
+DROP TABLE nameswithages;
+
+-- View definition cannot use any existing tables (or else ERR_DUPLICATE_PRIMARY_KEY_CONSTRAINT errorwill
+-- show up as mentioned above. So we use VALUES clause below.
+CREATE VIEW TCV056 as values(1);
+

tests/outref/TC080.ref
@@ -1,0 +1,19 @@
+# Create a user-defined table (by loading names.sql)
+# Set ^%ydboctoocto("seeddfnfmt") to 6 (before YDBOcto#929 changes)
+# Start octo which will automatically run auto upgrade due to previous step
+# We expect no errors in this step
+# This step used to previously issue an ERR_DUPLICATE_PRIMARY_KEY_CONSTRAINT error
+# Also verify that tables/function that were added to octo-seed.sql after YDBOcto#929 (23b2d60c) are still there
+
+-- TC080 : OCTO929 : Test that repeated auto upgrade runs work fine with user defined tables
+
+-- Below queries verify that tables/function that were added to octo-seed.sql after YDBOcto#929 (23b2d60c) are still there
+-- We take one representative function and table.
+select pg_tablespace_location(1);
+select * from pg_depend;
+
+pg_tablespace_location
+
+(1 row)
+classid|objid|objsubid|refclassid|refobjid|refobjsubid|deptype
+(0 rows)

commit 8642db7dd5f6ea18f994779c6d013a41899f0a41

[#929] [DEBUG-ONLY] Fix incorrect assert introduced in 358c9a39
Background
----------
* The in-house test-auto-upgrade nightly cron job failed while replaying queries from the `TSCP25`
  subtest with the following failure symptom.

  ```
  ERROR : [newsrc/octo -f bats-test.5HLoZ7/TSCP25_underscore_name_result_crosscheck-10.sql] > autoupgrade.TSCP25_underscore_name_result_crosscheck-10.sql.out : Exit status = 250
  ERROR : --> It is likely that bumping up FMT_BINARY_DEFINITION would fix such failures
  ```

* In this case, the chosen old commit was 8f6e404c.

* I was able to reproduce this manually using the following commands on an Octo built from 8f6e404c.

  ```sql
  $ octo
  OCTO> create function __(integer) returns integer as $$samevalue^functions;
  CREATE FUNCTION
  OCTO> quit
  $
  ```

  And then using an Octo built from the master branch, try to go to the Octo prompt.

  ```sql
  $ octo
  octo: src/parser/function_definition.c:130: function_definition: Assertion `0 == i' failed.
  $
  ```

Issue
-----
* The issue is that the assert did not take into account that there could be function names which are
  the same whether they are in upper case or lower case. That is there could be function names with no
  alphabets. For example `__`. In that case, the assert that was recently added in 358c9a39 is not valid.

Fix
---
* The fix is simple and is to remove the invalid assert in `src/parser/function_definition.c`. A comment
  is added in the code to describe why the assert cannot be done.

* A similar issue existed in `src/parser/table_definition.c` and that is fixed as well in this commit.

src/parser/function_definition.c
@@ -123,11 +123,13 @@ SqlStatement *function_definition(SqlStatement *identifier, SqlStatement *functi
 					octo929_drop_function = TRUE;
 				}
 			} else {
-				/* We already did a DROP FUNCTION of the upper case name. In that case, don't do a DROP FUNCTION
-				 * of the lower case name as it can incorrectly lead to permanently deleting a valid lower
-				 * case table name at the end of the auto upgrade.
+				/* In case the function name is an upper case name, coming here means we already did a DROP FUNCTION
+				 * of the upper case name. In that case, don't do a DROP FUNCTION of the lower case name as it can
+				 * incorrectly lead to deleting a valid lower case function name at the end of the auto upgrade.
+				 * Note though that function names could be of the form "__" which have the same name in upper or
+				 * lower case so those can come here when i=0 and i=1 hence we cannot "assert(0 == i)" below
+				 * even though that is true almost 99.99% of the time.
 				 */
-				assert(0 == i);
 				need_iteration_two = FALSE;
 			}
 		}

src/parser/table_definition.c
@@ -230,11 +230,13 @@ SqlStatement *table_definition(SqlStatement *tableName, SqlStatement *table_elem
 					break;
 				}
 			} else {
-				/* We already did a DROP TABLE of the upper case name. In that case, don't do a DROP TABLE
-				 * of the lower case name as it can incorrectly lead to permanently deleting a valid lower
-				 * case table name at the end of the auto upgrade.
+				/* In case the table name is an upper case name, coming here means we already did a DROP TABLE
+				 * of the upper case name. In that case, don't do a DROP TABLE of the lower case name as it can
+				 * incorrectly lead to deleting a valid lower case table name at the end of the auto upgrade.
+				 * Note though that table names could be of the form "__" which have the same name in upper or
+				 * lower case so those can come here when i=0 and i=1 hence we cannot "assert(0 == i)" below
+				 * even though that is true almost 99.99% of the time.
 				 */
-				assert(0 == i);
 				break;
 			}
 		}
YottaDB/YDBOcto

commit 23b2d60cf8882e0fd9ce89515d0ca4cb2a03d39f

[#985] Implement basic DBeaver client support; [#929] Store case-insensitive SQL identifiers in lowercase
Background
----------
* This commit started out with the objective of getting `DBeaver` client supported with Octo.

* While implementing the various hurdles encountered in that process, we noticed that #929 was
  also a pre-requisite as DBeaver generated queries which assumed that the column name was stored
  in lower case. For example, in the `names` table, it would generate queries using `"id"` as the
  column name where `id` is double quoted in lower case. This won't work in Octo as it stored the
  column name as `"ID"` internally. Therefore, the changes for #929 are also included in this commit.

[#985] : Implement basic DBeaver client support
===============================================
* Getting DBeaver to work meant getting various queries that it generated at rocto startup to work.

* This meant implementing the following functions.
  - format_type() function
  - substring() function
  - pg_catalog.pg_get_partkeydef() function
  - pg_catalog.pg_get_expr() function
  - pg_catalog.pg_total_relation_size() function
  - pg_catalog.pg_relation_size() function
  - pg_catalog.pg_get_constraintdef() function
  - pg_catalog.pg_get_ruledef() function
  - pg_catalog.pg_stat_get_numscans() function
  - pg_catalog.pg_tablespace_location() function

* And implementing the following tables.
  - pg_catalog.pg_depend table
  - pg_catalog.pg_trigger table
  - pg_catalog.pg_tablespace table
  - pg_catalog.pg_shdescription table
  - pg_catalog.pg_inherits table
  - pg_catalog.pg_policies table
  - pg_catalog.pg_rewrite table
  - pg_catalog.pg_conversion table
  - pg_catalog.pg_aggregate table
  - pg_catalog.pg_language table

* All the above functions/tables are implemented with minimal functionality so as to get DBeaver
  working enough that it lets us see table data and table metadata.

tools/ci/build.sh
@@ -299,6 +299,8 @@ if [[ ("test-auto-upgrade" == $jobname) && ("force" != $subtaskname) ]]; then
 		fi
 		commitsha=$autoupgrade_old_commit
 	else
+		# Checkout a random prior commit to test if auto-upgrade of plans/xrefs/triggers/binary-table-definitions etc.
+		# from that commit to the current/latest commit works fine in Octo.
 		if [[ $CI_COMMIT_BRANCH == "" ]]; then
 			# This is possible if the pipeline runs for example when a new tag is created on a pre-existing commit.
 			# (for example when the r1.0.0 tag was created). In this case, treat this job as a success.
@@ -308,8 +310,7 @@ if [[ ("test-auto-upgrade" == $jobname) && ("force" != $subtaskname) ]]; then
 		fi
 		# Record git log --all output in a file just in case it helps later. Not used by this script.
 		git log --graph --all --oneline --pretty=format:'%h%d; %ai; %an; %s' > gitlogall.txt
-		# Checkout a random prior commit to test if auto-upgrade of plans/xrefs/triggers/binary-table-definitions etc.
-		# from that commit to the current/latest commit works fine in Octo.
+		# Create and checkout a new branch from the current commit HEAD to avoid modifying the commit branch
 		git checkout -B $CI_COMMIT_BRANCH HEAD
 		# Copy M program that is needed for later before we switch to an older git branch.
 		cp ../tools/ci/testAutoUpgrade.m .
@@ -839,6 +840,13 @@ else
 		# we don't run into stack space issues (which can show up as a "Segmentation fault (core dumped)" error).
 		ulimit -s 131072
 
+		# As part of YDBOcto#929 upgrade, we could run a DROP TABLE followed by a CREATE TABLE of the same table name.
+		# If so, we could encounter asserts like the following when we replay tests which have DROP/CREATE TABLE commands
+		# (e.g. TSCP25 subtest which does a "DROP TABLE __" and "CREATE TABLE __").
+		#	octo: /builds/YDBDBMS/src/find_view_or_table.c:96: find_view_or_table_in_cache: Assertion `NULL != getenv("octo_dbg_drop_cache_expected")' failed.
+		# Avoid such assert failures by setting the env var below during the replay.
+		export octo_dbg_drop_cache_expected=1
+
 		# Point src to newsrc
 		ln -s newsrc src
 		for tstdir in bats-test.*
@@ -883,6 +891,11 @@ else
 				# (e.g. an error message format might be different etc.) and we want to use the latest version.
 				# Hence the use of "cp -f" below.
 				cp -f ../../tests/outref/${subtestName}_2.ref .
+				# Additionally, as part of YDBOcto#929, we execute a lot of queries during auto upgrade so
+				# it is possible to see DBFILEXT messages in the syslog (which will show up in stderr when
+				# run in the pipeline under docker) so filter those out before the diff.
+				mv output.txt output.txt.orig
+				grep -v DBFILEXT output.txt.orig > output.txt
 				diff ${subtestName}_2.ref output.txt > ${subtestName}_2_output.diff
 				set -e
 				if [[ -s ${subtestName}_2_output.diff ]]; then

commit 7c073a2bb2b33fc965c1aa86ab3dd0a8069f3da9

[#929] Fix column name case conversion logic in tools/ci/build.sh (regression in 23b2d60c)
Background
----------
* While analyzing the `test-auto-upgrade` job failure using the in-house script (see
  https://gitlab.com/YottaDB/DBMS/YDBOcto/-/merge_requests/1460#note_1550438831 for more details),
  @zylog1O1 noticed that a lot of tests failed when the random older commit chosen was close to
  the master branch.

* For example, the `hello_bats` test failed and along with a lot of tests (around 110 of them) failed.

Issue
-----
* All test failures had the following symptom.

  ```
  # Loading fixture names.sql
  # [ERROR]: ERR_UNKNOWN_COLUMN_NAME: Unknown column: ID
  # [ERROR]: ERR_AUTO_SEED_LOAD: Failed to load internal tables and functions
  ```

* I then tried running `octo -vv` to debug the above error and got the below output.

  ```sql
  [ERROR] /builds/YDBDBMS/src/parser/validate_global_keyword.c:61 2023-09-11 09:41:26: ERR_UNKNOWN_COLUMN_NAME: Unknown column: ID
  [ INFO] /builds/YDBDBMS/src/run_query.c:191 2023-09-11 09:41:26: INFO_PARSING_DONE: Parsing done for SQL command [/* Has exactly one row, and is used to allow SELECT without a FROM clause */
  CREATE TABLE octoOneRowTable (id INTEGER primary key) GLOBAL "^%ydboctoocto(""tables"",""octoOneRow"",keys(""ID""))" READONLY;]
  [ INFO] /builds/YDBDBMS/src/run_query.c:193 2023-09-11 09:41:26: INFO_RETURNING_FAILURE: Returning failure from run_query
  [ERROR] /builds/YDBDBMS/src/auto_load_octo_seed.c:109 2023-09-11 09:41:26: ERR_AUTO_SEED_LOAD: Failed to load internal tables and functions
  ```

* Notice the upper case `ID` in the `CREATE TABLE` command above.

* But as part of 23b2d60c, case insensitive column names are stored in lower case by default.
  Therefore, the `id` column should be used with lower case. That is the `CREATE TABLE` command above
  should have used `keys(""id"")` and not `keys(""ID"")`.

tools/ci/build.sh
@@ -419,6 +419,8 @@ if [[ ("test-auto-upgrade" == $jobname) && ("force" != $subtaskname) ]]; then
 	# Add line to verify_output helper function to remove `..` from test output when Octo is built with Ninja
 	# See https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/861 for more details.
 	sed -i "s,# Filter rule #s and line #s printed by flex in TRACE verbosity level output,sed -i 's/\\\.\\\.PATH/PATH/' clean_output\.txt," ../tests/test_helpers.bash.in
+
+	# Note down if older commit is prior to YDBOcto#929. If so, case insensitive column names are stored in upper case.
 	# If the chosen older commit has "*.sql" or "*.bats.in" files with CREATE TABLE commands containing the GLOBAL
 	# keyword with "keys(..)" expressions using a lower case column name, those will no longer work with the master
 	# after 7fa4406a (YDBOcto#519) as we expect "keys(..)" to use upper case column names for case insensitive column
@@ -426,14 +428,23 @@ if [[ ("test-auto-upgrade" == $jobname) && ("force" != $subtaskname) ]]; then
 	# those that were newly introduced in 7fa4406a and they are TC058, TCO59 and TCO60. So excepting those *.sql files,
 	# all other "*.sql" or "*.bats.in" files are better updated to ensure upper case column names are used inside "keys(..)"
 	# expressions. The following logic takes care of that.
-	shopt -s extglob	# enable extended pattern matching feature (we use ! syntax below)
-	fixtures="$(ls ../tests/*.bats.in ../tests/fixtures/!(TC058*|TC059*|TC060*).sql)"
-	sed -i 's/keys(\(""[A-Za-z_0-9]*""\))/keys(\U\1\E)/g' $fixtures
-	# In similar fashion, we could have M programs that generates DDLs (i.e. CREATE TABLE commands) containing "keys(...)"
-	# expressions with lower cased column names. Fix those as well.
-	fixtures="$(ls ../tests/fixtures/*.m)"
-	sed -i 's/keys(\(""""[A-Za-z_0-9]*""""\))/keys(\U\1\E)/g' $fixtures
-	shopt -u extglob	# reset now that extended pattern matching feature need is done
+	pre_octo929_commit="8f6e404c05cb695e32c2458d3c9ba71632926bb0"	# 1 commit before YDBOcto#929 commit
+	# Disable the "set -e" setting temporarily as the "git merge-base" can return exit status 0 or 1
+	set +e
+	git merge-base --is-ancestor $commitsha $pre_octo929_commit
+	is_post_octo929_commit=$?
+	# Re-enable "set -e" now that "git merge-base" invocation is done.
+	set -e
+	if [[ (0 == $is_post_octo929_commit) ]]; then
+		shopt -s extglob	# enable extended pattern matching feature (we use ! syntax below)
+		fixtures="$(ls ../tests/*.bats.in ../tests/fixtures/!(TC058*|TC059*|TC060*).sql)"
+		sed -i 's/keys(\(""[A-Za-z_0-9]*""\))/keys(\U\1\E)/g' $fixtures
+		# In similar fashion, we could have M programs that generates DDLs (i.e. CREATE TABLE commands)
+		# containing "keys(...)" expressions with lower cased column names. Fix those as well.
+		fixtures="$(ls ../tests/fixtures/*.m)"
+		sed -i 's/keys(\(""""[A-Za-z_0-9]*""""\))/keys(\U\1\E)/g' $fixtures
+		shopt -u extglob	# reset now that extended pattern matching feature need is done
+	fi
 	if [ "ALL" != "$autoupgrade_test_to_troubleshoot" ]; then
 		# Run only a random fraction of the bats tests as we will be running an auto upgrade test on the same queries
 		# once more a little later.
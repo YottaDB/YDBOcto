0871fe7e6068a379ff1d2ac64ab811f1d2a887f3
Ganesh Mahesh <ganesh@yottadb.com>
2024-05-29 11:51:47 -0400

    [#382] Fix TDTT053 such that second difference don't cause test failure
    Issue
    ------
    The post-merge pipeline of !1533 failed in the [make-ubuntu](https://gitlab.com/YottaDB/DBMS/YDBOcto/-/jobs/6966844482) and the [make-tls-ubuntu](https://gitlab.com/YottaDB/DBMS/YDBOcto/-/jobs/6966844493) jobs. Both failures were in the `TDTT053` subtest.
    
    ```sql
    $ cat TDTT053-7.sql
    select cast(localtime as time);
    ```
    
    ```diff
    $ cat TDTT053-7.diff
    1c1
    < 05:08:07
    ---
    > 05:08:08
    ```
    
    The reason for the failure is that we run the `localtime` function first with `MySql` and then with `Octo`. By the time the same query runs in Octo, the time has moved on to the next second and is the cause of the diff.
    
    This is a test issue in 41f13366 that can cause random pipeline failures.
    
    Fix
    ----
    * The localtime query is tested separately. Same query is executed in MySql before and after the test. If diff from the test is not empty then the output of Octo
      is verfied to be within the MySql executions results which were taken before and after the test.
    
    Misc
    ----
    * TDTT053 was setting `mysql_is_mariadb` but this is not required by the test so the code related to it is removed

--- tests/fixtures/TDTT053.sql
+++ tests/fixtures/TDTT053.sql
@@ -10,27 +10,16 @@
 #								#
 #################################################################
 
---select current_timestamp; -- YYYY-MM-DD hh:mm:ss no micro second or time zone
---select localtime(); -- YYYY-MM-DD hh:mm:ss no micro second or time zone
---select localtimestamp(); -- YYYY-MM-DD hh:mm:ss no micro second or time zone
---select now(); -- YYYY-MM-DD hh:mm:ss no micro second or time zone
-
--- COmmented tests below will not work as mysql has a return type of now and other functions are timestamp in mysql where
--- as in Postgres and Octo it is timestamp with time zone. During MYSQL emulation the result seen will be a timestamp but
--- it is declared as a timestamp with timezone in Octo
 select date_format(now(),'%m-%d-%Y');
 select CAST(now() as date);
 select CAST(now() as char(17));
---select now() between now() - time'01:01:01' and now() + time'01:01:01';
+select now() between now() - time'01:01:01' and now() + time'01:01:01';
 
 -- localtimestamp
 select cast(localtimestamp as date);
 select cast(localtimestamp as char(17));
---select localtimestamp between localtimestamp - time'01:01:01' and localtimestamp + time'01:01:01';
+select localtimestamp between localtimestamp - time'01:01:01' and localtimestamp + time'01:01:01';
 -- current_timestamp
 select cast(current_timestamp as date);
 select cast(current_timestamp as char(17));
---select current_timestamp between current_timestamp - time'01:01:01' and current_timestamp + time'01:01:01';
--- localtime -- returns timestamp in MYSQL but Octo returns time similar to Postgres, hence the cast below
-select cast(localtime as time);
---select localtime between localtime - time'01:01:01' and localtime + time'01:01:01';
+select current_timestamp between current_timestamp - time'01:01:01' and current_timestamp + time'01:01:01';
---
+++ tests/fixtures/TDTT053localtime.sql
@@ -0,0 +1,15 @@
+#################################################################
+#								#
+# Copyright (c) 2024 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+-- localtime -- returns timestamp in MYSQL but Octo returns time similar to Postgres, hence the cast below
+select cast(localtime as time);
+-- select localtime between localtime - time'01:01:01' and localtime + time'01:01:01';

d86bb3da4762fbc133c7f4a22c5547ffb1a04383
Sam Habiel <sam@yottadb.com>
2024-09-30 11:33:29 -0400

    [#1058] Full TBCR002 JDBC Test to replicate DBeaver commands
    The previous commit for #1058 was missing a full test for JDBC
    interactions that DBeaver performs. This is supplied here.
    
    TBCR002 performs the following:
    - Turn on SQL transactions, run updates, commit, verify updates are not
      committed (as we don't implement them yet).
    - Turn on SQL transactions, run updates, rollback, verify updates are not
      committed (as we don't implement them yet).
    - Turn off SQL transactions, run updates, verify that they are committed.
    - Test two rollbacks with a SQL transaction
    - Test rollback with no SQL transaction

--- tests/fixtures/TBCR002.java
+++ tests/fixtures/TBCR002.java
@@ -11,11 +11,6 @@
  ****************************************************************/
 
 // See https://docs.oracle.com/javase/tutorial/jdbc/basics/transactions.html
-//
-// ROLLBACK
-// ROLLBACK without a start
-// ROLLBACK + turn off transactions + readwrite + readonly
-
 import java.sql.*;
 import java.util.Properties;
 import java.sql.Connection;
@@ -35,12 +30,13 @@ public class TBCR002 {
 		int exitStatus = 0;
 		try (Connection conn = DriverManager.getConnection(connectionString, props)) {
 			if (conn != null) {
-				String[]		queryStrings = new String[4];
+				String[]		queryStrings = new String[5];
 				PreparedStatement	preparedStatement;
 				ResultSet		resultSet;
 				ResultSetMetaData	resultSetMetaData ;
-				int			columnCount, rowLimit=0, numQueries;
-				String selectStatement = "select count(*) from names;";
+				int			columnCount, numQueries;
+				String selectStatement = "select * from names;";
+
 				String insertStatement = "INSERT INTO names values (?, ?, ?);"; // 6, 'foo', 'boo'
 				String updateStatement = "UPDATE names SET lastname=? WHERE id=?;"; // 'foo', 1
 				String deleteStatement = "DELETE FROM names WHERE id=?;"; // 1
@@ -48,58 +44,115 @@ public class TBCR002 {
 
 				numQueries = 0;
 				// Comparison operation resulting in an error when type is not determined for PARAMETER_VALUE
-				queryStrings[numQueries++] = selectStatement;
-				queryStrings[numQueries++] = insertStatement;
-				queryStrings[numQueries++] = updateStatement;
-				queryStrings[numQueries++] = deleteStatement;
-				conn.setAutoCommit(false);
-				for (int query = 0; query < numQueries; query++) {
-					preparedStatement = conn.prepareStatement(queryStrings[query]);
-					switch(query) {
-						case 0:
-							break;
-						case 1:
-							preparedStatement.setInt(1,6);
-							preparedStatement.setString(2,"foo");
-							preparedStatement.setString(3,"boo");
-							break;
-						case 2:
-							preparedStatement.setString(1,"foo");
-							preparedStatement.setInt(2,1);
-							break;
-						case 3:
-							preparedStatement.setInt(1,1);
-							break;
-						default:
-							break;
-					}
-					System.out.printf("-------------------------------------------------------------------------%n");
-					System.out.printf(" --> Running query (row limit: %d): ", rowLimit);
-					System.out.printf(queryStrings[query]);
-					System.out.printf(" <--%n");
-					System.out.printf("-------------------------------------------------------------------------%n");
-					try {
-						resultSet = preparedStatement.executeQuery();
-						resultSetMetaData = resultSet.getMetaData();
-						columnCount = resultSetMetaData.getColumnCount();
-						while (resultSet.next()) {
-							for (int i = 1; i <= columnCount; i++) {
-								System.out.printf("%s", resultSet.getString(i));
-								if (i != columnCount) {
-									System.out.printf("|");
-								}
-							}
-							System.out.printf("%n");
+				queryStrings[numQueries++] = selectStatement; //0
+				queryStrings[numQueries++] = insertStatement; //1
+				queryStrings[numQueries++] = updateStatement; //2
+				queryStrings[numQueries++] = deleteStatement; //3
+                                queryStrings[numQueries++] = selectStatement; //4
+
+                                // Run the queries in an outer loop: 0 commits, 1 rollsback, 2 turns off transactions
+				//                                 // --> avoids having to write the same code over and over again.
+                                for (int commrollback = 0; commrollback < 3; commrollback++) {
+                                        if (commrollback == 0 || commrollback == 1) {
+                                                // Don't turn it on twice (for cleaner output)
+                                                if (conn.getAutoCommit() != false) {
+                                                        System.out.printf("--- TURNING ON TRANSACTIONS (expect to see 25006 errors, data not modified) ---%n");
+                                                        // This turns on transactions (sends BEGIN before queries start)
+                                                        conn.setAutoCommit(false);
+                                                }
+                                        }
+                                        else {
+						assert(commrollback == 2);
+                                                System.out.printf("--- TURNING OFF TRANSACTIONS (no errors expected, data will be modified) ---%n");
+                                                conn.setAutoCommit(true);
+                                        }
+                                        if (0 == commrollback) {
+                                                System.out.printf("--- PLAN TO COMMIT IN THE END ---%n");
+                                        }
+                                        if (1 == commrollback) {
+                                                System.out.printf("--- PLAN TO ROLLBACK IN THE END ---%n");
+                                        }
+
+					for (int query = 0; query < numQueries; query++) {
+						preparedStatement = conn.prepareStatement(queryStrings[query]);
+						switch(query) {
+							case 1:
+								preparedStatement.setInt(1,6);
+								preparedStatement.setString(2,"foo");
+								preparedStatement.setString(3,"boo");
+								break;
+							case 2:
+								preparedStatement.setString(1,"foo");
+								preparedStatement.setInt(2,1);
+								break;
+							case 3:
+								preparedStatement.setInt(1,1);
+								break;
+							default:
+								break;
 						}
-					} catch (SQLException e) {
-						System.err.format("SQL State: %s\n%s\n", e.getSQLState(), e.getMessage());
-						/* Note: Do not set "exitStatus = 1;" like is done in other *.java files
-						 * in this repository as we do expect some errors in this test.
-						 */
+                                                if (4 == query) {
+                                                        // Commit or rollback before the final select query that shows the result
+                                                        if (0 == commrollback) {
+                                                                System.out.printf("--- COMMITTING ---%n");
+                                                                conn.commit();
+                                                        }
+                                                        if (1 == commrollback) {
+                                                                System.out.printf("--- ROLLING BACK ---%n");
+                                                                conn.rollback();
+                                                        }
+							// Turn off transactions for the last query; otherwise the next loop will have transactions turned on
+							// without a corresponding commit/rollback
+							conn.setAutoCommit(true);
+                                                }
+
+						System.out.printf("-------------------------------------------------------------------------%n");
+						System.out.printf(" --> Running query: ");
+						System.out.printf(queryStrings[query]);
+						System.out.printf(" <--%n");
+						System.out.printf("-------------------------------------------------------------------------%n");
+						try {
+							resultSet = preparedStatement.executeQuery();
+							resultSetMetaData = resultSet.getMetaData();
+							columnCount = resultSetMetaData.getColumnCount();
+							while (resultSet.next()) {
+								for (int i = 1; i <= columnCount; i++) {
+									System.out.printf("%s", resultSet.getString(i));
+									if (i != columnCount) {
+										System.out.printf("|");
+									}
+								}
+								System.out.printf("%n");
+							}
+						} catch (SQLException e) {
+							System.err.format("SQL State: %s\n%s\n", e.getSQLState(), e.getMessage());
+							/* Note: Do not set "exitStatus = 1;" like is done in other *.java files
+							 * in this repository as we do expect some errors in this test.
+							 */
+						}
+						preparedStatement.close();
 					}
-					preparedStatement.close();
+					System.out.printf("%n");
 				}
-				conn.commit();
+
+				System.out.printf("%n--- TURN ON TRANSACTIONS THEN RUN TWO ROLLBACKS (no error expected) ---%n");
+				conn.setAutoCommit(false);
+				conn.prepareStatement(queryStrings[0]).executeQuery();
+				conn.rollback();
+				conn.rollback();
+				System.out.printf("%n");
+
+				try {
+					System.out.printf("%n--- TURN OFF TRANSACTIONS THEN RUN ROLLBACK (error expected) ---%n");
+					conn.setAutoCommit(true);
+					conn.prepareStatement(queryStrings[0]).executeQuery();
+					conn.rollback();
+				} catch (SQLException e) {
+					System.err.format("SQL State: %s\n%s\n", e.getSQLState(), e.getMessage());
+					/* Note: Do not set "exitStatus = 1;" like is done in other *.java files
+					 * in this repository as we do expect some errors in this test.
+					 */
+				}
 			} else {
 				System.out.println("Failed to make connection!");
 				exitStatus = 1;
@@ -115,6 +168,7 @@ public class TBCR002 {
 			/* Some error occurred. Caller (i.e. bats) relies on a non-zero exit status so it can signal
 			 * a test failure. So do just that using "exit".
 			 */
+			System.err.format("exitStatus: %d\n", exitStatus);
 			System.exit(exitStatus);
 		}
 	}

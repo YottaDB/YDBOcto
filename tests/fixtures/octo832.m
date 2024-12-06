2ce87fc1cf42df3e971f8b8ff9dcca612cf2957e
Narayanan Iyer <nars@yottadb.com>
2024-12-06 12:14:23 -0500

    Fix rare test_readline/TR21 subtest failure (test timing issue)
    Background
    ----------
    * The above subtest failed in one rare test run with the following diff.
    
      ```diff
      1,8d0
      < id|firstname|lastname
      < 0|Zero|Cool
      < 1|Acid|Burn
      < 2|Cereal|Killer
      < 3|Lord|Nikon
      < 4|Joey|
      < 5|Zero|Cool
      < (6 rows)
      ```
    
    Issue
    -----
    * The test runs an M program that spawns off `octo` through a pipe device and then asks
      it to execute `select * from names;` and expects 6 rows of output but got NO output.
    
    * After some analysis, I am positive the issue is in line 19 below where we do a timed read
      `read cmdop:1` with a timeout of 1 second. On a loaded system, it is possible this read
      times out without reading any output from `octo` and sets `$test` to 0 and causes us to
      exit the `for` loop in line 19 and eventually terminate with NO output.
    
      **tests/fixtures/octo832.m**
      ```m
         16         open dev:(command="octo":exception="goto readeof")::"PIPE"
         17         use dev
         18         write "select * from names;",!
         19         for  read cmdop:1 quit:$zeof  quit:'$test  use $PRINCIPAL write cmdop,! use dev
         20         write "quit;",!
         21         close dev
         22         if $zclose'=0 write "TEST-E-FAIL",!
         23         quit
         24
         25 readeof;
         26         close dev
         27         write "TEST-E-FAIL",!
         28         quit
      ```
    
    Fix
    ---
    * The fix is to introduce another `for` loop after line 20 to read the remainder of the octo
      output but with `untimed` reads this time.
    
      This way even if a read times out in line 19 and exits the for loop, we will read all of the
      octo output of `select * from names` in the new for loop after line 20 with untimed reads.
    
    * While at this, I noticed that we already have a device exception set up at line 16 to goto the
      `readeof` entryref. Therefore, there is no need for a `quit:$zeof` check in line 19.
    
    * Additionally, I noticed that we were checking `$zclose` and printing `TEST-E-FAIL` in 2 places
      and I did not think either of them was necessary so removed it and simplified the code path
      a bit so lines 21-23 and 26-28 are merged.
    
    Notes
    -----
    * My original fix attempt tried to replace the existing `for` loop in line 19 with untimed reads
      and a loop that runs for only 8 iterations (to read the 8 lines of output from octo for the
      `select * from names;` query) but that turned out to not exercise the code path that the test
      was originally written for.
    
      I verified this by temporarily reverting the original #832 fix by adding a `break` line below.
    
      ```diff
      > git diff -U1
      diff --git a/src/readline_get_more.c b/src/readline_get_more.c
      index 6181ccc4..a9f63dd8 100644
      --- a/src/readline_get_more.c
      +++ b/src/readline_get_more.c
      @@ -95,2 +95,3 @@ int readline_get_more(void) {
                              }
      +                       break;
                      } while ((-1 == data_read) && (EINTR == errno));
      ```
    
      And expected the test to fail but it did not.
    
      Hence went with the 2 for loop solution one with timed read and one with untimed read. I verified
      that with this approach and with the temporary change above, I did get TR21 test failures as
      expected (with the following symptom).
    
      ```
      [ERROR] ERR_SYSCALL : System call failed: read, return 4 (Interrupted system call)
      ```

--- tests/fixtures/octo832.m
+++ tests/fixtures/octo832.m
@@ -1,6 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;								;
-; Copyright (c) 2022 YottaDB LLC and/or its subsidiaries.	;
+; Copyright (c) 2022-2024 YottaDB LLC and/or its subsidiaries.	;
 ; All rights reserved.						;
 ;								;
 ;	This source code contains the intellectual property	;
@@ -16,13 +16,14 @@ pipe;
         open dev:(command="octo":exception="goto readeof")::"PIPE"
 	use dev
 	write "select * from names;",!
-        for  read cmdop:1 quit:$zeof  quit:'$test  use $PRINCIPAL write cmdop,! use dev
+	; Have a timed read for loop to ensure we exercise the EINTR code path that YDBOcto#832 fixed (see
+	; https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/832#description for more details).
+        for  read cmdop:1 quit:'$test  use $PRINCIPAL write cmdop,! use dev
 	write "quit;",!
+	; Have an untimed read for loop here to read any data that did not get read in the previous
+	; timed read for loop due to say a loaded system etc.
+        for  read cmdop use $PRINCIPAL write cmdop,! use dev
+readeof	;
 	close dev
-	if $zclose'=0 write "TEST-E-FAIL",!
 	quit
 
-readeof;
-	close dev
-	write "TEST-E-FAIL",!
-        quit

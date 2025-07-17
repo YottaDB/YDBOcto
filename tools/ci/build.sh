YottaDB/YDBOcto

commit 624a55857c76ec073dcb9a5749361dae996a818a

[YottaDB/DB/YDB#1148] Update .gitlab-ci.yml from Ubuntu 22.04 to 24.04
There were various issues that needed to be fixed:

* One enhancement and two fixes in `tools/ci/build.sh`:

  1. A small enhancement to git stash if there are changes. This is
     necessary as during local development, the local docker copy of the
     pipeline fails with in-progress changes because we use `clang-format`
     with in-place modification, and the way we detect that there are
     formtting problems is by looking at `git diff`. But git diff will
     find differences if there is in progress uncommited code.
  2. A newer version of shellcheck introduces a new check SC2317 for
     unreachable code. I followed the instructions in https://www.shellcheck.net/wiki/SC2317
     to disable the check for trap code that is normally unreachable
     in `tools/ci/vistatest.sh`.
     See https://gitlab.com/YottaDB/DBMS/YDBOcto/-/merge_requests/1644#note_2631746498
     for more discussion.
  3. This line `fixtures="$('ls ../tests/*.bats.in ../tests/fixtures/!(TC058*|TC059*|TC060*).sql')"`
     was not parsable anymore in the newer version of bash (`5.2.21`)
     unless you disable history expansion with `set +o histexpand`. This
     is now done.
  4. Misc: remove trailing whitespace in a line (done by pre-commit hook)

* In Ubuntu 24.04, we can no-longer install Python pip packages
  globally. We can either choose a virtual enviroment; or add them using
  apt. I chose the latter option, as there are enough packages to be
  able to build the documentation.

* For R tests (`tests/test_r_connection.bats.in`), we now need package
  libtirpc-dev to compile RJDBC.

tools/ci/build.sh
@@ -210,7 +210,14 @@ if [[ "test-auto-upgrade" != $jobname ]]; then
 		if CLANG_FORMAT="$(../tools/ci/find-llvm-tool.sh clang-format 15)"; then
 			echo "# Check code style using clang-format"
 			# This modifies the files in place so no need to record the output.
+			# We use git to check if clang format made a change... stash our changes so it won't give a false positive
+			set +e
+			git diff --stat
+			git diff-index --quiet HEAD; uncommited_change=$?
+			set -e
+			test $uncommited_change -eq 1 && git stash
 			../tools/ci/clang-format-all.sh $CLANG_FORMAT
+			test $uncommited_change -eq 1 && git stash pop
 		else
 			# Otherwise, fail the pipeline.
 			echo " -> A recent enough version of clang-format was not found!"
@@ -464,14 +471,18 @@ if [[ ("test-auto-upgrade" == $jobname) && ("force" != $subtaskname) ]]; then
 	# Re-enable "set -e" now that "git merge-base" invocation is done.
 	set -e
 	if [[ (0 == $is_post_octo929_commit) ]]; then
-		shopt -s extglob	# enable extended pattern matching feature (we use ! syntax below)
-		fixtures="$(ls ../tests/*.bats.in ../tests/fixtures/!(TC058*|TC059*|TC060*).sql)"
+		# enable extended pattern matching feature (we use ! syntax below)
+		# We also need to disable history expansion as the ! syntax otherwise causes an error
+		set +o histexpand
+		shopt -s extglob
+		fixtures="$('ls ../tests/*.bats.in ../tests/fixtures/!(TC058*|TC059*|TC060*).sql')"
+		shopt -u extglob	# reset now that extended pattern matching feature need is done
+		set -o histexpand
 		sed -i 's/keys(\(""[A-Za-z_0-9]*""\))/keys(\U\1\E)/g' $fixtures
 		# In similar fashion, we could have M programs that generates DDLs (i.e. CREATE TABLE commands)
 		# containing "keys(...)" expressions with lower cased column names. Fix those as well.
 		fixtures="$(ls ../tests/fixtures/*.m)"
 		sed -i 's/keys(\(""""[A-Za-z_0-9]*""""\))/keys(\U\1\E)/g' $fixtures
-		shopt -u extglob	# reset now that extended pattern matching feature need is done
 	fi
 	pre_octo519_commit="bee2b104e52874f79d728c98047a1cad2829e25f" # 1 commit before YDBOcto#519 commit
 	# Disable the "set -e" setting temporarily as the "git merge-base" can return exit status 0 or 1
@@ -635,7 +646,7 @@ if [[ "test-auto-upgrade" != $jobname ]]; then
 	}
 	compare /dev/null sorted_build_warnings.txt build_warnings.txt
 
-	# Skip Clang-tidy checking with Rocky Linux as the version keeps getting upgraded and is not fixed, resulting in changing 
+	# Skip Clang-tidy checking with Rocky Linux as the version keeps getting upgraded and is not fixed, resulting in changing
 	# warnings, some of which are false positives. Clang-tidy on Ubuntu is good enough for now.
 	if ! $is_rocky8; then
 		echo "# Check for unexpected warning(s) from clang-tidy ..."

tools/ci/vistatest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #################################################################
 #								#
-# Copyright (c) 2021-2024 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2021-2025 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -18,6 +18,8 @@ set -o pipefail # this way $? is set to zero only if ALL commands in a pipeline 
 set -e # Below ensures any errors in this script cause it to exit with a non-zero status right away
 
 # Copy of the same function in tools/ci/build.sh
+# See https://gitlab.com/YottaDB/DBMS/YDBOcto/-/merge_requests/1644#note_2631746498 for explanation of next line
+# shellcheck disable=SC2317
 cleanup_before_exit() {
 	echo "# Cleanup files and directories that don't need to be included in the pipeline artifacts"
 	rm -rf CMakeFiles _CPack_Packages bats-test.*/go src/CMakeFiles || true

tools/misc-dockerfiles/Dockerfile-Ubuntu-Tests.baseline
@@ -1,6 +1,6 @@
 #################################################################
 #								#
-# Copyright (c) 2023-2024 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2023-2025 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -16,7 +16,6 @@ RUN ln -fs /usr/share/zoneinfo/US/Eastern /etc/localtime
 RUN apt-get update && \
     apt-get install -y tzdata
 RUN dpkg-reconfigure --frontend noninteractive tzdata
-RUN apt-get install -y -qq --no-install-recommends build-essential clang clang-tidy-11 clang-format-15 cmake bison flex libcmocka-dev python3 python3-pip python3-setuptools python3-wheel libreadline-dev git libconfig-dev libssl-dev postgresql-client postgresql xxd wget libgcrypt20-dev libgpgme-dev expect default-jdk ninja-build unixodbc odbc-postgresql tcsh shellcheck libicu-dev pkg-config locales curl mysql-client golang ncat r-base r-base-dev libpq-dev
+RUN apt-get install -y -qq --no-install-recommends build-essential clang clang-tidy-15 clang-format-15 cmake bison flex libcmocka-dev python3 python3-pip python3-setuptools python3-wheel libreadline-dev git libconfig-dev libssl-dev postgresql-client postgresql xxd wget libgcrypt20-dev libgpgme-dev expect default-jdk ninja-build unixodbc odbc-postgresql tcsh shellcheck libicu-dev pkg-config locales curl mysql-client golang ncat r-base r-base-dev libpq-dev python3-sphinx python3-sphinx-rtd-theme libtirpc-dev
 RUN locale-gen en_US.UTF-8
-RUN pip3 install sphinx sphinx_rtd_theme==0.5.2 sphinxcontrib-jquery sphinxcontrib-fulltoc
 ENV USER=root

commit 441c6654deec275752e9c4865b46f9a4be9edcee

[YottaDB/DB/YDB#1148] Fix test-auto-upgrade job failure due to syntax error in tools/ci/build.sh (regression in 624a5585)
The previous commit 624a55857c76ec073dcb9a5749361dae996a818a tried to
fix this expression which didn't work on a newer version of bash `5.2.21`:

```
fixtures="$('ls ../tests/*.bats.in ../tests/fixtures/!(TC058*|TC059*|TC060*).sql')"
```

This failed in a pipeline in the auto-upgrade job:
https://gitlab.com/YottaDB/DBMS/YDBOcto/-/jobs/10708080927

The problem are the single quotes inside the `$()` expression. Instead
of using this expression, I rewrote the expression using `find` as it's
easier to maintain in the future.

It was locally tested using this command:

```
docker run -it --link mysql --rm tests-ubuntu "test-auto-upgrade" "debug" "8587b12086666c88ea2c8a19b55a736629269907"
```

tools/ci/build.sh
@@ -471,13 +471,7 @@ if [[ ("test-auto-upgrade" == $jobname) && ("force" != $subtaskname) ]]; then
 	# Re-enable "set -e" now that "git merge-base" invocation is done.
 	set -e
 	if [[ (0 == $is_post_octo929_commit) ]]; then
-		# enable extended pattern matching feature (we use ! syntax below)
-		# We also need to disable history expansion as the ! syntax otherwise causes an error
-		set +o histexpand
-		shopt -s extglob
-		fixtures="$('ls ../tests/*.bats.in ../tests/fixtures/!(TC058*|TC059*|TC060*).sql')"
-		shopt -u extglob	# reset now that extended pattern matching feature need is done
-		set -o histexpand
+		fixtures=$(find ../tests -name '*.bats.in'; find ../tests/fixtures -name '*.sql' ! -name 'TC058*' ! -name 'TC059*' ! -name 'TC060*')
 		sed -i 's/keys(\(""[A-Za-z_0-9]*""\))/keys(\U\1\E)/g' $fixtures
 		# In similar fashion, we could have M programs that generates DDLs (i.e. CREATE TABLE commands)
 		# containing "keys(...)" expressions with lower cased column names. Fix those as well.